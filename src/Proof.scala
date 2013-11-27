ade38fa537ec9dbafddc8d71685b74da156b65c2
Jan-David Quesel <jquesel@cs.cmu.edu>
2013-11-27 09:28:10 -0500

    started implementing rules

--- src/Proof.scala
+++ src/Proof.scala
@@ -1,25 +1,71 @@
 abstract class Proof
 
 abstract class ProofNode(s: Sequent, p: ProofNode, var ruleName: String, var children: Seq[ProofNode]) {
-  def apply(rule: String, pos: String);
+  def apply(rule: Rule);
+  def apply(rule: PositionRule, p: Position);
 }
+
+abstract class Rule extends Sequent => Seq[Sequent]
+
+abstract class PositionRule extends Position => Rule
+
+abstract class Position
 
 abstract class Signature
 
 // this only works if all quantifiers are the same, otherwise we have distinguish here
-class Sequent(pref: Seq[(Name, Sort)], ante: Seq[Term], succ: Seq[Term])
+class Sequent(pref: Seq[(Name, Sort)], ante: IndexedSeq[Term], succ: IndexedSeq[Term])
 
 // proof rules:
 
 // cut
+class Cut(f: Formula) extends Rule {
+  def apply(s: Sequent): Seq[Sequent] = {
+    val (pref, ante, succ) = s
+    val l = Sequent(pref, ante ++ f, succ)
+    val r = Sequent(pref, ante, succ ++ f)
+    new List(l, r)
+  }
+
+  def name: String = "cut"
+
+  def parameter: Formula = f
+}
+
+// equality/equivalence rewriting
+
+// uniform substitution
+
+// AX close
+// TODO: how do we represent this with just a single position?
+
+// Impl right
+
+// Impl left
+
+// Not right
+
+// Not left
+
+// And right
+
+// And left
+
+// Or right
+
+// Or left
+
+// Lookup Lemma (different justifications: Axiom, Lemma with proof, Oracle Lemma)
+
+
+
+// maybe:
+
+// close by true (do we need this or is this derived?)
 
 // alpha conversion
 
 // quantifier instantiation
-
-// AX close
-
-// close by true (do we need this or is this derived?)
 
 // remove known
 
