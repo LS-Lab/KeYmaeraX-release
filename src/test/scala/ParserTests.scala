e370d1e5fca38c40e2c93e5e9e4405ea87bb057c
Andre Platzer <aplatzer@cs.cmu.edu>
2014-04-28 15:31:22 -0400

    More parser test cases

--- src/test/scala/ParserTests.scala
+++ src/test/scala/ParserTests.scala
@@ -13,18 +13,28 @@ class ParserParenTests extends FlatSpec with Matchers {
   val parser = new KeYmaeraParser(false) 
   val alpParser = parser.ProofFileParser
 
-  "The Parser" should "place implicit parens correctly (a.k.a. resolve abiguities correctly))" in {
+  "The Parser" should "place implicit parens correctly (a.k.a. resolve ambiguities correctly))" in {
     val equalPairs =
       ("\\forall x . (x > 2) & a", "(\\forall x . (x > 2)) & a") ::
-      ("< ?p>q; >(p > 1)", "<?(p > q);>(p>1)") ::
+      ("< ?p>q; > p > 1", "<?(p > q);>(p>1)") ::
+      ("[ ?p>q; ] p > 1", "[?(p > q);](p>1)") ::
       ("p + q * r = s", "p + (q * r) = s") ::
+      ("p * q + r = s", "(p * q) + r = s") ::
+      ("p - q * r = s", "p - (q * r) = s") ::
+      ("p * q - r = s", "(p * q) - r = s") ::
+      ("-p + q = s", "(-p) + q = s") ::
+      ("p - q - s = 0", "(p-q) - s = 0") ::
+      ("p^2 >= 0", "(p^2) >= 0") ::
+      ("p^2 + q^2 = s^2", "(p^2) + (q^2) = (s^2)") ::
+      ("p^5 * p^3 * q^2 >= 0", "(p^5) * (p^3) * (q^2) >= 0")::
+      ("p^2^5 >= 0", "p^(2^5) >= 0")::
       ("< p:=1; > <p:=2; > p>0", "<p:=1;>(<p:=2;>p>0)") ::
       ("[ p:=1; ] <p:=2; > p>0", "[p:=1;](<p:=2;>p>0)") ::
       ("< p:=1; > [p:=2; ] p>0", "<p:=1;>([p:=2;]p>0)") ::
-      ("-p + q = s", "(-p) + q = s") ::
-      ("p^2 >= 0", "p^2 >= 0") ::
-      ("p^2 + q^2 = s^2", "(p^2) + (q^2) = (s^2)") ::
-      ("p^5 * p^3 * q^2 >= 0", "(p^5) * (p^3) * (q^2) >= 0")::
+      // implicit {} either assumed correctly or rejected
+      ("[ p:=1; p:=2; ++ p:=3] p>0", "[ {p:=1; p:=2;} ++ p:=3] p>0") ::
+      ("[ p:=1; ++ p:=2; p:=3] p>0", "[ p:=1; ++ {p:=2; p:=3;}] p>0") ::
+      ("[ p:=1; p:=2; p:=3*] p>0", "[ p:=1; p:=2; {p:=3;}*] p>0") ::
       Nil
 
     for(pair <- equalPairs) {
