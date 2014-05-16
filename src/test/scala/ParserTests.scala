d0c45d80f738405ef3d596fc1749cb46ba0905f1
Andre Platzer <aplatzer@cs.cmu.edu>
2014-05-16 09:50:10 -0400

    Soundness-critical clarification on axioms being immutable Map not mutable Map. Starting on random formula generator for testing.

--- src/test/scala/ParserTests.scala
+++ src/test/scala/ParserTests.scala
@@ -2,6 +2,7 @@ import org.scalatest._
 import edu.cmu.cs.ls.keymaera.core._
 import edu.cmu.cs.ls.keymaera.parser._
 import java.io.File
+import scala.util.Random
 
 class ParserParenTests extends FlatSpec with Matchers {
   // type declaration header for tests
@@ -133,6 +134,33 @@ class ParserParenTests extends FlatSpec with Matchers {
       a [Exception] should be thrownBy {
         alpParser.runParser(src)
       }
+    }
+  }
+}
+
+/**
+ * Random formula generator
+ * @author aplatzer
+ */
+class RandomFormula(val rand : Random = new Random()) {
+  def unfoldRight[A, B](seed: B)(f: B => Option[(A,B)]): List[A] = f(seed) match {
+    case Some((a,b)) => a :: unfoldRight(b)(f)
+    case None => Nil
+  }
+  private def nextNames(n : Int) : IndexedSeq[Variable] = unfoldRight(n) { n =>
+    if (n==0)
+      None
+    else
+      Some((Variable("z" + n, None, Real), n-1))
+      //Some(("x" + (rand.alphanumeric take 5).fold("")((s:String,t:String)=>s+t), n-1))
+  }.to[IndexedSeq]
+  def nextFormula(size : Int) = {
+    val vars = nextNames(size / 3 + 1)
+    rand.nextInt(10) match {
+      case 0 => False
+      case 1 => True
+      case it if 2 until 10 contains it => vars(rand.nextInt(vars.length))
+      //case it if 11 until 20 contains it => 
     }
   }
 }
