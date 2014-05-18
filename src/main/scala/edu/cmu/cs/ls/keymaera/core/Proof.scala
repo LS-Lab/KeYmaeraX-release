0d61c0e6a3ede7f6cc532d8cbaa874855032d94b
Andre Platzer <aplatzer@cs.cmu.edu>
2014-05-17 20:21:38 -0400

    Move automatic lemma database into lemmadb

--- src/main/scala/edu/cmu/cs/ls/keymaera/core/Proof.scala
+++ src/main/scala/edu/cmu/cs/ls/keymaera/core/Proof.scala
@@ -1104,6 +1104,7 @@ object LookupLemma {
       val parser = new KeYmaeraParser()
       val knowledge = parser.ProofFileParser.runParser(scala.io.Source.fromFile(file).mkString)
       val formula = LoadedKnowledgeTools.fromName(knowledge)(name).head.formula
+      //@TODO Are lemmas fine in every context? Including any s.pref?
       val newSequent = new Sequent(s.pref, s.ante :+ formula, s.succ) //TODO-nrf not sure about this.
       List(newSequent)
     }
@@ -1119,7 +1120,9 @@ object LookupLemma {
         //Save the solution to a file.
         //TODO-nrf create an interface for databases.
         def getUniqueLemmaFile(idx:Int=0):java.io.File = {
-          val f = new java.io.File("QE" + t.name + idx.toString() + ".alp")
+          val lemmadbpath = new java.io.File("lemmadb")
+          lemmadbpath.mkdirs
+          val f = new java.io.File(lemmadbpath, "QE" + t.name + idx.toString() + ".alp")
           if(f.exists()) getUniqueLemmaFile(idx+1)
           else f
         }
