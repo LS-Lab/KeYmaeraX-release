86790992c3f5f97efd89995562b927fd380d177b
Nathan Fulton <nathanfu@cs.cmu.edu>
2014-05-30 20:38:58 -0400

    Adding missing file.

---
+++ src/main/resources/static/js/ui.js
@@ -0,0 +1,21 @@
+var UI = {
+  showError : function(error, text) {
+    var messageDiv = document.createElement("div")
+    messageDiv.setAttribute("class", "errorMessage")
+    messageDiv.innerHTML = text
+    document.getElementById("errors").appendChild(messageDiv)
+    console.error(error)
+  },
+
+  //TODO-nrf use some layout manager for this functionality.
+  appendToProver : function(element) {
+    document.getElementById("prover").appendChild(element)
+  },
+
+  insertKeyTreeView : function(element, sequent, client) {
+    var tree = new Tree(sequent, null);
+    var treeView = new KeYTreeView(tree, client, element);
+    treeView.redrawIn(element);
+    HydraEventListeners.treeViews.push(treeView);
+  }
+}
