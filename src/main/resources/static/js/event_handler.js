d2e7ac10338e0e8f1bfcb14843abc13047bcb178
Nathan Fulton <nathanfu@cs.cmu.edu>
2014-05-30 20:43:15 -0400

    Added a try...catch around the entire event handler.

--- src/main/resources/static/js/event_handler.js
+++ src/main/resources/static/js/event_handler.js
@@ -7,66 +7,71 @@ function HydraEventHandler(evt, client) {
   var proverSpan = document.getElementById("provercontents")
   var errorSpan = document.getElementById("errors")
 
-  if(!(evt.eventType)) {
-    alert("Non-event found in event stream.");
-    console.log("non-event found in event stream: ");
-    console.log(evt);
-    return;
-  }
+  try {
+    if(!(evt.eventType)) {
+      alert("Non-event found in event stream.");
+      console.log("non-event found in event stream: ");
+      console.log(evt);
+      return;
+    }
 
-  else if(evt.eventType === "ErrorResponse") {
-    var messageSpan = document.createElement("div")
-    messageSpan.setAttribute("class", "errorMessage")
-    messageSpan.innerHTML = evt.message
-    errorSpan.appendChild(messageSpan)
+    else if(evt.eventType === "ErrorResponse") {
+      var messageSpan = document.createElement("div")
+      messageSpan.setAttribute("class", "errorMessage")
+      messageSpan.innerHTML = evt.message
+      errorSpan.appendChild(messageSpan)
 
-    console.error("Hydra server returned an ErrorResponse: ");
-    console.log(evt);
-  }
+      console.error("Hydra server returned an ErrorResponse: ");
+      console.log(evt);
+    }
 
-  else if(evt.eventType === "CreateRootNode") {
-    console.log("Root node for this problem: ");
-    console.log(evt);
-    UI.insertKeyTreeView('prover', evt.sequent, client);
-    //$("#provercontents").html(
-        //SequentGUI.staticView(client, evt.sequent)
-    //);
-  }
+    else if(evt.eventType === "CreateRootNode") {
+      console.log("Root node for this problem: ");
+      console.log(evt);
+      UI.insertKeyTreeView('prover', evt.sequent, client);
+      //$("#provercontents").html(
+          //SequentGUI.staticView(client, evt.sequent)
+      //);
+    }
 
-  else if(evt.eventType === "AddNode") {
-    console.log("AddNode event: ");
-    console.log(evt);
-    //Pass the event along to all of the tree listeners.
-    HydraEventListeners.treeViews.map(function(child) {
-      if(child == null) {
-        GenericGUI.showError("found a null tree view.")
+    else if(evt.eventType === "AddNode") {
+      console.log("AddNode event: ");
+      console.log(evt);
+      //Pass the event along to all of the tree listeners.
+      HydraEventListeners.treeViews.map(function(child) {
+        if(child == null) {
+          GenericGUI.showError("found a null tree view.")
+        }
+        var parentId = evt.parentId
+        var node = evt.node
+        child.addNode(parentId, node)
+      });
+
+      //Show an error message if there are no trees listening.
+      if(HydraEventListeners.treeViews.length === 0) {
+        console.error("Recieved an add node event, but there are no tree views to process the event.");
       }
-      var parentId = evt.parentId
-      var node = evt.node
-      child.addNode(parentId, node)
-    });
+    }
 
-    //Show an error message if there are no trees listening.
-    if(HydraEventListeners.treeViews.length === 0) {
-      console.error("Recieved an add node event, but there are no tree views to process the event.");
+    else if(evt.eventType === "TacticFinished") {
+      console.log("TacticFinished event: ");
+      console.log(evt);
+      GenericGUI.greenFlash(evt.uid);
+    }
+
+
+    //Add ignore cases for all syncronous calls.
+    else if(evt.eventType === "FormulaToStringResponse") {}
+    else if(evt.eventType === "FormulaToInteractiveStringResponse") {}
+    else if(evt.eventType === "FormulaFromUid") {}
+
+    //Add error case
+    else {
+      throw "HydraEventHandler received an event with unhandled type: " + evt.eventType;
     }
   }
-
-  else if(evt.eventType === "TacticFinished") {
-    console.log("TacticFinished event: ");
-    console.log(evt);
-    GenericGUI.greenFlash(evt.uid);
-  }
-
-
-  //Add ignore cases for all syncronous calls.
-  else if(evt.eventType === "FormulaToStringResponse") {}
-  else if(evt.eventType === "FormulaToInteractiveStringResponse") {}
-  else if(evt.eventType === "FormulaFromUid") {}
-
-  //Add error case
-  else {
-    throw "HydraEventHandler received an event with unhandled type: " + evt.eventType;
+  catch(e) {
+    UI.showError("Exception thrown during event handling", e);
   }
 }
 
