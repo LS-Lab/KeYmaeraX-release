60300875b1789e692047ae9a3dc3892f1649d8d0
Nathan Fulton <nathan.fulton@gmail.com>
2014-10-03 02:00:55 -0400

    cleaned up repo root.

--- Wilfried_readme.txt
+++
@@ -1,5 +0,0 @@
-Wilfried,
-
-Our front-end javascript and HTML is in src/main/resources/static
-
-Our Spray code is in src/main/edu/cmu/cs/ls/keymaera/hydra
--- doc/models.test.sh
+++ doc/models.test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This is how the form upload call goes; for now I'm just stuffing raw files
+# in the data portion of the http request because actual file uploads are a
+# huge pain...
 #From project root:
 echo "Creating a new model:";
 curl --form upload=@examples/dev/t/parsing/positive/test.key localhost:8090/user/t/model/test;
