2b4a7f97a175a777357f8e8a000bf29c17992aec
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-01-20 20:07:22 -0500

    Sequent factory for tests

---
+++ src/test/scala/testHelper/SequentFactory.scala
@@ -0,0 +1,22 @@
+package testHelper
+
+import edu.cmu.cs.ls.keymaera.core.{Sequent, Formula, NamedSymbol}
+
+/**
+ * Created by smitsch on 1/20/15.
+ * @author Stefan Mitsch
+ */
+object SequentFactory {
+  /**
+   * Creates a new sequent with the specified prefixes pref, antecedents ante, and succedents succ.
+   * @param pref The list of prefix symbols.
+   * @param ante The list of antecedent formulas.
+   * @param succ The list of succedent formulas.
+   * @return The new sequent.
+   */
+  def sequent(pref: Seq[NamedSymbol], ante: Seq[Formula], succ: Seq[Formula]) =
+    new Sequent(pref.to[scala.collection.immutable.Seq],
+      ante.to[scala.collection.immutable.IndexedSeq],
+      succ.to[scala.collection.immutable.IndexedSeq]
+    )
+}
