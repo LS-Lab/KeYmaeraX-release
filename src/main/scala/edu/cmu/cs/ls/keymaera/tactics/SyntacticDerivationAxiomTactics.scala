30d1994924792a020eefe5b165f3bd2a30fd9e63
Nathan Fulton <gitcommit@nfulton.org>
2015-02-04 17:38:52 -0500

    AndDerivative axiom

---
+++ src/main/scala/edu/cmu/cs/ls/keymaera/tactics/SyntacticDerivationAxiomTactics.scala
@@ -0,0 +1,88 @@
+package edu.cmu.cs.ls.keymaera.tactics
+
+//@todo minimize imports
+import edu.cmu.cs.ls.keymaera.core._
+import edu.cmu.cs.ls.keymaera.tactics.Tactics._
+
+/**
+ * Contains the axiom tactics and wrapper tactics for syntactic derivation of formulas and terms.
+ * The axiom tactics go both directions; for this reason, there are three tactics per axiom:
+ *    - The AxiomNameT tactic is the actual axiom.
+ *    - The AxiomNameAtomizeT tactic derives in the typical left-to-right direction; that is, it takes a derivative
+ *      term and constructs a non-derivative term.
+ *    - The AxiomNameAggregateT tactic goes in the typical right-to-left direction.
+ *
+ * Created by nfulton on 2/4/15.
+ */
+object SyntacticDerivationAxiomTactics {
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // "Derivatives" of Formulas
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  /*
+   * Axiom "&' derive and".
+   *   (p & q)' <-> ((p') & (q'))
+   * End.
+   */
+  def AndDerivativeT = new AxiomTactic("&' derive and", "&' derive and") {
+    override def applies(f: Formula): Boolean = f match {
+      case FormulaDerivative(And(_,_))              => true
+      case And(FormulaDerivative(_), FormulaDerivative(_)) => true
+    }
+
+    override def applies(s: Sequent, p: Position): Boolean = {
+      !p.isAnte && p.inExpr == HereP && super.applies(s, p)
+    }
+
+    override def constructInstanceAndSubst(f: Formula, ax: Formula, pos: Position):
+    Option[(Formula, Formula, Substitution, Option[PositionTactic], Option[PositionTactic])] = {
+      val aP  = PredicateConstant("p") //@todo not sure if this is correct.
+      val aQ  = PredicateConstant("q")
+
+      f match {
+        case FormulaDerivative(And(p,q)) => {
+          val g = And(FormulaDerivative(p), FormulaDerivative(q))
+          val axiomInstance = Imply(f, g)
+
+          val subst = Substitution(List(
+            SubstitutionPair(aP, p),
+            SubstitutionPair(aQ, q)
+          ))
+
+          Some(ax, axiomInstance, subst, None, None)
+        }
+        case And(FormulaDerivative(pSort, p), FormulaDerivative(qSort, q)) => {
+          val g = FormulaDerivative(And(p,q))
+          val axiomInstance = Imply(f,g)
+
+          val subst = Substitution(List(
+            SubstitutionPair(aP, p),
+            SubstitutionPair(aQ, q)
+          ))
+
+          Some(ax, axiomInstance, subst, None, None)
+        }
+      }
+    }
+  }
+
+  val AndDerivativeAtomizeT = new PositionTactic("&' derive and Atomize") {
+    override def applies(s: Sequent, p: Position): Boolean = {
+      !p.isAnte && p.inExpr == HereP && (s(p) match {
+        case FormulaDerivative(And(_,_)) => true
+        case _ => false
+      })
+    }
+
+    override def apply(p: Position): Tactic = AndDerivativeT(p)
+  }
+
+  val AndDerivativeAggregateT = new PositionTactic("&' derive and Aggregate") {
+    override def applies(s: Sequent, p: Position): Boolean = !p.isAnte && p.inExpr == HereP && (s(p) match {
+      case And(FormulaDerivative(_), FormulaDerivative(_)) => true
+      case _                                               => false
+    })
+
+    override def apply(p: Position): Tactic = AndDerivativeT(p)
+  }
+}
