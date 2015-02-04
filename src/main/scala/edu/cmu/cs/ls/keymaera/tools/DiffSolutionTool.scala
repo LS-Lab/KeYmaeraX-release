63065f172f2cdf5b8e85c6c00c271b2e97895cd6
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-02-04 15:57:38 -0500

    Hand names of initial values to diff. solution tool

--- src/main/scala/edu/cmu/cs/ls/keymaera/tools/DiffSolutionTool.scala
+++ src/main/scala/edu/cmu/cs/ls/keymaera/tools/DiffSolutionTool.scala
@@ -11,23 +11,17 @@ trait DiffSolutionTool {
    * Computes the symbolic solution of a differential equation in normal form.
    * @param diffSys The system of differential equations of the form x' = theta & H.
    * @param diffArg The name of the differential argument (dx/d diffArg = theta).
+   * @param iv The initial values per derivative.
    * @return The solution if found; None otherwise
    */
-  def diffSol(diffSys: ContEvolveProgram, diffArg: Variable): Option[Formula]
-
-  /**
-   * Computes the symbolic solution of a differential equation in normal form.
-   * @param diffEq The differential equation of the form x' = theta & H.
-   * @param diffArg The name of the differential argument (dx/d diffArg = theta).
-   * @return The solution if found; None otherwise
-   */
-  def diffSol(diffEq: NFContEvolve, diffArg: Variable): Option[Formula]
+  def diffSol(diffSys: ContEvolveProgram, diffArg: Variable, iv: Map[Variable, Variable]): Option[Formula]
 
   /**
    * Computes the symbolic solution of a system of differential equations.
    * @param diffSys The system of differential equations.
    * @param diffArg The name of the differential argument (dx/d diffArg = theta).
+   * @param iv The initial values per derivative.
    * @return The solution if found; None otherwise
    */
-  def diffSol(diffSys: ContEvolve, diffArg: Variable): Option[Formula]
+  def diffSol(diffSys: ContEvolve, diffArg: Variable, iv: Map[Variable, Variable]): Option[Formula]
 }
