5a4fa0cd0413ef9c855c645500200afa67ac85c9
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-02-20 09:10:00 -0500

    Base class for axiom tactics that need cascaded application of uniform substitution

--- src/main/scala/edu/cmu/cs/ls/keymaera/tactics/AxiomTactic.scala
+++ src/main/scala/edu/cmu/cs/ls/keymaera/tactics/AxiomTactic.scala
@@ -147,6 +147,123 @@ abstract class AxiomTactic(name: String, axiomName: String) extends PositionTact
 }
 
 /**
+ * Base class for axiom tactics that need cascaded application of uniform substitution.
+ * @param name The name of the tactic.
+ * @param axiomName The name of the axiom.
+ */
+abstract class CascadedAxiomTactic(name: String, axiomName: String) extends PositionTactic(name) {
+  //@todo a java.lang.ExceptionInInitializerError is thrown
+  require(Axiom.axioms != null, "the list of axioms should be defined.")
+  require(Axiom.axioms.keySet.contains(axiomName), "The requested axiom should be in the set of axioms.")
+  val axiom = Axiom.axioms.get(axiomName)
+  def applies(f: Formula): Boolean
+  override def applies(s: Sequent, p: Position): Boolean = axiom.isDefined && applies(getFormula(s, p))
+
+  /**
+   * This methods constructs the axiom before the renaming, axiom instance, substitution to be performed and a tactic
+   * that performs the renaming.
+   *
+   * An axiom tactic performs the following steps (Hilbert style):
+   * 1. Guess axiom
+   * 2. Rename bound variables to match the instance we want
+   * 3. Perform Uniform substitution to instantiate the axiom
+   *
+   * Axioms usually have the form ax = OP(a, b). The constructed instance either has the form OP(f, g) or OP(g, f).
+   * Here, f is an input to this function and g is derived from the axiom to be used. The output of this function
+   * should be 3 things:
+   * 1. The form of the axiom before apply the tactic provided in 4
+   * 2. The substitutions to turn 2 into 1 (can have intermediate results)
+   * 3. A tactic to turn the result of 3 into the actual axiom (and/or the other way around)
+   *
+   * In the long run all this should be computed by unification.
+   *
+   * @param f the formula that should be rewritten using the axiom
+   * @param ax the axiom to be used
+   * @param pos the position to which this rule is applied to
+   * @return (Axiom before executing the given position tactic;
+   *         the instance of the axiom,
+   *         the uniform substitution that transforms the first into the second axiom (Hilbert style);
+   *         an optional position tactic that transforms the first
+   *         argument into the actual axiom (usually alpha renaming)).
+   * @see #constructInstanceAndSubst(Formula)
+   */
+  def constructInstanceAndSubst(f: Formula, ax: Formula, pos: Position): Option[(Formula,
+    List[(Substitution, Map[Formula, Formula])], Option[PositionTactic])] =
+    constructInstanceAndSubst(f, ax)
+
+  def constructInstanceAndSubst(f: Formula, ax: Formula): Option[(Formula, List[(Substitution, Map[Formula, Formula])],
+    Option[PositionTactic])] = {
+    constructInstanceAndSubst(f) match {
+      case Some((instance, subst)) => Some((instance, subst, None))
+      case None => None
+    }
+  }
+
+  /**
+   * This is a simpler form of the constructInstanceAndSubst method. It just gets the formula to be rewritten
+   * and has to construct the axiom instance and the substitution to transform the axiom into its instance.
+   *
+   * This method will be called by the default implementation of constructInstanceAndSubst(f: Formula, a: Formula).
+   *
+   * @param f The formula to be rewritten using an axiom
+   * @return (The axiom instance to be constructed and used for rewriting;
+   *          Substitutions to transform the axiom into its instance)
+   * @see #constructInstanceAndSubst(Formula,Formula)
+   */
+  def constructInstanceAndSubst(f: Formula): Option[(Formula, List[(Substitution, Map[Formula, Formula])])] = ???
+
+  //@TODO Add contract that applies()=>\result fine
+  override def apply(pos: Position): Tactic = new ConstructionTactic(this.name) {
+    override def applicable(node: ProofNode): Boolean = applies(node.sequent, pos)
+
+    override def constructTactic(tool: Tool, node: ProofNode): Option[Tactic] = {
+      import EqualityRewritingImpl.equalityRewriting
+      import AxiomTactic.axiomT
+      axiom match {
+        case Some(ax) =>
+          constructInstanceAndSubst(getFormula(node.sequent, pos), ax, pos) match {
+            case Some((axiomInstance, subst, anteTac)) =>
+              val axiomInstPos = AntePosition(node.sequent.ante.length)
+              val axiomApplyTactic = assertPT(axiomInstance)(axiomInstPos) & (axiomInstance match {
+                //@TODO Prefer simpler sequent proof rule for <->left rather than congruence rewriting if the position to use it on is on top-level of sequent
+                //@TODO If Pos.isAnte the following position management seems wrong since unstable.
+                case Equiv(_, _) => equalityRewriting(axiomInstPos, pos) & ((assertPT(axiomInstance)&hideT)(axiomInstPos) & (assertPT(node.sequent(pos),"hiding original instance")&hideT)(pos.topLevel))
+                case Equals(Real, _, _) => equalityRewriting(axiomInstPos, pos, checkDisjoint = false) & ((assertPT(axiomInstance)&hideT)(axiomInstPos) & (assertPT(node.sequent(pos),"hiding original instance")&hideT)(pos.topLevel))
+                case Imply(_, _) if pos.isAnte  && pos.inExpr == HereP => modusPonensT(pos, axiomInstPos)
+                case Imply(_, _) if !pos.isAnte && pos.inExpr == HereP => ImplyLeftT(axiomInstPos) & ((assertPT(node.sequent(pos),"hiding original instance")&hideT)(pos), AxiomCloseT(axiomInstPos.topLevel, pos))
+                case _ => ???
+              })
+              // // hide in reverse order since hiding changes positions
+              // val hideAllAnte = for(i <- node.sequent.ante.length - 1 to 0 by -1) yield hideT(AntePosition(i))
+              // // this will hide all the formulas in the current succedent (the only remaining one will be the one we cut in)
+              // val hideAllSuccButLast = for(i <- node.sequent.succ.length - 1 to 0 by -1) yield hideT(SuccPosition(i))
+              // val hideAllAnteAllSuccButLast = (hideAllAnte ++ hideAllSuccButLast).reduce(seqT)
+              //@TODO Insert contract tactic after hiding all which checks that exactly the intended axiom formula remains and nothing else.
+              //@TODO Introduce a reusable tactic that hides all formulas except the ones given as argument and is followed up by a contract ensuring that exactly those formuals remain.
+              val anteCont = anteTac match {
+                // SuccPosition(0) is the only position remaining in axiom proof
+                case Some(tactic) => assertT(1, 1) & tactic(AntePosition(0))
+                case None => NilT
+              }
+              val axiomPos = SuccPosition(node.sequent.succ.length)
+              println("Axiom instance " + axiomInstance)
+
+              val substitutions = subst.map(e => uniformSubstT(e._1, e._2)).fold(NilT)((a, b) => a & b)
+
+              val axiomInstanceTactic = (assertPT(axiomInstance) & cohideT)(axiomPos) & (assertT(0,1) &
+                assertT(axiomInstance, SuccPosition(0)) & substitutions &
+                assertT(0, 1) & axiomT(axiomName) & assertT(1, 1) & anteCont & AxiomCloseT)
+              Some(cutT(Some(axiomInstance)) & onBranch((cutUseLbl, axiomApplyTactic), (cutShowLbl, axiomInstanceTactic)))
+            case None => None
+          }
+        case None => None
+      }
+    }
+  }
+
+}
+
+/**
  * Base class for tactics that want to use some knowledge in context. Derived classes have to close the
  * "knowledge subclass continue" branch (e.g., by axiom lookup or propositional tactics).
  * @param name The name of the tactic.
@@ -207,10 +324,10 @@ abstract class ContextualizeKnowledgeTactic(name: String) extends PositionTactic
             abstractionT(SuccPosition(0)) & hideT(SuccPosition(0)) & skolemizeT(SuccPosition(0)) &
             assertT(0, 1) & cutT(Some(axiomInstance)) &
             onBranch((cutUseLbl,
-              (equalityRewriting(axiomInstPos, pos) & ((assertPT(axiomInstance)&hideT)(axiomInstPos) & hideT(pos.topLevel)) & ImplyRightT(pos.topLevel) & AxiomCloseT) ~
+              (equalityRewriting(axiomInstPos, pos) & ((assertPT(axiomInstance)&hideT)(axiomInstPos) &
+                hideT(pos.topLevel)) & ImplyRightT(pos.topLevel) & AxiomCloseT) ~
                 (hideT(axiomInstPos) & LabelBranch("additional obligation"))), //for term stuff.
-              (cutShowLbl, TacticLibrary.debugT("Inside cut show") &
-                hideT(SuccPosition(0)) & cont & LabelBranch(BranchLabels.knowledgeSubclassContinue))))
+              (cutShowLbl, hideT(SuccPosition(0)) & cont & LabelBranch(BranchLabels.knowledgeSubclassContinue))))
 
           Some(cutT(Some(forKAxiomInstance)) & onBranch((cutUseLbl, axiomApplyTactic), (cutShowLbl, axiomInstanceTactic)))
         case None => None
@@ -245,7 +362,7 @@ abstract class DerivativeAxiomInContextTactic(name: String, axiomName: String)
 
     override def constructTactic(tool: Tool, node: ProofNode): Option[Tactic] = {
       import AxiomTactic.axiomT
-      Some(onBranch("knowledge subclass continue", assertT(0, 1) & TacticLibrary.debugT("foo") & axiomT(axiomName) & assertT(1, 1) & AxiomCloseT))
+      Some(onBranch("knowledge subclass continue", assertT(0, 1) & axiomT(axiomName) & assertT(1, 1) & AxiomCloseT))
     }
   }
 }
