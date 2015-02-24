d89dfe468c2ebe77ee43e07bba66278babc1c705
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-02-24 16:43:51 -0500

    Cleaned up NNF tests

--- src/test/scala/NNFTests.scala
+++ src/test/scala/NNFTests.scala
@@ -2,55 +2,46 @@
  * Created by nfulton on 2/11/15.
  */
 
-import edu.cmu.cs.ls.keymaera.core.{PosInExpr, SuccPosition}
+import edu.cmu.cs.ls.keymaera.core.{EquivRight, PosInExpr, SuccPosition}
+import edu.cmu.cs.ls.keymaera.tactics.{TacticLibrary, NNFRewrite}
 import edu.cmu.cs.ls.keymaera.tactics.NNFRewrite._
-import edu.cmu.cs.ls.keymaera.tactics.{NNFRewrite, TacticLibrary, PropositionalTacticsImpl, SearchTacticsImpl}
+import testHelper.StringConverter._
 
 class NNFTests extends TacticTestSuite {
   "Double negation elimination" should "eliminate" in {
-    val node = helper.formulaToNode(helper.parseFormula("!(!(1=1))"))
+    val node = helper.formulaToNode("!(!(1=1))".asFormula)
     val tactic = helper.positionTacticToTactic(rewriteDoubleNegationEliminationT)
-    helper.runTactic(tactic, node, true)
-    require(containsOnlyExactlyOpenGoal(node, helper.parseFormula("1=1")))
+    helper.runTactic(tactic, node, mustApply = true)
+    node.openGoals().flatMap(_.sequent.ante) shouldBe empty
+    node.openGoals().flatMap(_.sequent.succ) should contain only "1=1".asFormula
   }
 
   it should "place the new formula in the same location as the previous formula" in {
-    val node = helper.formulaToNode(helper.parseFormula("!(!(1=1)) & 2=2"))
+    val node = helper.formulaToNode("!(!(1=1)) & 2=2".asFormula)
     val doubleNegationPosition = SuccPosition(0, PosInExpr(0 :: Nil))
 
     val tactic = rewriteDoubleNegationEliminationT(doubleNegationPosition)
-    require(formulaAtExpr(node, doubleNegationPosition).get.equals(helper.parseFormula("!(!(1=1))")))
-    helper.runTactic(tactic, node)
-    println(formulaAtExpr(node, doubleNegationPosition).get)
-    require(formulaAtExpr(node, doubleNegationPosition).get.equals(helper.parseFormula("(1=1)")))
 
-    helper.report(node)
+    formulaAtExpr(node, doubleNegationPosition).get should be ("!(!(1=1))".asFormula)
+    helper.runTactic(tactic, node)
+    node.openGoals().flatMap(_.sequent.ante) shouldBe empty
+    node.openGoals().flatMap(_.sequent.succ) should contain only "1=1&2=2".asFormula
   }
 
   it should "work in subformulas" in {
-    val node = helper.formulaToNode(helper.parseFormula("[x:=1;](!(!(1=1)))"))
+    val node = helper.formulaToNode("[x:=1;](!(!(1=1)))".asFormula)
     val doubleNegationPosition = SuccPosition(0, PosInExpr(1 :: Nil))
     val tactic = rewriteDoubleNegationEliminationT(doubleNegationPosition)
-    helper.runTactic(tactic,node,true)
-    require(false) //@todo check this.
+    helper.runTactic(tactic, node, mustApply = true)
+    node.openGoals().flatMap(_.sequent.ante) shouldBe empty
+    node.openGoals().flatMap(_.sequent.succ) should contain only "[x:=1;]1=1".asFormula
   }
 
   "demorgan &" should "have a working proof" in {
-
     val node = helper.formulaToNode(helper.parseFormula("(!(x=x & y=y)) <-> (!(x=x) | !(y=y))"))
     val pos = SuccPosition(0)
     val tactic = proofOfDeMorganConjunction(pos)
-    helper.runTactic(tactic, node, true)
-    node.isClosed() shouldBe true
-  }
-
-  it should "prove example 1" in {
-    val string = "2=2&3=3->4=4<->!(2=2&3=3)|4=4"
-    val node = helper.formulaToNode(helper.parseFormula(string))
-    val pos = SuccPosition(0)
-    val tactic = proofOfDeMorganConjunction(pos)
-    helper.runTactic(tactic, node, true)
-    node.isClosed() shouldBe true
+    helper.runTactic(tactic, node, mustApply = true) shouldBe 'closed
   }
 
   it should "rewrite something" in {
@@ -58,60 +49,63 @@ class NNFTests extends TacticTestSuite {
     val pos = SuccPosition(0, PosInExpr(0 :: Nil))
 
     val tactic = rewriteNegConjunct(pos)
-    require(formulaAtExpr(node, pos).get.equals(helper.parseFormula("!(1=1 & 2=2)")))
 
     helper.runTactic(tactic, node)
-    containsOpenGoal(node, helper.parseFormula("(!(1=1) | !(2=2)) & 3=3"))
-    //@todo not sure how to test these...
+    node.openGoals().flatMap(_.sequent.ante) shouldBe empty
+    node.openGoals().flatMap(_.sequent.succ) should contain only "(!1=1 | !2=2) & 3=3".asFormula
   }
 
   "demorgan |" should "have a working proof" in {
-    val node = helper.formulaToNode(helper.parseFormula("(!(x=x | y=y)) <-> (!(x=x) & !(y=y))"))
+    val node = helper.formulaToNode("(!(x=x | y=y)) <-> (!(x=x) & !(y=y))".asFormula)
     val pos = SuccPosition(0)
     val tactic = proofOfDeMorganDisjunction(pos)
-    helper.runTactic(tactic, node, true)
-    node.isClosed() shouldBe true
+    helper.runTactic(tactic, node, mustApply = true) shouldBe 'closed
   }
 
   it should "rewrite something" in {
-    val node = helper.formulaToNode(helper.parseFormula("!(1=1 | 2=2) & 3=3"))
+    val node = helper.formulaToNode("!(1=1 | 2=2) & 3=3".asFormula)
     val pos = SuccPosition(0, PosInExpr(0 :: Nil))
 
     val tactic = rewriteNegDisjunct(pos)
-    require(formulaAtExpr(node, pos).get.equals(helper.parseFormula("!(1=1 | 2=2)")))
 
     helper.runTactic(tactic, node)
-    require(containsOpenGoal(node, helper.parseFormula("(!(1=1) & !(2=2)) & 3=3")))
+    node.openGoals().flatMap(_.sequent.ante) shouldBe empty
+    node.openGoals().flatMap(_.sequent.succ) should contain only "(!1=1 & !2=2) & 3=3".asFormula
   }
 
 
   "implication to disjunction" should "work" in {
-    val node = helper.formulaToNode(helper.parseFormula("1=1 -> 2=2"))
+    val node = helper.formulaToNode("1=1 -> 2=2".asFormula)
     val tactic = rewriteImplicationToDisjunction(SuccPosition(0))
     helper.runTactic(tactic, node)
-    helper.report(node)
-    require(containsOpenGoal(node, helper.parseFormula("(!(1=1) | (2=2))")))
-
+    node.openGoals().flatMap(_.sequent.ante) shouldBe empty
+    node.openGoals().flatMap(_.sequent.succ) should contain only "(!1=1 | 2=2)".asFormula
   }
 
   "the entire rewrite" should "work" in {
-    val node = helper.formulaToNode(helper.parseFormula("!( (1=1 | 2=2) & !(!(2=2)) )"))
+    val node = helper.formulaToNode("!( (1=1 | 2=2) & !(!(2=2)) )".asFormula)
     val tactic = NNFRewrite(SuccPosition(0))
 
     helper.runTactic(tactic, node)
+    node.openGoals().flatMap(_.sequent.ante) shouldBe empty
+    node.openGoals().flatMap(_.sequent.succ) should contain only "( !(1=1) & !(2=2) ) | !(2=2)".asFormula //OK.
+  }
 
-    helper.report(node)
-    val expected = helper.parseFormula("( !(1=1) & !(2=2) ) | !(2=2)") //OK.
-    containsOpenGoal(node, expected) shouldBe (true)
+  it should "prove example 1" in {
+    import TacticLibrary.{EquivRightT,AxiomCloseT}
+
+    val node = helper.formulaToNode("((2=2&3=3)->4=4)<->(!(2=2&3=3)|4=4)".asFormula)
+    val pos = SuccPosition(0)
+    val tactic = NNFRewrite(pos) ~ (EquivRightT(pos) & AxiomCloseT)
+    helper.runTactic(tactic, node, mustApply = true) shouldBe 'closed
   }
 
   it should "work 2" in {
-    val node = helper.formulaToNode(helper.parseFormula("!( (1=1 | 2=2) -> !(!(2=2)) )"))
+    val node = helper.formulaToNode("!( (1=1 | 2=2) -> !(!(2=2)) )".asFormula)
     val tactic = NNFRewrite(SuccPosition(0))
 
     helper.runTactic(tactic, node)
-
-    val expected = helper.parseFormula("(1=1|2=2)&!(2=2)") //OK.
-    containsOpenGoal(node, expected) shouldBe(true)
+    node.openGoals().flatMap(_.sequent.ante) shouldBe empty
+    node.openGoals().flatMap(_.sequent.succ) should contain only "(1=1|2=2)& !2=2".asFormula //OK.
   }
 }
