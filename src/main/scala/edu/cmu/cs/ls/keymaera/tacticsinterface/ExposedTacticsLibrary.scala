f84cb9878ab8e0788a504b41ef6a9589264d58d5
Nathan Fulton <gitcommit@nfulton.org>
2015-02-27 11:01:20 -0500

    Tactics with input work
    But we cannot do something like cutT(Nil).
    We'll need a bit of a redesign to allow something like that.

--- src/main/scala/edu/cmu/cs/ls/keymaera/tacticsinterface/ExposedTacticsLibrary.scala
+++ src/main/scala/edu/cmu/cs/ls/keymaera/tacticsinterface/ExposedTacticsLibrary.scala
@@ -1,16 +1,22 @@
 package edu.cmu.cs.ls.keymaera.tacticsinterface
 
+import edu.cmu.cs.ls.keymaera.core.Formula
 import edu.cmu.cs.ls.keymaera.tactics.{Tactics, TacticLibrary}
 import edu.cmu.cs.ls.keymaera.tactics.Tactics.{PositionTactic, Tactic}
 
 /**
  * These are tactics which are exposed to the tactics interface.
+ *
+ * All methods which take arguments must take a single argument of type Option[Formula].
+ * For example, debugT isn't currently supported.
+ *
  * Created by nfulton on 2/26/15.
  */
 object ExposedTacticsLibrary {
   // Utility Tactics
   def NilT : Tactic = Tactics.NilT
 
-  // sections go here...
+  // The Sequent Calculus
+  def cutT(inv : Option[Formula]) : Tactic = TacticLibrary.cutT(inv)
   def AxiomCloseT : Tactic = TacticLibrary.AxiomCloseT
 }
