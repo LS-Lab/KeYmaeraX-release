b3baddb86cbb98d71f0fe1d4efddf0ef0ff2dbc9
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-03-31 09:40:20 -0400

    Context: term or formula context

--- src/main/scala/edu/cmu/cs/ls/keymaera/tactics/Context.scala
+++ src/main/scala/edu/cmu/cs/ls/keymaera/tactics/Context.scala
@@ -1,16 +1,23 @@
 package edu.cmu.cs.ls.keymaera.tactics
 
 import edu.cmu.cs.ls.keymaera.core._
+import edu.cmu.cs.ls.keymaera.core.StaticSemantics.signature
 
 /**
  * Created by smitsch on 3/29/15.
  * @author Stefan Mitsch
  */
 sealed case class Context(ctx: Formula) {
+  // either a term or a formula context, not both
+  assert(!(signature(ctx).contains(CDotFormula) && signature(ctx).contains(CDot)))
+
   def apply(e: Expr) = e match {
     case f: Formula => instantiate(f)
     case t: Term => instantiate(t)
   }
+
+  def isFormulaContext = signature(ctx).contains(CDotFormula)
+  def isTermContext = signature(ctx).contains(CDot)
 
   /**
    * Instantiates the context fml with the formula withF
