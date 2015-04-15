bdf9d8289d2d306bbb24feba243e3584d593ff03
Andre Platzer <aplatzer@cs.cmu.edu>
2015-04-14 21:30:18 -0400

    Move

--- src/main/scala/edu/cmu/cs/ls/keymaera/Expr.scala
+++ src/main/scala/edu/cmu/cs/ls/keymaera/Expr.scala
@@ -4,7 +4,7 @@
  * @see "Andre Platzer. A uniform substitution calculus for differential dynamic logic.  arXiv 1503.01981, 2015."
  * @see "Andre Platzer. The complete proof theory of hybrid systems. ACM/IEEE Symposium on Logic in Computer Science, LICS 2012, June 25–28, 2012, Dubrovnik, Croatia, pages 541-550. IEEE 2012"
  */
-package edu.cmu.cs.ls.keymaera.core.edu.cmu.cs.ls.keymaera
+package edu.cmu.cs.ls.keymaera
 
 // require favoring immutable Seqs for soundness
 
