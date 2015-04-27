71985476c61c6950d9ced03f001e551ec833c29c
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-04-27 18:59:21 -0400

    LemmaDB trait and file-based implementation

---
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaera/core/LemmaDB.scala
@@ -0,0 +1,27 @@
+package edu.cmu.cs.ls.keymaera.core
+
+import edu.cmu.cs.ls.keymaera.parser.ToolEvidence
+
+/**
+ * Store and retrieve lemmas.
+ *
+ * Created by smitsch on 4/27/15.
+ * @author Stefan Mitsch
+ */
+trait LemmaDB {
+  /**
+   * Returns the lemma's conclusion.
+   * @param lemmaID Identifies the lemma.
+   * @return The conclusion.
+   */
+  def getLemmaConclusion(lemmaID: String): Formula
+
+  /**
+   * Adds a lemma to this lemma DB. The lemma is proved using some tool, which provided evidence of the correctness of
+   * the lemma.
+   * @param conclusion The lemma's conclusion.
+   * @param evidence Evidence that the lemma is correct: (input to the tool, tool output)
+   * @return The lemma ID, if added successfully; None otherwise.
+   */
+  def addLemma(conclusion: Formula, evidence: (String, String)): Option[String]
+}
