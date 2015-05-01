268af7d268557cd4528a93c38460d50d42422ebc
Andre Platzer <aplatzer@cs.cmu.edu>
2015-05-01 12:58:46 -0400

    Code Review: KeYmaera X Core: QETool.scala, LemmaDB.scala, FileLemmaDB.scala
    Soundness-critical: only save isProved lemmas, because the stored file ignores remaining open subgoals.
    
    Completeness fix: header of variable declarations to include all free variables even if some of them are also bound variables.
    
    Commenting and type quality distinctions.
    
    TODO Code Reviews.
    
    Signed-off-by: Andre Platzer <aplatzer@cs.cmu.edu>

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaera/core/LemmaDB.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaera/core/LemmaDB.scala
@@ -7,24 +7,27 @@ package edu.cmu.cs.ls.keymaera.core
  * @author Stefan Mitsch
  */
 trait LemmaDB {
+
+  type LemmaID = String
+
   /**
    * Indicates whether or not this lemma DB contains a lemma with the specified ID.
    * @param lemmaID Identifies the lemma.
    * @return True, if this lemma DB contains a lemma with the specified ID; false otherwise.
    */
-  def contains(lemmaID: String): Boolean
+  def contains(lemmaID: LemmaID): Boolean
 
   /**
    * Returns a lemma.
    * @param lemmaID Identifies the lemma.
    * @return The lemma, if found. None otherwise.
    */
-  def get(lemmaID: String): Option[Lemma]
+  def get(lemmaID: LemmaID): Option[Lemma]
 
   /**
    * Adds a lemma to this lemma DB.
    * @param lemma The lemma to add.
    * @return The lemma ID.
    */
-  private[core] def add(lemma: Lemma): String
+  private[core] def add(lemma: Lemma): LemmaID
 }
