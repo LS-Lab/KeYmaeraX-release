74eb04c05a9b521479b75843ae93e187caf32fe2
Andre Platzer <aplatzer@cs.cmu.edu>
2015-05-27 23:27:14 -0400

    Advocatus Diavoli

--- keymaerax-webui/src/test/scala/advocatus/AdvocatusReflecti.scala
+++ keymaerax-webui/src/test/scala/advocatus/AdvocatusReflecti.scala
@@ -10,6 +10,7 @@ import org.scalatest.{BeforeAndAfterEach, Matchers, FlatSpec}
 /**
  * "helpful" ways of proving false by weird Java reflection mechanisms.
 * @author aplatzer
+ * @todo write a similar test that recursively crawls all declared fields starting from Provable in search for something mutable and screams when found.
 */
 class AdvocatusReflecti extends FlatSpec with Matchers {
   val verum = new Sequent(immutable.Seq(), immutable.IndexedSeq(), immutable.IndexedSeq(True))
@@ -43,8 +44,40 @@ class AdvocatusReflecti extends FlatSpec with Matchers {
     a [IllegalAccessException] should be thrownBy fld.set(proof, falsum)
     println("Suddenly " + proof + " " + (if (proof.isProved) "proved" else "not proved"))
   }
-  //@todo same with security manager again
 
+  it should "not allow immutable Provable conclusion to be written to after accessible with security manager via index" in {
+    var proof = Provable.startProof(verum)(CloseTrue(SuccPos(0)), 0)
+    println("Provable " + proof + " " + (if (proof.isProved) "proved" else "not proved"))
+    println(classOf[Provable].getDeclaredFields.map(c => c.getName + " of type " + c.getType).mkString("\n"))
+    System.setSecurityManager(new SecurityManager() {
+      override def checkPermission(perm: Permission) = {
+        !(perm.isInstanceOf[ReflectPermission] && "suppressAccessChecks".equals(perm.getName()))
+      }
+    })
+    val fld = classOf[Provable].getDeclaredFields.apply(0)
+    a [SecurityException] should be thrownBy fld.setAccessible(true)
+    a [IllegalAccessException] should be thrownBy fld.set(proof, falsum)
+    println("Suddenly " + proof + " " + (if (proof.isProved) "proved" else "not proved"))
+  }
+
+  it should "not allow immutable Provable conclusion to be written to after accessible with security manager reset via index" in {
+    var proof = Provable.startProof(verum)(CloseTrue(SuccPos(0)), 0)
+    println("Provable " + proof + " " + (if (proof.isProved) "proved" else "not proved"))
+    println(classOf[Provable].getDeclaredFields.map(c => c.getName + " of type " + c.getType).mkString("\n"))
+    System.setSecurityManager(new SecurityManager() {
+      override def checkPermission(perm: Permission) = {
+        !(perm.isInstanceOf[RuntimePermission] && "setSecurityManager".equals(perm.getName)) &&
+          !(perm.isInstanceOf[ReflectPermission] && "suppressAccessChecks".equals(perm.getName()))
+      }
+    })
+    a [SecurityException] should be thrownBy System.setSecurityManager(new SecurityManager())
+    val fld = classOf[Provable].getDeclaredFields.apply(0)
+    a [SecurityException] should be thrownBy fld.setAccessible(true)
+    a [IllegalAccessException] should be thrownBy fld.set(proof, falsum)
+    println("Suddenly " + proof + " " + (if (proof.isProved) "proved" else "not proved"))
+  }
+
+  //@todo also test against wrapping SecurityManager replacement inside a PrivilegedAction
 
 
   //@TODO turn the following into indexedSeq overwriting. Not sure why they don't yell nor effect.
