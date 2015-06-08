2e7f2a6317fc2ac4cc375023714fc61a6a5e5d67
Andre Platzer <aplatzer@cs.cmu.edu>
2015-06-08 12:28:18 -0400

    Declare terminal types

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/KeYmaeraXParser.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/KeYmaeraXParser.scala
@@ -19,12 +19,40 @@ abstract class OPERATORS(opcode: String) extends Terminal(opcode)
 case class IDENT(name: String) extends Terminal(name)
 case class NUMBER(value: String) extends Terminal(value)
 case class OPERATOR(opcode: String) extends OPERATORS(opcode)
+
+object EOF extends Terminal("<EOF>")
+
 object LPARENS extends Terminal("(")
 object RPARENS extends Terminal(")")
 object LBRACK extends Terminal("{")
 object RBRACK extends Terminal("}")
+
 object PRIME extends OPERATORS("'")
-object EOF extends Terminal("<EOF>")
+object POWER extends OPERATORS("^")
+object STAR extends OPERATORS("*")
+object SLASH extends OPERATORS("/")
+object PLUS extends OPERATORS("+")
+object MINUS extends OPERATORS("+")
+
+object NOT extends OPERATORS("!")
+object AND extends OPERATORS("&")
+object OR extends OPERATORS("|")
+object EQUIV extends OPERATORS("<->")
+object IMPLY extends OPERATORS("->")
+
+object FORALL extends OPERATORS("\\forall")
+object EXISTS extends OPERATORS("\\exists")
+
+object GREATEREQUAL extends OPERATORS(">=")
+object LESSEQUAL extends OPERATORS("<=")
+object LDIA extends OPERATORS("<")
+object RDIA extends OPERATORS(">")
+
+object COMPOSE extends OPERATORS(";")
+object CHOICE extends OPERATORS("++")
+object LBOX extends OPERATORS("[")
+object RBOX extends OPERATORS("]")
+
 
 sealed abstract class Location
 object UnknownLocation extends Location
