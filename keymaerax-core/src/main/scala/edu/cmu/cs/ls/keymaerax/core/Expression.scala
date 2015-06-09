090637458e0232ae3a56f3b2c37d8b46a516063b
Andre Platzer <aplatzer@cs.cmu.edu>
2015-06-08 23:10:39 -0400

    Simplify toString notation to use getSimpleName instead of full name and reference

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/core/Expression.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/core/Expression.scala
@@ -62,7 +62,8 @@ case class ObjectSort(name : String) extends Sort
 sealed trait Expression {
   def kind : Kind
   def sort : Sort
-  override def toString : String = "(" + prettyString() + ")@" + canonicalString
+  //override def toString : String = "(" + prettyString() + ")@" + canonicalString
+  override def toString : String = "(" + prettyString() + ")@" + getClass.getSimpleName
   /** Pretty-printed string representing this expression */
   def prettyString() : String = PrettyPrinter.printer(this)
   private[core] def canonicalString : String = super.toString
