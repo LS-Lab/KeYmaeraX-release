6a38e2509a150f57aa46e155497181a69445a6d3
Nathan Fulton <gitcommit@nfulton.org>
2015-07-07 10:20:20 -0400

    Make img of ident the string that was parsed to produce the token.

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/KeYmaeraXLexer.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/KeYmaeraXLexer.scala
@@ -31,7 +31,7 @@ abstract class OPERATOR(val opcode: String) extends Terminal(opcode) {
   //final def opcode: String = img
   override def toString = getClass.getSimpleName //+ "\"" + img + "\""
 }
-case class IDENT(name: String, index: Option[Int] = None) extends Terminal(name + (index match {case Some(x) => x.toString.length + 1 case None => 0})) {
+case class IDENT(name: String, index: Option[Int] = None) extends Terminal(name + (index match {case Some(x) => "_"+index.toString case None => ""})) {
   override def toString = "ID(\"" + (index match {
     case None => name
     case Some(idx) => name + "," + idx
