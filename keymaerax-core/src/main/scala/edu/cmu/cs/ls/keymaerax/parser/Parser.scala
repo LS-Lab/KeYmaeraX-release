bc6852b1999e2b1c87dabede88b728521d178b69
Andre Platzer <aplatzer@cs.cmu.edu>
2015-07-09 08:13:54 -0400

    Documentation

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/Parser.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/Parser.scala
@@ -13,6 +13,11 @@ import edu.cmu.cs.ls.keymaerax.core._
 
 /**
  * Parser interface for KeYmaera X.
+ * Provides a parser to read string and file inputs with differential dynamic logic.
+ * A parser is essentially a function from input string to differential dynamic logic [[edu.cmu.cs.ls.keymaerax.core.Expression expressions]].
+ * {{{
+ *     Parser: String => Expression
+ * }}}
  * @author aplatzer
  */
 trait Parser extends (String => Expression) {
