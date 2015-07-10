fa2cb37468552db5961f57a435019a066e2c92ed
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-07-10 09:23:30 +0200

    Adapts some tests to new parser

--- keymaerax-webui/src/test/scala/testHelper/ParserFactory.scala
+++ keymaerax-webui/src/test/scala/testHelper/ParserFactory.scala
@@ -2,7 +2,7 @@ package testHelper
 
 import edu.cmu.cs.ls.keymaerax.api.ComponentConfig
 import edu.cmu.cs.ls.keymaerax.core.{Sequent, Formula}
-import edu.cmu.cs.ls.keymaerax.parser.KeYmaeraParser
+import edu.cmu.cs.ls.keymaerax.parser.{KeYmaeraXProblemParser, ProblemFileMode, KeYmaeraXParser, KeYmaeraParser}
 
 /**
  * Created by ran on 2/4/15.
@@ -19,21 +19,8 @@ object ParserFactory {
    */
   def parseToSequent(in: java.io.InputStream) = {
     val content = io.Source.fromInputStream(in).mkString
-    new KeYmaeraParser(false, ComponentConfig).runParser(content) match {
+    KeYmaeraXProblemParser(content) match {
       case f: Formula => Sequent(List(), collection.immutable.IndexedSeq[Formula](), collection.immutable.IndexedSeq[Formula](f))
-      case a => throw new IllegalArgumentException("Parsing the input did not result in a formula but in: " + a)
-    }
-  }
-
-  /**
-   * Return the formula from a .key input stream.
-   * @param in The input stream.
-   * @return The formula.
-   */
-  def parseToFormula(in: java.io.InputStream) = {
-    val content = io.Source.fromInputStream(in).mkString
-    new KeYmaeraParser(false, ComponentConfig).runParser(content) match {
-      case f: Formula => f
       case a => throw new IllegalArgumentException("Parsing the input did not result in a formula but in: " + a)
     }
   }
