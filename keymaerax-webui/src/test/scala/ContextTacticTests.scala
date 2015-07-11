815d678ea6ebee729b1c84fde89571289c631480
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-07-11 11:10:50 +0200

    Adapts context tactics tests to new parser

--- keymaerax-webui/src/test/scala/ContextTacticTests.scala
+++ keymaerax-webui/src/test/scala/ContextTacticTests.scala
@@ -33,40 +33,40 @@ class ContextTacticTests extends FlatSpec with Matchers with BeforeAndAfterEach 
   }
 
   "Cut equivalence in context" should "cut in an equivalence in context if lhs is present" in {
-    val s = sucSequent("(x>5 | y<2 & z=3 -> \\forall x. \\exists y. <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula)
+    val s = sucSequent("(x>5 | y<2 & z=3 -> \\forall x \\exists y <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula)
     val tactic = ContextTactics.cutInContext("(x<=1 & x>=1) <-> x=1".asFormula, SuccPosition(0, PosInExpr(1::0::0::1::1::Nil)))
 
     val result = helper.runTactic(tactic, new RootNode(s))
     result.openGoals() should have size 2
 
     result.openGoals()(0).sequent.ante should contain only
-      "(x>5 | y<2 & z=3 -> \\forall x. \\exists y. <?y>5;>[x:=1;](x<=1 & x>=1)) <-> (x>5 | y<2 & z=3 -> \\forall x. \\exists y. <?y>5;>[x:=1;]x=1)".asFormula
+      "(x>5 | y<2 & z=3 -> \\forall x \\exists y <?y>5;>[x:=1;](x<=1 & x>=1)) <-> (x>5 | y<2 & z=3 -> \\forall x \\exists y <?y>5;>[x:=1;]x=1)".asFormula
     result.openGoals()(0).sequent.succ should contain only
-      "(x>5 | y<2 & z=3 -> \\forall x. \\exists y. <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula
+      "(x>5 | y<2 & z=3 -> \\forall x \\exists y <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula
 
     result.openGoals()(1).sequent.ante shouldBe empty
     result.openGoals()(1).sequent.succ should contain only (
-      "(x>5 | y<2 & z=3 -> \\forall x. \\exists y. <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula,
-      "(x>5 | y<2 & z=3 -> \\forall x. \\exists y. <?y>5;>[x:=1;](x<=1 & x>=1)) <-> (x>5 | y<2 & z=3 -> \\forall x. \\exists y. <?y>5;>[x:=1;]x=1)".asFormula
+      "(x>5 | y<2 & z=3 -> \\forall x \\exists y <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula,
+      "(x>5 | y<2 & z=3 -> \\forall x \\exists y <?y>5;>[x:=1;](x<=1 & x>=1)) <-> (x>5 | y<2 & z=3 -> \\forall x \\exists y <?y>5;>[x:=1;]x=1)".asFormula
       )
   }
 
   "Cut equivalence in context" should "cut in an equivalence in context if rhs is present" in {
-    val s = sucSequent("(x>5 | y<2 & z=3 -> \\forall x. \\exists y. <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula)
+    val s = sucSequent("(x>5 | y<2 & z=3 -> \\forall x \\exists y <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula)
     val tactic = ContextTactics.cutInContext("x=1 <-> (x<=1 & x>=1)".asFormula, SuccPosition(0, PosInExpr(1::0::0::1::1::Nil)))
 
     val result = helper.runTactic(tactic, new RootNode(s))
     result.openGoals() should have size 2
 
     result.openGoals()(0).sequent.ante should contain only
-      "(x>5 | y<2 & z=3 -> \\forall x. \\exists y. <?y>5;>[x:=1;]x=1) <-> (x>5 | y<2 & z=3 -> \\forall x. \\exists y. <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula
+      "(x>5 | y<2 & z=3 -> \\forall x \\exists y <?y>5;>[x:=1;]x=1) <-> (x>5 | y<2 & z=3 -> \\forall x \\exists y <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula
     result.openGoals()(0).sequent.succ should contain only
-      "(x>5 | y<2 & z=3 -> \\forall x. \\exists y. <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula
+      "(x>5 | y<2 & z=3 -> \\forall x \\exists y <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula
 
     result.openGoals()(1).sequent.ante shouldBe empty
     result.openGoals()(1).sequent.succ should contain only (
-      "(x>5 | y<2 & z=3 -> \\forall x. \\exists y. <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula,
-      "(x>5 | y<2 & z=3 -> \\forall x. \\exists y. <?y>5;>[x:=1;]x=1) <-> (x>5 | y<2 & z=3 -> \\forall x. \\exists y. <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula
+      "(x>5 | y<2 & z=3 -> \\forall x \\exists y <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula,
+      "(x>5 | y<2 & z=3 -> \\forall x \\exists y <?y>5;>[x:=1;]x=1) <-> (x>5 | y<2 & z=3 -> \\forall x \\exists y <?y>5;>[x:=1;](x<=1 & x>=1))".asFormula
       )
   }
 }
