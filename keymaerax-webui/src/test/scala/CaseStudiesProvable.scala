bc8a300d4a4853edf376198344a1abeb629671d1
Andre Platzer <aplatzer@cs.cmu.edu>
2015-07-15 22:51:17 -0400

    Miscellaneous fixes to several test cases related to parser notation changes

--- keymaerax-webui/src/test/scala/CaseStudiesProvable.scala
+++ keymaerax-webui/src/test/scala/CaseStudiesProvable.scala
@@ -210,7 +210,7 @@ class CaseStudiesProvable extends FlatSpec with Matchers with BeforeAndAfterEach
 //    ),immutable.IndexedSeq(
 //
 //    ))
-    val node = helper.formulaToNode("[x'=v,v'=(a()),kxtime_1'=(0*kxtime_1+1) & (true&kxtime_1>=kxtime_4()&v=v_2()+a()*kxtime_1);][kxtime_1':=0*kxtime_1+1;][v':=a();][x':=v;](x)'=(0*2-1*0)/2^2*(2*x_2()+2*v_2()*kxtime_1+a()*kxtime_1^2)+1/2*(0*x_2()+2*0+((0*v_2()+2*0)*kxtime_1+2*v_2()*(kxtime_1)')+(0*kxtime_1^2+a()*(kxtime_1^2)'))".asFormula)
+    val node = helper.formulaToNode("[{x'=v,v'=(a()),kxtime_1'=(0*kxtime_1+1) & (true&kxtime_1>=kxtime_4()&v=v_2()+a()*kxtime_1)}][kxtime_1':=0*kxtime_1+1;][v':=a();][x':=v;](x)'=(0*2-1*0)/2^2*(2*x_2()+2*v_2()*kxtime_1+a()*kxtime_1^2)+1/2*(0*x_2()+2*0+((0*v_2()+2*0)*kxtime_1+2*v_2()*(kxtime_1)')+(0*kxtime_1^2+a()*(kxtime_1^2)'))".asFormula)
 
     val tactic = SearchTacticsImpl.locateTerm(SyntacticDerivationInContext.PowerDerivativeT, false)
 
