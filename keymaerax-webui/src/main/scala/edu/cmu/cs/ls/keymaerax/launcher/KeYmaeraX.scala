3456f97ba821ada51adf2f31225c633112e6bbed
Andre Platzer <aplatzer@cs.cmu.edu>
2015-07-17 15:38:19 -0400

    Improve formatting of license agreement

--- keymaerax-webui/src/main/scala/edu/cmu/cs/ls/keymaerax/launcher/KeYmaeraX.scala
+++ keymaerax-webui/src/main/scala/edu/cmu/cs/ls/keymaerax/launcher/KeYmaeraX.scala
@@ -38,7 +38,7 @@ object KeYmaeraX {
       |Additional options:
       |  -verify   check the resulting proof certificate (recommended)
       |  -noverify skip checking proof certificates
-      |  -ui       start the web user interface passing given optional arguments along
+      |  -ui       start the web user interface, passing given optional arguments along
       |  -help     Display this usage information
       |  -license  Show license agreement for using this software
       |
@@ -272,63 +272,60 @@ object KeYmaeraX {
       |SOFTWARE LICENSE AGREEMENT
       |ACADEMIC OR NON-PROFIT ORGANIZATION NONCOMMERCIAL RESEARCH USE ONLY
       |
-      |BY CLICKING THE "I Agree" BUTTON OR BY USING THE SOFTWARE, YOU ARE
-      |AGREEING TO THE TERMS OF THIS LICENSE AGREEMENT. IF YOU DO NOT
-      |AGREE WITH THESE TERMS, YOU MUST CLICK "Reject" AND YOU MAY NOT
-      |USE OR DOWNLOAD THE SOFTWARE.
+      |BY USING THE SOFTWARE, YOU ARE AGREEING TO THE TERMS OF THIS LICENSE
+      |AGREEMENT. IF YOU DO NOT AGREE WITH THESE TERMS, YOU MAY NOT USE OR
+      |DOWNLOAD THE SOFTWARE.
       |
       |This is a license agreement ("Agreement") between your academic
-      |institution or non-profit organization or self (called "Licensee"
-      |or "You" in this Agreement) and Carnegie Mellon University (called
+      |institution or non-profit organization or self (called "Licensee" or
+      |"You" in this Agreement) and Carnegie Mellon University (called
       |"Licensor" in this Agreement). All rights not specifically granted
       |to you in this Agreement are reserved for Licensor.
       |
-      |RESERVATION OF OWNERSHIP AND GRANT OF LICENSE: Licensor retains
-      |exclusive ownership of any copy of the Software (as defined below)
-      |licensed under this Agreement and hereby grants to Licensee a
-      |personal, non-exclusive, non-transferable license to use the
-      |Software for noncommercial research purposes, without the right to
-      |sublicense, pursuant to the terms and conditions of this
-      |Agreement. As used in this Agreement, the term "Software" means
-      |(i) the executable file made accessible to Licensee by Licensor
-      |pursuant to this Agreement, inclusive of backups, and updates
-      |permitted hereunder or subsequently supplied by Licensor,
-      |including all or any file structures, programming instructions,
-      |user interfaces and screen formats and sequences as well as any
-      |and all documentation and instructions related to it.
+      |RESERVATION OF OWNERSHIP AND GRANT OF LICENSE:
+      |
+      |Licensor retains exclusive ownership of any copy of the Software (as
+      |defined below) licensed under this Agreement and hereby grants to
+      |Licensee a personal, non-exclusive, non-transferable license to use
+      |the Software for noncommercial research purposes, without the right
+      |to sublicense, pursuant to the terms and conditions of this
+      |Agreement. As used in this Agreement, the term "Software" means (i)
+      |the executable file made accessible to Licensee by Licensor pursuant
+      |to this Agreement, inclusive of backups, and updates permitted
+      |hereunder or subsequently supplied by Licensor, including all or any
+      |file structures, programming instructions, user interfaces and
+      |screen formats and sequences as well as any and all documentation
+      |and instructions related to it.
       |
       |CONFIDENTIALITY: Licensee acknowledges that the Software is
-      |proprietary to Licensor, and as such, Licensee agrees to receive
-      |all such materials in confidence and use the Software only in
-      |accordance with the terms of this Agreement. Licensee agrees to
-      |use reasonable effort to protect the Software from unauthorized
-      |use, reproduction, distribution, or publication.
+      |proprietary to Licensor, and as such, Licensee agrees to receive all
+      |such materials in confidence and use the Software only in accordance
+      |with the terms of this Agreement. Licensee agrees to use reasonable
+      |effort to protect the Software from unauthorized use, reproduction,
+      |distribution, or publication.
       |
       |COPYRIGHT: The Software is owned by Licensor and is protected by
       |United States copyright laws and applicable international treaties
       |and/or conventions.
       |
-      |PERMITTED USES: The Software may be used for your own
-      |noncommercial internal research purposes. You understand and agree
-      |that Licensor is not obligated to implement any suggestions and/or
-      |feedback you might provide regarding the Software, but to the
-      |extent Licensor does so, you are not entitled to any compensation
-      |related thereto.
+      |PERMITTED USES: The Software may be used for your own noncommercial
+      |internal research purposes. You understand and agree that Licensor
+      |is not obligated to implement any suggestions and/or feedback you
+      |might provide regarding the Software, but to the extent Licensor
+      |does so, you are not entitled to any compensation related thereto.
       |
-      |BACKUPS: If Licensee is an organization, it may make that number
-      |of copies of the Software necessary for internal noncommercial use
-      |at a single site within its organization provided that all
-      |information appearing in or on the original labels, including the
-      |copyright and trademark notices are copied onto the labels of the
-      |copies.
+      |BACKUPS: If Licensee is an organization, it may make that number of
+      |copies of the Software necessary for internal noncommercial use at a
+      |single site within its organization provided that all information
+      |appearing in or on the original labels, including the copyright and
+      |trademark notices are copied onto the labels of the copies.
       |
-      |USES NOT PERMITTED: You may not modify, translate, reverse
-      |engineer, decompile, disassemble, distribute, copy or use the
-      |Software except as explicitly permitted herein. Licensee has not
-      |been granted any trademark license as part of this Agreement and
-      |may not use the name or mark "KeYmaera X", "Carnegie Mellon" or
-      |any renditions thereof without the prior written permission of
-      |Licensor.
+      |USES NOT PERMITTED: You may not modify, translate, reverse engineer,
+      |decompile, disassemble, distribute, copy or use the Software except
+      |as explicitly permitted herein. Licensee has not been granted any
+      |trademark license as part of this Agreement and may not use the name
+      |or mark "KeYmaera X", "Carnegie Mellon" or any renditions thereof
+      |without the prior written permission of Licensor.
       |
       |You may not sell, rent, lease, sublicense, lend, time-share or
       |transfer, in whole or in part, or provide third parties access to
@@ -339,22 +336,22 @@ object KeYmaeraX {
       |attempted assignment without such consent shall be null and void.
       |
       |TERM: The term of the license granted by this Agreement is from
-      |Licensee's acceptance of this Agreement by clicking "I Agree"
-      |below or by using the Software until terminated as provided below.
+      |Licensee's acceptance of this Agreement by clicking "I Agree" below
+      |or by using the Software until terminated as provided below.
       |
-      |The Agreement automatically terminates without notice if you fail
-      |to comply with any provision of this Agreement. Licensee may
-      |terminate this Agreement by ceasing using the Software. Upon any
-      |termination of this Agreement, Licensee will delete any and all
-      |copies of the Software. You agree that all provisions which
-      |operate to protect the proprietary rights of Licensor shall remain
-      |in force should breach occur and that the obligation of
-      |confidentiality described in this Agreement is binding in
-      |perpetuity and, as such, survives the term of the Agreement.
+      |The Agreement automatically terminates without notice if you fail to
+      |comply with any provision of this Agreement. Licensee may terminate
+      |this Agreement by ceasing using the Software. Upon any termination
+      |of this Agreement, Licensee will delete any and all copies of the
+      |Software. You agree that all provisions which operate to protect the
+      |proprietary rights of Licensor shall remain in force should breach
+      |occur and that the obligation of confidentiality described in this
+      |Agreement is binding in perpetuity and, as such, survives the term
+      |of the Agreement.
       |
-      |FEE: Provided Licensee abides completely by the terms and
-      |conditions of this Agreement, there is no fee due to Licensor for
-      |Licensee's use of the Software in accordance with this Agreement.
+      |FEE: Provided Licensee abides completely by the terms and conditions
+      |of this Agreement, there is no fee due to Licensor for Licensee's
+      |use of the Software in accordance with this Agreement.
       |
       |DISCLAIMER OF WARRANTIES: THE SOFTWARE IS PROVIDED "AS-IS" WITHOUT
       |WARRANTY OF ANY KIND INCLUDING ANY WARRANTIES OF PERFORMANCE OR
@@ -365,40 +362,38 @@ object KeYmaeraX {
       |SUPPORT AND MAINTENANCE: No Software support or training by the
       |Licensor is provided as part of this Agreement.
       |
-      |EXCLUSIVE REMEDY AND LIMITATION OF LIABILITY: To the maximum
-      |extent permitted under applicable law, Licensor shall not be
-      |liable for direct, indirect, special, incidental, or consequential
-      |damages or lost profits related to Licensee's use of and/or
-      |inability to use the Software, even if Licensor is advised of the
-      |possibility of such damage.
+      |EXCLUSIVE REMEDY AND LIMITATION OF LIABILITY: To the maximum extent
+      |permitted under applicable law, Licensor shall not be liable for
+      |direct, indirect, special, incidental, or consequential damages or
+      |lost profits related to Licensee's use of and/or inability to use
+      |the Software, even if Licensor is advised of the possibility of such
+      |damage.
       |
       |EXPORT REGULATION: Licensee agrees to comply with any and all
-      |applicable U.S. export control laws, regulations, and/or other
-      |laws related to embargoes and sanction programs administered by
-      |the Office of Foreign Assets Control.
+      |applicable U.S. export control laws, regulations, and/or other laws
+      |related to embargoes and sanction programs administered by the
+      |Office of Foreign Assets Control.
       |
-      |SEVERABILITY: If any provision(s) of this Agreement shall be held
-      |to be invalid, illegal, or unenforceable by a court or other
-      |tribunal of competent jurisdiction, the validity, legality and
-      |enforceability of the remaining provisions shall not in any way be
-      |affected or impaired thereby.
+      |SEVERABILITY: If any provision(s) of this Agreement shall be held to
+      |be invalid, illegal, or unenforceable by a court or other tribunal
+      |of competent jurisdiction, the validity, legality and enforceability
+      |of the remaining provisions shall not in any way be affected or
+      |impaired thereby.
       |
-      |NO IMPLIED WAIVERS: No failure or delay by Licensor in enforcing
-      |any right or remedy under this Agreement shall be construed as a
-      |waiver of any future or other exercise of such right or remedy by
-      |Licensor.
+      |NO IMPLIED WAIVERS: No failure or delay by Licensor in enforcing any
+      |right or remedy under this Agreement shall be construed as a waiver
+      |of any future or other exercise of such right or remedy by Licensor.
       |
       |GOVERNING LAW: This Agreement shall be construed and enforced in
-      |accordance with the laws of the Commonwealth of Pennsylvania
-      |without reference to conflict of laws principles. You consent to
-      |the personal jurisdiction of the courts of this County and waive
-      |their rights to venue outside of Allegheny County, Pennsylvania.
+      |accordance with the laws of the Commonwealth of Pennsylvania without
+      |reference to conflict of laws principles. You consent to the
+      |personal jurisdiction of the courts of this County and waive their
+      |rights to venue outside of Allegheny County, Pennsylvania.
       |
-      |ENTIRE AGREEMENT AND AMENDMENTS: This Agreement constitutes the
-      |sole and entire agreement between Licensee and Licensor as to the
-      |matter set forth herein and supersedes any previous agreements,
-      |understandings, and arrangements between the parties relating
-      |hereto.
+      |ENTIRE AGREEMENT AND AMENDMENTS: This Agreement constitutes the sole
+      |and entire agreement between Licensee and Licensor as to the matter
+      |set forth herein and supersedes any previous agreements,
+      |understandings, and arrangements between the parties relating hereto.
       |
     """.stripMargin
 }
