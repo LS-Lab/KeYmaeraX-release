8c053cc30566fd2f4874cecac6213597a182171e
Andre Platzer <aplatzer@cs.cmu.edu>
2015-07-22 16:24:59 -0400

    Fix axiom parsing notation by adapting to old parser syntax. Make axiom parser report problems immediately rather than silently ignore problems until the axioms are used first.

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/core/AxiomBase.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/core/AxiomBase.scala
@@ -165,13 +165,17 @@ private[core] object AxiomBase {
    * @todo In the long run, could benefit from asserting expected parse of axioms to remove parser from soundness-critical core. This, obviously, introduces redundancy.
    */
   private[core] def loadAxioms: immutable.Map[String, Formula] = {
-    val parser = new KeYmaeraParser(enabledLogging = false)
-    val alp = parser.ProofFileParser
-    val res = alp.runParser(loadAxiomString())
+    try {
+      val parser = new KeYmaeraParser(enabledLogging = false)
+      val alp = parser.ProofFileParser
+      val res = alp.runParser(loadAxiomString())
 
-    assert(res.length == res.map(k => k.name).distinct.length, "No duplicate axiom names during parse")
+      assert(res.length == res.map(k => k.name).distinct.length, "No duplicate axiom names during parse")
 
-    res.map(k => (k.name -> k.formula)).toMap
+      res.map(k => (k.name -> k.formula)).toMap
+    } catch {
+      case e: Exception => e.printStackTrace(); println("Problem while reading axioms " + e); sys.exit(10)
+    }
   } ensuring(assertCheckAxiomFile _, "checking parse of axioms against expected outcomes")
 
   private def assertCheckAxiomFile(axs : Map[String, Formula]) = {
@@ -450,16 +454,16 @@ End.
 End.*/
 
 /* DG differential ghost, general Lipschitz case */
-Axiom "DG differential Lipschitz ghost".
-  ([x'=f(x)&q(x);]p(x) <-> \exists y. [(x'=f(x),y'=g(x,y))&q(x);]p(x))
+/*Axiom "DG differential Lipschitz ghost".
+  ([x'=f(x)&q(x);]p(x) <-> \exists y. [x'=f(x),y'=g(x,y)&q(x);]p(x))
   <- (\exists L \forall x \forall y \forall z (y>=z -> (-L*(y-z) <= g(x,y)-g(x,z) & g(x,y)-g(x,z) <= L*(y-z))))
-End.
+End.*/
 
 /* DG differential ghost, general Lipschitz case, system case */
 Axiom "DG differential Lipschitz ghost system".
   /* @see "DG differential Lipschitz ghost" THEORY */
   [c&H(?);]p(?) <-> \exists y. [c,y'=g(?)&H(?);]p(?)
-  <- (\exists L \forall x \forall a \forall b \forall u \forall v (a>=b -> [y:=a;u:=g(?);y:=b;v:=g(?)] (-L*(a-b) <= u-v & u-v <= L*(a-b))))
+  <- (\exists L . \forall x . \forall a . \forall b . \forall u . \forall v . (a>=b -> [y:=a;u:=g(?);y:=b;v:=g(?)] (-L*(a-b) <= u-v & u-v <= L*(a-b))))
 End.
 
 /* Formatter axioms for diff eqs. @todo unused except in tactics implementation of itself */
