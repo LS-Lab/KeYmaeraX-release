7677f1a1ddd63e1c0b3461fd4da307cac5c5ea62
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-07-25 18:42:30 +0200

    Example on obtaining and applying a lemma

--- keymaerax-webui/src/test/scala/LemmaTests.scala
+++ keymaerax-webui/src/test/scala/LemmaTests.scala
@@ -4,13 +4,17 @@
 */
 import edu.cmu.cs.ls.keymaerax.core._
 import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
-import edu.cmu.cs.ls.keymaerax.tactics.{RootNode, TacticLibrary}
+import edu.cmu.cs.ls.keymaerax.parser.ToolEvidence
+import edu.cmu.cs.ls.keymaerax.tactics.Tactics.ApplyRule
+import edu.cmu.cs.ls.keymaerax.tactics._
+import edu.cmu.cs.ls.keymaerax.tactics.TactixLibrary.{assignb, closeId, composeb, cut, ls, onBranch}
+import edu.cmu.cs.ls.keymaerax.tactics.BranchLabels._
 import edu.cmu.cs.ls.keymaerax.tools.{Tool, Mathematica}
 import testHelper.ProvabilityTestHelper
 
 import org.scalatest.{BeforeAndAfterEach, Matchers, FlatSpec}
 
-import scala.collection.immutable.{Nil, Map}
+import scala.collection.immutable
 
 /**
  * Created by smitsch on 3/5/15.
@@ -89,5 +93,36 @@ class LemmaTests extends FlatSpec with Matchers with BeforeAndAfterEach {
     val r = new RootNode(new Sequent(Nil, Vector(), Vector(res.fact.conclusion.succ.head)))
     val t = LookupLemma(lemmaDB, id)
     r.apply(t) shouldBe 'closed
+  }
+
+  "A lemma" should "be learned and used" in {
+    val lemmaDB = new FileLemmaDB
+
+    val f = "[x:=2;]x=2".asFormula
+    val s = Sequent(Nil, immutable.IndexedSeq(), immutable.IndexedSeq(f))
+    val r1 = helper.runTactic(ls(assignb) & closeId, new RootNode(s))
+    r1 shouldBe 'closed
+    // create evidence (traces input into tool and output from tool)
+    val evidence = new ToolEvidence(immutable.Map("input" -> f.prettyString, "output" -> "true")) :: Nil
+    // cannot use lemma names for testing, because running the test multiple times results in duplicate lemmas
+    val lemma = Lemma(r1.provableWitness, evidence /*, Some("My first lemma")*/)
+    // add lemma into DB, which creates an ID for it. use the ID to apply the lemma
+    val lemmaID = LookupLemma.addLemma(lemmaDB, lemma)
+
+    val h = "[y:=3; x:=2;]x=2".asFormula
+    val t = Sequent(Nil, immutable.IndexedSeq(), immutable.IndexedSeq(h))
+    val applyLemma = new ApplyRule(LookupLemma(lemmaDB, lemmaID)) {
+      override def applicable(node: ProofNode): Boolean = node.sequent.sameSequentAs(s)
+    }
+    val r2 = helper.runTactic(
+      ls(composeb) & ls(assignb) &
+      // cut in exact shape of lemma
+      cut("[x:=2;]x=2".asFormula) & onBranch(
+        (cutShowLbl,
+          // hide everything exact lemma shape, then apply lemma
+          SearchTacticsImpl.lastSucc(PropositionalTacticsImpl.cohideT) & applyLemma),
+        (cutUseLbl, closeId)
+      ), new RootNode(t))
+    r2 shouldBe 'closed
   }
 }
