1d847b76cfcae4918ef5fd81233d600f9806c524
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-08-01 11:01:56 +0200

    Min tactic

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tactics/ArithmeticTacticsImpl.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tactics/ArithmeticTacticsImpl.scala
@@ -609,4 +609,55 @@ object ArithmeticTacticsImpl {
     }
     axiomLookupBaseT("abs", subst, _ => NilPT, (f, ax) => ax)
   }
+
+  def MinT: PositionTactic = new PositionTactic("Min") {
+    override def applies(s: Sequent, pos: Position): Boolean = getTerm(s, pos) match {
+      case FuncOf(Function("min", None, Tuple(Real, Real), Real), Pair(f, g)) => true
+      case _ => false
+    }
+
+    override def apply(pos: Position): Tactic = new ConstructionTactic("Min") {
+      override def applicable(node: ProofNode): Boolean = applies(node.sequent, pos)
+
+      override def constructTactic(tool: Tool, node: ProofNode): Option[Tactic] = getTerm(node.sequent, pos) match {
+        case min@FuncOf(Function("min", None, Tuple(Real, Real), Real), Pair(f, g)) =>
+          val freshMinIdx = TacticLibrary.TacticHelper.freshIndexInSequent("min", node.sequent)
+          val minVar = Variable("min", freshMinIdx)
+          Some(cutT(Some(Exists(minVar :: Nil, Equal(min, minVar)))) & onBranch(
+            (cutShowLbl, lastSucc(cohideT) & MinAxiomT(SuccPosition(0).first) & TacticLibrary.arithmeticT),
+            (cutUseLbl, lastAnte(FOQuantifierTacticsImpl.skolemizeT) &
+              lastAnte(EqualityRewritingImpl.eqLeft(exhaustive=true)) &
+              //@todo may find wrong equality, if min(x) = ... is already present in antecedent
+              locateAnte(MinAxiomT, { case Equal(a, _) if a == min => true case _ => false }))
+          ))
+        case _ => throw new IllegalStateException("Impossible by applies")
+      }
+    }
+  }
+
+  /**
+   * Expands minimum value function as a disjunction.
+   * Axiom min: (min(f(), g()) = h()) <-> ((f()<=g() & h()=f()) | (f()>=g() & h()=g()))
+   * @return The tactic to replace lhs of axiom min with rhs.
+   */
+  def MinAxiomT: PositionTactic = {
+    def axiomInstance(fml: Formula): Formula = fml match {
+      case Equal(FuncOf(Function("min", None, Tuple(Real, Real), Real), Pair(f, g)), h) =>
+        Equiv(fml, Or(And(LessEqual(f, g), Equal(h, f)), And(GreaterEqual(f, g), Equal(h, g))))
+      case _ => False
+    }
+    uncoverAxiomT("min", axiomInstance, _ => MinAxiomBaseT)
+  }
+  /** Base tactic for min */
+  private def MinAxiomBaseT: PositionTactic = {
+    def subst(fml: Formula): List[SubstitutionPair] = fml match {
+      case Equiv(Equal(FuncOf(Function("min", _, _, _), Pair(f, g)), h), _) =>
+        val aF = FuncOf(Function("f", None, Unit, Real), Nothing)
+        val aG = FuncOf(Function("g", None, Unit, Real), Nothing)
+        val aH = FuncOf(Function("h", None, Unit, Real), Nothing)
+        SubstitutionPair(aF, f) :: SubstitutionPair(aG, g) :: SubstitutionPair(aH, h) :: Nil
+      case _ => throw new IllegalStateException("Impossible by axiomInstance of MinAxiomT")
+    }
+    axiomLookupBaseT("min", subst, _ => NilPT, (f, ax) => ax)
+  }
 }
