728e658a2c5f412a78e36cdbc576dd0f170f357c
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-08-01 10:36:26 +0200

    Expression traversal: support for pairs

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tactics/ExpressionTraversal.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tactics/ExpressionTraversal.scala
@@ -228,6 +228,7 @@ object ExpressionTraversal {
         case Times(a, b) => matchTwo(p, Times.apply(_: Term, _: Term), f, a, b)
         case Divide(a, b) => matchTwo(p, Divide.apply(_: Term, _: Term), f, a, b)
         case Power(a, b) => matchTwo(p, Power.apply(_: Term, _: Term), f, a, b)
+        case Pair(a, b) => matchTwo(p, Pair.apply(_: Term, _: Term), f, a, b)
 
         // Programs
         case ProgramConst(_) => matchZero(p, f, e)
