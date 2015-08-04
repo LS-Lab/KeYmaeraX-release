16233ff73efc4fa05b06aec3b9740f9e68309b92
Andre Platzer <aplatzer@cs.cmu.edu>
2015-08-04 14:39:34 +0200

    Suppress semantic analysis warnings

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/KeYmaeraXParser.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/KeYmaeraXParser.scala
@@ -142,7 +142,7 @@ object KeYmaeraXParser extends Parser {
     }
     semanticAnalysis(parse) match {
       case None => parse
-      case Some(error) => if (LAX) {println("WARNING: " + "Semantic analysis error" + "\nin " + "parsed: " + printer.stringify(parse) + "\n" + error); parse}
+      case Some(error) => if (LAX) {if (false) println("WARNING: " + "Semantic analysis" + "\nin " + "parsed: " + printer.stringify(parse) + "\n" + error); parse}
       else throw new ParseException("Semantic analysis error", UnknownLocation, "parsed: " + printer.stringify(parse) + "\n" + error)
     }
   }
