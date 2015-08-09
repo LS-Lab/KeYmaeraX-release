821269ced5bc6a09406f8a93cb290203cc816e00
Andre Platzer <aplatzer@cs.cmu.edu>
2015-08-09 06:39:06 +0200

    no package launcher forwarder

---
+++ keymaerax-webui/src/main/scala/KeYmaeraX.scala
@@ -0,0 +1,9 @@
+package
+
+/**
+ * Command-line interface for KeYmaera X (convenience wrapper).
+ * @author Andre Platzer
+ */
+object KeYmaeraX {
+  def main (args: Array[String]): Unit = edu.cmu.cs.ls.keymaerax.launcher.KeYmaeraX.main(args)
+}
