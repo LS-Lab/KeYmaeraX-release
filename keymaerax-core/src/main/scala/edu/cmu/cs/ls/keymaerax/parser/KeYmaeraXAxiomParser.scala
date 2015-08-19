3543fd868eee4346f1917737e1a166c1f62a5dc2
Andre Platzer <aplatzer@cs.cmu.edu>
2015-08-19 16:58:29 -0400

    !Lax type analysis only

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/KeYmaeraXAxiomParser.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/KeYmaeraXAxiomParser.scala
@@ -26,7 +26,7 @@ object KeYmaeraXAxiomParser extends (String => List[(String,Formula)]) {
     try {
       val (decls, axiomTokens) = KeYmaeraXDeclarationsParser(tokens)
       val axioms = parseAxioms(axiomTokens)
-      assert(axioms.forall(ax => KeYmaeraXDeclarationsParser.typeAnalysis(decls, ax._2)), "type analysis of axioms")
+      if (!KeYmaeraXParser.LAX) assert(axioms.forall(ax => KeYmaeraXDeclarationsParser.typeAnalysis(decls, ax._2)), "type analysis of axioms")
       axioms
     } catch {case e: ParseException => throw e.inContext("axiom file"/*input*/)}
   }
