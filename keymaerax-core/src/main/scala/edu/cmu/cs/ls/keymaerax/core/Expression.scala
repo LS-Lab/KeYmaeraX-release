72022046c17909fe2ed502e30a5f7f43fbd3980d
Nathan Fulton <gitcommit@nfulton.org>
2015-08-26 10:13:09 -0400

    Disabled Regexp check; eventually revert me.
    Before reverting, the converters need to never create named symbols that
    violate this convention. See E.g. KeYmaeraToMathematica.convertFnApply,
    where we create a function with an invalid name for conversion.
    
    There are two options going forward:
     * We need to find all such occurances and use a standard work-around (e.g.,
       generate name prefixes / trafos on the fly); or,
     * We need to create a new function that's private to core and tools that
       allows for the creation of names with exceptional patterns, and then use
       that function everywhere in core and tools where we need unique names.
    
    Even if option 1 makes sense in the case of tools, I still think 2 is the best.
    In particular because NamedSymbol currently allows user-invalid names like
    \_ and \anything that we really should be disallowing be default.

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/core/Expression.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/core/Expression.scala
@@ -106,13 +106,13 @@ sealed trait ApplicationOf extends Expression {
  * @note User-level symbols should not use underscores, which are reserved for the core.
  */
 sealed trait NamedSymbol extends Expression with Ordered[NamedSymbol] {
-//  require(!name.isEmpty && !name.substring(0, name.length-1).contains("_"),
-//    "non-empty names without underscores (except at end for internal names): " + name)
-//  require(!name.isEmpty && !name.substring(0, name.length-1).contains("_"),
-//    "WARNING: non-empty names without underscores (except at end for internal names): " + name)
-//  require(!name.contains("'"), "names cannot mention primes, not even the names of differential symbols: " + name)
-  require(name.matches("""\\\_|\\?([a-zA-Z])*|([a-zA-Z][a-zA-Z0-9]*\_?)"""), "alphanumerical identifier without primes and without underscores " +
-    "(internal names allow _ at the end, \\_ at the beginning, and \\ followed by letters only): " + name)
+  require(!name.isEmpty && !name.substring(0, name.length-1).contains("_"),
+    "non-empty names without underscores (except at end for internal names): " + name)
+  require(!name.isEmpty && !name.substring(0, name.length-1).contains("_"),
+    "WARNING: non-empty names without underscores (except at end for internal names): " + name)
+  require(!name.contains("'"), "names cannot mention primes, not even the names of differential symbols: " + name)
+//  require(name.matches("""\\\_|\\?([a-zA-Z])*|([a-zA-Z][a-zA-Z0-9]*\_?)"""), "alphanumerical identifier without primes and without underscores " +
+//    "(internal names allow _ at the end, \\_ at the beginning, and \\ followed by letters only): " + name)
   require(index.getOrElse(0)>=0, "nonnegative index if any " + this)
 
   def name: String
