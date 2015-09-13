6499082401044b8e81fad8bcd7f2cd5ed2182e5b
Andre Platzer <aplatzer@cs.cmu.edu>
2015-09-13 14:56:45 -0400

    Improve error message

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/core/Proof.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/core/Proof.scala
@@ -471,7 +471,7 @@ final case class Provable private (conclusion: Sequent, subgoals: immutable.Inde
    */
   final def apply(newConsequence: Sequent, rule: Rule): Provable = {
     //@note the following requirement is redundant and not soundness-critical. It just gives a better error message.
-    require(rule(newConsequence)==List(this.conclusion), "Rule " + rule + " was expected to justify\n" + this.conclusion.prettyString + "\n-----------------------------\n" + newConsequence.prettyString +
+    require(rule(newConsequence)==List(this.conclusion), "Rule " + rule + " was expected to justify\n" + this.conclusion.prettyString + "\n-----------------------------" + rule + "??\n" + newConsequence.prettyString +
       "\n\nThat is, applying the rule backwards to new consequence\n" + newConsequence + "\nshould result in\n" + this.conclusion + "\nwhich is the conclusion of this " + this + "\nThe rule instead led to " + rule(newConsequence) +
       "\n\nExpected: " + edu.cmu.cs.ls.keymaerax.parser.FullPrettyPrinter(this.conclusion) +
       "\nFound:    " + rule(newConsequence).map(s=>edu.cmu.cs.ls.keymaerax.parser.FullPrettyPrinter(s)).mkString(", "))
