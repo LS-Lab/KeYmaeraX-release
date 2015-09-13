7ae3a19b9ca4941ad7e6e5a205163156b59aef65
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-09-13 09:21:02 -0400

    Moves polarity computation into FormulaTools

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tactics/FormulaTools.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tactics/FormulaTools.scala
@@ -5,6 +5,7 @@
 package edu.cmu.cs.ls.keymaerax.tactics
 
 import edu.cmu.cs.ls.keymaerax.core._
+import edu.cmu.cs.ls.keymaerax.tactics.Augmentors.FormulaAugmentor
 
 /**
  * Tactic tools for formula manipulation and extraction.
@@ -32,5 +33,20 @@ object FormulaTools {
     case Or(p,q) => disjuncts(p) ++ disjuncts(q)
     case f => List(f)
   }
+
+  /**
+   * Computes the polarity of the subformula at position pos in formula.
+   * @param formula The formula.
+   * @param pos The position within formula for which the polarity is searched.
+   * @return -1 for negative polarity, 1 for positive polarity, 0 for unknown polarity.
+   */
+  def polarityAt(formula: Formula, pos: PosInExpr): Int =
+    if (pos.pos.isEmpty) 1 else FormulaAugmentor(formula).at(pos.parent) match {
+      case (_, Not(_)) => -polarityAt(formula, pos.parent)
+      case (_, Imply(_, _)) if pos.pos.last == 0 => -polarityAt(formula, pos.parent)
+      case (_, Imply(_, _)) if pos.pos.last == 1 => polarityAt(formula, pos.parent)
+      case (_, Equiv(_, _)) => 0
+      case _ => polarityAt(formula, pos.parent)
+    }
 
 }
