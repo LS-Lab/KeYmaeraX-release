fd555891e3a19910d3f8e545ecd5b5831566783b
Nathan Fulton <gitcommit@nfulton.org>
2015-09-15 11:40:31 -0400

    Ignored tests that are only relevant to 0*t+1.

--- keymaerax-webui/src/test/scala/LODESolverTests/LODEBugs.scala
+++ keymaerax-webui/src/test/scala/LODESolverTests/LODEBugs.scala
@@ -27,11 +27,12 @@ class LODESolverBugs extends TacticTestSuite {
     s.succ.head shouldBe "\\forall T (T>=0->\\forall S (0<=S&S<=T->2=2)->[z:=z+1*T;]1=1)".asFormula
   }
 
-  //NOTE: This is all related to rewriteConstantTime and t=0*t+1 form, which we're going to avoid.
+
   /**
    * @note causes a clash b/c z occurs on the RHS of z'.
    * @note wontfix -- instead, use a ODETactics.rewriteConstantTime phase to rewrite 0*z+1.
    *       See next test case.
+   * @note related to rewriteConstantTime which we don't use anymore.
    */
   ignore should "work for z' = 0*z + 1" in {
     val formula = "[{z' = 0*z+1 & 2=2}]1=1".asFormula
@@ -47,6 +48,7 @@ class LODESolverBugs extends TacticTestSuite {
     fail("wontfix")
   }
 
+  /** @note related to rewriteConstantTime, which we aren't using anymore. */
   ignore should "get correct input from the workaround tactic" in {
     val formula = "[{z' = 0*z+1 & 2=2}]1=1".asFormula
     val node = helper.formulaToNode(formula)
@@ -61,6 +63,7 @@ class LODESolverBugs extends TacticTestSuite {
     s.succ.head   shouldBe "[{z' = 1 & 2=2}]1=1".asFormula
   }
 
+  /** @note related to rewriteConstantTime, which we aren't using anymore. */
   ignore should "handle output of workwaround tactic" in {
     val formula = "[{z' = 1 & 2=2}]1=1".asFormula
     val node = helper.formulaToNode(formula)
