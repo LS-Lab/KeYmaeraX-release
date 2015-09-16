8f211ae559e4679e7c99be4706cb6a981ee3bbac
Nathan Fulton <gitcommit@nfulton.org>
2015-09-16 12:28:21 -0400

    More robust lemma db initializer.

--- keymaerax-webui/src/main/scala/edu/cmu/cs/ls/keymaerax/launcher/LemmaDatabaseInitializer.scala
+++ keymaerax-webui/src/main/scala/edu/cmu/cs/ls/keymaerax/launcher/LemmaDatabaseInitializer.scala
@@ -1,9 +1,9 @@
 package edu.cmu.cs.ls.keymaerax.launcher
 
-import java.io.{OutputStreamWriter, FileOutputStream, File}
+import java.io.{FileOutputStream, File}
 import java.nio.channels.Channels
 import java.nio.file.{Paths, Files}
-import java.util.zip.{ZipOutputStream, ZipEntry, ZipFile, ZipInputStream}
+import java.util.zip.{ZipFile}
 
 
 
@@ -13,38 +13,72 @@ import java.util.zip.{ZipOutputStream, ZipEntry, ZipFile, ZipInputStream}
  */
 object LemmaDatabaseInitializer {
   val ZIP_FILE_LOCATION: String = 
-    System.getProperty("user.home") + File.separator + ".keymaerax/cache/lemmadb/lemmadb.zip"
+    System.getProperty("user.home") +
+    File.separator +
+    ".keymaerax" +
+    File.separator +
+    "cache" +
+    File.separator +
+    "lemmadb" +
+    File.separator +
+    "lemmadb.zip"
+
+  val LEMMA_DB_PATH =
+    System.getProperty("user.home") +
+    File.separator +
+    ".keymaerax" +
+    File.separator +
+    "cache" +
+    File.separator +
+    "lemmadb"
 
   /** initializes the lemmas database from a ZIP file contained in a JAR */
   def initializeFromJAR = {
     // Create the user's cache if it does not already exist.
-    val lemmaDbPath = System.getProperty("user.home") + File.separator + ".keymaerax/cache/lemmadb"
-    if(!new File(lemmaDbPath).exists) new File(lemmaDbPath).mkdirs
+    if(!new File(LEMMA_DB_PATH).exists) new File(LEMMA_DB_PATH).mkdirs
 
-    val lemmasZipFile = copyZipFromJar(lemmaDbPath)
-    copyfilesFromZip(lemmasZipFile, lemmaDbPath)
+    val lemmasZipFile = copyZipFromJar(LEMMA_DB_PATH)
+    unzipTo(lemmasZipFile, LEMMA_DB_PATH)
     Files.delete(Paths.get(ZIP_FILE_LOCATION))
   }
 
   /** Copies a ZIP file out of a JAR and to the user's cache/lemmadb directory  */
   private def copyZipFromJar(lemmaPath : String): ZipFile = {
     // Copy the .zip file into the user's lemmadb.
-    val lemmas = Channels.newChannel(this.getClass.getResourceAsStream("/resources/lemmadb.zip"))
-    new FileOutputStream(lemmaPath).getChannel.transferFrom(lemmas, 0, Long.MaxValue)
+    val resource = this.getClass.getResourceAsStream("/lemmadb.zip")
+    val lemmas = Channels.newChannel(resource)
+    new FileOutputStream(ZIP_FILE_LOCATION).getChannel.transferFrom(lemmas, 0, Long.MaxValue)
+
+    if(new File(ZIP_FILE_LOCATION).exists())
+      println("Successfully copied ZIP file to " + ZIP_FILE_LOCATION)
+    else
+      throw new Exception("Failed to copy the lemmas database ZIP file")
+
     new ZipFile(ZIP_FILE_LOCATION)
   }
 
   /**
-   * Copies all files in `zip` into the directory located at `destinationPathPrefix`
+   * Copies all files in `zip` into the directory located at `destinationPathPrefix`.
+   * Skips files that already exist in the destination.
    */
-  private def copyfilesFromZip(zip : ZipFile, destinationPathPrefix : String) = {
+  private def unzipTo(zip : ZipFile, destinationPathPrefix : String) = {
     val entries = zip.entries()
     while(entries.hasMoreElements) {
       val nextFile = entries.nextElement()
       val is = zip.getInputStream(nextFile)
-      val outputLocation = Paths.get(zip + File.separator + nextFile.getName)
-      println("Copying " + nextFile.getName + " to " + outputLocation)
-      Files.copy(is, outputLocation)
+
+      val outputFileLocation: String = LEMMA_DB_PATH   + File.separator + nextFile.getName
+      val outputFile: File = new File(outputFileLocation)
+      val outputPath = Paths.get(outputFileLocation)
+
+      //only copy the file if it doesn't already exist.
+      if(!outputFile.exists()) {
+        println("[Lemma DB Initializer] Copying a file from the ZIP file (" + nextFile.getName + ") to " + outputFileLocation)
+        Files.copy(is, outputPath)
+      }
+      else {
+        println("[Lemma DB Initializer] Skipping " + nextFile.getName + " because it already exists in the lemma database")
+      }
     }
   }
 }
