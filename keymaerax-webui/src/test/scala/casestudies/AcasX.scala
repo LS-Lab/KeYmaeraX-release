c80ce7530e815bb57c7fc6b65c3cb1caec1f085a
Jean-Baptiste Jeannin <jeannin@cs.cmu.edu>
2015-10-03 15:10:05 -0700

    2-sided KeY file now parses

--- keymaerax-webui/src/test/scala/casestudies/AcasX.scala
+++ keymaerax-webui/src/test/scala/casestudies/AcasX.scala
@@ -478,7 +478,239 @@ class AcasX extends FlatSpec with Matchers with BeforeAndAfterEach {
     safeTheorem shouldBe 'closed
   }
 
+  "ACAS X 2-sided safe implicit with lemmas" should "be provable" in {
 
+    /*** Helper tactics ***/
+    /*def dT(s : String) = debugT(s)
+
+    val crushw = la(orL, "w=-1|w=1") && (QE, QE)
+    // Q: Stefan, why did you change this from w() ?
+
+    val crushor = (la(orL)*) & QE
+
+    val absmax = abbrv("max((0,w*(dhf-dhd)))".asTerm, Some(Variable("maxA"))) &
+      la(MinMaxT, "", Some("max(0,w*(dhf-dhd))".asTerm)) &
+      ls(AbsT, "", Some("abs(r)".asTerm)) &
+      ls(AbsT, "", Some("abs(h)".asTerm)) &
+      la(AbsT, "", Some("abs(r-0)".asTerm))
+
+    val absmax2 = (ls(AbsT, "", Some("abs(r_3-ro_0)".asTerm)) | dT("abs(r_3-ro_0) not present")) &
+      ( abbrv("max((0,w*(dhf-dhd)))".asTerm, Some(Variable("maxI"))) &
+        la(MinMaxT, "", Some("max((0,w*(dhf-dhd)))".asTerm)) | dT("max(0,w*(dhf-dhd)) not present")) &
+      ( abbrv("max((0,w*(dhf-dhd_3)))".asTerm, Some(Variable("maxF"))) &
+        la(MinMaxT, "", Some("max((0,w*(dhf-dhd_3)))".asTerm)) | dT("max(0,w*(dhf-dhd_3)) not present"))
+
+    def cutEZ(c:Formula, t:Tactic) = cut(c) & onBranch(
+      (cutShowLbl, t | dT("Cut didn't close") & Tactics.stopT)
+    )
+
+    val crushabsmax = absmax & crushor
+
+    /*** Invariants etc. ***/
+
+    val invariant = ("( (w=-1 | w=1) &"+
+      "  ("+
+      "    ("+
+      "      \\forall t \\forall ro \\forall ho"+
+      "        ((0 <= t & t < max(0, w * (dhf - dhd)) / a &"+
+      "          ro = rv * t & ho = (w * a) / 2 * t^2 + dhd * t) |"+
+      "          (t >= max(0, w * (dhf - dhd)) / a &"+
+      "            ro = rv * t & ho = dhf * t - w * max(0, w * (dhf - dhd))^2 / (2*a))"+
+      "            -> (abs(r - ro) > rp | w * h < w * ho - hp))"+
+      "      ) |"+
+      "      ("+
+      "        \\forall t \\forall ro \\forall ho"+
+      "          ((0 <= t & t < max(0, w * (dhfM - dhd)) / aM &"+
+      "            ro = rv * t & ho = (w * aM) / 2 * t^2 + dhd * t) |"+
+      "            (t >= max(0, w * (dhfM - dhd)) / aM &"+
+      "              ro = rv * t & ho = (dhd + w * max(0, w * (dhfM-dhd)) * t - w * max(0, w * (dhfM - dhd))^2 / (2*aM)))"+
+      "              -> (abs(r - ro) > rp | w * h > w * ho + hp))"+
+      "        )"+
+      "    )"+
+      "  ) & (hp > 0 & rp > 0 & rv >= 0 & a > 0)").asFormula
+
+    val initDomain = "w*dhd>=w*dhf|w*ao>=a"*/
+
+    /*** Lower bound safe lemma and its tactic ***/
+
+/*    val safeLemmaFormula =
+      "max0=max((0,w*(dhf-dhd))) & " +
+        "(w*dhd>=w*dhf|w*ao>=a) & " +
+        "h_3=1/2*(2*h+-2*dhd*kxtime_5+-1*ao*kxtime_5^2) & " +
+        "(w=-1|w=1) & " +
+        "(\\forall t \\forall ro \\forall ho (0<=t&t < max0/a&ro=rv*t&ho=w*a/2*t^2+dhd*t|t>=max0/a&ro=rv*t&ho=dhf*t-w*max0^2/(2*a)->abs(r-ro)>rp|w*h < w*ho-hp)) & " +
+        "hp>0 & " +
+        "dhd_3=dhd+ao*kxtime_5 & " +
+        "rp>0 & " +
+        "r_3=r+-1*kxtime_5*rv & " +
+        "rv>=0 & " +
+        "a>0 & " +
+        "(w*dhd_3>=w*dhf|w*ao>=a) & " +
+        "kxtime_5>=0" +
+        "->" +
+        "((w=-1|w=1)&\\forall t \\forall ro \\forall ho (0<=t&t < max((0,w*(dhf-dhd_3)))/a&ro=rv*t&ho=w*a/2*t^2+dhd_3*t|t>=max((0,w*(dhf-dhd_3)))/a&ro=rv*t&ho=dhf*t-w*max((0,w*(dhf-dhd_3)))^2/(2*a)->abs(r_3-ro)>rp|w*h_3 < w*ho-hp))&hp>0&rp>0&rv>=0&a>0"
+    val safeLemmaSeq = sequent(Nil, Nil, safeLemmaFormula.asFormula :: Nil)
+    val safeLemmaTac = dT("lemma") & ls(implyR) & (la(andL)*) && ls(andR) && (
+      ls(andR) && (
+        closeId,
+        dT("Before skolemization") & (ls(skolemizeT)*) & dT("After skolemization") & ls(implyR) & ls(orR) &
+          //here we'd want to access previously introduced skolem symbol and time introduced by diffSolution;goal 90
+          la(instantiateT(Variable("t"), "kxtime_5 + t_0".asTerm)) & // t_22+t_23: kxtime_5 == t_22, t_0 == t_23
+          la(instantiateT(Variable("ro"), "rv*(kxtime_5 + t_0)".asTerm)) & // rv*(t_22+t_23)
+          dT("Before CUT") &
+          cut("(0<=t_0+kxtime_5 & t_0+kxtime_5<max0/a) | t_0+kxtime_5 >= max0/a".asFormula) & onBranch(
+          (cutShowLbl, dT("Show Cut") & la(hide, "max0=max((0,w*(dhf-dhd)))") &
+            la(hide, "\\forall ho (0<=kxtime_5+t_0&kxtime_5+t_0 < max0/a&rv*(kxtime_5+t_0)=rv*(kxtime_5+t_0)&ho=w*a/2*(kxtime_5+t_0)^2+dhd*(kxtime_5+t_0)|kxtime_5+t_0>=max0/a&rv*(kxtime_5+t_0)=rv*(kxtime_5+t_0)&ho=dhf*(kxtime_5+t_0)-w*max0^2/(2*a)->abs(r-rv*(kxtime_5+t_0))>rp|w*h < w*ho-hp)")
+            & ls(hide, "abs(r_3-ro_0)>rp") & ls(hide, "w*h_3 < w*ho_0-hp") & dT("Show Cut 2") & ls(orR) &
+            la(orL, "0<=t_0&t_0 < max((0,w*(dhf-dhd_3)))/a&ro_0=rv*t_0&ho_0=w*a/2*t_0^2+dhd_3*t_0|t_0>=max((0,w*(dhf-dhd_3)))/a&ro_0=rv*t_0&ho_0=dhf*t_0-w*max((0,w*(dhf-dhd_3)))^2/(2*a)")
+            & (la(andL)*) & (ls(andR)*) & (QE | dT("Should be closed") & Tactics.stopT)),
+          (cutUseLbl, dT("Use Cut") &
+            la(orL, "0<=t_0+kxtime_5&t_0+kxtime_5 < max0/a|t_0+kxtime_5>=max0/a") && (
+            dT("Goal 110") & la(hide, initDomain) &
+              la(instantiateT(Variable("ho"), "w*a/2*(t_0+kxtime_5)^2 + dhd*(t_0+kxtime_5)".asTerm)) //, { case Forall(Variable("ho", None, Real) :: Nil, _) => true case _ => false })
+              & dT("instantiate ho") & ((closeId | l(NonBranchingPropositionalT))*) &
+              la(implyL, "0<=kxtime_5+t_0&kxtime_5+t_0 < max0/a&rv*(kxtime_5+t_0)=rv*(kxtime_5+t_0)&w*a/2*(t_0+kxtime_5)^2+dhd*(t_0+kxtime_5)=w*a/2*(kxtime_5+t_0)^2+dhd*(kxtime_5+t_0)|kxtime_5+t_0>=max0/a&rv*(kxtime_5+t_0)=rv*(kxtime_5+t_0)&w*a/2*(t_0+kxtime_5)^2+dhd*(t_0+kxtime_5)=dhf*(kxtime_5+t_0)-w*max0^2/(2*a)->abs(r-rv*(kxtime_5+t_0))>rp|w*h < w*(w*a/2*(t_0+kxtime_5)^2+dhd*(t_0+kxtime_5))-hp")
+              && (
+              (ls(orR)*) &
+                ls(hide, "kxtime_5+t_0>=max0/a&rv*(kxtime_5+t_0)=rv*(kxtime_5+t_0)&w*a/2*(t_0+kxtime_5)^2+dhd*(t_0+kxtime_5)=dhf*(kxtime_5+t_0)-w*max0^2/(2*a)")
+                & (ls(andR)*) & (closeId | absmax2 & dT("before QE") & QE | dT("Shouldn't get here")) & dT("Shouldn't get here 2"),
+              dT("cut 3") & la(orL, "0<=t_0&t_0 < max((0,w*(dhf-dhd_3)))/a&ro_0=rv*t_0&ho_0=w*a/2*t_0^2+dhd_3*t_0|t_0>=max((0,w*(dhf-dhd_3)))/a&ro_0=rv*t_0&ho_0=dhf*t_0-w*max((0,w*(dhf-dhd_3)))^2/(2*a)")
+                && (
+                dT("Goal 124") &
+                  la(orL,"abs(r-rv*(kxtime_5+t_0))>rp|w*h < w*(w*a/2*(t_0+kxtime_5)^2+dhd*(t_0+kxtime_5))-hp")&& (
+                  dT("lSucc2") & ls(hide, "w*h_3 < w*ho_0-hp") & absmax2 & QE,
+                  dT("Goal 135") & ls(hide, "abs(r_3-ro_0)>rp") & (la(andL)*) &
+                    la(orL, "w*dhd_3>=w*dhf|w*ao>=a") && (
+                    dT("Goal 146") & absmax2 & crushw,
+                    dT("Goal 148") & absmax2 & crushw
+                    )
+                  ),
+                dT("Goal 125") &
+                  la(orL,"abs(r-rv*(kxtime_5+t_0))>rp|w*h < w*(w*a/2*(t_0+kxtime_5)^2+dhd*(t_0+kxtime_5))-hp")&& (
+                  dT("Goal 280") & absmax2 & QE,
+                  dT("Goal 281") & absmax2 & (la(andL)*) & (la(orL)*) & QE
+                  )
+                ) ),
+            // goal 111
+            dT("Goal 111") &
+              la(instantiateT(Variable("ho"), "dhf*(t_0+kxtime_5) - w*max0^2/(2*a)".asTerm)) //, { case Forall(Variable("ho", None, Real) :: Nil, _) => true case _ => false })
+              & dT("Goal 120-1") &
+              la(implyL, "0<=kxtime_5+t_0&kxtime_5+t_0 < max0/a&rv*(kxtime_5+t_0)=rv*(kxtime_5+t_0)&dhf*(t_0+kxtime_5)-w*max0^2/(2*a)=w*a/2*(kxtime_5+t_0)^2+dhd*(kxtime_5+t_0)|kxtime_5+t_0>=max0/a&rv*(kxtime_5+t_0)=rv*(kxtime_5+t_0)&dhf*(t_0+kxtime_5)-w*max0^2/(2*a)=dhf*(kxtime_5+t_0)-w*max0^2/(2*a)->abs(r-rv*(kxtime_5+t_0))>rp|w*h < w*(dhf*(t_0+kxtime_5)-w*max0^2/(2*a))-hp")
+              && (
+              dT("Goal 122") & la(hide, initDomain) & absmax2 & QE,
+              dT("Goal 123") & la(orL, "0<=t_0&t_0 < max((0,w*(dhf-dhd_3)))/a&ro_0=rv*t_0&ho_0=w*a/2*t_0^2+dhd_3*t_0|t_0>=max((0,w*(dhf-dhd_3)))/a&ro_0=rv*t_0&ho_0=dhf*t_0-w*max((0,w*(dhf-dhd_3)))^2/(2*a)")
+                && (
+                la(hide, initDomain) & absmax2 & crushor, // takes a while (about 170 seconds)
+                dT("Goal 127") &
+                  (la(andL)*) & dT("Goal 193") &
+                  la(orL, "abs(r-rv*(kxtime_5+t_0))>rp|w*h < w*(dhf*(t_0+kxtime_5)-w*max0^2/(2*a))-hp") && (
+                  dT("Goal 194") & absmax2 & crushor, // takes a while (100 seconds or so)
+                  dT("Goal 195") & ls(hide, "abs(r_3-ro_0)>rp") & absmax2 &
+                    la(orL, "0>=w*(dhf-dhd_3)&max_1=0|0 < w*(dhf-dhd_3)&max_1=w*(dhf-dhd_3)") && (
+                    dT("Goal 214") & cut("w*ao>=a|!w*ao>=a".asFormula) & onBranch(
+                      (cutShowLbl, ls(cohide, "w*ao>=a|!w*ao>=a") & QE),
+                      (cutUseLbl, dT("Goal 214-2") & la(orL, "w*ao>=a|!w*ao>=a") && (
+                        dT("Goal 214-3") /*& la(hide, initDomain)*/ & QE,
+                        dT("Goal 231") & la(orL, "w*dhd_3>=w*dhf|w*ao>=a") && (
+                          dT("Goal 233") & la(orL, "w*dhd>=w*dhf|w*ao>=a") && (
+                            crushor,
+                            la(notL) & closeId
+                            ),
+                          la(notL) & closeId
+                          ) ) ) ),
+                    la(hide, initDomain) & crushor
+                    )
+                  )
+
+                )
+              )
+            )
+            )
+        )
+        ), QE /* End AndRight */
+      )
+
+    val safeLemma = helper.runTactic(safeLemmaTac, new RootNode(safeLemmaSeq))
+    safeLemma shouldBe 'closed
+
+    /*** Lemma machinery, TODO clean up ***/
+    // create evidence (traces input into tool and output from tool)
+    val evidence = new ToolEvidence(immutable.Map("input" -> safeLemmaFormula, "output" -> "true")) :: Nil
+    // add lemma into DB, which creates an ID for it. use the ID to apply the lemma
+    val lemmaDB = LemmaDBFactory.lemmaDB
+    val lemmaID = lemmaDB.add(Lemma(safeLemma.provableWitness, evidence))
+    val applyLemma = new ApplyRule(LookupLemma(lemmaDB, lemmaID)) {
+      override def applicable(node: ProofNode): Boolean = node.sequent.sameSequentAs(safeLemmaSeq)
+    } */
+
+    /*** Main safe theorem and its tactic ***/
+    val safeSeq = parseToSequent(getClass.getResourceAsStream("/examples/casestudies/acasx/nodelay_2sided.key"))
+    val safeTac = ls(implyR) & la(andL) /*& ls(wipeContextInductionT(Some(invariant))) & onBranch(
+      (indInitLbl, dT("Base case") & ls(andR) & closeId),
+      (indUseCaseLbl, dT("Use case") & ls(implyR) & (la(andL)*) & ls(andR) && (
+        la(instantiateT(Variable("t"), Number(0))) &
+          la(instantiateT(Variable("ro"), Number(0))) &
+          la(instantiateT(Variable("ho"), Number(0))) & la(implyL) && (
+          dT("Use case 1") & ls(hide, "abs(r)>rp|abs(h)>hp") &
+            /*abbrv(Variable("max0"))(SuccPosition(0, PosInExpr(0::0::0::1::1::0::Nil))) // But more fragile */
+            abbrv("max((0,w*(dhf-dhd)))".asTerm, Some(Variable("maxI"))) & dT("abbrv") &
+            la(MinMaxT, "", Some("max(0,w*(dhf-dhd))".asTerm)) & QE, //MinMaxT(AntePosition(9, PosInExpr(1 :: Nil)))
+          dT("Absolute value") &
+            ls(AbsT, "", Some("abs(r)".asTerm)) &   //AbsT(SuccPosition(0, PosInExpr(0 :: 0 :: Nil))) &
+            ls(AbsT, "", Some("abs(h)".asTerm)) &   //AbsT(SuccPosition(0, PosInExpr(1 :: 0 :: Nil)))
+            la(AbsT, "", Some("abs(r-0)".asTerm)) & //AbsT(AntePosition(9, PosInExpr(0 :: 0 :: Nil))) &
+            dT("Use case 2") & QE
+          ), closeId
+        )),
+      (indStepLbl, dT("Step") & ls(implyR) & ls(boxSeqGenT(invariant)) & onBranch(
+        (cutShowLbl, dT("Generalization Holds") &
+          ls(boxSeqT) & ls(boxChoiceT) & ls(andR) && (
+          dT("1.1") & ls(boxTestT) & ls(implyR) & ls(boxNDetAssign) & ls(skolemizeT) & closeId, /* closed */
+          dT("1.2") & ls(boxSeqT) & ls(boxNDetAssign) & ls(skolemizeT) & ls(boxSeqT) & ls(boxChoiceT) & dT("1.2.1") &
+            la(hide, "((w=-1|w=1)&\\forall t \\forall ro \\forall ho (0<=t&t < max((0,w*(dhf-dhd)))/a&ro=rv*t&ho=w*a/2*t^2+dhd*t|t>=max((0,w*(dhf-dhd)))/a&ro=rv*t&ho=dhf*t-w*max((0,w*(dhf-dhd)))^2/(2*a)->abs(r-ro)>rp|w*h < w*ho-hp))&(hp>0&rp>0&rv>=0&a>0)")
+            & ls(andR) & /* almost identical branches */
+            ls(substitutionBoxAssignT) & ls(boxTestT) & dT("1.2.2") & ls(implyR) & ls(boxNDetAssign) & ls(skolemizeT) &
+            ls(andR) && (ls(andR) && (dT("cohide") & cohide(SuccPosition(0)) & QE, closeId), closeId)
+          /* last line used to be handled by QE, but Max broke that */
+          /* Would like to replace cohide by: ls(cohide, "-1=-1|-1=1") OR ls(cohide, "1=-1|1=1") (BUT different branches)*/
+          )),
+        (cutUseLbl, dT("Generalization Strong Enough") &
+          abbrv("max((0,w*(dhf-dhd)))".asTerm, Some(Variable("max0"))) & dT("abbrv2") &
+          /*abbrv(Variable("max0"))(AntePosition(0, PosInExpr(0::1::0::0::0::0::0::0::0::1::1::0::Nil)))*/
+          cutEZ("!(w*dhd>=w*dhf|w*ao>=a) | (w*dhd>=w*dhf|w*ao>=a)".asFormula,
+            ls(cohide, "!(w*dhd>=w*dhf|w*ao>=a) | (w*dhd>=w*dhf|w*ao>=a)") & QE) &
+          la(orL, "!(w*dhd>=w*dhf|w*ao>=a) | (w*dhd>=w*dhf|w*ao>=a)") && (
+          la(hide, "max0=max((0,w*(dhf-dhd)))") &
+            la(hide, "((w=-1|w=1)&\\forall t \\forall ro \\forall ho (0<=t&t < max0/a&ro=rv*t&ho=w*a/2*t^2+dhd*t|t>=max0/a&ro=rv*t&ho=dhf*t-w*max0^2/(2*a)->abs(r-ro)>rp|w*h < w*ho-hp))&(hp>0&rp>0&rv>=0&a>0)") &
+            dT("Before DI") &
+            cutEZ("[{r'=-rv,dhd'=ao,h'=-dhd&w*dhd>=w*dhf|w*ao>=a}](0=1)".asFormula, // false as postcondition doesn't work
+              ls(hide, "[{r'=-rv,dhd'=ao,h'=-dhd&w*dhd>=w*dhf|w*ao>=a}](((w=-1|w=1)&\\forall t \\forall ro \\forall ho (0<=t&t < max((0,w*(dhf-dhd)))/a&ro=rv*t&ho=w*a/2*t^2+dhd*t|t>=max((0,w*(dhf-dhd)))/a&ro=rv*t&ho=dhf*t-w*max((0,w*(dhf-dhd)))^2/(2*a)->abs(r-ro)>rp|w*h < w*ho-hp))&(hp>0&rp>0&rv>=0&a>0))")
+                & ls(DI)) &
+            la(hide, "!(w*dhd>=w*dhf|w*ao>=a)") &
+            dT("After DI") & ls(DC("0=1".asFormula)) & onBranch(
+            (cutShowLbl, dT("After DC 1") & closeId),
+            (cutUseLbl, dT("After DC 2") & ls(DW) & dT("after DW") &
+              ls(implyR) & la(andL) & la(cohide, "0=1") & dT("before QE") & QE)
+          ),
+          ls(diffSolution(None, la(hide, "max0=max((0,w*(dhf-dhd)))"))) & dT("Diff. Solution") &
+            ls(implyR) & (la(andL)*) &
+            la(TacticLibrary.eqLeft(exhaustive=true), "kxtime_1=0") & la(hideT, "kxtime_1=0") &
+            la(TacticLibrary.eqLeft(exhaustive=true), "kxtime_4()=0") & la(hideT, "kxtime_4()=0") &
+            la(TacticLibrary.eqLeft(exhaustive=true), "r_2()=r") & la(hideT, "r_2()=r") &
+            la(TacticLibrary.eqLeft(exhaustive=true), "dhd_2()=dhd") & la(hideT, "dhd_2()=dhd") &
+            la(TacticLibrary.eqLeft(exhaustive=true), "h_2()=h") & la(hideT, "h_2()=h") &
+            dT("bla") & cut(safeLemmaFormula.asFormula) & onBranch(
+            (cutShowLbl, ls(cohideT, safeLemmaFormula) & dT("apply Lemma") & applyLemma),
+            (cutUseLbl, dT("use lemma") & QE)
+          )
+          ) /* end orL on cutEZ */
+          ) /* End cutUseLbl "Generalization strong enough" */
+      )) /* End indStepLbl */
+    )*/
+
+    val safeTheorem = helper.runTactic(safeTac, new RootNode(safeSeq))
+    safeTheorem shouldBe 'closed
+  }
 
 /*******************/
 /**** Playground ***/
