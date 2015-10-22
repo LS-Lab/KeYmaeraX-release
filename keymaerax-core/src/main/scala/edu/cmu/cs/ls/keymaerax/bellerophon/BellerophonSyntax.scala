139008e3dbe0dde2fe7e54f2d25d372fe5d72340
Nathan Fulton <gitcommit@nfulton.org>
2015-10-22 13:43:04 -0400

    Implicitly, type of Λt.e|Λt.d = type of Λt.(e|d)

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/bellerophon/BellerophonSyntax.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/bellerophon/BellerophonSyntax.scala
@@ -19,10 +19,12 @@ abstract class BelleExpr {
     case builtIn: BuiltInTactic => builtIn.belleTypeAnnotation
     case builtIn: BuiltInPosTactic => builtIn.belleTypeAnnotation
     case SeqTactic(l,r)  => (l.belleType, r.belleType) match {
+      //@todo this case if rather verbose
+      //@todo to clean up, just handle the good cases and then do a bunch of conditional logic to report the correct exception message. See Either case below.
       case (ForAllType(lVar, lTyBody), ForAllType(rVar, rTyBody)) => {
         if (lVar == rVar) (l, r) match {
           case (TAbs(leftVar, leftBody), TAbs(rightVar, rightBody)) =>
-            if (leftVar == rightVar) ForAllType(leftVar, SeqTactic(leftBody, rightBody).belleType)
+            if (leftVar == rightVar) ForAllType(leftVar, (leftBody & rightBody).belleType)
             else throw BelleError("Cannot synthesize a type for (\\Lambda x. e) & (\\Lambda y . d) where x != y")
           case _ => throw BelleError("Synthesized a non-\\Lambda expression for a \\ForAll type -- should not happen.")
         }
@@ -32,7 +34,21 @@ abstract class BelleExpr {
         assert(lType.getClass == rType.getClass, "Left and Right tactics of a sequential composition should mappings of the same syntactic category.")
         lType(lType, rType)
       }
-      case _ => throw BelleError("Could not synthesize a type of e & d where e,d aren't either both mappings or both type abstractions")
+      case _ => throw BelleError("Cannot synthesize a type of e & d where e,d aren't either both mappings or else both type abstractions.")
+    }
+    case EitherTactic(l, r) => (l, r, l.belleType, r.belleType) match {
+      case (ForAllType(lv, lType), ForAllType(rv, rType), TAbs(lv2, lBody), TAbs(rv2, rBody)) if(lv == lv2 && rv == rv2 && lv == rv) =>
+        ForAllType(lv, (lBody | rBody).belleType)
+      case (leftType: MappingType, rightType: MappingType, left, right) if leftType.getClass == rightType.getClass =>
+        leftType(left, right)
+      case (ForAllType(lv, lType), ForAllType(rv, rType), TAbs(lv2, lBody), TAbs(rv2, rBody)) if(lv != lv2 || rv != rv2) =>
+        throw BelleError("Should never encounter \\Lambda x . e : \\ForAll y . T for x!=y")
+      case (ForAllType(lv, lType), ForAllType(rv, rType), _, _) =>
+        throw BelleError("Cannot synthesize type of e | d when both are of \\ForAll types but one or more isn't a \\Lambda-expr.")
+      case (leftType: MappingType, rightType: MappingType, _, _) =>
+        throw BelleError("Cannot synthesize a type for e ~> e' | d ~> d' when the e and d mappings are of differing syntactic categories.")
+      case _ =>
+        throw BelleError("Unkown error when trying the synthesize a type for e | d")
     }
   }
 }
