d7663fabff82c15b125c32cc57f583d8a9fa803f
Nathan Fulton <gitcommit@nfulton.org>
2015-10-23 12:28:44 -0400

    Uncommented out AndR for Bellerophon

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/PropositionalTactics.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/PropositionalTactics.scala
@@ -1,67 +1,67 @@
-//package edu.cmu.cs.ls.keymaerax.btactics
-//
-//import edu.cmu.cs.ls.keymaerax.bellerophon._
-//import edu.cmu.cs.ls.keymaerax.core
-//import edu.cmu.cs.ls.keymaerax.core._
-//import edu.cmu.cs.ls.keymaerax.pt.ProofTerm
-//import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
-//
-///**
-// * [[PropositionalTactics]] contains tactical implementations of the propositional sequent calculus.
-// * @author Nathan Fulton
-// */
-//object PropositionalTactics {
-//  //////////////////////////////////////////////////////////////////////////////////////////////////
-//  // Helper methods and definitions
-//  //////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//  private val Gamma1 = TypeVar("GammaOne")
-//  private val Gamma2 = TypeVar("GammaTwo")
-//  private val Delta1 = TypeVar("DeltaOne")
-//  private val Delta2 = TypeVar("DeltaTwo")
-//
-//  /** Returns G, ante, G' |- D, succ, D'*/
-//  private def inCtx(ante: Seq[BelleType], succ: Seq[BelleType]) : SequentType =
-//    SequentType((Gamma1 +: ante) :+ Gamma2, (Delta1 +: succ) :+ Delta2)
-//
-////  /** ty -> \Lambda G1 \Lambda G2 \Lambda D1 \Lambda D2 ty
-////    * Assumption: ty has free type vars G1,G2,D1,D2
-////    * Assumption: ty has no other bound or free type vars.
-////    * @todo replace with more general helper methods in typechecker.
-////    */
-////  private def wrap(ty : BelleType) =
-////    TypeAbs(Gamma1, TypeAbs(Gamma2, TypeAbs(Delta1, TypeAbs(Delta2, ty))))
-//
-//  /** e ~> \Forall G1,G2,D1,D2 . e */
-//  private def abstractOver(e : BelleExpr) =
-//    ParametricTactic(Gamma1,
-//      ParametricTactic(Gamma2,
-//        ParametricTactic(Delta1,
-//          ParametricTactic(Delta2,
-//            e))))
-//
-//  //////////////////////////////////////////////////////////////////////////////////////////////////
-//  // Proof rule implementations
-//  //////////////////////////////////////////////////////////////////////////////////////////////////
-//  /**
-//   * @todo show the proof rule being implemented.
-//   */
-//  def AndR = abstractOver(new BuiltInPositionTactic("AndR") {
-//    override def apply(provable: Provable, pos: SeqPos) =
-//      provable(core.AndRight(pos.asInstanceOf[SuccPos]), 0)
-//
-//
-//    override def pt(provable: Provable): (Seq[Provable], (Seq[ProofTerm]) => ProofTerm) = ???
-//
-//    override val belleType : BelleType = {
-//      val pTy      = FormulaType("p()".asFormula)
-//      val qTy      = FormulaType("q()".asFormula)
-//      val andTy    = FormulaType(And(pTy.f, qTy.f))
-//      val domain   = inCtx(Nil, Seq(andTy))     // G1, G2 |- D1, p() ^ q(), D2
-//      val codomain = inCtx(Nil, Seq(pTy, qTy))  // G1, G2 |- D1, p(), q(), D2
-//
-//      // \forall G1,G2,D1,D2 . (G1, G2 |- D1, p() ^ q(), D2) ~> G1, G2 |- D1, p(), q(), D2
-//      SequentMapType(domain, codomain)
-//    }
-//  })
-//}
+package edu.cmu.cs.ls.keymaerax.btactics
+
+import edu.cmu.cs.ls.keymaerax.bellerophon._
+import edu.cmu.cs.ls.keymaerax.core
+import edu.cmu.cs.ls.keymaerax.core._
+import edu.cmu.cs.ls.keymaerax.pt.ProofTerm
+import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
+
+/**
+ * [[PropositionalTactics]] contains tactical implementations of the propositional sequent calculus.
+ * @author Nathan Fulton
+ */
+object PropositionalTactics {
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  // Helper methods and definitions
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  private val Gamma1 = TypeVar("GammaOne")
+  private val Gamma2 = TypeVar("GammaTwo")
+  private val Delta1 = TypeVar("DeltaOne")
+  private val Delta2 = TypeVar("DeltaTwo")
+
+  /** Returns G, ante, G' |- D, succ, D'*/
+  private def inCtx(ante: Seq[BelleType], succ: Seq[BelleType]) : SequentType =
+    SequentType((Gamma1 +: ante) :+ Gamma2, (Delta1 +: succ) :+ Delta2)
+
+//  /** ty -> \Lambda G1 \Lambda G2 \Lambda D1 \Lambda D2 ty
+//    * Assumption: ty has free type vars G1,G2,D1,D2
+//    * Assumption: ty has no other bound or free type vars.
+//    * @todo replace with more general helper methods in typechecker.
+//    */
+//  private def wrap(ty : BelleType) =
+//    TypeAbs(Gamma1, TypeAbs(Gamma2, TypeAbs(Delta1, TypeAbs(Delta2, ty))))
+
+  /** e ~> \Forall G1,G2,D1,D2 . e */
+  private def abstractOver(e : BelleExpr) =
+    TAbs(Gamma1,
+      TAbs(Gamma2,
+        TAbs(Delta1,
+          TAbs(Delta2,
+            e))))
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  // Proof rule implementations
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  /**
+   * @todo show the proof rule being implemented.
+   */
+  def AndR = abstractOver(new BuiltInTactic("AndR") {
+    override def apply(provable: Provable) = {
+      if(provable.subgoals.length != 1)
+        throw BelleError(s"Expected 1 subgoal but found ${provable.subgoals.length} when running non-positional tactic")
+      provable(core.AndRight(SuccPos(0)), 0)
+    } ensuring(r => r.subgoals.length == 2)
+
+    override def belleTypeAnnotation : BelleType = {
+      val pTy      = FormulaType("p()".asFormula)
+      val qTy      = FormulaType("q()".asFormula)
+      val andTy    = FormulaType(And(pTy.f, qTy.f))
+      val domain   = inCtx(Nil, Seq(andTy))     // G1, G2 |- D1, p() ^ q(), D2
+      val codomain = inCtx(Nil, Seq(pTy, qTy))  // G1, G2 |- D1, p(), q(), D2
+
+      // \forall G1,G2,D1,D2 . (G1, G2 |- D1, p() ^ q(), D2) ~> G1, G2 |- D1, p(), q(), D2
+      SequentMapping(domain, codomain)
+    }
+  })
+}
