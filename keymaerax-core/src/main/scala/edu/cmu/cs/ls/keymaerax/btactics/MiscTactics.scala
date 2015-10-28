7777cc7c8ab55f3dd96d5237cb9717adef95d24c
Nathan Fulton <gitcommit@nfulton.org>
2015-10-28 14:05:59 -0400

    Scheduled tactics in new framework.

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/MiscTactics.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/MiscTactics.scala
@@ -3,6 +3,8 @@ package edu.cmu.cs.ls.keymaerax.btactics
 import edu.cmu.cs.ls.keymaerax
 import edu.cmu.cs.ls.keymaerax.bellerophon._
 import edu.cmu.cs.ls.keymaerax.core.Provable
+import edu.cmu.cs.ls.keymaerax.tactics.{TacticWrapper, Interpreter, Tactics, ProofNode}
+import edu.cmu.cs.ls.keymaerax.tools.{KeYmaera, Mathematica}
 
 /**
  * @author Nathan Fulton
@@ -36,25 +38,48 @@ object Idioms {
 }
 
 object Legacy {
-  def Scheduled(tool: keymaerax.tools.Tool,
-                tactic : keymaerax.tactics.Tactics.Tactic,
-                timeout: Int = 0) = new BuiltInTactic(s"Scheduled(${tactic.name})") {
+  def defaultInitialization(mathematicaConfig:  Map[String,String]) = {
+    Tactics.KeYmaeraScheduler = new Interpreter(KeYmaera)
+    Tactics.MathematicaScheduler = new Interpreter(new Mathematica)
+
+    Tactics.KeYmaeraScheduler.init(Map())
+    Tactics.Z3Scheduler.init
+    Tactics.MathematicaScheduler.init(mathematicaConfig)
+  }
+
+//  def defaultDeinitialization = {
+//    if (Tactics.KeYmaeraScheduler != null) {
+//      Tactics.KeYmaeraScheduler.shutdown()
+//      Tactics.KeYmaeraScheduler = null
+//    }
+//    if (Tactics.MathematicaScheduler != null) {
+//      Tactics.MathematicaScheduler.shutdown()
+//      Tactics.MathematicaScheduler = null
+//    }
+//    if(Tactics.Z3Scheduler != null) {
+//      Tactics.Z3Scheduler = null
+//    }
+//  }
+
+  def InitializedScheduledTactic(mathematicaConfig : Map[String,String], tactic: keymaerax.tactics.Tactics.Tactic) = {
+    defaultInitialization(mathematicaConfig)
+    ScheduledTactic(tactic)
+  }
+
+  def ScheduledTactic(tactic : keymaerax.tactics.Tactics.Tactic) = new BuiltInTactic(s"Scheduled(${tactic.name})") {
+    //@see [[Legacy.defaultInitialization]]
+    if(!Tactics.KeYmaeraScheduler.isInitialized)
+      throw BelleError("Need to initialize KeYmaera scheduler and possibly also the Mathematica scheduler before running a Legacy.ScheduledTactic.")
+
     override def result(provable: Provable): Provable = {
       //@todo don't know if we can create a proof node from a provable.
       if(provable.subgoals.length != 1) throw new Exception("Cannot run scheduled tactic on something with more than one subgoal.")
+
       val node = new keymaerax.tactics.RootNode(provable.subgoals.head)
-      tactic(tool, node)
 
-      //Note: completion events aren't used because they don't work... see the fact that some tactics
-      //don't work in hte GUI.
-      var waitTime = 0;
-      while(!tactic.isComplete && (waitTime == 0 || waitTime > timeout)) {
-        synchronized(wait(500));
-        waitTime += 500
-      };
+      Tactics.KeYmaeraScheduler.dispatch(new TacticWrapper(tactic, node))
 
-      if(tactic.isComplete) node.provable
-      else throw BelleError("Waited but the tactic still never finished.")
+      node.provableWitness
     }
   }
 }
