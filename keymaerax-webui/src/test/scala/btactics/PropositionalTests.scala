88022d0f0e552caa1f5f60ed2e2feb87e032612d
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-12-10 13:43:45 +0100

    Sequential inverse implyR

--- keymaerax-webui/src/test/scala/btactics/PropositionalTests.scala
+++ keymaerax-webui/src/test/scala/btactics/PropositionalTests.scala
@@ -6,15 +6,10 @@ package btactics
 */
 
 
-import edu.cmu.cs.ls.keymaerax.bellerophon._
 import edu.cmu.cs.ls.keymaerax.btactics.PropositionalTactics._
 import edu.cmu.cs.ls.keymaerax.core._
-import edu.cmu.cs.ls.keymaerax.parser.KeYmaeraXPrettyPrinter
 import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
 import edu.cmu.cs.ls.keymaerax.tags.{SummaryTest, UsualTest}
-import org.scalatest.{BeforeAndAfterEach, FlatSpec, Matchers}
-import test.RandomFormula
-import testHelper.KeYmaeraXTestTags
 
 import scala.collection.immutable._
 
@@ -40,5 +35,21 @@ class PropositionalTests extends TacticTestBase {
     result.subgoals should have size 1
     result.subgoals.head.ante should contain only ("x>0".asFormula, "y>0".asFormula)
     result.subgoals.head.succ shouldBe empty
+  }
+
+  "implyRi" should "introduce implication from antecedent and succedent" in {
+    val result = proveBy(Sequent(Nil, IndexedSeq("x>0".asFormula), IndexedSeq("y>0".asFormula)),
+      implyRi())
+    result.subgoals should have size 1
+    result.subgoals.head.ante shouldBe empty
+    result.subgoals.head.succ should contain only "x>0 -> y>0".asFormula
+  }
+
+  it should "work as two-position tactic" in {
+    val result = proveBy(Sequent(Nil, IndexedSeq("a=2".asFormula, "x>0".asFormula), IndexedSeq("y>0".asFormula, "b=3".asFormula)),
+      implyRi(AntePos(1), SuccPos(0)))
+    result.subgoals should have size 1
+    result.subgoals.head.ante should contain only "a=2".asFormula
+    result.subgoals.head.succ should contain only ("x>0 -> y>0".asFormula, "b=3".asFormula)
   }
 }
