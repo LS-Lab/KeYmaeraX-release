8c6f8616a0d8f67ee0c1e8b565e5b8219865a2ec
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-12-16 12:15:17 +0100

    Sequential exists generalize tactic

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/FOQuantifierTactics.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/FOQuantifierTactics.scala
@@ -4,7 +4,7 @@ import edu.cmu.cs.ls.keymaerax.bellerophon._
 import edu.cmu.cs.ls.keymaerax.btactics.TactixLibrary._
 import edu.cmu.cs.ls.keymaerax.core._
 import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
-import edu.cmu.cs.ls.keymaerax.tactics._
+import edu.cmu.cs.ls.keymaerax.tactics.{AntePosition, ExpressionTraversal, FormulaTools, Position, PosInExpr, SubstitutionHelper}
 import edu.cmu.cs.ls.keymaerax.tactics.Augmentors._
 
 import scala.collection.immutable
@@ -74,4 +74,61 @@ object FOQuantifierTactics {
           allInstantiate(quantified, instance)(pos.first) & useAt("!! double negation")(pos)
       }
     }
+
+  /**
+   * Generalizes existential quantifiers, but only at certain positions. All positions have to refer to the same term.
+   * @example{{{
+   *           \exists z z=a+b |-
+   *           ------------------existentialGenPosT(Variable("z"), PosInExpr(0::Nil) :: Nil)(AntePosition(0))
+   *                 a+b = a+b |-
+   * }}}
+   * @example{{{
+   *           \exists z z=z |-
+   *           ----------------existentialGenPosT(Variable("z"), PosInExpr(0::Nil) :: PosInExpr(1::Nil) :: Nil)(AntePosition(0))
+   *               a+b = a+b |-
+   * }}}
+   * @param x The new existentially quantified variable.
+   * @param where Points to the term to generalize.
+   * @return The tactic.
+   */
+  def existsGeneralize(x: Variable, where: List[PosInExpr]): DependentPositionTactic = new DependentPositionTactic("exists generalize") {
+    override def apply(pos: Position): DependentTactic = new DependentTactic(name) {
+      override def computeExpr(v: BelleValue): BelleExpr = v match {
+        case BelleProvable(provable) =>
+          require(provable.subgoals.size == 1, "Exactly 1 subgoal expected, but got " + provable.subgoals.size)
+          require(where.nonEmpty, "Need at least one position to generalize")
+          val sequent = provable.subgoals.head
+          require(where.map(w => sequent.sub(pos.navigate(w))).toSet.size == 1, "Not all positions refer to the same term")
+          sequent.sub(pos) match {
+            case Some(fml: Formula) =>
+              val fmlRepl = replaceWheres(fml, x)
+
+              //@note create own substitution since UnificationMatch doesn't get it right yet
+              val aT = FuncOf(Function("t", None, Unit, Real), Nothing)
+              val aP = PredOf(Function("p", None, Real, Bool), DotTerm)
+              val pDot = replaceWheres(fml, DotTerm)
+              val subst = USubst(
+                SubstitutionPair(aP, pDot) ::
+                SubstitutionPair(aT, sequent.sub(pos.navigate(where.head)).get) :: Nil)
+              val origin = Sequent(Nil, immutable.IndexedSeq(),
+                immutable.IndexedSeq(Imply("p(t())".asFormula, Exists(x::Nil, PredOf(Function("p", None, Real, Bool), x)))))
+
+              cut(Imply(fml, Exists(x :: Nil, fmlRepl))) <(
+                /* use */ implyL('Llast) <(closeId, hide(fml)(pos) partial) partial,
+                /* show */ cohide('Rlast) & ProofRuleTactics.US(subst, origin) & byUS("exists generalize")
+                )
+            case _ => throw new BelleError("Position " + pos + " must refer to a formula in sequent " + provable.subgoals.head)
+          }
+      }
+
+      private def replaceWheres(fml: Formula, repl: Term) =
+        ExpressionTraversal.traverse(new ExpressionTraversal.ExpressionTraversalFunction {
+          override def preT(p: PosInExpr, e: Term): Either[Option[ExpressionTraversal.StopTraversal], Term] =
+            if (where.contains(p)) Right(repl) else Left(None)
+        }, fml) match {
+          case Some(f) => f
+          case _ => throw new IllegalArgumentException(s"Position $where is not a term")
+        }
+    }
+  }
 }
