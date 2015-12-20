9415fb9d4ed6a8510318537c0937fa0e9a8d2c2b
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-12-20 10:11:16 +0100

    Unit tests: trait MathematicaBuilder for easy test setup

--- keymaerax-webui/src/test/scala/bellerophon/QETests.scala
+++ keymaerax-webui/src/test/scala/bellerophon/QETests.scala
@@ -4,64 +4,40 @@ import btactics.TacticTestBase
 import edu.cmu.cs.ls.keymaerax.bellerophon.BelleError
 import edu.cmu.cs.ls.keymaerax.btactics.ToolTactics
 import edu.cmu.cs.ls.keymaerax.core.Sequent
-import edu.cmu.cs.ls.keymaerax.launcher.DefaultConfiguration
-import edu.cmu.cs.ls.keymaerax.tools.Mathematica
 import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
 import scala.collection.immutable.IndexedSeq
 
 /**
+ * Tests [[ToolTactics.fullQE]] and [[ToolTactics.partialQE]].
  * @author Nathan Fulton
+ * @author Stefan Mitsch
  */
 class QETests extends TacticTestBase {
 
-  "Implicit params" should "work for Mathematica" in {
-    implicit val qeTool = new Mathematica()
-    qeTool.init(DefaultConfiguration.defaultMathematicaConfig)
-    qeTool.isInitialized shouldBe true
-
+  "Implicit params" should "work for Mathematica" in new MathematicaBuilder {
     proveBy("1=1".asFormula, ToolTactics.fullQE) shouldBe 'proved
   }
 
-  "Full QE" should "prove x>0 & y>0 |- x*y>0" in {
-    implicit val qeTool = new Mathematica()
-    qeTool.init(DefaultConfiguration.defaultMathematicaConfig)
-    qeTool.isInitialized shouldBe true
-
+  "Full QE" should "prove x>0 & y>0 |- x*y>0" in new MathematicaBuilder {
     proveBy(Sequent(Nil, IndexedSeq("x>0".asFormula, "y>0".asFormula), IndexedSeq("x*y>0".asFormula)), ToolTactics.fullQE) shouldBe 'proved
   }
 
-  it should "prove x^2<0 |-" in {
-    implicit val qeTool = new Mathematica()
-    qeTool.init(DefaultConfiguration.defaultMathematicaConfig)
-    qeTool.isInitialized shouldBe true
-
+  it should "prove x^2<0 |-" in new MathematicaBuilder {
     proveBy(Sequent(Nil, IndexedSeq("x^2<0".asFormula), IndexedSeq()), ToolTactics.fullQE) shouldBe 'proved
   }
 
-  it should "fail on |-" in {
-    implicit val qeTool = new Mathematica()
-    qeTool.init(DefaultConfiguration.defaultMathematicaConfig)
-    qeTool.isInitialized shouldBe true
-
+  it should "fail on |-" in new MathematicaBuilder {
     a [BelleError] should be thrownBy proveBy(Sequent(Nil, IndexedSeq(), IndexedSeq()), ToolTactics.fullQE)
   }
 
-  "Partial QE" should "not fail on |-" in {
-    implicit val qeTool = new Mathematica()
-    qeTool.init(DefaultConfiguration.defaultMathematicaConfig)
-    qeTool.isInitialized shouldBe true
-
+  "Partial QE" should "not fail on |-" in new MathematicaBuilder {
     val result = proveBy(Sequent(Nil, IndexedSeq(), IndexedSeq()), ToolTactics.partialQE)
     result.subgoals should have size 1
     result.subgoals.head.ante shouldBe empty
     result.subgoals.head.succ should contain only "false".asFormula
   }
 
-  it should "turn x^2>=0 |- y>1 into |- y>1" in {
-    implicit val qeTool = new Mathematica()
-    qeTool.init(DefaultConfiguration.defaultMathematicaConfig)
-    qeTool.isInitialized shouldBe true
-
+  it should "turn x^2>=0 |- y>1 into |- y>1" in new MathematicaBuilder {
     val result = proveBy(Sequent(Nil, IndexedSeq("x^2>=0".asFormula), IndexedSeq("y>1".asFormula)), ToolTactics.partialQE)
     result.subgoals should have size 1
     result.subgoals.head.ante shouldBe empty
--- keymaerax-webui/src/test/scala/btactics/TacticTestBase.scala
+++ keymaerax-webui/src/test/scala/btactics/TacticTestBase.scala
@@ -2,7 +2,9 @@ package btactics
 
 import edu.cmu.cs.ls.keymaerax.bellerophon.{BelleProvable, BelleExpr, SequentialInterpreter}
 import edu.cmu.cs.ls.keymaerax.core.{Sequent, Provable, Formula, PrettyPrinter}
+import edu.cmu.cs.ls.keymaerax.launcher.DefaultConfiguration
 import edu.cmu.cs.ls.keymaerax.parser.KeYmaeraXPrettyPrinter
+import edu.cmu.cs.ls.keymaerax.tools.Mathematica
 import org.scalatest.{BeforeAndAfterEach, Matchers, FlatSpec}
 
 /**
@@ -10,6 +12,20 @@ import org.scalatest.{BeforeAndAfterEach, Matchers, FlatSpec}
  */
 class TacticTestBase extends FlatSpec with Matchers with BeforeAndAfterEach {
   val theInterpreter = SequentialInterpreter()
+
+  /**
+   * Creates and initializes Mathematica for tests that want to use QE.
+   * @example{{{
+   *    "My test" should "prove something with Mathematica" in new MathematicaBuilder {
+   *      // ... your test code here
+   *    }
+   * }}}
+   * */
+  trait MathematicaBuilder {
+    implicit val qeTool = new Mathematica()
+    qeTool.init(DefaultConfiguration.defaultMathematicaConfig)
+    qeTool shouldBe 'initialized
+  }
 
   /** Test setup */
   override def beforeEach() = {
