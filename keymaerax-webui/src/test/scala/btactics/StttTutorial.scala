8fbb957a5bc7de95e8dc2c019e5a84569e8b1e9a
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-12-23 19:01:28 +0100

    Sequential STTT tutorial example 1

---
+++ keymaerax-webui/src/test/scala/btactics/StttTutorial.scala
@@ -0,0 +1,39 @@
+/**
+* Copyright (c) Carnegie Mellon University.
+* See LICENSE.txt for the conditions of this license.
+*/
+package btactics
+
+import edu.cmu.cs.ls.keymaerax.bellerophon.TheType
+import edu.cmu.cs.ls.keymaerax.btactics.TactixLibrary._
+import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
+import edu.cmu.cs.ls.keymaerax.tags.SlowTest
+import testHelper.ParserFactory._
+
+import scala.language.postfixOps
+
+/**
+ * Tutorial test cases.
+ * @author Stefan Mitsch
+ */
+@SlowTest
+class StttTutorial extends TacticTestBase {
+
+  "Example 1" should "be provable" in withMathematica { implicit qeTool =>
+    val s = parseToSequent(getClass.getResourceAsStream("/examples/tutorials/sttt/example1.key"))
+    val tactic = implyR('_) & andL('_) & DC("v>=0".asFormula)(1) <(
+      /* use */ diffWeaken(1) & prop,
+      /* show */ diffInd(qeTool)(1)
+      )
+    proveBy(s, tactic) shouldBe 'proved
+  }
+
+  "Example 1a" should "be provable" in withMathematica { implicit qeTool =>
+    val s = parseToSequent(getClass.getResourceAsStream("/examples/tutorials/sttt/example1a.key"))
+    val tactic = implyR('_) & andL('_)*@TheType() & diffCut("v>=0".asFormula, "x>=old(x)".asFormula)(1) &
+        diffWeaken(1) & exhaustiveEqL2R(-3) & exhaustiveEqL2R(-4) & prop
+
+    proveBy(s, tactic) shouldBe 'proved
+  }
+
+}
