0179d735cf78f3232d8d6273cd771041828515e6
Stefan Mitsch <smitsch@cs.cmu.edu>
2015-12-28 16:42:35 +0100

    STTT tutorial: partial example 2 with master (no diffsolve)

--- keymaerax-webui/src/test/scala/btactics/StttTutorial.scala
+++ keymaerax-webui/src/test/scala/btactics/StttTutorial.scala
@@ -6,7 +6,9 @@ package btactics
 
 import edu.cmu.cs.ls.keymaerax.bellerophon.TheType
 import edu.cmu.cs.ls.keymaerax.btactics.TactixLibrary._
+import edu.cmu.cs.ls.keymaerax.core.{Imply, Box}
 import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
+import edu.cmu.cs.ls.keymaerax.tactics.ConfigurableGenerate
 import edu.cmu.cs.ls.keymaerax.tags.SlowTest
 import testHelper.ParserFactory._
 
@@ -28,12 +30,32 @@ class StttTutorial extends TacticTestBase {
     proveBy(s, tactic) shouldBe 'proved
   }
 
+  //@todo DiffSolve
+  ignore should "be provable with master" in withMathematica { implicit qeTool =>
+    val s = parseToSequent(getClass.getResourceAsStream("/examples/tutorials/sttt/example1.key"))
+    proveBy(s, master()) shouldBe 'proved
+  }
+
   "Example 1a" should "be provable" in withMathematica { implicit qeTool =>
     val s = parseToSequent(getClass.getResourceAsStream("/examples/tutorials/sttt/example1a.key"))
     val tactic = implyR('_) & andL('_)*@TheType() & diffCut("v>=0".asFormula, "x>=old(x)".asFormula)(1) &
         diffWeaken(1) & exhaustiveEqL2R(-3) & exhaustiveEqL2R(-4) & prop
 
     proveBy(s, tactic) shouldBe 'proved
+  }
+
+  "Example 2" should "be provable with master and custom loop invariant" in withMathematica { implicit qeTool =>
+    val s = parseToSequent(getClass.getResourceAsStream("/examples/tutorials/sttt/example2.key"))
+    val Imply(_, Box(loop, _)) = s.succ.head
+
+    val result = proveBy(s, master(new ConfigurableGenerate(Map((loop, "v>=0".asFormula)))))
+    result.subgoals should have size 3
+    result.subgoals.head.succ should contain only "[{x'=v,v'=A&v>=0}]v>=0".asFormula
+    result.subgoals(1).succ should contain only "[{x'=v,v'=0&v>=0}]v>=0".asFormula
+    result.subgoals(2).succ should contain only "[{x'=v,v'=-B&v>=0}]v>=0".asFormula
+
+    //@todo when DiffSolve is available
+    //proveBy(s, master(new ConfigurableGenerate(Map((loop, "v>=0".asFormula))))) shouldBe 'proved
   }
 
 }
