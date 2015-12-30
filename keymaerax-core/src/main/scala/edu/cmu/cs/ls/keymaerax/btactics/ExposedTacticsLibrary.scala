2383dfbbc28dff12e3e9a319b9a7df57af45363b
Nathan Fulton <gitcommit@nfulton.org>
2015-12-29 19:23:00 -0500

    Commented out cases of ExposedTacticsLibrary that cause immediate runtime errors.

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/ExposedTacticsLibrary.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/ExposedTacticsLibrary.scala
@@ -13,12 +13,34 @@ import edu.cmu.cs.ls.keymaerax.tactics.Tactics.{PositionTactic, Tactic}
 import scala.collection.immutable.HashMap
 
 /**
+  * A bidirectional mapping
+  * @author Nathan Fulton
+  */
+class BiMap[S, T](private val underlyingMap: Map[S, T]) extends Map[S, T] {
+  override def +[TPrime >: T](kv: (S, TPrime)): BiMap[S, TPrime] = {
+    if(underlyingMap.keySet.contains(kv._1) || underlyingMap.values.toSet.contains(kv._2))
+      throw new Exception("Bidirectional maps must be bijective.")
+    else
+      new BiMap(underlyingMap + kv)
+  }
+
+  override def get(key: S): Option[T] = underlyingMap.get(key)
+
+  override def -(key: S): BiMap[S, T] = new BiMap(underlyingMap - key)
+
+  def inverse : BiMap[T, S] = new BiMap(underlyingMap.map(kvp => (kvp._2, kvp._1)))
+
+  override def iterator: Iterator[(S, T)] = underlyingMap.iterator
+}
+
+/**
   * Created by bbohrer on 12/19/15.
   */
 object ExposedTacticsLibrary {
   /** Positional Tactics */
-  val positionalTactics: Map[String, BelleExpr] = Map(
-   "StepAt" ->  HilbertCalculus.stepAt
+  val tactics: BiMap[String, BelleExpr] = new BiMap(Map(
+   "nil"    ->  Idioms.nil
+  ,"StepAt" ->  HilbertCalculus.stepAt
   ,"CloseFalse " ->  ProofRuleTactics.closeFalse
   ,"CloseTrue " ->  ProofRuleTactics.closeTrue
   ,"NotL" ->  ProofRuleTactics.notL
@@ -48,25 +70,25 @@ object ExposedTacticsLibrary {
   ,"BoxAssign" ->  HilbertCalculus.assignb
   ,"BoxRandom" ->  HilbertCalculus.randomb
   ,"BoxTest" ->  HilbertCalculus.testb
-  ,"DiffSolution" ->  HilbertCalculus.diffSolve
+//  ,"DiffSolution" ->  HilbertCalculus.diffSolve @todo diffSolve needs an implementation
   ,"BoxChoice" ->  HilbertCalculus.choiceb
   ,"BoxCompose" ->  HilbertCalculus.composeb
   ,"BoxIterate" ->  HilbertCalculus.iterateb
-  ,"BoxDual" ->  HilbertCalculus.dualb
+//  ,"BoxDual" ->  HilbertCalculus.dualb //@todo commented out b/c we get an error whenever dualb is initialized.
   ,"DiamondAssign" ->  HilbertCalculus.assignd
-  ,"DiamondRandom" ->  HilbertCalculus.randomd
+//  ,"DiamondRandom" ->  HilbertCalculus.randomd //@todo causes some other error.
   ,"DiamondTest" ->  HilbertCalculus.testd
-  ,"DiamondDiffSolution" ->  HilbertCalculus.diffSolved
+//  ,"DiamondDiffSolution" ->  HilbertCalculus.diffSolved //@todo diffSolved needs an implementation
   ,"DiamondChoice" ->  HilbertCalculus.choiced
-  ,"DiamondCompose" ->  HilbertCalculus.composed
-  ,"DiamondIterate" ->  HilbertCalculus.iterated
-  ,"DiamondDual" ->  HilbertCalculus.duald
-  ,"V" ->  HilbertCalculus.V
+//  ,"DiamondCompose" ->  HilbertCalculus.composed
+//  ,"DiamondIterate" ->  HilbertCalculus.iterated
+//  ,"DiamondDual" ->  HilbertCalculus.duald
+//  ,"V" ->  HilbertCalculus.V
   ,"DW" ->  HilbertCalculus.DW
-  /* @TODO DC, DG */
+//  /* @TODO DC, DG */
   ,"DE" ->  HilbertCalculus.DE
   ,"DI" ->  HilbertCalculus.DI
-//  ,"DiffInd" ->  //@todo this no longer exists: HilbertCalculus.diffInd
+////  ,"DiffInd" ->  //@todo this no longer exists: HilbertCalculus.diffInd
   ,"DS" ->  HilbertCalculus.DS
   ,"BoxDifferentialAssign" ->  HilbertCalculus.Dassignb
   ,"Derive" ->  HilbertCalculus.derive
@@ -77,10 +99,10 @@ object ExposedTacticsLibrary {
   ,"DeriveQuotient" ->  HilbertCalculus.Dquotient
   ,"DerivePower" ->  HilbertCalculus.Dpower
   ,"DeriveConstant" ->  HilbertCalculus.Dconst
-  ,"DeriveVariable" ->  HilbertCalculus.Dvariable
+//  ,"DeriveVariable" ->  HilbertCalculus.Dvariable //@todo causes some error
   ,"DeriveAnd" ->  HilbertCalculus.Dand
   ,"DeriveOr" ->  HilbertCalculus.Dor
-  ,"DeriveImply" ->  HilbertCalculus.Dimply
+//  ,"DeriveImply" ->  HilbertCalculus.Dimply //@todo causes some error
   ,"DeriveEqual" ->  HilbertCalculus.Dequal
   ,"DeriveNotEqual" ->  HilbertCalculus.Dnotequal
   ,"DeriveLess" ->  HilbertCalculus.Dless
@@ -91,28 +113,22 @@ object ExposedTacticsLibrary {
   ,"DeriveExists" ->  HilbertCalculus.Dexists
   ,"BoxSplit" -> HilbertCalculus.splitb
   ,"VacuousAll" ->  HilbertCalculus.vacuousAll
-  ,"DiamondSplit" -> HilbertCalculus.splitd
-  ,"VacuousExists" ->  HilbertCalculus.vacuousExists
-  ,"Step" -> TactixLibrary.step)
-
-  val twoPositionTactics: Map[String, BuiltInTwoPositionTactic] = Map(
-   "Close" -> ProofRuleTactics.close
+//  ,"DiamondSplit" -> HilbertCalculus.splitd //@todo causes some error
+//  ,"VacuousExists" ->  HilbertCalculus.vacuousExists //@todo causes some error
+  ,"Step" -> TactixLibrary.step
+  //two position tactics
+  ,"Close" -> ProofRuleTactics.close
   ,"CoHide2" -> ProofRuleTactics.coHide2
   ,"ExchangeL" -> ProofRuleTactics.exchangeL
-  ,"ExchangeR" -> ProofRuleTactics.exchangeR)
-
-  val nullaryTactics: Map[String, BelleExpr] = Map(
-   "TrivialCloser" -> ProofRuleTactics.trivialCloser
-  ,"Prop" -> TactixLibrary.prop)
+  ,"ExchangeR" -> ProofRuleTactics.exchangeR
+  //nullary tactics
+  , "TrivialCloser" -> ProofRuleTactics.trivialCloser
+  ,"Prop" -> TactixLibrary.prop
 //  ,"Master" -> TactixLibrary.master) @todo cannot do this b/c master needs a generator
-
-  val formulaPositionTactics: Map[String, Formula => Position => BelleExpr] = Map (
-    "CutLR" -> {case fml => {case pos => ProofRuleTactics.cutLR(fml)(pos)}}
-  )
-
-  val formulaTactics: Map[String, Formula => BelleExpr] = Map (
-    "Cut" -> {case fml => ProofRuleTactics.cut(fml)}
-  )
+  //formula position tactics @todo these are also not okay because we need a map of BelleExpr's.
+//  , "CutLR" -> {case fml => {case pos => ProofRuleTactics.cutLR(fml)(pos)}}
+//  , "Cut" -> {case fml => ProofRuleTactics.cut(fml)}
+  ))
   /* @TODO US, axiomatic, uniformRenaming, boundRenaming
   *   All of PropositionalTactics.scala */
 }
