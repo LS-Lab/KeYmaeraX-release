1acc59b3dd9c65c45a1fc91969c97d22cfb4d893
bbohrer <bbohrer@andrew.cmu.edu>
2016-01-03 16:48:59 -0500

    Remove debug code from AxiomIndex

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tactics/AxiomIndex.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tactics/AxiomIndex.scala
@@ -199,9 +199,9 @@ object AxiomIndex {
           (ode, constraint) match {
             // @todo diamond duals
             case (_: AtomicODE, True) => tactics ++ odeList ++ ("DE differential effect" :: rules)
-            case (_: AtomicODE, domain) => /* tactics ++ */("DW differential weakening" :: (odeList ++ ("DE differential effect" :: rules)))
+            case (_: AtomicODE, domain) =>  tactics ++ ("DW differential weakening" :: (odeList ++ ("DE differential effect" :: rules)))
             case (_: DifferentialProduct, True) => tactics ++ odeList ++ ("DE differential effect (system)" :: rules)
-            case (_: DifferentialProduct, domain) => tactics ++ ("DW differential weakening" :: (/*odeList ++ */("DE differential effect (system)" :: rules)))
+            case (_: DifferentialProduct, domain) => tactics ++ ("DW differential weakening" :: (RemodeList ++ ("DE differential effect (system)" :: rules)))
             case _ => tactics
           }
         case _ => Nil
