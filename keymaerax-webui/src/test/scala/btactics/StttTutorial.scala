3c831441a7b593cd8d32416c79832c79289154d0
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-01-05 15:42:13 +0100

    STTT tutorial test cases: example 1 and 2 by master

--- keymaerax-webui/src/test/scala/btactics/StttTutorial.scala
+++ keymaerax-webui/src/test/scala/btactics/StttTutorial.scala
@@ -30,15 +30,14 @@ class StttTutorial extends TacticTestBase {
     proveBy(s, tactic) shouldBe 'proved
   }
 
-  //@todo DiffSolve
-  ignore should "be provable with master" in withMathematica { implicit qeTool =>
+  it should "be provable with master" in withMathematica { implicit qeTool =>
     val s = parseToSequent(getClass.getResourceAsStream("/examples/tutorials/sttt/example1.key"))
     proveBy(s, master()) shouldBe 'proved
   }
 
   "Example 1a" should "be provable" in withMathematica { implicit qeTool =>
     val s = parseToSequent(getClass.getResourceAsStream("/examples/tutorials/sttt/example1a.key"))
-    val tactic = implyR('_) & andL('_)*@TheType() & diffCut("v>=0".asFormula, "x>=old(x)".asFormula)(1) &
+    val tactic = implyR('_) & andL('_)*@TheType() & diffInvariant("v>=0".asFormula, "x>=old(x)".asFormula)(1) &
         diffWeaken(1) & exhaustiveEqL2R(-3) & exhaustiveEqL2R(-4) & prop
 
     proveBy(s, tactic) shouldBe 'proved
@@ -47,15 +46,12 @@ class StttTutorial extends TacticTestBase {
   "Example 2" should "be provable with master and custom loop invariant" in withMathematica { implicit qeTool =>
     val s = parseToSequent(getClass.getResourceAsStream("/examples/tutorials/sttt/example2.key"))
     val Imply(_, Box(loop, _)) = s.succ.head
+    proveBy(s, master(new ConfigurableGenerate(Map((loop, "v>=0".asFormula))))) shouldBe 'proved
+  }
 
-    val result = proveBy(s, master(new ConfigurableGenerate(Map((loop, "v>=0".asFormula)))))
-    result.subgoals should have size 3
-    result.subgoals.head.succ should contain only "[{x'=v,v'=A&v>=0}]v>=0".asFormula
-    result.subgoals(1).succ should contain only "[{x'=v,v'=0&v>=0}]v>=0".asFormula
-    result.subgoals(2).succ should contain only "[{x'=v,v'=-B&v>=0}]v>=0".asFormula
-
-    //@todo when DiffSolve is available
-    //proveBy(s, master(new ConfigurableGenerate(Map((loop, "v>=0".asFormula))))) shouldBe 'proved
+  it should "be provable with master and loop invariant from file" in withMathematica { implicit qeTool =>
+    val s = parseToSequent(getClass.getResourceAsStream("/examples/tutorials/sttt/example2.key"))
+    proveBy(s, master()) shouldBe 'proved
   }
 
 }
