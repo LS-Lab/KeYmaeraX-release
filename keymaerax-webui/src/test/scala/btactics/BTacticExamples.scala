bdbad8c93bd9a2aaf9bddd823683ec23473e7494
Andre Platzer <aplatzer@cs.cmu.edu>
2016-01-06 11:10:11 -0500

    Port TacticExamples -> BTacticExamples for new Bellerophon

--- keymaerax-webui/src/test/scala/btactics/BTacticExamples.scala
+++ keymaerax-webui/src/test/scala/btactics/BTacticExamples.scala
@@ -7,6 +7,7 @@ package btactics
 
 import edu.cmu.cs.ls.keymaerax.btactics._
 import edu.cmu.cs.ls.keymaerax.core._
+import edu.cmu.cs.ls.keymaerax.tactics.{PosInExpr, Context}
 import edu.cmu.cs.ls.keymaerax.tags.SummaryTest
 import scala.collection.immutable._
 import edu.cmu.cs.ls.keymaerax.tools.{KeYmaera, Mathematica}
@@ -77,129 +78,129 @@ class BTacticExamples extends TacticTestBase  {
   }
 
 
-//  "Proof by Pointing" should "prove <v:=2*v+1;>q(v) <-> q(2*v+1)" in {
-//    import TactixLibrary._
-//    import DerivedAxioms._
-//    // Proof by pointing of  |- <v:=2*v+1;>v!=0 <-> 2*v+1!=0
-//    val proof = TactixLibrary.proveBy(
-//      Sequent(Nil, IndexedSeq(), IndexedSeq("<v:=2*v+1;>q(v) <-> q(2*v+1)".asFormula)),
-//      // use "<> diamond" axiom backwards at the indicated position on
-//      // |- __<v:=2*v+1;>q(v)__ <-> q(2*v+1)
-//      useAt("<> diamond", PosInExpr(1::Nil))(SuccPosition(0, 0::Nil)) &
-//        // use "[:=] assign" axiom forward at the indicated position on
-//        // |- !__[v:=2*v+1;]!q(v)__ <-> q(2*v+1)
-//        useAt("[:=] assign")(SuccPosition(0, 0::0::Nil)) &
-//        // use double negation at the indicated position on
-//        // |- __!!q(2*v+1)__ <-> q(2*v+1)
-//        useAt(doubleNegationAxiom)(SuccPosition(0, 0::Nil)) &
-//        // close by (an instance of) reflexivity |- p() <-> p()
-//        // |- q(2*v+1) <-> q(2*v+1)
-//        byUS(equivReflexiveAxiom)
-//    )
-//    proof shouldBe 'proved
-//    proof.proved shouldBe Sequent(Nil, IndexedSeq(), IndexedSeq("<v:=2*v+1;>q(v) <-> q(2*v+1)".asFormula))
-//  }
-//
-//  it should "prove <a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))" in {
-//    import TactixLibrary._
-//    // Proof by pointing of  |- <a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))
-//    val proof = TactixLibrary.proveBy(
-//      Sequent(Nil, IndexedSeq(), IndexedSeq("<a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))".asFormula)),
-//      // use "<> diamond" axiom backwards at the indicated position on
-//      // |- __<a;++b;>p(x)__  <->  <a;>p(x) | <b;>p(x)
-//      useAt("<> diamond", PosInExpr(1::Nil))(SuccPosition(0, 0::Nil)) &
-//        // use "[++] choice" axiom forward at the indicated position on
-//        // |- !__[a;++b;]!p(x)__  <->  <a;>p(x) | <b;>p(x)
-//        useAt("[++] choice")(SuccPosition(0, 0::0::Nil)) &
-//        // use "<> diamond" axiom forward at the indicated position on
-//        // |- !([a;]!p(x) & [b;]!p(x))  <->  __<a;>p(x)__ | <b;>p(x)
-//        useAt("<> diamond", PosInExpr(1::Nil))(SuccPosition(0, 1::0::Nil)) &
-//        // use "<> diamond" axiom forward at the indicated position on
-//        // |- !([a;]!p(x) & [b;]!p(x))  <->  ![a;]!p(x) | __<b;>p(x)__
-//        useAt("<> diamond", PosInExpr(1::Nil))(SuccPosition(0, 1::1::Nil)) &
-//        // use propositional logic to show
-//        // |- !([a;]!p(x) & [b;]!p(x))  <->  ![a;]!p(x) | ![b;]!p(x)
-//        prop
-//    )
-//    proof shouldBe 'proved
-//    proof.proved shouldBe Sequent(Nil, IndexedSeq(), IndexedSeq("<a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))".asFormula))
-//  }
-//
-//  it should "prove with steps <a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))" in {
-//    import TactixLibrary._
-//    // Proof by pointing with steps of  |- <a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))
-//    val proof = TactixLibrary.proveBy(
-//      Sequent(Nil, IndexedSeq(), IndexedSeq("<a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))".asFormula)),
-//      // use "<> diamond" axiom backwards at the indicated position on
-//      // |- __<a;++b;>p(x)__  <->  <a;>p(x) | <b;>p(x)
-//      useAt("<> diamond", PosInExpr(1::Nil))(SuccPosition(0, 0::Nil)) &
-//        // |- !__[a;++b;]!p(x)__  <->  <a;>p(x) | <b;>p(x)
-//        // step "[++] choice" axiom forward at the indicated position
-//        stepAt(SuccPosition(0, 0::0::Nil)) &
-//        // |- __!([a;]!p(x) & [b;]!p(x))__  <-> <a;>p(x) | <b;>p(x)
-//        // step deMorgan forward at the indicated position
-//        stepAt(SuccPosition(0, 0::Nil)) &
-//        // |- __![a;]!p(x)__ | ![b;]!p(x)  <-> <a;>p(x) | <b;>p(x)
-//        // step "<> diamond" forward at the indicated position
-//        stepAt(SuccPosition(0, 0::0::Nil)) &
-//        // |- <a;>p(x) | __![b;]!p(x)__  <-> <a;>p(x) | <b;>p(x)
-//        // step "<> diamond" forward at the indicated position
-//        stepAt(SuccPosition(0, 0::1::Nil)) &
-//        // |- <a;>p(x) | <b;>p(x)  <-> <a;>p(x) | <b;>p(x)
-//        byUS("<-> reflexive")
-//    )
-//    proof shouldBe 'proved
-//    proof.proved shouldBe Sequent(Nil, IndexedSeq(), IndexedSeq("<a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))".asFormula))
-//  }
-//
-//  "Proof by Congruence" should "prove x*(x+1)>=0 -> [y:=0;x:=x^2+x;]x>=y" in {
-//    import TactixLibrary._
-//    // |- x*(x+1)>=0 -> [y:=0;x:=x^2+x;]x>=y
-//    val proof = proveBy("x*(x+1)>=0 -> [y:=0;x:=x^2+x;]x>=y".asFormula,
-//      CE(proveBy("x*(x+1)=x^2+x".asFormula, QE)) (SuccPosition(0, 1::0::1::1::Nil)) &
-//        // |- x*(x+1)>=0 -> [y:=0;x:=x*(x+1);]x>=y by CE/CQ using x*(x+1)=x^2+x
-//        // step uses top-level operator [;]
-//      stepAt(SuccPosition(0, 1::Nil)) &
-//        // |- x*(x+1)>=0 -> [y:=0;][x:=x*(x+1);]x>=y
-//        // step uses top-level operator [:=]
-//        stepAt(SuccPosition(0, 1::Nil)) &
-//        // |- x*(x+1)>=0 -> [x:=x*(x+1);]x>=0
-//        // step uses top-level [:=]
-//        stepAt(SuccPosition(0, 1::Nil)) &
-//        // |- x*(x+1)>=0 -> x*(x+1)>=0
-//        prop
-//    )
-//    proof shouldBe 'proved
-//    proof.proved shouldBe Sequent(Nil, IndexedSeq(), IndexedSeq("x*(x+1)>=0 -> [y:=0;x:=x^2+x;]x>=y".asFormula))
-//  }
-//
-//  it should "prove x^2<4 -> [{x'=9*x^2-x&x^2<4}](-2<x&x<2)" in {
-//    import TactixLibrary._
-//    // |- x^2<4 -> [{x'=9*x^2-x&x^2<4}](-2<x&x<2)
-//    val proof = proveBy("x^2<4 -> [{x'=9*x^2-x&x^2<4}](-2<x&x<2)".asFormula,
-//      CE(proveBy("-2<x&x<2<->x^2<4".asFormula, QE)) (SuccPosition(0, 1::0::1::Nil)) &
-//        // |- x^2<4 -> [{x'=9*x^2-x&(-2<x&<2)}](-2<x&x<2) by CE using -2<x&x<2<->x^2<4
-//        useAt("DW")(SuccPosition(0, 1::Nil)) &
-//        // |- x^2<4 -> true by DW
-//        prop
-//    )
-//    proof shouldBe 'proved
-//    proof.proved shouldBe Sequent(Nil, IndexedSeq(), IndexedSeq("x^2<4 -> [{x'=9*x^2-x&x^2<4}](-2<x&x<2)".asFormula))
-//  }
-//
-//  it should "reduce x<5 & x^2<4 -> [{x' = 5*x & x^2<4}](x^2<4 & x>=1) to x<5 & (-2<x&x<2) -> [{x' = 5*x & -2<x&x<2}]((-2<x&x<2) & x>=1)" in {
-//    import TactixLibrary._
-//    val C = Context("x<5 & ⎵ -> [{x' = 5*x & ⎵}](⎵ & x>=1)".asFormula)
-//    // |- x<5 & __x^2<4__ -> [{x' = 5*x & __x^2<4__}](__x^2<4__ & x>=1)
-//    val proof = proveBy("x<5 & x^2<4 -> [{x' = 5*x & x^2<4}](x^2<4 & x>=1)".asFormula,
-//      CE(proveBy("-2<x&x<2<->x^2<4".asFormula, QE), C) (SuccPosition(0)))
-//    // |- x<5 & (__-2<x&x<2__) -> [{x' = 5*x & __-2<x&x<2__}]((__-2<x&x<2__) & x>=1) by CE
-//    proof.subgoals should contain only (
-//      new Sequent(Nil, IndexedSeq(), IndexedSeq("x<5 & (-2<x&x<2) -> [{x' = 5*x & -2<x&x<2}]((-2<x&x<2) & x>=1)".asFormula))
-//      )
-//  }
-//
-//
+  "Proof by Pointing" should "prove <v:=2*v+1;>q(v) <-> q(2*v+1)" in {
+    import TactixLibrary._
+    import DerivedAxioms._
+    // Proof by pointing of  |- <v:=2*v+1;>v!=0 <-> 2*v+1!=0
+    val proof = TactixLibrary.proveBy(
+      Sequent(Nil, IndexedSeq(), IndexedSeq("<v:=2*v+1;>q(v) <-> q(2*v+1)".asFormula)),
+      // use "<> diamond" axiom backwards at the indicated position on
+      // |- __<v:=2*v+1;>q(v)__ <-> q(2*v+1)
+      useAt("<> diamond", PosInExpr(1::Nil))(1, 0::Nil) &
+        // use "[:=] assign" axiom forward at the indicated position on
+        // |- !__[v:=2*v+1;]!q(v)__ <-> q(2*v+1)
+        useAt("[:=] assign")(1, 0::0::Nil) &
+        // use double negation at the indicated position on
+        // |- __!!q(2*v+1)__ <-> q(2*v+1)
+        useAt(doubleNegationAxiom)(1, 0::Nil) &
+        // close by (an instance of) reflexivity |- p() <-> p()
+        // |- q(2*v+1) <-> q(2*v+1)
+        byUS(equivReflexiveAxiom)
+    )
+    proof shouldBe 'proved
+    proof.proved shouldBe Sequent(Nil, IndexedSeq(), IndexedSeq("<v:=2*v+1;>q(v) <-> q(2*v+1)".asFormula))
+  }
+
+  it should "prove <a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))" in {
+    import TactixLibrary._
+    // Proof by pointing of  |- <a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))
+    val proof = TactixLibrary.proveBy(
+      Sequent(Nil, IndexedSeq(), IndexedSeq("<a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))".asFormula)),
+      // use "<> diamond" axiom backwards at the indicated position on
+      // |- __<a;++b;>p(x)__  <->  <a;>p(x) | <b;>p(x)
+      useAt("<> diamond", PosInExpr(1::Nil))(1, 0::Nil) &
+        // use "[++] choice" axiom forward at the indicated position on
+        // |- !__[a;++b;]!p(x)__  <->  <a;>p(x) | <b;>p(x)
+        useAt("[++] choice")(1, 0::0::Nil) &
+        // use "<> diamond" axiom forward at the indicated position on
+        // |- !([a;]!p(x) & [b;]!p(x))  <->  __<a;>p(x)__ | <b;>p(x)
+        useAt("<> diamond", PosInExpr(1::Nil))(1, 1::0::Nil) &
+        // use "<> diamond" axiom forward at the indicated position on
+        // |- !([a;]!p(x) & [b;]!p(x))  <->  ![a;]!p(x) | __<b;>p(x)__
+        useAt("<> diamond", PosInExpr(1::Nil))(1, 1::1::Nil) &
+        // use propositional logic to show
+        // |- !([a;]!p(x) & [b;]!p(x))  <->  ![a;]!p(x) | ![b;]!p(x)
+        prop
+    )
+    proof shouldBe 'proved
+    proof.proved shouldBe Sequent(Nil, IndexedSeq(), IndexedSeq("<a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))".asFormula))
+  }
+
+  it should "prove with steps <a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))" in {
+    import TactixLibrary._
+    // Proof by pointing with steps of  |- <a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))
+    val proof = TactixLibrary.proveBy(
+      Sequent(Nil, IndexedSeq(), IndexedSeq("<a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))".asFormula)),
+      // use "<> diamond" axiom backwards at the indicated position on
+      // |- __<a;++b;>p(x)__  <->  <a;>p(x) | <b;>p(x)
+      useAt("<> diamond", PosInExpr(1::Nil))(1, 0::Nil) &
+        // |- !__[a;++b;]!p(x)__  <->  <a;>p(x) | <b;>p(x)
+        // step "[++] choice" axiom forward at the indicated position
+        stepAt(1, 0::0::Nil) &
+        // |- __!([a;]!p(x) & [b;]!p(x))__  <-> <a;>p(x) | <b;>p(x)
+        // step deMorgan forward at the indicated position
+        stepAt(1, 0::Nil) &
+        // |- __![a;]!p(x)__ | ![b;]!p(x)  <-> <a;>p(x) | <b;>p(x)
+        // step "<> diamond" forward at the indicated position
+        stepAt(1, 0::0::Nil) &
+        // |- <a;>p(x) | __![b;]!p(x)__  <-> <a;>p(x) | <b;>p(x)
+        // step "<> diamond" forward at the indicated position
+        stepAt(1, 0::1::Nil) &
+        // |- <a;>p(x) | <b;>p(x)  <-> <a;>p(x) | <b;>p(x)
+        byUS("<-> reflexive")
+    )
+    proof shouldBe 'proved
+    proof.proved shouldBe Sequent(Nil, IndexedSeq(), IndexedSeq("<a;++b;>p(x) <-> (<a;>p(x) | <b;>p(x))".asFormula))
+  }
+
+  "Proof by Congruence" should "prove x*(x+1)>=0 -> [y:=0;x:=x^2+x;]x>=y" in withMathematica { implicit qeTool =>
+    import TactixLibrary._
+    // |- x*(x+1)>=0 -> [y:=0;x:=x^2+x;]x>=y
+    val proof = TactixLibrary.proveBy("x*(x+1)>=0 -> [y:=0;x:=x^2+x;]x>=y".asFormula,
+      CE(TactixLibrary.proveBy("x*(x+1)=x^2+x".asFormula, QE)) (1, 1::0::1::1::Nil) &
+        // |- x*(x+1)>=0 -> [y:=0;x:=x*(x+1);]x>=y by CE/CQ using x*(x+1)=x^2+x
+        // step uses top-level operator [;]
+      stepAt(1, 1::Nil) &
+        // |- x*(x+1)>=0 -> [y:=0;][x:=x*(x+1);]x>=y
+        // step uses top-level operator [:=]
+        stepAt(1, 1::Nil) &
+        // |- x*(x+1)>=0 -> [x:=x*(x+1);]x>=0
+        // step uses top-level [:=]
+        stepAt(1, 1::Nil) &
+        // |- x*(x+1)>=0 -> x*(x+1)>=0
+        prop
+    )
+    proof shouldBe 'proved
+    proof.proved shouldBe Sequent(Nil, IndexedSeq(), IndexedSeq("x*(x+1)>=0 -> [y:=0;x:=x^2+x;]x>=y".asFormula))
+  }
+
+  it should "prove x^2<4 -> [{x'=9*x^2-x&x^2<4}](-2<x&x<2)" in withMathematica { implicit qeTool =>
+    import TactixLibrary._
+    // |- x^2<4 -> [{x'=9*x^2-x&x^2<4}](-2<x&x<2)
+    val proof = TactixLibrary.proveBy("x^2<4 -> [{x'=9*x^2-x&x^2<4}](-2<x&x<2)".asFormula,
+      CE(TactixLibrary.proveBy("-2<x&x<2<->x^2<4".asFormula, QE)) (1, 1::0::1::Nil) &
+        // |- x^2<4 -> [{x'=9*x^2-x&(-2<x&<2)}](-2<x&x<2) by CE using -2<x&x<2<->x^2<4
+        useAt("DW")(1, 1::Nil) &
+        // |- x^2<4 -> true by DW
+        prop
+    )
+    proof shouldBe 'proved
+    proof.proved shouldBe Sequent(Nil, IndexedSeq(), IndexedSeq("x^2<4 -> [{x'=9*x^2-x&x^2<4}](-2<x&x<2)".asFormula))
+  }
+
+  it should "reduce x<5 & x^2<4 -> [{x' = 5*x & x^2<4}](x^2<4 & x>=1) to x<5 & (-2<x&x<2) -> [{x' = 5*x & -2<x&x<2}]((-2<x&x<2) & x>=1)" in withMathematica { implicit qeTool =>
+    import TactixLibrary._
+    val C = Context("x<5 & ⎵ -> [{x' = 5*x & ⎵}](⎵ & x>=1)".asFormula)
+    // |- x<5 & __x^2<4__ -> [{x' = 5*x & __x^2<4__}](__x^2<4__ & x>=1)
+    val proof = TactixLibrary.proveBy("x<5 & x^2<4 -> [{x' = 5*x & x^2<4}](x^2<4 & x>=1)".asFormula,
+      CE(TactixLibrary.proveBy("-2<x&x<2<->x^2<4".asFormula, QE), C) (1))
+    // |- x<5 & (__-2<x&x<2__) -> [{x' = 5*x & __-2<x&x<2__}]((__-2<x&x<2__) & x>=1) by CE
+    proof.subgoals should contain only (
+      new Sequent(Nil, IndexedSeq(), IndexedSeq("x<5 & (-2<x&x<2) -> [{x' = 5*x & -2<x&x<2}]((-2<x&x<2) & x>=1)".asFormula))
+      )
+  }
+
+
   "Proof by Chase" should "chase the prime away in [{x'=22}](2*x+x*y>=5)'" in withMathematica { implicit qeTool =>
     import TactixLibrary._
     val proof = TactixLibrary.proveBy("[{x'=22}](2*x+x*y>=5)'".asFormula,
@@ -209,7 +210,7 @@ class BTacticExamples extends TacticTestBase  {
     )
     proof.subgoals shouldBe List(Sequent(Nil, IndexedSeq(), IndexedSeq("[{x'=22}]2*x'+(x'*y+x*y')>=0".asFormula)))
   }
-//
+
   it should "prove [{x'=22}](2*x+x*y>=5)' <-> [{x'=22}]2*x'+(x'*y+x*y')>=0" in withMathematica { implicit qeTool =>
     import TactixLibrary._
     val proof = TactixLibrary.proveBy("[{x'=22}](2*x+x*y>=5)' <-> [{x'=22}]2*x'+(x'*y+x*y')>=0".asFormula,
