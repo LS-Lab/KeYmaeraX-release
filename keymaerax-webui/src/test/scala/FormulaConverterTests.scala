8abcdbf92b8482afd131437848bef8c2862dde87
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-01-07 20:14:33 +0100

    Another import fix

--- keymaerax-webui/src/test/scala/FormulaConverterTests.scala
+++ keymaerax-webui/src/test/scala/FormulaConverterTests.scala
@@ -3,11 +3,11 @@
 * See LICENSE.txt for the conditions of this license.
 */
 
-import edu.cmu.cs.ls.keymaerax.btactics.Context
 import edu.cmu.cs.ls.keymaerax.core._
 import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
 import edu.cmu.cs.ls.keymaerax.tactics.PosInExpr
 import org.scalatest.{Matchers, FlatSpec}
+import edu.cmu.cs.ls.keymaerax.tactics.Context
 import edu.cmu.cs.ls.keymaerax.tactics.FormulaConverter._
 
 /**
