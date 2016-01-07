9b34d60e601b212a2e3cb03e2b5cac803d6ce07f
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-01-07 14:40:59 +0100

    Proof messages controller for error reporting

---
+++ keymaerax-webui/src/main/resources/js/controllers/proofmessages.js
@@ -0,0 +1,19 @@
+angular.module('keymaerax.controllers').controller('ProofMessagesCtrl',
+  function($rootScope, $scope) {
+
+  $scope.proofMessage = {
+    text: "",
+    isVisible: false
+  }
+
+  $rootScope.$on('agenda.updateWithoutProgress', function() {
+    $scope.proofMessage.text = "The tactic did not make progress";
+    $scope.proofMessage.isVisible = true;
+  });
+
+  $rootScope.$on('proof.message', function(event, message) {
+    $scope.proofMessage.text = message;
+    $scope.proofMessage.isVisible = true;
+  });
+
+})
