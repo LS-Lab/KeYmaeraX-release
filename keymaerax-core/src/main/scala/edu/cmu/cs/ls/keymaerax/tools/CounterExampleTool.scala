54a0f2e67f880d5d1c2239e70acf464738d2eb7b
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-01-10 21:58:15 +0100

    Counter-example tool trait

---
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/CounterExampleTool.scala
@@ -0,0 +1,22 @@
+/**
+* Copyright (c) Carnegie Mellon University.
+* See LICENSE.txt for the conditions of this license.
+*/
+/**
+ * @note Code Review: 2015-08-24
+ */
+package edu.cmu.cs.ls.keymaerax.tools
+
+import edu.cmu.cs.ls.keymaerax.core.{Term, NamedSymbol, Formula}
+
+/**
+ * Quantifier elimination tool.
+ */
+trait CounterExampleTool {
+  /**
+   * Returns a counterexample for the specified formula.
+   * @param formula The formula.
+   * @return A counterexample, if found. None otherwise.
+   */
+  def findCounterExample(formula: Formula): Option[Map[NamedSymbol, Term]]
+}
