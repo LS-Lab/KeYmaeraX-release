a3b9b50ee733d027f9544fc8822fa9b1009d3c8f
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-01-20 09:33:57 +0100

    Angular drag-and-drop directive

--- keymaerax-webui/src/main/resources/js/keymaera-ui/mouseevents.js
+++ keymaerax-webui/src/main/resources/js/keymaera-ui/mouseevents.js
@@ -1,6 +1,8 @@
-/** Right-click directive */
 angular.module('keymaerax.ui.mouseevents', [])
-  .directive('ngRightClick', function($parse) {
+
+/** Right-click directive */
+angular.module('keymaerax.ui.mouseevents')
+  .directive('ngRightClick', ['$parse', function($parse) {
     return function(scope, element, attrs) {
         var fn = $parse(attrs.ngRightClick);
         element.bind('contextmenu', function(event) {
@@ -10,4 +12,61 @@ angular.module('keymaerax.ui.mouseevents', [])
             });
         });
     };
+  }]);
+
+angular.module('keymaerax.ui.mouseevents')
+  .directive('k4Draggable', function() {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        element[0].draggable = true;
+
+        element[0].addEventListener('dragstart', function(event) {
+          event.dataTransfer.effectAllowed = 'move';
+          event.dataTransfer.setData('dragData', JSON.stringify(attrs.dragData));
+          angular.element(event.target).addClass('k4-drag');
+        });
+
+        element[0].addEventListener('dragend', function(event) {
+          angular.element(event.target).removeClass('k4-drag');
+        });
+      }
+    }
   });
+
+angular.module('keymaerax.ui.mouseevents')
+  .directive('k4Droppable', ['$parse', function($parse) {
+    return {
+      restrict: 'A',
+      scope: {
+        onDrop: '&'
+      },
+      link: function(scope, element, attrs) {
+        element[0].addEventListener('dragover', function(event) {
+          event.dataTransfer.dropEffect = 'move';
+          // allows us to drop
+          if (event.preventDefault) event.preventDefault();
+          angular.element(event.target).addClass('k4-drag-over');
+          // see JavaScript spec: event.dataTransfer.getData('dragData') does not return the data set during dragstart!
+        });
+
+        element[0].addEventListener('dragenter', function(event) {
+          angular.element(event.target).addClass('k4-drag-over');
+          // see JavaScript spec: event.dataTransfer.getData('dragData') does not return the data set during dragstart!
+        });
+
+        element[0].addEventListener('dragleave', function(event) {
+          angular.element(event.target).removeClass('k4-drag-over');
+          // see JavaScript spec: event.dataTransfer.getData('dragData') does not return the data set during dragstart!
+        });
+
+        element[0].addEventListener('drop', function(event) {
+          // Stops some browsers from redirecting
+          if (event.stopPropagation) event.stopPropagation();
+          angular.element(event.target).removeClass('k4-drag-over');
+          var dragData = JSON.parse(event.dataTransfer.getData('dragData'));
+          scope.$apply(scope.onDrop({dragData: dragData}));
+        });
+      }
+    }
+  }]);
