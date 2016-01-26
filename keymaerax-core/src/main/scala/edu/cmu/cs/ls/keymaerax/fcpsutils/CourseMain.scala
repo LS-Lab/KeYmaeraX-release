a2deebf38dc56b7f2d0afebc22a9d814973497fa
Nathan Fulton <gitcommit@nfulton.org>
2016-01-26 16:01:50 -0500

    Added --problem --solution options.

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/fcpsutils/CourseMain.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/fcpsutils/CourseMain.scala
@@ -2,10 +2,10 @@ package edu.cmu.cs.ls.keymaerax.fcpsutils
 
 import java.io.File
 
-import edu.cmu.cs.ls.keymaerax.bellerophon.BTacticParser
+import edu.cmu.cs.ls.keymaerax.bellerophon.{BelleProvable, SequentialInterpreter, BTacticParser}
 import edu.cmu.cs.ls.keymaerax.btactics.{NoneGenerate, TactixLibrary, DerivedAxioms}
-import edu.cmu.cs.ls.keymaerax.core.Formula
-import edu.cmu.cs.ls.keymaerax.parser.ParseException
+import edu.cmu.cs.ls.keymaerax.core.{Provable, Formula}
+import edu.cmu.cs.ls.keymaerax.parser.{KeYmaeraXProblemParser, ParseException}
 import edu.cmu.cs.ls.keymaerax.tools.Mathematica
 
 /**
@@ -21,6 +21,8 @@ object CourseMain {
       "bparse" -> StrArgType(),
       "tparse" -> StrArgType(),
       "exists" -> StrArgType(),
+      "problem" -> StrArgType(),
+      "solution" -> StrArgType(),
       "is-exported-db" -> StrArgType()
     ))(input)
 
@@ -33,6 +35,13 @@ object CourseMain {
         else if (parameterName == "exists") fileExistsOrFail(value)
         else if (parameterName == "is-exported-db") isExportedDatabaseOrFail(value)
       })
+
+      if(args.keySet.contains("problem") || args.keySet.contains("solution")) {
+        val problemFile = args.getOrElse("problem",   throw new Exception("--problem and --solution flags should be both defined or both undefined."))
+        val solutionFile = args.getOrElse("solution", throw new Exception("--problem and --solution flags should be both defined or both undefined."))
+        isSolutionOrFail(problemFile, solutionFile)
+      }
+
       System.exit(0)
     }
     catch {
@@ -40,6 +49,26 @@ object CourseMain {
         e.printStackTrace()
         System.exit(-1)
       }
+    }
+  }
+
+  private def isSolutionOrFail(problem: ArgValue, solution: ArgValue) = {
+    val f = parseProblemFileOrFail(problem)
+    val expr = parseTacticFileOrFail(solution)
+
+    val result = SequentialInterpreter(List())(expr, BelleProvable(Provable.startProof(f)))
+    result match {
+      case BelleProvable(p) => {
+        if(p.isProved) {
+          //ok
+        }
+        else {
+          println(s"Proof of ${fileExistsOrFail(problem)} using ${fileExistsOrFail(solution)} did not close. Remaining open goals follow:")
+          println(p.prettyString)
+          System.exit(-1)
+        }
+      }
+      case _ => throw new Exception("expected tactic execution to result in provable but found something else.")
     }
   }
 
@@ -56,12 +85,16 @@ object CourseMain {
   private def parseTacticFileOrFail(v: ArgValue) = {
     val fileName = fileExistsOrFail(v)
     BTacticParser(scala.io.Source.fromFile(new File(fileName)).mkString, false, Some(new NoneGenerate[Formula]())) match {
-      case Some(e) => //ok
-      case None => throw new Exception(s"Tactic in ${fileName} did not parse")
+      case Some(e) => e
+      case None => {
+        println(s"Tactic in ${fileName} did not parse")
+        System.exit(-1)
+        ???
+      }
     }
   }
 
-  private def parseProblemFileOrFail(v: ArgValue) = {
+  private def parseProblemFileOrFail(v: ArgValue) : Formula = {
     val fileName = fileExistsOrFail(v)
     try {
       edu.cmu.cs.ls.keymaerax.parser.KeYmaeraXProblemParser(scala.io.Source.fromFile(fileName).mkString)
@@ -71,10 +104,12 @@ object CourseMain {
         println(s"Auto-grader failed because file ${fileName} needs to exist and parse but failed to parse.")
         e.printStackTrace()
         System.exit(-1)
+        ???
       }
       case e : Error => {
         println(s"Unkown error encountered while parsing ${fileName}")
         System.exit(-1)
+        ???
       }
     }
   }
