603022d573f788441554d356c99c475d982b3f0d
Nathan Fulton <gitcommit@nfulton.org>
2016-02-24 11:50:39 -0500

    Fixed GUI/tactic input error in AxiomInfo

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/AxiomInfo.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/AxiomInfo.scala
@@ -127,7 +127,7 @@ object DerivationInfo {
       , /* premises */ List((List("&Gamma;"), List("[{x′ = f(x) & q(x)}]r(x)", "&Delta;")),
         (List("&Gamma;"), List("[{x′ = f(x) & (q(x) ∧ r(x))}]p(x)","&Delta;"))))
     , List(ListArg("r(x)", "formula"))
-    , {case () => (fmls: Seq[Formula]) => TactixLibrary.diffCut(fmls:_*)}),
+    , {case () => (fml: Formula) => TactixLibrary.diffCut(Seq(fml):_*)}),
     new CoreAxiomInfo("DE differential effect"
       , AxiomDisplayInfo("DE", "[{x′=f(x)&q(x)}]P↔[x′=f(x)&q(x)][x′:=f(x)]P")
       , "DE", {case () => HilbertCalculus.DE}),
@@ -597,7 +597,7 @@ object DerivationInfo {
       , /* premises */ List((List("&Gamma;"), List("[{x′ = f(x) & q(x)}]r(x)", "&Delta;"), true),
         (List("&Gamma;"), List("[{x′ = f(x) & (q(x) ∧ r(x))}]p(x)","&Delta;"))))
     , List(ListArg("r(x)", "formula"))
-    , {case () => (fmls:Seq[Formula]) => TactixLibrary.diffInvariant(fmls : _*)}),
+    , {case () => (fml:Formula) => TactixLibrary.diffInvariant(Seq(fml) : _*)}),
     new PositionTacticInfo("diffSolve",
       RuleDisplayInfo("[′]R",
         (List("&Gamma;"),List("[{x′ = f(x) & q(x)}]p(x)","&Delta;")),
