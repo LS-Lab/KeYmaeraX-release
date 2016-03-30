5cedeaa499a265e916a5be176539467f112cd8e2
Nathan Fulton <gitcommit@nfulton.org>
2016-03-30 18:20:14 -0400

    Introduced TACTIC_ARGUMENT heirarchy.

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/bellerophon/parser/BelleTerminal.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/bellerophon/parser/BelleTerminal.scala
@@ -61,8 +61,10 @@ object CLOSE_PAREN extends BelleTerminal(")") {
 }
 object COMMA extends BelleTerminal(",")
 
+trait TACTIC_ARGUMENT
+
 // Positions
-case class ABSOLUTE_POSITION(positionString: String) extends BelleTerminal(positionString) {
+case class ABSOLUTE_POSITION(positionString: String) extends BelleTerminal(positionString) with TACTIC_ARGUMENT {
   override def regexp = ABSOLUTE_POSITION.regexp
   override val startPattern = ABSOLUTE_POSITION.startPattern
   override def toString = s"ABSOLUTE_POSITION($positionString)"
@@ -71,9 +73,9 @@ object ABSOLUTE_POSITION {
   def regexp = """(-?\d+(?:\.\d+)*)""".r
   val startPattern: Regex = ("^" + regexp.pattern.pattern + "[\\s\\S]*").r
 }
-object SEARCH_SUCCEDENT extends BelleTerminal("'R")
-object SEARCH_ANTECEDENT extends BelleTerminal("'L")
-object SEARCH_EVERYWHERE extends BelleTerminal("'-") {
+object SEARCH_SUCCEDENT extends BelleTerminal("'R") with TACTIC_ARGUMENT
+object SEARCH_ANTECEDENT extends BelleTerminal("'L") with TACTIC_ARGUMENT
+object SEARCH_EVERYWHERE extends BelleTerminal("'-") with TACTIC_ARGUMENT {
   override def regexp = "'\\-".r
 }
 
@@ -82,7 +84,7 @@ object PARTIAL extends BelleTerminal("partial") {
 }
 
 /** A dL expression. We allow both terms and formulas as arguments; e.g. in diffGhost. */
-case class EXPRESSION(exprString: String) extends BelleTerminal(exprString) {
+case class EXPRESSION(exprString: String) extends BelleTerminal(exprString) with TACTIC_ARGUMENT {
   val expression: Expression = {
     assert(exprString.startsWith("{`") && exprString.endsWith("`}"),
       s"EXPRESSION.regexp should ensure delimited expression begin and end with {` `}, but an EXPRESSION was constructed with argument: ${exprString}")
