ec90a25d48040f0464174578d06cd3fa8323d1c6
Ran Ji <ran.ji@me.com>
2016-03-31 15:18:58 -0400

    Supports Z3 and Polya as QE tools for testing

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/Z3.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/Z3.scala
@@ -4,24 +4,42 @@
 */
 package edu.cmu.cs.ls.keymaerax.tools
 
-import edu.cmu.cs.ls.keymaerax.core.{Evidence, Formula, QETool}
+import edu.cmu.cs.ls.keymaerax.core._
+
+import scala.collection.immutable.Map
 
 /**
  * Z3 quantifier elimination tool.
+ * Still need Mathematica to do diffSolve and CounterExample
  *
  * Created by smitsch on 4/27/15.
  * @author Ran Ji
  * @author Stefan Mitsch
  */
-class Z3 extends ToolBase("Z3") with QETool {
+class Z3 extends ToolBase("Z3") with QETool with DiffSolutionTool with CounterExampleTool {
   private val z3 = new Z3Solver
+  private val jlink = new JLinkMathematicaLink
 
-  def init(config : Map[String,String]) = {initialized = true}
+  override def init(config: Map[String,String]) = {
+    val linkName = config.get("linkName") match {
+      case Some(l) => l
+      case None => throw new IllegalArgumentException("Mathematica not configured. Configure Mathematica and restart KeYmaera X.\nMissing configuration parameter 'linkName'\n")
+      //        "You should configure settings in the UI and restart KeYmaera X." +
+      //        "Or specify the paths explicitly from command line by running\n" +
+      //        "  java -jar keymaerax.jar -mathkernel pathtokernel -jlink pathtojlink")
+    }
+    val libDir = config.get("libDir") // doesn't need to be defined
+    initialized = jlink.init(linkName, libDir)
+  }
 
   def qe(formula: Formula): Formula = z3.qe(formula)
   override def qeEvidence(formula: Formula): (Formula, Evidence) = z3.qeEvidence(formula)
 
-  override def restart() = {}
+  override def diffSol(diffSys: DifferentialProgram, diffArg: Variable,
+                       iv: Predef.Map[Variable, Variable]): Option[Formula] = jlink.diffSol(diffSys, diffArg, iv)
+  override def findCounterExample(formula: Formula): Option[Predef.Map[NamedSymbol, Term]] = jlink.findCounterExample(formula)
 
-  override def shutdown() = {}
+  override def restart() = ???
+
+  override def shutdown() = jlink.shutdown()
 }
