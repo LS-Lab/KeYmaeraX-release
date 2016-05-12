89e39541f92811f17416036f387d144c9d7fd112
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-05-12 14:13:08 -0400

    Introduces test tag IgnoreInBuildTest for tests that should be ignored in automated builds

--- keymaerax-webui/src/test/scala/testHelper/TestTags.scala
+++ keymaerax-webui/src/test/scala/testHelper/TestTags.scala
@@ -65,4 +65,7 @@ object KeYmaeraXTestTags {
 
   /** Tests that are obsolete, e.g. from old tactic framework. */
   object ObsoleteTest extends Tag("edu.cmu.cs.ls.keymaerax.tags.ObsoleteTest")
+
+  /** Tests that should be ignored in an automated build. */
+  object IgnoreInBuildTest extends Tag("edu.cmu.cs.ls.keymaerax.tags.IgnoreInBuildTest")
 }
