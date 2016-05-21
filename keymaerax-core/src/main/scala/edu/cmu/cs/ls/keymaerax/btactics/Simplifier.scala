72f6bfc51ffd37b77b83939d06944bf0a2eb9183
Brandon Bohrer <bbohrer@andrew.cmu.edu>
2016-05-21 15:38:21 -0400

    Simplify less-trivial tactics

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/Simplifier.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/Simplifier.scala
@@ -20,16 +20,167 @@ object Simplifier {
 
   val timesZeroLeft:Simplification = {
     case Times(n: Number, t: Term) =>
-      if (n.value == 0) {
-        Some((n, QE))
-      } else {
-        None
-      }
+      if (n.value == 0) {Some((n, QE))} else None
     case _ => None
   }
 
-  val simps: List[Simplification] = List(
-    timesZeroLeft
+  val timesZeroRight:Simplification = {
+    case Times(t: Term, n: Number) =>
+      if (n.value == 0) {Some((n, QE))} else None
+    case _ => None
+  }
+
+  val timesOneLeft:Simplification = {
+    case Times(n: Number, t: Term) =>
+      if (n.value == 1) {Some((t, QE))} else None
+    case _ => None
+  }
+
+  val timesOneRight:Simplification = {
+    case Times(t: Term, n: Number) =>
+      if (n.value == 1) {Some((t, QE))} else None
+    case _ => None
+  }
+
+  val plusZeroRight:Simplification = {
+    case Plus(t: Term, n: Number) =>
+      if (n.value == 0) {Some((t, QE))} else None
+    case _ => None
+  }
+
+  val plusZeroLeft:Simplification = {
+    case Plus(n: Number, t: Term) =>
+      if (n.value == 0) {Some((t, QE))} else None
+    case _ => None
+  }
+
+  val powZero:Simplification = {
+    case Power(t:Term, n:Number) =>
+      if (n.value == 0) {Some (Number(1), QE)} else None
+    case _ => None
+  }
+
+  val powOne:Simplification = {
+    case Power(t:Term, n:Number) =>
+      if (n.value == 1) {Some (t, QE)} else None
+    case _ => None
+  }
+
+  val divOne:Simplification = {
+    case Divide(t:Term, n:Number) =>
+      if (n.value == 1) {Some (t, QE)} else None
+    case _ => None
+  }
+
+  val divCancel:Simplification = {
+    case Divide(t1:Term, t2:Term) =>
+      if (t1 == t2) {Some (Number(1), QE)} else None
+    case _ => None
+  }
+
+  val minusCancel:Simplification = {
+    case Minus(t1:Term, t2:Term) =>
+      if(t1 == t2) {Some ((Number(0), QE))} else None
+    case _ => None
+  }
+
+  val plusConst:Simplification = {
+    case Plus(n: Number, m:Number) => Some ((Number(n.value + m.value), QE))
+    case _ => None
+  }
+
+  val minusConst:Simplification = {
+    case Minus(n: Number, m:Number) => Some ((Number(n.value - m.value), QE))
+    case _ => None
+  }
+
+  val negConst:Simplification = {
+    case Neg(n: Number) => Some ((Number(-n.value), QE))
+    case _ => None
+  }
+
+  val timesConst:Simplification = {
+    case Times(n: Number, m:Number) => Some ((Number(n.value * m.value), QE))
+    case _ => None
+  }
+
+  val divConst:Simplification = {
+    case Divide(n: Number, m:Number) if m.value == 0 => None
+    case Divide(n: Number, m:Number) => Some ((Number(n.value / m.value), QE))
+    case _ => None
+  }
+
+  val powConst:Simplification = {
+    case Power(n: Number, m:Number) => Some ((Number(n.value.pow(m.value.toIntExact)), QE))
+    case _ => None
+  }
+
+  val assocPlus:Simplification = {
+    case Plus(Plus(a:Term, b:Term), c:Term) => print("\n\nputtin assoc in (" + a +"+" + b+") +"+c+"\n\n");Some(Plus(a, Plus(b,c)), QE)
+    case _ => None
+  }
+
+  val assocTimes:Simplification = {
+    case Times(Times(a:Term, b:Term), c:Term) => Some(Times(a, Times(b,c)), QE)
+    case _ => None
+  }
+
+  val pushConstPlus:Simplification = {
+    case Plus(n: Number, Plus(t1:Term, t2:Term)) =>
+      if (!t1.isInstanceOf[Number] && !t1.isInstanceOf[Plus]) {print("\n\npushin down " + n + " vs " + t1+"\n\n"); Some(Plus(t1, Plus(n, t2)), QE)} else None
+    case _ => None
+  }
+
+  val flipConstPlus:Simplification = {
+    case Plus(n: Number, t1:Term) =>
+      if (!t1.isInstanceOf[Number] && !t1.isInstanceOf[Plus]) {print("\n\nflippin "+ t1 + " wit " + n+"\n\n"); Some(Plus(t1, n), QE)} else None
+    case _ => None
+  }
+
+  val flipConstTimes:Simplification = {
+    case Times(n: Number, t1:Term) =>
+      if (!t1.isInstanceOf[Number] && !t1.isInstanceOf[Times]) {Some(Times(t1, n), QE)} else None
+    case _ => None
+  }
+
+  val pushConstTimes:Simplification = {
+    case Times(n: Number, Times(t1:Term, t2:Term)) =>
+      if (!t1.isInstanceOf[Number] && !t1.isInstanceOf[Times]) {Some(Times(t1, Times(n, t2)), QE)} else None
+    case _ => None
+  }
+
+  /* By default, only use simplifications that obviously terminate */
+  val defaultSimps: List[Simplification] = List(
+    timesZeroLeft,
+    timesZeroRight,
+    timesOneLeft,
+    timesOneRight,
+    plusZeroRight,
+    plusZeroLeft,
+    plusConst,
+    minusConst,
+    minusCancel,
+    negConst,
+    timesConst,
+    divConst,
+    divOne,
+    divCancel,
+    powConst,
+    powOne,
+    powZero
+  )
+
+  /* When asked, normalize terms by flattening trees of * or + into lists and then
+   * pushing all constants toward the end of the list. This enables a more powerful
+   * form of constant folding / cancellation */
+  val extendedSimps: List[Simplification] =
+    defaultSimps ++ List (
+      assocPlus,
+      assocTimes,
+      pushConstPlus,
+      flipConstPlus,
+      pushConstTimes,
+      flipConstTimes
     )
 
   def trav(simps:List[Simplification]) = new ExpressionTraversalFunction {
@@ -75,11 +226,12 @@ object Simplifier {
     }
   }
 
-  def simp(simps:List[Simplification]= simps):DependentPositionTactic = "diffWeaken" by ((pos, sequent) => sequent.sub(pos) match {
+  def simpOnce(simps:List[Simplification]= defaultSimps):DependentPositionTactic = "simpOnce" by ((pos, sequent) => sequent.sub(pos) match {
     case Some(fml : Formula) => makeCE(fml, simp(simps, fml), pos)
-    /*case Some(fml : Term)    => makeCE(fml, simp(simps, fml))
-    case Some(fml : Program) => makeCE(simp(simps, fml))*/
     case None => TactixLibrary.nil
   })
+
+  def simp(simps:List[Simplification] = defaultSimps):DependentPositionTactic = "simp" by ((pos, sequent) =>
+    simpOnce(simps)(pos) *@ TheType ())
 }
 
--- keymaerax-webui/src/test/scala/btactics/SimplifierTests.scala
+++ keymaerax-webui/src/test/scala/btactics/SimplifierTests.scala
@@ -27,4 +27,25 @@ class SimplifierTests extends TacticTestBase {
       Sequent(Nil, IndexedSeq(), IndexedSeq("0 = x".asFormula))
   }
 
+  it should "fold constants" in withMathematica { implicit qeTool =>
+    val fml = "(((2 * (3 + (-(1))))/2)^3)-1 = x".asFormula
+    val tactic = Simplifier.simp()(1) & debug("After simping", true)
+    proveBy(fml, tactic).subgoals should contain only
+      Sequent(Nil, IndexedSeq(), IndexedSeq("7 = x".asFormula))
+  }
+
+  it should "simp deep inside a formula" in withMathematica{ implicit qeTool =>
+    val fml = "(p() -> (q() & (x + 0) >= y * (9 * 3))) & r() ".asFormula
+    val tactic = Simplifier.simp()(1) & debug("After simping", true)
+    proveBy(fml, tactic).subgoals should contain only
+      Sequent(Nil, IndexedSeq(), IndexedSeq("(p() -> (q() & x >= y * 27)) & r() ".asFormula))
+  }
+
+  it should "handle association and inconveniently-placed addition" in withMathematica { implicit qeTool =>
+    val fml = "x = (((x + 1) + (2 + x))  +  ((x + 3) + (4 + x)))".asFormula
+    val tactic = Simplifier.simp(Simplifier.extendedSimps)(1)
+    proveBy(fml, tactic).subgoals should contain only
+      Sequent(Nil, IndexedSeq(), IndexedSeq("x = x + (x + (x + (x + 10)))".asFormula))
+  }
+
 }
