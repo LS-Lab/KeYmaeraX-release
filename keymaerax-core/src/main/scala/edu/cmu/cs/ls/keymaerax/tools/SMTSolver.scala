de776e9f2d2c2ce53f1c4a0a2d5ab445e4b8f6cb
Andre Platzer <aplatzer@cs.cmu.edu>
2016-06-02 19:34:39 -0400

    Update code review document a little bit. Forgot to stamp @note Code Review: 2016-06-01 during extended code review.

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/SMTSolver.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/SMTSolver.scala
@@ -2,6 +2,9 @@
 * Copyright (c) Carnegie Mellon University.
 * See LICENSE.txt for the conditions of this license.
 */
+/**
+  * @note Code Review: 2016-06-01
+  */
 package edu.cmu.cs.ls.keymaerax.tools
 
 import java.io.File
