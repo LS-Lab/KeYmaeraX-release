09424d1d19997dfef4cc3af282532b4e4f245a11
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-06-25 12:21:24 -0400

    Moves license request into own controller (dashboard is per user)

--- keymaerax-webui/src/main/resources/js/controllers/dashboard.js
+++ keymaerax-webui/src/main/resources/js/controllers/dashboard.js
@@ -62,22 +62,6 @@ angular.module('keymaerax.controllers').controller('DashboardCtrl', ['$scope', '
           }
        });
 
-  $http.get("/keymaeraXVersion")
-      .success(function(data) {
-          if(data.errorThrown) showCaughtErrorMessage($uibModal, data, "Could not get the server's KeYmaera X version")
-          else  {
-              $scope.keymaeraXVersion = data.keymaeraXVersion
-              if(data.upToDate != null) {
-                  $scope.versionInfoAvailable = true
-                  $scope.upToDate = data.upToDate
-                  $scope.latestVersion = data.latestVersion
-              }
-              else {
-                  $scope.versionInfoAvailable = false
-              }
-          }
-      });
-
   $scope.mathematicaIsConfigured = true;
   $http.get("/config/mathematicaStatus")
       .success(function(data) {
