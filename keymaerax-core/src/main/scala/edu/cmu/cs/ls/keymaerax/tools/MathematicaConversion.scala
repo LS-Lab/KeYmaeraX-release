23f659e4caec2e2ce0fb9e4fdcc9692c5b789b20
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-06-30 13:09:57 -0400

    Mathematica expression converter for equality despite conversion differences

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/MathematicaConversion.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/MathematicaConversion.scala
@@ -5,9 +5,11 @@
 package edu.cmu.cs.ls.keymaerax.tools
 
 import edu.cmu.cs.ls.keymaerax.tools.MathematicaConversion.MExpr
+import KMComparator._
 
 /**
   * Conversion types.
+  *
   * @author Stefan Mitsch
   */
 
@@ -31,7 +33,7 @@ trait M2KConverter[T] extends (MExpr => T) {
 }
 
 trait BaseM2KConverter[T] extends M2KConverter[T] {
-  def apply(e: MExpr): T = convert(e) ensuring(r => k2m.convert(r) == e, "Roundtrip conversion is identity." +
+  def apply(e: MExpr): T = convert(e) ensuring(r => k2m.convert(r) === e, "Roundtrip conversion is identity." +
     "\nMathematica expression:   " + e.toString + "\t@[" + e.args.map(_.head()).mkString(", ") + "]" +
     "\nConverted to KeYmaera X:  " + convert(e) + "\t@" + convert(e).getClass.getSimpleName +
     "\nRoundtrip to Mathematica: " + k2m.convert(convert(e)).toString + "\t@[" + k2m.convert(convert(e)).args.map(_.head()).mkString(", ") + "]")
@@ -53,4 +55,35 @@ trait BaseK2MConverter[T] extends K2MConverter[T] {
     "\nKeYmaera X expression    " + e + "\t@" + e.getClass.getSimpleName +
     "\nConverted to Mathematica " + convert(e).toString +
     "\nRoundtrip to KeYmaera X  " + m2k.convert(convert(e)) + "\t@" + m2k.convert(convert(e)).getClass.getSimpleName)
+}
+
+/** Implicit conversion from Mathematica expressions to comparator. */
+object KMComparator {
+  /**
+    * Whether e is thing or starts with head thing.
+    * @return true if ``e" and ``thing" are .equals-related.
+    */
+  def hasHead(e: MExpr, thing: MExpr) = e.equals(thing) || e.head().equals(thing)
+
+  import scala.language.implicitConversions
+  implicit def MExprToKMComparator(e: MExpr): KMComparator = new KMComparator(e)
+}
+/** Compares Mathematica expressions for equality (handles conversion differences). */
+class KMComparator(val l: MExpr) {
+  import KMComparator.hasHead
+
+  /** Equality of Mathematica expressions */
+  def ===(r: MExpr): Boolean = l == r || (
+    if (hasHead(l, MathematicaSymbols.INEQUALITY)) {
+      def checkInequality(l: Array[MExpr], r: MExpr): Boolean = hasHead(r, l(1)) && r.args.length == 2 && r.args().head == l(0) && r.args().last == l(2)
+      def checkInequalities(l: Array[MExpr], r: MExpr): Boolean = {
+        require(l.length % 2 == 1 && r.args().length % 2 == 0, "Expected pairs of expressions separated by operators")
+        if (l.length <= 3) checkInequality(l, r)
+        // And[c[a,b], ...] == {a c b ... }
+        else hasHead(r, MathematicaSymbols.AND) && checkInequality(l, r.args().head) && checkInequalities(l.tail.tail, r.args().last)
+      }
+      checkInequalities(l.args(), r)
+    } else if (hasHead(r, MathematicaSymbols.INEQUALITY)) {
+      new KMComparator(r).===(l)
+    } else false)
 }
