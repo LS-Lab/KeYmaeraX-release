0e36d47f89cdc2c69961b739f03429b46b362934
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-06-30 17:57:28 -0400

    Fixes Lemma assertion bug: KeYmaeraXLexer modifies evidence when reparsing

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/ToolEvidence.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/ToolEvidence.scala
@@ -1,6 +1,7 @@
 package edu.cmu.cs.ls.keymaerax.tools
 
 import edu.cmu.cs.ls.keymaerax.core.Evidence
+import edu.cmu.cs.ls.keymaerax.parser.KeYmaeraXLexer
 
 // require favoring immutable Seqs for unmodifiable Lemma evidence
 
@@ -9,7 +10,18 @@ import scala.collection.immutable
 /**
  * Created by aplatzer on 8/24/15.
  */
-case class ToolEvidence(info : immutable.List[(String,String)]) extends Evidence {
+case class ToolEvidence(info: immutable.List[(String,String)]) extends Evidence {
   override def toString: String =
     "Tool.\n  " + info.map(entry => entry._1 + " \"\"\"\"" + entry._2 + "\"\"\"\"").mkString("\n  ") + "\nEnd."
+
+  /** Equality modulo CRLF == LF in the evidence values (Lexer normalizes, so parsed evidence disagrees with in-memory evidence) */
+  override def equals(e: Any): Boolean = e match {
+    case other: ToolEvidence => info.length == other.info.length && info.zip(other.info).forall {
+      case ((k1,v1), (k2,v2)) => k1 == k2 && KeYmaeraXLexer.normalizeNewlines(v1) == KeYmaeraXLexer.normalizeNewlines(v2)
+      case _ => false
+    }
+    case _ => false
+  }
+
+  override def hashCode: Int = info.map({ case (k,v) => (k, KeYmaeraXLexer.normalizeNewlines(v))}).hashCode()
 }
