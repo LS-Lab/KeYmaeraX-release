b7a9cfc660936a08bf8bb1fa71187957b1fe6b14
Nathan Fulton <gitcommit@nfulton.org>
2016-07-19 12:58:40 -0400

    cleaned up axiomatic ode solver tests

--- keymaerax-webui/src/test/scala/btactics/AxiomaticODESolverTests.scala
+++ keymaerax-webui/src/test/scala/btactics/AxiomaticODESolverTests.scala
@@ -20,24 +20,13 @@ class AxiomaticODESolverTests extends TacticTestBase {
   import Augmentors._
   import TacticFactory._
 
-  //region stand-along integrator
-
-  "Integrator.apply" should "world on x'=v, v'=a" in {
-    val initialConds = conditionsToValues(extractInitialConditions(None)("x=1&v=2&a=3&t=0".asFormula))
-    val system = "{x'=v,v'=a, t'=1}".asProgram.asInstanceOf[ODESystem]
-    val result = Integrator.apply(initialConds, system)
-    println(result.mkString(","))
-  }
-  //endregion
-
-
   //region integration tests
 
   "axiomatic ode solver" should "work!" in withMathematica(implicit qeTool => {
     val f = "x=1&v=2&a=0 -> [{x'=v,v'=a}]x^3>=1".asFormula
     val t = TactixLibrary.implyR(1) & apply(qeTool)(1)
     val result = proveBy(f, t)
-    println(result.prettyString)
+    loneSucc(result) shouldBe "\\forall t_ (t_>=0->\\forall s_ (0<=s_&s_<=t_->true)->[kyxtime:=kyxtime+1*t_;](a/2*kyxtime^2+2*kyxtime+1)^3>=1)".asFormula
   })
 
   it should "work using my own time" in withMathematica(implicit qeTool => {
@@ -46,6 +35,13 @@ class AxiomaticODESolverTests extends TacticTestBase {
     val result = proveBy(f, t)
     loneSucc(result) shouldBe "\\forall t_ (t_>=0->\\forall s_ (0<=s_&s_<=t_->true)->[t:=t+1*t_;](a/2*t^2+2*t+1)^3>=1)".asFormula
   })
+
+
+  "axiomatic ode solver" should "solve double integrator" in {withMathematica(implicit qeTool => {
+    val f = "x=1&v=2&a=3&t=0 -> [{x'=v,v'=a, t'=1}]x>=0".asFormula
+    val t = TactixLibrary.implyR(1) & AxiomaticODESolver(qeTool)(1)
+    loneSucc(proveBy(f,t)) shouldBe "\\forall t_ (t_>=0->\\forall s_ (0<=s_&s_<=t_->true)->[t:=t+1*t_;]a/2*t^2+2*t+1>=0))".asFormula
+  })}
 
   //endregion
 
@@ -57,7 +53,7 @@ class AxiomaticODESolverTests extends TacticTestBase {
     val system = "[{x'=v}]1=1".asFormula
     val tactic = addTimeVarIfNecessary
     val result = proveBy(system, tactic(SuccPosition(1, 0::Nil)))
-    loneSucc(result) shouldBe "[{x'=v,kyxtime'=0*kyxtime+1&true}]1=1".asFormula
+    loneSucc(result) shouldBe "[{x'=v,kyxtime'=1&true}]1=1".asFormula
   }
 
   "cutInSolns" should "solve x'=y,t'=1" in {withMathematica(implicit qeTool => {
@@ -66,32 +62,15 @@ class AxiomaticODESolverTests extends TacticTestBase {
     loneSucc(proveBy(f,t)) shouldBe "[{x'=y,t'=1&true&x=y*t+0}]x>=0".asFormula
   })}
 
-  //@todo fix this -- we need to produce 1*t+0 as the recurrence for x. Also, rename recurrence!
+  //@todo Does this need to be fixed?
   it should "solve single time dependent eqn" ignore {withMathematica(implicit qeTool => {
     val f = "x=0&y=0&t=0 -> [{x'=t, t'=1}]x>=0".asFormula
     val t = TactixLibrary.implyR(1) &  AxiomaticODESolver.cutInSoln(qeTool)(1).*@(TheType())
     println(proveBy(f,t))
-//    loneSucc(proveBy(f,t)) shouldBe "[{x'=y,t'=1&true&x=0*t+0}]x>=0".asFormula
-  })}
-
-  //@todo I think we need to actually saturate cutInSolns because otherwise we don't include enough domain constraint stuff???
-  it should "solve double integrator" in {withMathematica(implicit qeTool => {
-    val f = "x=1&v=2&a=3&t=0 -> [{x'=v,v'=a, t'=1}]x>=0".asFormula
-    val t =
-      TactixLibrary.implyR(1) &
-      AxiomaticODESolver.cutInSoln(qeTool)(1) &
-      AxiomaticODESolver.cutInSoln(qeTool)(1)
-    println(proveBy(f,t))
+    loneSucc(proveBy(f,t)) shouldBe ???
   })}
 
   //endregion
-
-  "axiomatic ode solver" should "solve double integrator" in {withMathematica(implicit qeTool => {
-    val f = "x=1&v=2&a=3&t=0 -> [{x'=v,v'=a, t'=1}]x>=0".asFormula
-    val t = TactixLibrary.implyR(1) & AxiomaticODESolver(qeTool)(1)
-    println(proveBy(f,t))
-  })}
-
 
   //region proveAs construct
 
@@ -104,49 +83,20 @@ class AxiomaticODESolverTests extends TacticTestBase {
   it should "work in simplifyPostCondition" in {withMathematica(implicit qeTool => {
     val f = "[{x'=1}](x=22 -> x>0)".asFormula
     val t = simplifyPostCondition(qeTool)(1)
-    proveBy(f,t) shouldBe "[{x'=1&true}]22>0".asFormula
+    loneSucc(proveBy(f,t)) shouldBe "[{x'=1&true}]22>0".asFormula
   })}
 
   //endregion
 
+  //region stand-alone integrator
 
-  //region inverse diff cut/ghost missing around.
-
-  "inverse diff cut" should "work" in { withMathematica(implicit qeTool => {
-//    val fwd = "blah" by ((p: Provable, s: SuccPosition) => {
-//      HilbertCalculus.useFor("DC differential cut")(s)(p)
-//    })
-
-    val f = "v=0&a=0&x=0&t=0 -> [{x'=v, v'=a, t'=1 &true&v=0*t+0}]x>=0".asFormula
-    val t = TactixLibrary.implyR(1) & inverseDiffCut(qeTool)(1)
-
-    println(proveBy(f,t).prettyString)
-  })}
-
-
-  "inverse diff ghost" should "work" in { withMathematica(implicit qeTool => {
-    //    val fwd = "blah" by ((p: Provable, s: SuccPosition) => {
-    //      HilbertCalculus.useFor("DC differential cut")(s)(p)
-    //    })
-
-    val f = "v=0&a=0&x=0&t=0 -> [{x'=v, v'=a, t'=1 &true}]x>=0".asFormula
-    val t = TactixLibrary.implyR(1) & inverseDiffGhost(qeTool)(1) & inverseDiffGhost(qeTool)(1)
-
-    println(proveBy(f,t).prettyString)
-  })}
-
-  "cq" should "work for time thing" in { withMathematica(implicit qeTool => {
-    val f = "[{kyxtime'=1&true}](a/2*kyxtime^2+2*kyxtime+1)^3>=1 <-> [{kyxtime'=0*kyxtime+1&true}](a/2*kyxtime^2+2*kyxtime+1)^3>=1".asFormula
-    val t = HilbertCalculus.CQ(PosInExpr(0::0::1::Nil))
-    println(proveBy(f,t).prettyString)
-  })}
-
-  it should "work at all" in { withMathematica(implicit qeTool => {
-    val f = "[{x'=1 & 1=1}]2=2 <-> [{x'=1 & 0+1=1}]2=2".asFormula
-    val t = HilbertCalculus.CQ(PosInExpr(0::1::0::Nil))
-    println(proveBy(f,t).prettyString)
-  })}
-
+  "Integrator.apply" should "world on x'=v, v'=a" in {
+    val initialConds = conditionsToValues(extractInitialConditions(None)("x=1&v=2&a=3&t=0".asFormula))
+    val system = "{x'=v,v'=a, t'=1}".asProgram.asInstanceOf[ODESystem]
+    val result = Integrator.apply(initialConds, system)
+    println(result.mkString(","))
+    result shouldBe "x=a/2*t^2+2*t+1".asFormula :: "v=a*t+2".asFormula :: Nil
+  }
   //endregion
 
 }
