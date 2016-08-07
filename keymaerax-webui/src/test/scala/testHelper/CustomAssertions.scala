8e7179013ac86978b78bc05a19348ab299a530e8
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-08-07 12:02:31 -0400

    Test support for throwOrNoop

--- keymaerax-webui/src/test/scala/testHelper/CustomAssertions.scala
+++ keymaerax-webui/src/test/scala/testHelper/CustomAssertions.scala
@@ -5,7 +5,9 @@
 
 package testHelper
 
-import org.scalatest.Assertions.{fail,withClue}
+import edu.cmu.cs.ls.keymaerax.core.Provable
+import org.scalatest.Assertions.{fail, withClue}
+import org.scalatest.matchers.{MatchResult, Matcher}
 
 /** Custom test assertions.
   * Created by smitsch on 5/21/16.
@@ -16,5 +18,21 @@ object CustomAssertions {
     * */
   def withSafeClue[T](clue: Any)(fun: => T): T = {
     try { fun } catch { case e: Any => withClue(clue) { fail(e) } }
+  }
+
+  /** Turns exceptions into 'None', most useful with Matcher throwOrNoop
+    * {{{theDeductionOf { proveBy(formula, tactic) } should throwOrNoop }}}*/
+  def theDeductionOf[T <: Provable](f: => T): Option[T] = try { Some(f) } catch { case e: Throwable => None }
+
+  /** Checks that a provable, if present, has a sole subgoal equal to its conclusion. */
+  val throwOrNoop = Matcher {
+    (pr: Option[Provable]) => MatchResult(
+      pr match {
+        case Some(p) => p.subgoals.size == 1 && p.subgoals.head == p.conclusion
+        case None => true
+      },
+      pr + " is noop",
+      pr + " is not noop"
+    )
   }
 }
