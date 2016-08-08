ae80915980d3b152d6e2741848de84ac0f9f7ecc
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-08-08 09:33:01 -0400

    Axiom file parser test

--- keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/parser/AxiomFileParser.scala
+++ keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/parser/AxiomFileParser.scala
@@ -4,7 +4,7 @@
 */
 package edu.cmu.cs.ls.keymaerax.parser
 
-import edu.cmu.cs.ls.keymaerax.parser.{KeYmaeraXLexer, KeYmaeraXAxiomParser}
+import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
 import edu.cmu.cs.ls.keymaerax.tags.CheckinTest
 import org.scalatest.{PrivateMethodTester, Matchers, FlatSpec}
 
@@ -17,9 +17,12 @@ class AxiomFileParser extends FlatSpec with Matchers with PrivateMethodTester {
   val loadAxiomString = PrivateMethod[String]('loadAxiomString)
 
   "The AxiomFileParser" should "parse the axiom file" in {
-    // even AxiomBase is private[core], so get Class by reflection
-    val clazz = Class.forName("edu.cmu.cs.ls.keymaerax.core.AxiomBase")
-    val axiomFile = clazz.getField("$MODULE").get() invokePrivate loadAxiomString()
-    KeYmaeraXAxiomParser(axiomFile)
+    // even AxiomBase is private[core], so get Class by Java reflection
+    val clazz = Class.forName("edu.cmu.cs.ls.keymaerax.core.AxiomBase$")
+    val axiomFile = clazz.getField("MODULE$").get() invokePrivate loadAxiomString()
+    val axioms = KeYmaeraXAxiomParser(axiomFile)
+    axioms.size shouldNot be <= 0
+    // check for a sample
+    axioms should contain ("<> diamond", "![a;]!p(||) <-> <a;>p(||)".asFormula)
   }
 }
