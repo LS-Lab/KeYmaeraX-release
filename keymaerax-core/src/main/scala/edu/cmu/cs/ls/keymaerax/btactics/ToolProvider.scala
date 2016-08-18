5b4f97875fea6e837d7341f8f067f27781ce73a8
Andre Platzer <aplatzer@cs.cmu.edu>
2016-08-18 09:55:48 -0400

    Documentation

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/ToolProvider.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/ToolProvider.scala
@@ -9,14 +9,17 @@ import edu.cmu.cs.ls.keymaerax.core.QETool
 import edu.cmu.cs.ls.keymaerax.tools._
 
 
-/** Provides arithmetic tools.
+/** Central repository providing access to arithmetic tools.
   * @note Never keep references to the tools, the tool provider may decide to shutdown/switch out tools and thereby
   *       render all tool references invalid.
   * @note Do especially not keep references in singletons, the tool provider will hand out nulls until properly
   *       initialized.
-  * */
+  * @author Stefan Mitsch
+  * @see [[edu.cmu.cs.ls.keymaerax.tools]]
+  */
 object ToolProvider {
 
+  /** Configuration options for tools. */
   type Configuration = Map[String, String]
 
   /* @note mutable state for switching out default tool providers, which defaults to just returning null */
@@ -33,16 +36,16 @@ object ToolProvider {
 
   // convenience methods forwarding to the current factory
 
-  /** Returns a QE tool. */
+  /** Returns a quantifier elimination tool for real arithmetic. */
   def qeTool(): QETool = f.qeTool()
 
-  /** Returns an ODE tool. */
+  /** Returns a differential equation solving helper tool. */
   def odeTool(): DiffSolutionTool = f.odeTool()
 
-  /** Returns a counterexample tool. */
+  /** Returns a counterexample finding helper tool. */
   def cexTool(): CounterExampleTool = f.cexTool()
 
-  /** Returns a simulation tool. */
+  /** Returns a simulation helper tool. */
   def simulationTool(): SimulationTool = f.simulationTool()
 
   /** Shutdown the tools provided by the current provider. After shutdown, the provider hands out null only. */
@@ -50,7 +53,9 @@ object ToolProvider {
 
 }
 
-/** A tool factory creates various tools. */
+/** A tool factory creates various tools.
+  * @author Stefan Mitsch
+  */
 trait ToolProvider {
   /** The provided tools. */
   def tools(): List[Tool]
@@ -71,7 +76,9 @@ trait ToolProvider {
   def shutdown(): Unit
 }
 
-/** A tool provider that points all tools to the specified `tool` */
+/** A tool provider that points all tools to the specified `tool`
+  * @author Stefan Mitsch
+  */
 class SingleToolProvider[T <: Tool](val tool: T) extends ToolProvider {
   require(tool != null && tool.isInitialized, "Initialized tool expected")
   private[this] var theTool: T = tool
@@ -89,7 +96,9 @@ class SingleToolProvider[T <: Tool](val tool: T) extends ToolProvider {
   }
 }
 
-/** A tool provider that initializes tools to null. */
+/** A tool provider that initializes tools to null.
+  * @author Stefan Mitsch
+  */
 class NullToolProvider extends ToolProvider {
   override def tools(): List[Tool] = Nil
   override def qeTool(): QETool = null
@@ -99,11 +108,18 @@ class NullToolProvider extends ToolProvider {
   override def shutdown(): Unit = {}
 }
 
-/** A tool provider that initializes QE tools to Polya, everything else to null. */
+/** A tool provider that initializes QE tools to Polya, everything else to null.
+  * @author Stefan Mitsch
+  */
 class PolyaToolProvider extends SingleToolProvider({ val p = new Polya; p.init(Map()); p }) { }
 
-/** A tool provider that initializes tools to Mathematica. */
+/** A tool provider that initializes tools to Mathematica.
+  * @param config
+  * @author Stefan Mitsch
+  */
 class MathematicaToolProvider(config: Configuration) extends SingleToolProvider({ val m = new Mathematica(); m.init(config); m }) { }
 
-/** A tool provider that initializes QE tools to Z3, everything else to null. */
+/** A tool provider that initializes QE tools to Z3, everything else to null.
+  * @author Stefan Mitsch
+  */
 class Z3ToolProvider extends SingleToolProvider({ val z = new Z3; z.init(Map()); z }) { }
