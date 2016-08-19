aacc47aa8e7ff3e8874b654479def55266b5f0ba
Nathan Fulton <gitcommit@nfulton.org>
2016-08-19 13:57:34 -0400

    Makes Integrator an ODESolutionTool (glue code is untested).

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/Integrator.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/Integrator.scala
@@ -15,6 +15,12 @@ import StaticSemantics.freeVars
   * @author Nathan Fulton
   */
 object Integrator {
+  /**
+    * Integrates the differential equation and returns the solution as a list of equalities for each of the primed variables occuring in the system.
+    * @param initialValues Initial conditions for each of the variables that occur primed in the ODE.
+    * @param system The ODE system. @todo this could be a DifferentialProgram instead because we never use the contraint.
+    * @return The solution as a list of equalities, one for each of the primed variables.
+    */
   def apply(initialValues: Map[Variable, Term], system: ODESystem): List[Equal] = {
     val sortedOdes = sortAtomicOdes(atomicOdes(system))
     val primedVars = sortedOdes.map(ode => ode.xp.x)
@@ -37,9 +43,28 @@ object Integrator {
     })
   }
 
+  /**
+    * Glue code that implements the [[edu.cmu.cs.ls.keymaerax.tools.DiffSolutionTool]] interface using the Integrator.
+    * @todo untested
+    */
+  def diffSol(diffSys: DifferentialProgram, diffArg: Variable, iv: Map[Variable, Variable]): Option[Formula] = {
+    //Ensure that diffArg is what the Integrator will consider to be the independent variable.
+    if(timeVar(diffSys) != diffArg)
+      throw new AxiomaticODESolver.AxiomaticODESolverExn(s"Expected ${diffArg.prettyString} to be the time variable of the system ${diffSys.prettyString}")
+
+    apply(iv, ODESystem(diffSys)).foldLeft[Formula](True)((fml, eqn) => And(fml, eqn)) match {
+      case True => None
+      case And(l,r) => {
+        //throw away the initial True
+        if(l != True) throw new AxiomaticODESolver.AxiomaticODESolverExn("Expected the left-most component to be a True.")
+        Some(r)
+      }
+    }
+  }
+
   /** Returns true if `t` contains variables that have solutions in `solvedComponents`
     * @param solvedComponents Should be a list of equalities with Variables on the LHS. */
-  def containsSolvedComponents(t: Term, solvedComponents: List[Equal]) = {
+  private def containsSolvedComponents(t: Term, solvedComponents: List[Equal]) = {
     assert(solvedComponents.forall(eq => eq.left.isInstanceOf[Variable]))
     val solutions = conditionsToValues(solvedComponents)
 
@@ -71,7 +96,7 @@ object Integrator {
     * @param t Time variable
     * @return Integral term dt
     */
-  def integrator(term : Term, t : Variable, system: ODESystem) : Term = term match {
+  private def integrator(term : Term, t : Variable, system: ODESystem) : Term = term match {
     case Plus(l, r) => Plus(integrator(l, t, system), integrator(r, t, system))
     case Minus(l, r) => Minus(integrator(l, t, system), integrator(r, t, system))
     case Times(c, x) if x.equals(t) && !StaticSemantics.freeVars(c).contains(t) => Times(Divide(c, Number(2)), Power(x, Number(2)))
