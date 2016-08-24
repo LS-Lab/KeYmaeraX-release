093f03f2bd1b9aaf4cf91b3520a64e7bdc95b5d9
Nathan Fulton <gitcommit@nfulton.org>
2016-08-24 14:44:49 -0400

    Adds an object for Hybrid Program helpers.

---
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/helpers/ProgramHelpers.scala
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Carnegie Mellon University.
+ * See LICENSE.txt for the conditions of this license.
+ */
+
+package edu.cmu.cs.ls.keymaerax.btactics.helpers
+
+/**
+  * @author Nathan Fulton
+  */
+object ProgramHelpers {
+
+}
