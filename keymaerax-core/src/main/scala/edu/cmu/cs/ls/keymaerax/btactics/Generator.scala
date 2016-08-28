49346b5a7ac38b628f6b7c886bc876936b891974
Andre Platzer <aplatzer@cs.cmu.edu>
2016-08-27 20:38:55 -0400

    Documentation

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/Generator.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/Generator.scala
@@ -9,31 +9,37 @@ import edu.cmu.cs.ls.keymaerax.bellerophon.Position
 
 /**
  * Created by smitsch on 12/23/14.
- * @author Stefan Mitsch
  */
 
 /**
- * apply results in a formula to try.
+ * Invariant generators etc, where `apply` results in an object of type `A` to try.
  * Results do not have to be deterministic, e.g., calls to apply might advance to the next candidate.
- * Results can also be deterministic.
   * @todo change to (Sequent,Position) => Iterator[A] maybe?
   * @todo add implementation for something like (Sequent, Position) => Stream[A] for example?
+  * @tparam A the type of results that are being generated.
+  * @author Stefan Mitsch
  */
 trait Generator[A] extends ((Sequent, Position) => Option[A]) {
   def peek(s: Sequent, p: Position): Option[A]
 }
 
+/** Singleton generator only providing a single fixed output `f` every time it is asked. */
 class Generate[A](f: A) extends Generator[A] {
   def apply(s: Sequent, p: Position) = Some(f)
   def peek(s: Sequent, p: Position) = Some(f)
 }
 
+/** Empty generator never providing any output */
 class NoneGenerate[A] extends Generator[A] {
   def apply(s: Sequent, p: Position) = None
   def peek(s: Sequent, p: Position) = None
 }
 
+/** Map-based generator providing output according to the fixed map `product` according to its program or whole formula.
+  * @author Stefan Mitsch
+  * */
 class ConfigurableGenerate[A](var products: Map[Expression,A] = Map[Expression,A]()) extends Generator[A] {
+  //@todo why p.top instead of p?
   def apply(s: Sequent, p: Position) = s(p.top) match {
     case Box(prg, _) => products.get(prg)
     case Diamond(prg, _) => products.get(prg)
