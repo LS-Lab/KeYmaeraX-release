158aefb0fecdaeea088e83c78a11b8db2014a50d
Andre Platzer <aplatzer@cs.cmu.edu>
2016-08-31 14:31:02 -0400

    Code structuring for tools

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/SimulationTool.scala
+++
@@ -1,9 +0,0 @@
-/**
-* Copyright (c) Carnegie Mellon University.
-* See LICENSE.txt for the conditions of this license.
-*/
-package edu.cmu.cs.ls.keymaerax.tools
-
-import edu.cmu.cs.ls.keymaerax.core.{Formula, NamedSymbol, Number}
-import edu.cmu.cs.ls.keymaerax.tools.SimulationTool.{SimRun, SimState, Simulation}
-
