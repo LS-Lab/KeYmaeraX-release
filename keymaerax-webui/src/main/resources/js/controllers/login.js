002b75fb26dd2be3d946db16ba8a3bc4a70d2c06
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-09-02 08:34:49 -0400

    Web UI: license agreement for guest on registration instead of after every login (guest is now an ordinary user)

--- keymaerax-webui/src/main/resources/js/controllers/login.js
+++ keymaerax-webui/src/main/resources/js/controllers/login.js
@@ -24,40 +24,17 @@ angular.module('keymaerax.controllers').controller('LoginCtrl',
     }
 
     login = function(username, password) {
-      if (username === "guest") {
-        // guests have to accept the license every time
-        var modalInstance = $uibModal.open({
-          templateUrl: 'partials/license_dialog.html',
-          controller: 'LicenseDialogCtrl',
-          backdrop: "static",
-          size: 'lg'
-        });
-        modalInstance.result.then(function() {
-          $http.get("/user/" + username + "/" + password).then(function(response) {
-            if(response.data.type == "LoginResponse") {
-              if(response.data.success) {
-                sessionService.setToken(response.data.sessionToken);
-                sessionService.setUser(response.data.value);
-                document.location.href = "/dashboard.html"
-              } else {
-                showMessage($uibModal, "Login failed", "Please check user name and/or password");
-              }
-            }
-          });
-        });
-      } else {
-        $http.get("/user/" + username + "/" + password)
-        .then(function(response) {
-          if(response.data.type == "LoginResponse") {
-            if(response.data.success) {
-              sessionService.setToken(response.data.sessionToken);
-              sessionService.setUser(response.data.value);
-              document.location.href = "/dashboard.html"
-            } else {
-              showMessage($uibModal, "Login failed", "Please check user name and/or password");
-            }
+      $http.get("/user/" + username + "/" + password)
+      .then(function(response) {
+        if(response.data.type == "LoginResponse") {
+          if(response.data.success) {
+            sessionService.setToken(response.data.sessionToken);
+            sessionService.setUser(response.data.value);
+            document.location.href = "/dashboard.html"
+          } else {
+            showMessage($uibModal, "Login failed", "Please check user name and/or password");
           }
-        });
-      }
+        }
+      });
     }
   });
