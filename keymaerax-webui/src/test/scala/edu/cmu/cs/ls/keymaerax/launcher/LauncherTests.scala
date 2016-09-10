43771317d117dd7efc7bd910bd14fb0527a08611
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-09-10 18:09:18 -0400

    Command line -tactic support for .kyt and .scala

--- keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/launcher/LauncherTests.scala
+++ keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/launcher/LauncherTests.scala
@@ -9,15 +9,28 @@ import testHelper.KeYmaeraXTestTags
 @SlowTest
 class LauncherTests extends FlatSpec with Matchers with BeforeAndAfterEach {
 
-  "Launcher" should "prove the bouncing ball from command line" taggedAs KeYmaeraXTestTags.IgnoreInBuildTest in {
+  "Launcher" should "prove the bouncing ball from command line with scala tactic" taggedAs KeYmaeraXTestTags.IgnoreInBuildTest in {
     val inputFileName = "keymaerax-webui/src/test/resources/examples/simple/bouncing-ball/bouncing-ball-tout.key"
     val tacticFileName = "keymaerax-webui/src/test/resources/examples/simple/bouncing-ball/BouncingBallTacticGenerator.scala"
-    val outputFileName = File.createTempFile("bouncing-ball-tout", ".kyx.proof").getAbsolutePath
+    val outputFileName = File.createTempFile("bouncing-ball-tout", ".kyp").getAbsolutePath
 
     KeYmaeraX.main(Array("-prove", inputFileName, "-tactic", tacticFileName, "-out", outputFileName))
 
     val actualFileContent = scala.io.Source.fromFile(outputFileName).mkString
     val expectedProof = scala.io.Source.fromFile("keymaerax-webui/src/test/resources/examples/simple/bouncing-ball/bouncing-ball-tout.kyx.proof").mkString
+    //@note actual file content contains evidence comments: temporary file name in comments changes on every test run
+    actualFileContent should include (expectedProof)
+  }
+
+  it should "prove the bouncing ball from command line with kyt tactic" taggedAs KeYmaeraXTestTags.IgnoreInBuildTest in {
+    val inputFileName = "keymaerax-webui/src/test/resources/examples/simple/bouncing-ball/bouncing-ball-tout.key"
+    val tacticFileName = "keymaerax-webui/src/test/resources/examples/simple/bouncing-ball/bouncing-ball-tout.kyt"
+    val outputFileName = File.createTempFile("bouncing-ball-tout", ".kyp").getAbsolutePath
+
+    KeYmaeraX.main(Array("-prove", inputFileName, "-tactic", tacticFileName, "-out", outputFileName))
+
+    val actualFileContent = scala.io.Source.fromFile(outputFileName).mkString
+    val expectedProof = scala.io.Source.fromFile("keymaerax-webui/src/test/resources/examples/simple/bouncing-ball/bouncing-ball-tout.kyp").mkString
     //@note actual file content contains evidence comments: temporary file name in comments changes on every test run
     actualFileContent should include (expectedProof)
   }
