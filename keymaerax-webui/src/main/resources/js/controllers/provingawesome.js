f490d2ba2c4c32ec7fad1b028d48ffb61e1e9abc
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-10-05 13:24:54 -0400

    Web UI: avoids multiple event handler executions by broadcasting events on rootScope, but listening only in specific scopes

--- keymaerax-webui/src/main/resources/js/controllers/provingawesome.js
+++ keymaerax-webui/src/main/resources/js/controllers/provingawesome.js
@@ -6,41 +6,42 @@ angular.module('keymaerax.controllers').controller('ProofCtrl',
 //    ['$http', '$rootScope', '$cookies', '$routeParams', '$q', '$uibModal', '$timeout', 'sequentProofData', 'spinnerService',
     function($scope, $rootScope, $http, $cookies, $routeParams, $q, $uibModal, $timeout, sequentProofData, spinnerService) {
 
-  $http.get('proofs/user/' + $cookies.get('userId') + "/" + $routeParams.proofId).success(function(data) {
+  $scope.userId = $cookies.get('userId');
+  $scope.proofId = $routeParams.proofId;
+
+  $http.get('proofs/user/' + $scope.userId + "/" + $scope.proofId).success(function(data) {
       $scope.proofId = data.id;
       $scope.proofName = data.name;
       $scope.modelId = data.model;
       $scope.closed = data.closed;
       $scope.stepCount= data.stepCount;
       $scope.date = data.date;
-      sequentProofData.fetchAgenda($scope, $cookies.get('userId'), $scope.proofId);
+      sequentProofData.fetchAgenda($scope, $scope.userId, $scope.proofId);
   });
   $scope.$emit('routeLoaded', {theview: 'proofs/:proofId'});
 
-  if(!$rootScope.agnedaIsEmptyEventIsDefined) {
-      $rootScope.$on('agenda.isEmpty', function(event) {
-          $rootScope.agnedaIsEmptyEventIsDefined = true;
-          
-          $http.get('proofs/user/' + $cookies.get('userId') + "/" + $routeParams.proofId + '/progress').success(function(data) {
-              if (data.status == 'closed') {
-                  var modalInstance = $uibModal.open({
-                      templateUrl: 'partials/prooffinisheddialog.html',
-                      controller: 'ProofFinishedDialogCtrl',
-                      size: 'md',
-                      resolve: {
-                          proofId: function() { return $scope.proofId; },
-                          proofName: function() { return $scope.proofName; },
-                          status: function() { return data.status; },
-                          proofData: function() { return sequentProofData; }
-                      }
-                  });
-              } else {
-                  // should never happen
-                  showMessage($uibModal, 'Empty agenda even though proof is not closed (' + data.status + ')')
-              }
+  $scope.$on('agenda.isEmpty', function(event, data) {
+    if (data.proofId == $scope.proofId) {
+      // the current controller is responsible
+      $http.get('proofs/user/' + $scope.userId + "/" + $scope.proofId + '/progress').success(function(data) {
+        if (data.status == 'closed') {
+          var modalInstance = $uibModal.open({
+            templateUrl: 'partials/prooffinisheddialog.html',
+            controller: 'ProofFinishedDialogCtrl',
+            size: 'md',
+            resolve: {
+                userId: function() { return $scope.userId; },
+                proofId: function() { return $scope.proofId; },
+                proofName: function() { return $scope.proofName; }
+            }
           });
+        } else {
+          // should never happen
+          showMessage($uibModal, 'Empty agenda even though proof ' + $scope.proofId + ' is not closed (' + data.status + ')')
+        }
       });
-  }
+    }
+  });
 
   $scope.runningTask = {
     nodeId: undefined,
@@ -53,15 +54,13 @@ angular.module('keymaerax.controllers').controller('ProofCtrl',
       $scope.runningTask.future = $q.defer();
       $scope.runningTask.future.promise.then(
         /* future resolved */ function(taskId) {
-          var userId = $cookies.get('userId');
-          var proofId = $routeParams.proofId;
-          $http.get('proofs/user/' + userId + '/' + proofId + '/' + $scope.runningTask.nodeId + '/' + taskId + '/result')
+          $http.get('proofs/user/' + $scope.userId + '/' + $scope.proofId + '/' + $scope.runningTask.nodeId + '/' + taskId + '/result')
             .then(function(response) {
               if (response.data.type === 'taskresult') {
-                $rootScope.$emit('proof.message', { textStatus: "", errorThrown: "" });
+                $rootScope.$broadcast('proof.message', { textStatus: "", errorThrown: "" });
                 if ($scope.runningTask.nodeId === response.data.parent.id) {
-                  sequentProofData.updateAgendaAndTree(response.data);
-                  sequentProofData.tactic.fetch(userId, proofId);
+                  sequentProofData.updateAgendaAndTree($scope.proofId, response.data);
+                  sequentProofData.tactic.fetch($scope.userId, $scope.proofId);
                 } else {
                   showMessage($uibModal, "Unexpected tactic result, parent mismatch: " + " expected " +
                     $scope.runningTask.nodeId + " but got " + response.data.parent.id);
@@ -71,12 +70,12 @@ angular.module('keymaerax.controllers').controller('ProofCtrl',
               }
             })
             .catch(function(err) {
-              $rootScope.$emit('proof.message', err.data);
+              $rootScope.$broadcast('proof.message', err.data);
             })
             .finally(function() { spinnerService.hide('tacticExecutionSpinner'); });
         },
         /* future rejected */ function(reason) {
-          $rootScope.$emit('proof.message', { textStatus: "", errorThrown: "" });
+          $rootScope.$broadcast('proof.message', { textStatus: "", errorThrown: "" });
           if (reason !== 'stopped') showMessage($uibModal, reason);
           spinnerService.hide('tacticExecutionSpinner');
         }
@@ -84,9 +83,7 @@ angular.module('keymaerax.controllers').controller('ProofCtrl',
       $scope.runningTask.poll(taskId);
     },
     poll: function(taskId) {
-      var userId = $cookies.get('userId');
-      var proofId = $routeParams.proofId;
-      $http.get('proofs/user/' + userId + '/' + proofId + '/' + $scope.runningTask.nodeId + '/' + taskId + '/status')
+      $http.get('proofs/user/' + $scope.userId + '/' + $scope.proofId + '/' + $scope.runningTask.nodeId + '/' + taskId + '/status')
         .then(function(response) {
           if (response.data.lastStep !== undefined) $scope.runningTask.lastStep = response.data.lastStep.ruleName;
           if (response.data.status === 'done') $scope.runningTask.future.resolve(taskId);
@@ -95,9 +92,7 @@ angular.module('keymaerax.controllers').controller('ProofCtrl',
         .catch(function(error) { $scope.runningTask.future.reject(error); });
     },
     stop: function() {
-      var userId = $cookies.get('userId');
-      var proofId = $routeParams.proofId;
-      $http.get('proofs/user/' + userId + '/' + proofId + '/' + $scope.runningTask.nodeId + '/' + $scope.runningTask.taskId + '/stop')
+      $http.get('proofs/user/' + $scope.userId + '/' + $scope.proofId + '/' + $scope.runningTask.nodeId + '/' + $scope.runningTask.taskId + '/stop')
         .then(function(response) { $scope.runningTask.future.reject('stopped'); })
         .catch(function(err) { $scope.runningTask.future.reject(err); });
     }
@@ -136,7 +131,7 @@ angular.module('keymaerax.controllers').controller('TaskCtrl',
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     $scope.fetchNodeInfo = function(dispatched) {
-      var uri = "/proofs/user/" + $cookies.get('userId') + "/" + dispatched.proofId + "/agendaDetails/" + dispatched.nodeId;
+      var uri = "/proofs/user/" + $scope.userId + "/" + dispatched.proofId + "/agendaDetails/" + dispatched.nodeId;
       $http.get(uri)
         .success(function(data) {
         data.readOnly = true;
@@ -153,11 +148,9 @@ angular.module('keymaerax.controllers').controller('TaskCtrl',
     };
 
     $scope.exportSubgoal = function() {
-        var proofId = $routeParams.proofId;
-        var userId = $cookies.get('userId');
         var nodeId = sequentProofData.agenda.selectedId();
 
-        var uri = 'proofs/user/export/' + userId + '/' + proofId + '/' + nodeId
+        var uri = 'proofs/user/export/' + $scope.userId + '/' + $scope.proofId + '/' + nodeId
 
         $http.get(uri)
             .then(function(response) {
@@ -171,46 +164,39 @@ angular.module('keymaerax.controllers').controller('TaskCtrl',
     }
 
     $scope.doTactic = function(formulaId, tacticId) {
-      var proofId = $routeParams.proofId;
-      var userId = $cookies.get('userId');
       var nodeId = sequentProofData.agenda.selectedId();
-
-      var base = 'proofs/user/' + userId + '/' + proofId + '/' + nodeId;
+      var base = 'proofs/user/' + $scope.userId + '/' + $scope.proofId + '/' + nodeId;
       var uri = formulaId !== undefined ?  base + '/' + formulaId + '/doAt/' + tacticId : base + '/do/' + tacticId;
       spinnerService.show('tacticExecutionSpinner')
       $http.get(uri)
         .then(function(response) { $scope.runningTask.start(nodeId, response.data.taskId); })
         .catch(function(err) {
           spinnerService.hide('tacticExecutionSpinner');
-          $rootScope.$emit("proof.message", err.data);
+          $rootScope.$broadcast("proof.message", err.data);
         });
     }
 
     $scope.doInputTactic = function(formulaId, tacticId, input) {
-      var proofId = $routeParams.proofId;
-      var userId = $cookies.get('userId');
       var nodeId = sequentProofData.agenda.selectedId();
       spinnerService.show('tacticExecutionSpinner');
-      var base = 'proofs/user/' + userId + '/' + proofId + '/' + nodeId;
+      var base = 'proofs/user/' + $scope.userId + '/' + $scope.proofId + '/' + nodeId;
       var uri = formulaId !== undefined ? base + '/' + formulaId + '/doInputAt/' + tacticId : base + '/doInput/' + tacticId
       $http.post(uri, input)
         .then(function(response) { $scope.runningTask.start(nodeId, response.data.taskId); })
         .catch(function(err) {
           spinnerService.hide('tacticExecutionSpinner');
-          $rootScope.$emit("proof.message", err.data);
+          $rootScope.$broadcast("proof.message", err.data);
         });
     }
 
     $scope.doTwoPositionTactic = function(fml1Id, fml2Id, tacticId) {
-      var proofId = $routeParams.proofId;
-      var userId = $cookies.get('userId');
       var nodeId = sequentProofData.agenda.selectedId();
       spinnerService.show('tacticExecutionSpinner');
-      $http.get('proofs/user/' + userId + '/' + proofId + '/' + nodeId + '/' + fml1Id + '/' + fml2Id + '/doAt/' + tacticId)
+      $http.get('proofs/user/' + $scope.userId + '/' + $scope.proofId + '/' + nodeId + '/' + fml1Id + '/' + fml2Id + '/doAt/' + tacticId)
         .then(function(response) { $scope.runningTask.start(nodeId, response.data.taskId); })
         .catch(function(err) {
           spinnerService.hide('tacticExecutionSpinner');
-          $rootScope.$emit("proof.message", err.data);
+          $rootScope.$broadcast("proof.message", err.data);
         });
     }
 
@@ -224,23 +210,21 @@ angular.module('keymaerax.controllers').controller('TaskCtrl',
       request.then(function(response) { $scope.runningTask.start(nodeId, response.data.taskId); })
         .catch(function(err) {
           spinnerService.hide('tacticExecutionSpinner');
-          $rootScope.$emit('proof.message', err.data);
+          $rootScope.$broadcast('proof.message', err.data);
         });
     }
 
     $scope.onTacticScript = function(tacticText) {
-      var proofId = $routeParams.proofId;
-      var userId = $cookies.get('userId');
       var nodeId = sequentProofData.agenda.selectedId();
       spinnerService.show('tacticExecutionSpinner');
-      $http.post('proofs/user/' + userId + '/' + proofId + '/' + nodeId + '/doCustomTactic', tacticText)
+      $http.post('proofs/user/' + $scope.userId + '/' + $scope.proofId + '/' + nodeId + '/doCustomTactic', tacticText)
         .then(function(response) { $scope.runningTask.start(nodeId, response.data.taskId); })
         .catch(function(err) {
             if(err.data.errorThrown) {
                 console.error("Error while executing custom tactic: " + err.data.textStatus);
                 spinnerService.hide('tacticExecutionSpinner');
                 //For custom tactics, show the tactic message and also the little yellow status bar.
-                $rootScope.$emit('proof.message', err.data);
+                $rootScope.$broadcast('proof.message', err.data);
                 showCaughtTacticErrorMessage($uibModal, err.data.errorThrown, err.data.textStatus, err.data.tacticMsg)
             }
             else {
@@ -292,17 +276,15 @@ angular.module('keymaerax.controllers').controller('TaskCtrl',
         controller: 'SimulatorCtrl',
         size: 'lg',
         resolve: {
-          proofId: function() { return $routeParams.proofId; },
-          userId: function() { return $cookies.get('userId'); },
+          proofId: function() { return $scope.proofId; },
+          userId: function() { return $scope.userId; },
           nodeId: function() { return sequentProofData.agenda.selectedId(); }
         }
       })
     }
 
     $scope.downloadProblemSolution = function() {
-        var proofId = $routeParams.proofId;
-        var userId = $cookies.get('userId');
-        $http.get('proofs/user/' + userId + '/' + proofId + '/download').success(function (data) {
+        $http.get('proofs/user/' + $scope.userId + '/' + $scope.proofId + '/download').success(function (data) {
             $uibModal.open({
                 templateUrl: 'templates/tacticExtracted.html',
                 controller: 'TacticExtractionCtrl',
@@ -318,23 +300,17 @@ angular.module('keymaerax.controllers').controller('TaskCtrl',
       
     //Save a name edited using the inline editor.
     $scope.saveProofName = function(newName) {
-      var proofId = $routeParams.proofId;
-      var userId = $cookies.get('userId');
-      $http.post("proofs/user/" + userId + "/" + proofId + "/name/" + newName, {})
+      $http.post("proofs/user/" + $scope.userId + "/" + $scope.proofId + "/name/" + newName, {})
     }
 
     $scope.saveTaskName = function(newName) {
-      var proofId = $routeParams.proofId;
-      var userId = $cookies.get('userId');
       var nodeId = sequentProofData.agenda.selectedId();
-      $http.post("proofs/user/" + userId + "/" + proofId + "/" + nodeId + "/name/" + newName, {});
+      $http.post("proofs/user/" + $scope.userId + "/" + $scope.proofId + "/" + nodeId + "/name/" + newName, {});
     }
   });
 
 angular.module('keymaerax.controllers').controller('ProofFinishedDialogCtrl',
-        function($scope, $http, $cookies, $uibModalInstance, FileSaver, Blob, proofId, proofName) {
-
-    $scope.userId = $cookies.get('userId')
+        function($scope, $http, $cookies, $uibModalInstance, FileSaver, Blob, userId, proofId, proofName) {
 
     // empty open proof until fetched from server
     $scope.proof = {
@@ -345,7 +321,7 @@ angular.module('keymaerax.controllers').controller('ProofFinishedDialogCtrl',
     }
 
     // fetch proof
-    $http.get("/proofs/user/" + $scope.userId + "/" + proofId + "/validatedStatus").then(function(response) {
+    $http.get("/proofs/user/" + userId + "/" + proofId + "/validatedStatus").then(function(response) {
       $scope.proof = response.data; // no transformation, pass on to HTML as is
     });
 
