d7e7b78ad60a641d28f27b16ab02ec52d91b0678
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-10-18 12:02:12 -0400

    ETCS essentials and controllability lemma

---
+++ keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/btactics/Etcs.scala
@@ -0,0 +1,45 @@
+/**
+* Copyright (c) Carnegie Mellon University.
+* See LICENSE.txt for the conditions of this license.
+*/
+package edu.cmu.cs.ls.keymaerax.btactics
+
+import edu.cmu.cs.ls.keymaerax.bellerophon._
+import edu.cmu.cs.ls.keymaerax.btactics.ArithmeticSimplification._
+import edu.cmu.cs.ls.keymaerax.btactics.DebuggingTactics.{print, printIndexed}
+import edu.cmu.cs.ls.keymaerax.btactics.TactixLibrary._
+import edu.cmu.cs.ls.keymaerax.btactics.arithmetic.speculative.ArithmeticSpeculativeSimplification._
+import edu.cmu.cs.ls.keymaerax.core._
+import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
+import edu.cmu.cs.ls.keymaerax.tags.SlowTest
+import testHelper.ParserFactory._
+
+import scala.language.postfixOps
+
+/**
+  * ETCS test cases.
+  * Essentials of European Train Control System (ETCS).
+  * @see "Andre Platzer, Jan-David Quesel. European Train Control System: A case study in formal verification. ICFEM, 2009"
+  * @see "Andre Platzer. Differential dynamic logic for hybrid systems. Journal of Automated Reasoning, 41(2), pages 143-189, 2008."
+  * @author Stefan Mitsch
+  */
+@SlowTest
+class Etcs extends TacticTestBase {
+
+  "ETCS controllability lemma" should "be provable with master" in withMathematica { tool =>
+    val s = parseToSequent(getClass.getResourceAsStream("/examples/casestudies/etcs/controllability-lemma.kyx"))
+    proveBy(s, master()) shouldBe 'proved
+  }
+
+  "ETCS RBC controllability lemma" should "be provable with master" in withMathematica { tool =>
+    val s = parseToSequent(getClass.getResourceAsStream("/examples/casestudies/etcs/rbc-controllability-lemma.kyx"))
+    proveBy(s, master()) shouldBe 'proved
+  }
+
+  "ETCS essentials" should "be provable with master" in withMathematica { tool =>
+    val s = parseToSequent(getClass.getResourceAsStream("/examples/casestudies/etcs/ETCS-essentials.kyx"))
+    proveBy(s, master()) shouldBe 'proved
+  }
+
+
+}
