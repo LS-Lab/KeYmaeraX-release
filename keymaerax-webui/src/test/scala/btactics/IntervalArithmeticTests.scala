f11dd57ca6e000849a4d1b57550349d7c7e579f3
Yong Kiam <tanyongkiam@gmail.com>
2016-10-28 22:19:14 -0400

    Brief start on Isabelle syntax

--- keymaerax-webui/src/test/scala/btactics/IntervalArithmeticTests.scala
+++ keymaerax-webui/src/test/scala/btactics/IntervalArithmeticTests.scala
@@ -18,7 +18,7 @@ class IntervalArithmeticTests extends TacticTestBase  {
 
 
   "interval arithmetic" should "derive repeated addition bounds" in withMathematica { qeTool =>
-    val fml = "(x+y)+z+((x+y)+z) <= a+b".asFormula
+    val fml = "(x+y)+z+((x+y)+z+5) <= a+b".asFormula
     println(deriveFormulaProof(fml))
   }
 
@@ -46,13 +46,22 @@ class IntervalArithmeticTests extends TacticTestBase  {
     val uf = "((dpost()>=0&d^2-dpost()^2<=2*b*(mpost()-m)&vdespost()>=0)&(((((((((SBpost()=SB)&vpost()=v)&statepost()=state)&dopost()=d)&zpost()=z)&tpost()=t)&mopost()=m)))&apost()=a|(((((((((vdespost()=vdes&SBpost()=SB)&vpost()=v)&statepost()=brake)&dopost()=do)&zpost()=z)&tpost()=t)&mopost()=mo)&mpost()=m)&dpost()=d)&apost()=a)|v<=vdes&(apost()>=-b&apost()<=A)&((m-z<=(v^2-d^2)/(2*b)+(A/b+1)*(A/2*ep^2+ep*v)|state=brake)&(v>=0&0<=ep)&(((((((((vdespost()=vdes&SBpost()=(v^2-d^2)/(2*b)+(A/b+1)*(A/2*ep^2+ep*v))&vpost()=v)&statepost()=state)&dopost()=do)&zpost()=z)&tpost()=0)&mopost()=mo)&mpost()=m)&dpost()=d)&apost()=-b|!(m-z<=(v^2-d^2)/(2*b)+(A/b+1)*(A/2*ep^2+ep*v)|state=brake)&(v>=0&0<=ep)&(((((((((vdespost()=vdes&SBpost()=(v^2-d^2)/(2*b)+(A/b+1)*(A/2*ep^2+ep*v))&vpost()=v)&statepost()=state)&dopost()=do)&zpost()=z)&tpost()=0)&mopost()=mo)&mpost()=m)&dpost()=d))|v>=vdes&(apost() < 0&apost()>=-b)&((m-z<=(v^2-d^2)/(2*b)+(A/b+1)*(A/2*ep^2+ep*v)|state=brake)&(v>=0&0<=ep)&(((((((((vdespost()=vdes&SBpost()=(v^2-d^2)/(2*b)+(A/b+1)*(A/2*ep^2+ep*v))&vpost()=v)&statepost()=state)&dopost()=do)&zpost()=z)&tpost()=0)&mopost()=mo)&mpost()=m)&dpost()=d)&apost()=-b|!(m-z<=(v^2-d^2)/(2*b)+(A/b+1)*(A/2*ep^2+ep*v)|state=brake)&(v>=0&0<=ep)&(((((((((vdespost()=vdes&SBpost()=(v^2-d^2)/(2*b)+(A/b+1)*(A/2*ep^2+ep*v))&vpost()=v)&statepost()=state)&dopost()=do)&zpost()=z)&tpost()=0)&mopost()=mo)&mpost()=m)&dpost()=d))".asFormula
     //Conversion to NNF
     val refl = proveBy(Equiv(uf,uf),byUS("<-> reflexive"))
-    val pr = chaseFor((exp: Expression) => exp match {
+    val nnf = chaseFor((exp: Expression) => exp match {
       case And(_,_) => "& recursor" :: Nil
       case Or(_,_) => "| recursor" :: Nil
       case Imply(_,_) => "-> expand" :: Nil
       case Not(_) => AxiomIndex.axiomsFor(exp)
       case _ => Nil
     }, (s,p)=>pr=>pr)(SuccPosition(1,1::Nil))(refl)
+
+    val pr = chaseFor((exp: Expression) => exp match {
+      case And(_,_) => "& recursor" :: Nil
+      case Or(_,_) => "| recursor" :: Nil
+      case Greater(l,r) => "> flip"::Nil
+      case GreaterEqual(l,r) => ">= flip"::Nil
+      case _ => Nil
+    }, (s,p)=>pr=>pr)(SuccPosition(1,1::Nil))(nnf)
+
     val f =pr.conclusion.succ(0).sub(PosInExpr(1::Nil)).get.asInstanceOf[Formula]
     println(deriveFormulaProof(f))
   }
