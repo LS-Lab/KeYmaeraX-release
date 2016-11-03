4ff0e4654359ed8b45e8e125529f8a0a7051992c
Stefan Mitsch <smitsch@cs.cmu.edu>
2016-11-03 15:44:44 -0400

    Web UI content editable: disable HTML vs. plaintext position guessing

--- keymaerax-webui/src/main/resources/js/keymaera-ui/contenteditable.js
+++ keymaerax-webui/src/main/resources/js/keymaera-ui/contenteditable.js
@@ -5,29 +5,29 @@ angular.module('keymaerax.ui.binding', ['ngSanitize']).directive('contenteditabl
     link: function(scope, element, attrs, ngModel) {
       if (!ngModel) return;
 
-      rangy.init();
-      var savedSel = {};
+//      rangy.init();
+//      var savedSel = {};
 
       ngModel.$render = function() {
         element.html($sce.getTrustedHtml(ngModel.$viewValue || ''));
         //@note set cursor
-        if (savedSel.element !== undefined && savedSel.range !== undefined) {
-          rangy.getSelection().restoreCharacterRanges(savedSel.element, savedSel.range);
-        }
+//        if (savedSel.element !== undefined && savedSel.range !== undefined) {
+//          rangy.getSelection().restoreCharacterRanges(savedSel.element, savedSel.range);
+//        }
         read(); // initialize
       };
 
       element.on('blur keyup change', function(event) {
         // save range to reset cursor
-        savedSel.element = event.target;
-        savedSel.range = rangy.getSelection().saveCharacterRanges(event.target);
-        //@note set attributes for auto-completion
-        if (savedSel.element !== undefined && savedSel.range !== undefined) {
-          event.target.selectionDirection = 'none';
-          event.target.selectionStart = savedSel.range[0].characterRange.start;
-          event.target.selectionEnd = savedSel.range[0].characterRange.end;
-          event.target.value = element.text();
-        }
+//        savedSel.element = event.target;
+//        savedSel.range = rangy.getSelection().saveCharacterRanges(event.target);
+//        //@note set attributes for auto-completion
+//        if (savedSel.element !== undefined && savedSel.range !== undefined) {
+//          event.target.selectionDirection = 'none';
+//          event.target.selectionStart = savedSel.range[0].characterRange.start;
+//          event.target.selectionEnd = savedSel.range[0].characterRange.end;
+//          event.target.value = element.text();
+//        }
         scope.$evalAsync(read);
       });
 
