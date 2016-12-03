9660697a3c0656535931884e945f3be7c92a1edb
Brandon Bohrer <bbohrer@andrew.cmu.edu>
2016-12-02 19:26:53 -0500

    Begin implementing Kaisar

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/bellerophon/Kaisar.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/bellerophon/Kaisar.scala
@@ -10,9 +10,41 @@ object Kaisar {
   case class ProgramVariable(a:Variable) extends Resource
   case class FactVariable(p:Variable) extends Resource
 
-  case class ProofStep(using: List[Resource], by: BelleExpr)
+  type ProofStep = (List[Resource], BelleExpr)
 
   abstract class Statement
   case class Run (a:Variable, hp:Program) extends Statement
   case class Show (x:Variable, phi:Formula, proof: ProofStep)
+
+  type Proof = (Formula, List[Statement])
+
+  class History {
+    var progs:Seq[Program] = Seq()
+    var nextTime:Int = 0
+  }
+
+  class Context {
+    def concat(other: Context):Context =
+      ???
+  }
+  object Context {
+    var empty = new Context()
+
+  }
+
+  def eval(hist: History, ctx: Context, step:Statement):(History,Context) = {
+    /* step match */
+    ???
+  }
+
+  def eval(hist: History, ctx: Context, steps:List[Statement]):(History,Context) = {
+    steps match {
+      case (Nil) => (hist, Context.empty)
+      case (step :: steps) =>
+        var AD1 = eval(hist, ctx, step)
+        var AD2 = eval(AD1._1, AD1._2, steps)
+        (AD2._1, AD1._2.concat(AD2._2))
+    }
+  }
+
 }
