954a2288c757651e185246f6fd85099fe6acd9c1
Yong Kiam <tanyongkiam@gmail.com>
2016-12-21 15:56:25 -0500

    Reorganize the custom rewriting indexes

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/SimplifierV3.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/SimplifierV3.scala
@@ -11,7 +11,7 @@ import edu.cmu.cs.ls.keymaerax.pt.ProvableSig
 import scala.collection.immutable.{Map, _}
 
 /**
-  * Note: this is meant to a watered down version of SimplifierV2
+  * Note: this is meant to be a watered down version of SimplifierV2
   * Goals: Faster, more predictable and customizable
   *
   * Given a list of rewriting axioms, this traverses a term/formula bottom up and exhaustively tries the list of axioms
@@ -21,9 +21,11 @@ import scala.collection.immutable.{Map, _}
   *
   * Created by yongkiat on 12/19/16.
   */
+
 object SimplifierV3 {
 
-  //The internal lemmas for binary connectives
+  /** Term simplifier */
+  //Internal lemmas for binary connectives
   private def termAx(ctor:(Term,Term)=>Term) : (ProvableSig,ProvableSig,ProvableSig) = {
     val limp = "A_() -> (L_() = LL_())".asFormula
     val rimp = "B_() -> (R_() = RR_())".asFormula
@@ -39,44 +41,7 @@ object SimplifierV3 {
   private val timesAxs = termAx(Times.apply)
   private val divAxs = termAx(Divide.apply)
   private val powAxs = termAx(Power.apply)
-
   private val equalTrans = proveBy("(P_() -> (F_() = FF_())) & (Q_() -> (FF_() = FFF_())) -> (P_() & Q_() -> (F_() = FFF_())) ".asFormula,prop & QE)
-
-  private def qeTermProof(t:String,tt:String,pre:Option[String] = None): ProvableSig =
-  {
-    pre match{
-      case None => proveBy(Equal(t.asTerm,tt.asTerm),QE)
-      case Some(f) => proveBy(Imply(f.asFormula,Equal(t.asTerm,tt.asTerm)),QE)
-    }
-  }
-
-  //These may also be useful:
-  //qeTermProof("F_()*(F_()^-1)","1",Some("F_()>0")), qeTermProof("(F_()^-1)*F_()","1",Some("F_()>0")))
-  //qeTermProof("F_()+(-F_())","0"),qeTermProof("(-F_())+F_()","0"),
-  //  qeTermProof("x-x","0"),
-  //  qeTermProof("F_()+G_()-F_()","G_()"),
-  //  qeTermProof("F_()+G_()-G_()","F_()"),
-
-  //These are mostly just the basic unit and identity rules
-  private val mulArith = List(qeTermProof("0*F_()","0"), qeTermProof("F_()*0","0"), qeTermProof("1*F_()","F_()"),
-    qeTermProof("F_()*1","F_()"), qeTermProof("-1*F_()","-F_()"), qeTermProof("F_()*-1","-F_()"))
-  private val plusArith = List(qeTermProof("0+F_()","F_()"), qeTermProof("F_()+0","F_()"))
-  private val minusArith = List(qeTermProof("0-F_()","-F_()"), qeTermProof("F_()-0","F_()"))
-  //This is enabled by dependent rewriting
-  private val divArith = List(qeTermProof("0/F_()","0",Some("F_()>0")), qeTermProof("0/F_()","0",Some("F_()<0")))
-  private val powArith = List(qeTermProof("F_()^0","1",Some("F_()>0")), qeTermProof("F_()^0","1",Some("F_()<0")),
-    qeTermProof("F_()^1","F_()"))
-
-  def arithBaseIndex (t:Term) : List[ProvableSig] = {
-    t match {
-      case Plus(_,_) => plusArith
-      case Minus(_,_) => minusArith
-      case Times(_,_) => mulArith
-      case Divide(_,_) => divArith
-      case Power(_,_) => divArith
-      case _ => List()
-    }
-  }
 
   //Dependent term simplification workhorse
   //Given provable A -> (t = t') or (t = t'), a context, and a term x,
@@ -109,8 +74,7 @@ object SimplifierV3 {
     }
   }
 
-  /** Term simplifier */
-  def termSimp(t:Term, ctx:HashSet[Formula], taxs:Term => List[ProvableSig] = arithBaseIndex) : (Term, Option[(Formula,ProvableSig)]) =
+  def termSimp(t:Term, ctx:HashSet[Formula], taxs:Term => List[ProvableSig]) : (Term, Option[(Formula,ProvableSig)]) =
   {
     val (rect,recpropt) =
       t match {
@@ -165,6 +129,7 @@ object SimplifierV3 {
         }
     }
   }
+
   /** Formula simplifier */
   //Lemmas used internally by formula simplifier
 
@@ -187,7 +152,9 @@ object SimplifierV3 {
   private val gtAxs = fmlAx(Greater.apply)
   private val leqAxs = fmlAx(LessEqual.apply)
   private val ltAxs = fmlAx(Less.apply)
+  private val neAxs = fmlAx(NotEqual.apply)
 
+  //Other customized internal lemmas
   //And
   //only L changes
   private val andAxL =
@@ -226,70 +193,8 @@ object SimplifierV3 {
 
   private val equivTrans = proveBy("(P_() -> (F_() <-> FF_())) & (Q_() -> (FF_() <-> FFF_())) -> (P_() & Q_() -> (F_() <-> FFF_())) ".asFormula,prop)
 
-  private def propProof(f:String,ff:String,precond:Option[String] = None):ProvableSig =
-  {
-    precond match {
-      case Some(pre) =>
-        proveBy(Imply(pre.asFormula,Equiv(f.asFormula,ff.asFormula)), prop)
-      case None =>
-        proveBy(Equiv(f.asFormula,ff.asFormula), prop)
-    }
-  }
-
-  //F_() -> (F_() <-> true)
-  val tauto = propProof("F_()","true",Some("F_()"))
-  //!(F_()) -> (F_() <-> false)
-  val tauto2 = propProof("F_()","false",Some("!F_()"))
-
-  val andT = propProof("F_() & true","F_()")
-  val Tand = propProof("true & F_()","F_()")
-  val andF = propProof("F_() & false","false")
-  val Fand = propProof("false & F_()","false")
-
-  val implyT = propProof("F_() -> true","true")
-  val Timply = propProof("true -> F_()","F_()")
-  val implyF = propProof("F_() -> false","!F_()")
-  val Fimply = propProof("false -> F_()","true")
-
-  val orT = propProof("F_() | true","true")
-  val Tor = propProof("true | F_()","true")
-  val orF = propProof("F_() | false","F_()")
-  val For = propProof("false | F_()","F_()")
-
-  val equivT = propProof("F_() <-> true","F_()")
-  val Tequiv = propProof("true <-> F_()","F_()")
-  val equivF = propProof("F_() <-> false","!F_()")
-  val Fequiv = propProof("false <-> F_()","!F_()")
-
-  val notT = propProof("!true","false")
-  val notF = propProof("!false","true")
-  //val andSym = propProof("P_() & Q_()","true","Q_() & P_()")
-
-  def baseIndex (f:Formula) : List[ProvableSig] = {
-    f match {
-      case True => List()
-      case False => List()
-      case _ => List(tauto,tauto2)
-    }
-  }
-
-  def boolIndex (f:Formula) : List[ProvableSig] ={
-    f match {
-      //Note: more pattern matching possible here
-      case And(l,r) => List(andT,Tand,andF,Fand)
-      case Imply(l,r) => List(implyT,Timply,implyF,Fimply)
-      case Or(l,r) => List(orT,Tor,orF,For)
-      case Equiv(l,r) =>  List(equivT,Tequiv,equivF,Fequiv)
-      case Not(u) => List(notT,notF)
-      case _ => List()
-    }
-  }
-
-//  def deMorganIndex (f:Formula) : List[ProvableSig] = {
-//  }
-
-  def composeIndex( i1:Formula => List[ProvableSig], i2:Formula => List[ProvableSig] )
-                  (f:Formula) : List[ProvableSig] = i1(f) ++ i2(f)
+  def composeIndex[A<:Expression]( is:(A => List[ProvableSig])*)
+                  (f:A) : List[ProvableSig] = is.flatMap(axs => axs(f)).toList
 
   //Dependent formula simplification workhorse
   //Given provable A -> (P <-> P') or (P<->P'), a context, and a formula f,
@@ -335,15 +240,14 @@ object SimplifierV3 {
   }
 
   def formulaSimp(f:Formula, ctx:HashSet[Formula] = HashSet(),
-                  faxs: Formula => List[ProvableSig] = composeIndex(baseIndex,boolIndex),
-                  taxs: Term => List[ProvableSig] = arithBaseIndex) : (Formula,Option[(Formula,ProvableSig)]) =
+                  faxs: Formula => List[ProvableSig], taxs: Term => List[ProvableSig]) : (Formula,Option[(Formula,ProvableSig)]) =
   {
     //todo: enable flag to toggle left to right, right to left rewriting?
     val (recf,recpropt) =
     f match {
       case And(l,r) =>
-        val (lf,lpropt) = formulaSimp(l,ctx,faxs)
-        val (rf,rpropt) = formulaSimp(r,addCtx(ctx,lf),faxs)
+        val (lf,lpropt) = formulaSimp(l,ctx,faxs,taxs)
+        val (rf,rpropt) = formulaSimp(r,addCtx(ctx,lf),faxs,taxs)
         val concl = And(lf,rf)
         (lpropt,rpropt) match {
           case (None,None) => (f,None)
@@ -363,8 +267,8 @@ object SimplifierV3 {
             (concl,Some((premise,pr)))
         }
       case Imply(l,r) =>
-        val (lf,lpropt) = formulaSimp(l,ctx,faxs)
-        val (rf,rpropt) = formulaSimp(r,addCtx(ctx,lf),faxs)
+        val (lf,lpropt) = formulaSimp(l,ctx,faxs,taxs)
+        val (rf,rpropt) = formulaSimp(r,addCtx(ctx,lf),faxs,taxs)
         val concl = Imply(lf,rf)
         (lpropt,rpropt) match {
           case (None,None) => (f,None)
@@ -385,8 +289,8 @@ object SimplifierV3 {
         }
       case Or(l,r) =>
         //todo: Add some rewriting to handle the negation
-        val (lf,lpropt) = formulaSimp(l,ctx,faxs)
-        val (rf,rpropt) = formulaSimp(r,addCtx(ctx,Not(lf)),faxs)
+        val (lf,lpropt) = formulaSimp(l,ctx,faxs,taxs)
+        val (rf,rpropt) = formulaSimp(r,addCtx(ctx,Not(lf)),faxs,taxs)
         val concl = Or(lf,rf)
         (lpropt,rpropt) match {
           case (None,None) => (f,None)
@@ -418,6 +322,7 @@ object SimplifierV3 {
           case Greater(_,_) => gtAxs
           case LessEqual(_,_) => leqAxs
           case Less(_,_) => ltAxs
+          case NotEqual(_,_) => neAxs
         }
 
         (lpropt,rpropt) match {
@@ -442,6 +347,7 @@ object SimplifierV3 {
 
     //todo: Should this rewrite to saturation? Or is once enough?
     val rw = faxs(recf).toStream.flatMap( pr => applyFormulaProvable(recf,ctx,pr)).headOption
+
     rw match {
       case None => (recf,recpropt)
       case Some((ff,prem,pr)) =>
@@ -461,40 +367,246 @@ object SimplifierV3 {
     }
   }
 
-  def simpWithDischarge(ctx:IndexedSeq[Formula],f:Formula) : ProvableSig = {
+  def simpWithDischarge(ctx:IndexedSeq[Formula],f:Formula,
+                        faxs:Formula=>List[ProvableSig],taxs:Term=>List[ProvableSig]) : (Formula,Option[ProvableSig]) = {
     val hs = HashSet(ctx: _*) //todo: Apply simple decomposition that prop can handle here
-    val (recf,recpropt) = formulaSimp(f,hs,composeIndex(baseIndex,boolIndex))
+    val (recf,recpropt) = formulaSimp(f,hs,faxs,taxs)
 
-    proveBy( Sequent(ctx,IndexedSeq(Equiv(f,recf))),
-      recpropt match {
-        case None => cohideR(1) & byUS("= reflexive")
-        case Some((prem,recpr)) =>
-          cut(prem) <( implyRi & cohideR(1) & by(recpr) , hideR(1) & prop )
+    (recf,
+    recpropt match {
+      case None => None
+      case Some((prem,recpr)) =>
+        val pr =
+        Some(proveBy( Sequent(ctx,IndexedSeq(Equiv(f,recf))),
+          cut(prem) <( cohide2(-(ctx.length+1),1) & implyRi & by(recpr) , hideR(1) & prop )))
+        pr
       }
     )
-
   }
 
-  private def weaken(ctx:IndexedSeq[Formula]): ForwardTactic = pr => {
-    val p = ProvableSig.startProof(pr.conclusion.glue(Sequent(ctx, IndexedSeq())))
-    proveBy(p,
-      cohideR(1) & //('Llast)*ctx.length &
-        by(pr))
-  }
+  val defaultFaxs:Formula=>List[ProvableSig] = composeIndex(baseIndex,boolIndex,cmpIndex)
+  val defaultTaxs:Term=>List[ProvableSig] = arithBaseIndex
 
-  val divSimp = proveBy("t_()>0 -> 0 / t_() = 0".asFormula,QE)
+  //Simplifies a formula including sub-terms occuring in the formula
+  def simpTac(faxs:Formula=>List[ProvableSig]=defaultFaxs,
+              taxs:Term=>List[ProvableSig]=defaultTaxs):DependentPositionTactic = new DependentPositionTactic("simp"){
+    override def factory(pos: Position): DependentTactic = new SingleGoalDependentTactic(name) {
+      override def computeExpr(sequent: Sequent): BelleExpr = {
+        sequent.sub(pos) match
+        {
+          case Some(f:Formula) =>
+            //If simplification was at the top level, then we can use the existing context
+            if (pos.isTopLevel)
+            {
+              val (ctx, cutPos, commute) =
+                if (pos.isSucc) (sequent.ante, pos, commuteEquivR(1))
+                else (sequent.ante.patch(pos.top.getIndex, Nil, 1), SuccPosition.base0(sequent.succ.length), skip)
 
-  //make tail rec
-  //Returns the list of premises, the key and the rewritten value
-  def decomposeTermAx(f:Formula) : (List[Formula],Term,Term) = {
-    f match {
-      case Equal(l,r) => (List(),l,r)
-      case Imply(l,r) =>
-        val (ls,lrec,rrec) = decomposeTermAx(r)
-        (l::ls,lrec,rrec)
-      case _ => ??? //Invalid
+              val (ff,pr) = simpWithDischarge(ctx,f,faxs,taxs)
+
+              pr match {
+                case None => ident
+                case Some(pr) =>
+                  cutAt (ff) (pos) < (
+                  ident,
+                  cohideOnlyR (cutPos) & equivifyR (1) & commute & by (pr)
+                  )
+              }
+            }
+            //Otherwise we only do the simplification under empty context and CEat the result
+            else
+            {
+              ???
+            }
+          case Some(t:Term) => ???
+          case _ => ident
+        }
+      }
     }
   }
 
+  //Full sequent simplification tactic
+  val fullSimpTac:DependentTactic = new SingleGoalDependentTactic("full simp") {
+    override def computeExpr(seq: Sequent): BelleExpr = {
+      val succ =
+        (List.range(1,seq.succ.length+1,1).foldRight(ident)
+        ((i:Int,tac:BelleExpr)=> simpTac()(i) & tac))
+      (List.range(-1,-(seq.ante.length+1),-1).foldRight(succ)
+        ((i:Int,tac:BelleExpr)=> simpTac()(i) & tac))
+    }
+  }
 
+  /** Term simplification indices */
+
+  private def qeTermProof(t:String,tt:String,pre:Option[String] = None): ProvableSig =
+  {
+    pre match{
+      case None => proveBy(Equal(t.asTerm,tt.asTerm),QE)
+      case Some(f) => proveBy(Imply(f.asFormula,Equal(t.asTerm,tt.asTerm)),QE)
+    }
+  }
+
+  //These are mostly just the basic unit and identity rules
+  private val mulArith = List(qeTermProof("0*F_()","0"), qeTermProof("F_()*0","0"), qeTermProof("1*F_()","F_()"),
+    qeTermProof("F_()*1","F_()"), qeTermProof("-1*F_()","-F_()"), qeTermProof("F_()*-1","-F_()"))
+  private val plusArith = List(qeTermProof("0+F_()","F_()"), qeTermProof("F_()+0","F_()"))
+  private val minusArith = List(qeTermProof("0-F_()","-F_()"), qeTermProof("F_()-0","F_()"))
+  //This is enabled by dependent rewriting
+  private val divArith = List(qeTermProof("0/F_()","0",Some("F_()>0")), qeTermProof("0/F_()","0",Some("F_()<0")))
+  private val powArith = List(qeTermProof("F_()^0","1",Some("F_()>0")), qeTermProof("F_()^0","1",Some("F_()<0")),
+    qeTermProof("F_()^1","F_()"))
+
+  //These may also be useful:
+  //qeTermProof("F_()*(F_()^-1)","1",Some("F_()>0")), qeTermProof("(F_()^-1)*F_()","1",Some("F_()>0")))
+  //qeTermProof("F_()+(-F_())","0"),qeTermProof("(-F_())+F_()","0"),
+  //  qeTermProof("x-x","0"),
+  //  qeTermProof("F_()+G_()-F_()","G_()"),
+  //  qeTermProof("F_()+G_()-G_()","F_()"),
+
+  def arithBaseIndex (t:Term) : List[ProvableSig] = {
+    t match {
+      case Plus(_,_) => plusArith
+      case Minus(_,_) => minusArith
+      case Times(_,_) => mulArith
+      case Divide(_,_) => divArith
+      case Power(_,_) => divArith
+      case _ => List()
+    }
+  }
+
+  /** Formula simplification indices */
+
+  private def propProof(f:String,ff:String,precond:Option[String] = None):ProvableSig =
+  {
+    precond match {
+      case Some(pre) =>
+        proveBy(Imply(pre.asFormula,Equiv(f.asFormula,ff.asFormula)), prop)
+      case None =>
+        proveBy(Equiv(f.asFormula,ff.asFormula), prop)
+    }
+  }
+
+  //F_() -> (F_() <-> true)
+  val tauto = propProof("F_()","true",Some("F_()"))
+  //!(F_()) -> (F_() <-> false)
+  val tauto2 = propProof("F_()","false",Some("!F_()"))
+
+  //This is a critical
+  def baseIndex (f:Formula) : List[ProvableSig] = {
+    f match {
+      case True => List()
+      case False => List()
+      case _ => List(tauto,tauto2)
+    }
+  }
+
+  private def qeFormulaProof(f:Option[String],t:String,tt:String):ProvableSig =
+  {
+    val ttt  = tt.asFormula
+    f match{
+      case None => proveBy(Equiv(t.asFormula,ttt),prop & QE)
+      case Some(f) => proveBy(Imply(f.asFormula,Equiv(t.asFormula,ttt)),prop & QE)
+    }
+  }
+
+  val ltNotReflex = qeFormulaProof(None,"F_()<F_()","false")
+  val gtNotReflex = qeFormulaProof(None,"F_()>F_()","false")
+  val neqNotReflex = qeFormulaProof(None,"F_()!=F_()","false")
+  val equalReflex = qeFormulaProof(None,"F_() = F_()","true")
+  val lessequalReflex = qeFormulaProof(None,"F_() <= F_()","true")
+  val greaterequalReflex = qeFormulaProof(None,"F_() >= F_()","true")
+
+  val eqSym = qeFormulaProof(Some("F_() = G_()"),"G_() = F_()","true")
+  val neqSym = qeFormulaProof(Some("F_() != G_()"),"G_() != F_()","true")
+  val gtNotSym = qeFormulaProof(Some("F_() > G_()"),"G_() > F_()","false")
+  val ltNotSym = qeFormulaProof(Some("F_() < G_()"),"G_() < F_()","false")
+
+  private def qeSearch(cmp1:(Term,Term)=>Formula,cmps:List[(Term,Term)=>Formula]) : List[ProvableSig] = {
+    //Use partial QE because I don't want to do everything by hand..
+    val f = "F_()".asTerm
+    val g = "G_()".asTerm
+    val key = cmp1(f,g)
+    cmps.flatMap(
+      (cmp:(Term,Term) => Formula) => {
+          List(
+            proveBy(Imply(cmp(f, g), Equiv(key, True)), prop & (QE*)),
+            proveBy(Imply(cmp(f, g), Equiv(key, False)), prop & (QE*)),
+            proveBy(Imply(cmp(g, f), Equiv(key, True)), prop & (QE*)),
+            proveBy(Imply(cmp(g, f), Equiv(key, False)), prop & (QE*))
+          )
+      }
+    ).filter(_.isProved)
+  }
+
+  val eqs = eqSym::qeSearch(Equal.apply,List(NotEqual.apply,Greater.apply,GreaterEqual.apply,Less.apply,LessEqual.apply))
+  val neqs = neqSym::qeSearch(NotEqual.apply,List(Equal.apply,Greater.apply,GreaterEqual.apply,Less.apply,LessEqual.apply))
+  val gts = gtNotSym::qeSearch(Greater.apply,List(Equal.apply,NotEqual.apply,GreaterEqual.apply,Less.apply,LessEqual.apply))
+  val ges = qeSearch(GreaterEqual.apply,List(Equal.apply,NotEqual.apply,Greater.apply,Less.apply,LessEqual.apply))
+  val lts = ltNotSym::qeSearch(Less.apply,List(Equal.apply,NotEqual.apply,Greater.apply,GreaterEqual.apply,LessEqual.apply))
+  val les = qeSearch(LessEqual.apply,List(Equal.apply,NotEqual.apply,Greater.apply,GreaterEqual.apply,Less.apply))
+
+  //This contains the basic heuristics for closing a comparison formula
+  def cmpIndex (f:Formula) : List[ProvableSig] = {
+
+    f match {
+      // Reflexive cases
+      // This protects against unification errors using Scala to inspect the term directly
+      case bop:ComparisonFormula if bop.left==bop.right =>
+        bop match{
+          case Less(_,_) => List(ltNotReflex)
+          case Greater(_,_) => List(gtNotReflex)
+          case NotEqual(_,_) => List(neqNotReflex)
+          case Equal(_,_) => List(equalReflex)
+          case GreaterEqual(_,_) => List(greaterequalReflex)
+          case LessEqual(_,_) => List(lessequalReflex)
+        }
+      //Closing by search
+      case bop:ComparisonFormula =>
+        bop match{
+          case Less(_,_) => lts
+          case Greater(_,_) => gts
+          case NotEqual(_,_) => neqs
+          case Equal(_,_) => eqs
+          case GreaterEqual(_,_) => ges
+          case LessEqual(_,_) => les
+        }
+      case _ => List()
+    }
+
+  }
+
+  val andT = propProof("F_() & true","F_()")
+  val Tand = propProof("true & F_()","F_()")
+  val andF = propProof("F_() & false","false")
+  val Fand = propProof("false & F_()","false")
+
+  val implyT = propProof("F_() -> true","true")
+  val Timply = propProof("true -> F_()","F_()")
+  val implyF = propProof("F_() -> false","!F_()")
+  val Fimply = propProof("false -> F_()","true")
+
+  val orT = propProof("F_() | true","true")
+  val Tor = propProof("true | F_()","true")
+  val orF = propProof("F_() | false","F_()")
+  val For = propProof("false | F_()","F_()")
+
+  val equivT = propProof("F_() <-> true","F_()")
+  val Tequiv = propProof("true <-> F_()","F_()")
+  val equivF = propProof("F_() <-> false","!F_()")
+  val Fequiv = propProof("false <-> F_()","!F_()")
+
+  val notT = propProof("!true","false")
+  val notF = propProof("!false","true")
+
+  def boolIndex (f:Formula) : List[ProvableSig] ={
+    f match {
+      //Note: more pattern matching possible here
+      case And(l,r) => List(andT,Tand,andF,Fand)
+      case Imply(l,r) => List(implyT,Timply,implyF,Fimply)
+      case Or(l,r) => List(orT,Tor,orF,For)
+      case Equiv(l,r) =>  List(equivT,Tequiv,equivF,Fequiv)
+      case Not(u) => List(notT,notF)
+      case _ => List()
+    }
+  }
 }
--- keymaerax-webui/src/test/scala/btactics/SimplifierV3Tests.scala
+++ keymaerax-webui/src/test/scala/btactics/SimplifierV3Tests.scala
@@ -15,14 +15,51 @@ class SimplifierV3Tests extends TacticTestBase {
 
   "SimplifierV3" should "simplify propositions" in withMathematica { qeTool =>
     val fml = "R() -> P() & Q() -> P() & (R() & P()) & Q() & (R() & P() & Z() & Y())".asFormula
-    println(formulaSimp(fml))
-    println(simpWithDischarge(IndexedSeq(),fml))
+    val result = proveBy(fml, SimplifierV3.simpTac()(1))
+    println(result)
+
   }
 
   "SimplifierV3" should "do dependent arithmetic simplification" in withMathematica { qeTool =>
     val fml = "ar > 0 -> (x - 0 + 0 * y + 0 + 0/ar >= 0 - k)".asFormula
-    //V2 would not be able to simplify 0/ar
-    println(formulaSimp(fml))
-    println(simpWithDischarge(IndexedSeq(),fml))
+    val result = proveBy(fml, SimplifierV3.simpTac()(1))
+    println(result)
   }
+
+  "SimplifierV3" should "do full sequent simplification" in withMathematica { qeTool =>
+    val antes = IndexedSeq(
+      "(x - 0 + 0 * y + 0 + 0/ar >= 0 - k)".asFormula,
+      "ar>0".asFormula,
+      "x * y = z + y + 0 - 0^2".asFormula,
+      "dhd-(a*t_+dho)=(-w*ad)*0".asFormula
+    )
+    val succs = IndexedSeq(
+      "P_() | Q_() & ar >0 | P_() | Q()".asFormula,
+      "P_() | Q_() & ar >0 | P_() | Q()".asFormula,
+      "dhd-(a*t_+dho)=-w*ad*0".asFormula
+    )
+    //todo: A 'not' like mechanism to simplify across multiple succedents?
+    val pr = proveBy(Sequent(antes,succs),fullSimpTac)
+    println(pr)
+  }
+
+  "SimplifierV3" should "search for close heuristics" in withMathematica { qeTool =>
+    val fml = " 0 > x -> x <= 0 & y = 0 & z<x -> x != y+z | x >= 5 -> 5 < x | (x !=5 -> 5<x ) & a = 0 & y = z+a+b & a+z+b = y".asFormula
+    val result = proveBy(fml, SimplifierV3.simpTac()(1))
+    result.subgoals.head.succ should contain only "0>x->y=0&z < x->x!=y+z|x>=5->5 < x|!x!=5&a=0&y=z+a+b&a+z+b=y".asFormula
+  }
+
+  "SimplifierV3" should "allow controlled custom rewrites" in withMathematica { qeTool =>
+    //Force any =0s to be rewritten
+    val custom1 = proveBy("F_() = 0 -> (F_() = 0)".asFormula,TactixLibrary.QE)
+    //Get rid of deMorgan once
+    val custom2 = DerivedAxioms.notNotEqual.fact
+
+    val fml = " 0 > x -> x <= 0 & y = 0 & z<x -> x != y+z | x >= 5 -> 5 < x | (x !=5 -> 5<x ) & a = 0 & y = z+a+b & a+z+b = y".asFormula
+    val result = proveBy(fml,
+      SimplifierV3.simpTac(taxs = composeIndex(defaultTaxs, (t => List(custom1))))(1)&
+      SimplifierV3.simpTac(faxs = composeIndex((f=>List(custom2)),defaultFaxs))(1))
+    result.subgoals.head.succ should contain only "0>x->y=0&z < x->5 < x|x=5&a=0&0=z+b".asFormula
+  }
+
 }
