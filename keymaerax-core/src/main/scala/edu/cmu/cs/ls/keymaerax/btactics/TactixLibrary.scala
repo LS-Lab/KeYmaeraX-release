90fecbe3c0e3ac8b4814440cf2d036cf086b3522
Stefan Mitsch <smitsch@cs.cmu.edu>
2017-01-12 15:53:01 -0500

    Master ODE: try instantiating with end time

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/TactixLibrary.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/TactixLibrary.scala
@@ -155,9 +155,9 @@ object TactixLibrary extends HilbertCalculus with SequentCalculus {
           (close
             | (must(normalize)
             | (loop(gen)('R)
-            | (ODE('R)
-            | (diffSolve('L) // somehow ODE aborts before even trying diffSolve
-            | exhaustiveEqL2R('L) ) ) ) ) ) ) ))*) &
+            | ((ODE('R) & ?(allR('R) & implyR('R)*2 & allL(Variable("t_"))('Llast) & auto & done)) // try evolution domain proof with end time, but only if it works out
+            | (diffSolve('L)
+            | exhaustiveEqL2R('L) ) ) ) ) ) )))*) &
       ?(OnAll(QE))
   }
 
@@ -169,7 +169,7 @@ object TactixLibrary extends HilbertCalculus with SequentCalculus {
         | (must(normalize)
         | (loopauto('R)
         | (ODE('R)
-        | (diffSolve('L) // somehow ODE aborts before even trying diffSolve
+        | (diffSolve('L)
         | exhaustiveEqL2R('L) ) ) ) ) ) ) ))*) &
       ?(OnAll(QE)) & done
   }
