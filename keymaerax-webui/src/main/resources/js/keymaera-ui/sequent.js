c140a542c6d571ac96c4b024bfb6b68ecce04cee
Stefan Mitsch <smitsch@cs.cmu.edu>
2017-02-10 16:01:51 -0500

    Removes modal formula editing dialog

--- keymaerax-webui/src/main/resources/js/keymaera-ui/sequent.js
+++ keymaerax-webui/src/main/resources/js/keymaera-ui/sequent.js
@@ -99,22 +99,6 @@ angular.module('sequent', ['ngSanitize', 'formula', 'ui.bootstrap', 'ngCookies',
               return true;
             }
 
-            scope.transformFormula = function(formulaId, formula, isAnte) {
-              var modal = $uibModal.open({
-                templateUrl: 'templates/transformFormula.html',
-                controller: 'TransformFormulaCtrl',
-                size: 'md',
-                resolve: {
-                  formula: function() { return formula; },
-                  isAnte: function() { return isAnte; }
-                }
-              });
-
-              modal.result.then(function(input) {
-                scope.onApplyInputTactic({formulaId: formulaId, tacticId: 'transform', input: input});
-              });
-            }
-
             turnstileTooltipOpen = false;
             scope.turnstileDragEnter = function(dragData) { turnstileTooltipOpen = true; }
             scope.turnstileDragLeave = function(dragData) { turnstileTooltipOpen = false; }
