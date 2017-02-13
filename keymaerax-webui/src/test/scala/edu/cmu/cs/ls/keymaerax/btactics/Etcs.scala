d90736ce91a3234584bf48f96a0b78e5cb8f0fe4
Stefan Mitsch <smitsch@cs.cmu.edu>
2017-02-13 14:27:03 -0500

    ETCS with acceleration disturbance: add constant disturbance in ODE

--- keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/btactics/Etcs.scala
+++ keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/btactics/Etcs.scala
@@ -128,12 +128,12 @@ class Etcs extends TacticTestBase {
     proveBy(s, master()) shouldBe 'proved
   }
 
-  it should "prove safety with piecewise constant actuation disturbance" in withMathematica { _ =>
+  it should "prove safety with piecewise constant actuation disturbance" in withZ3 { _ =>
     val s = parseToSequent(getClass.getResourceAsStream("/examples/casestudies/etcs/rephrased/safety-lemma-disturbed-simplified-piecewise.kyx"))
     proveBy(s, master()) shouldBe 'proved
-    //@note tactic solves early to avoid repeated effort (need delayed tactic execution) - ca. 3x faster
-    val tactic = BelleParser(io.Source.fromInputStream(getClass.getResourceAsStream("/examples/casestudies/etcs/rephrased/safety-lemma-disturbed-simplified-piecewise.kyt")).mkString)
-    proveBy(s, tactic) shouldBe 'proved
+    //@note tactic solves early to avoid repeated effort (need delayed tactic execution)
+    //val tactic = BelleParser(io.Source.fromInputStream(getClass.getResourceAsStream("/examples/casestudies/etcs/rephrased/safety-lemma-disturbed-simplified-piecewise.kyt")).mkString)
+    //proveBy(s, tactic) shouldBe 'proved
   }
 
   "ETCS ModelPlex" should "synthesize a ctrl monitor from essentials" in withMathematica { tool =>
