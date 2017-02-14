8edeac0a31e5375c85253c333239277a7e044945
Stefan Mitsch <smitsch@cs.cmu.edu>
2017-02-13 20:24:29 -0500

    Shortcut command line launcher for JDK 1.7 compatibility startup

---
+++ keymaerax-webui/src/main/scala/KeYmaeraX.scala
@@ -0,0 +1,13 @@
+/**
+  * Copyright (c) Carnegie Mellon University.
+  * See LICENSE.txt for the conditions of this license.
+  */
+
+/**
+  * Package- and argument-less shortcut.
+  */
+object KeYmaeraX {
+  def main (args: Array[String]): Unit = {
+    edu.cmu.cs.ls.keymaerax.launcher.KeYmaeraX.main(args ++ ("-launch" :: "-ui" :: Nil))
+  }
+}
