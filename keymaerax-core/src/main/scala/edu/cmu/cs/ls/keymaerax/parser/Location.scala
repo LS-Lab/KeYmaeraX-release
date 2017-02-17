a54b9a81ea5eb0b5d1dfafd32f270930401f2d8e
Nathan Fulton <gitcommit@nfulton.org>
2017-02-17 11:43:09 -0500

    Adds an addLines : Int => Location to the Location interface.
    For computing a new location by offsetting that location by some number of
    lines. Useful for problem and solution file parsing.

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/Location.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/Location.scala
@@ -16,6 +16,10 @@ sealed trait Location extends Serializable {
   /** The range from this region to the other region, inclusive. Starting here and ending at other. */
   //@todo review choices
   def --(other: Location): Location
+
+  /** Returns the current location offset by `numLines` lines. */
+  def addLines(numLines : Int) : Location
+
   /** whether other location is adjacent to this location with no whitespace in between */
   def adjacentTo(other: Location): Boolean = end match {
     case Region(_,_,l,c) => other.begin match {
@@ -25,12 +29,14 @@ sealed trait Location extends Serializable {
 
   def spanTo(other: Location) = Region(this.begin.line, this.begin.column, other.begin.line, other.begin.column)
 }
+
 object UnknownLocation extends Location {
   override def toString = "<somewhere>"
   def begin = this
   def end = this
   def line = -1
   def column = -1
+  def addLines(numLines: Int): Location = this
   def --(other: Location): Location = this
 }
 case class Region(line: Int, column: Int, endLine: Int, endColumn: Int) extends Location {
@@ -43,6 +49,8 @@ case class Region(line: Int, column: Int, endLine: Int, endColumn: Int) extends 
     case _: SuffixRegion => this
     case UnknownLocation => UnknownLocation
   }
+  def addLines(numLines: Int): Location = Region(line + numLines, column, endLine + numLines, endColumn)
+
   override def toString = line + ":" + column + (if (column!=endColumn || line!=endLine) " to " + endLine + ":" + endColumn else "")
 }
 /**
@@ -54,5 +62,6 @@ case class SuffixRegion(line: Int, column: Int) extends Location {
   def begin = Region(line, column, line, column)
   def end = UnknownLocation
   def --(other: Location) = this
+  def addLines(numLines: Int): Location = SuffixRegion(line + numLines, column)
   override def toString = line + ":" + column + " to " + EOF
 }
