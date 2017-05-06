e56761dc99ae0d6c49823ba307a513a18af24bc9
Stefan Mitsch <smitsch@cs.cmu.edu>
2017-05-06 11:02:58 -0400

    QELogger inside named QE for less verbose tactic extraction

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/ToolTactics.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/ToolTactics.scala
@@ -26,9 +26,9 @@ private object ToolTactics {
   /** Performs QE and fails if the goal isn't closed. */
   def fullQE(order: List[NamedSymbol] = Nil)(qeTool: QETool): BelleExpr = {
     require(qeTool != null, "No QE tool available. Use parameter 'qeTool' to provide an instance (e.g., use withMathematica in unit tests)")
-    QELogger.getLogTactic &
     Idioms.NamedTactic("QE",
-        done | //@note don't fail QE if already proved
+      QELogger.getLogTactic &
+      (done | //@note don't fail QE if already proved
         ((alphaRule*) &
         (close |
           ((atomExhaustiveEqL2R('L)*) &
@@ -42,6 +42,7 @@ private object ToolTactics {
             )
           )
         )
+      )
   )}
   def fullQE(qeTool: QETool): BelleExpr = fullQE()(qeTool)
 
