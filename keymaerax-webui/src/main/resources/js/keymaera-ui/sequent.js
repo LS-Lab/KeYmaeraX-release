a6088d2eb8985231a14df740a62352067f32699a
Stefan Mitsch <smitsch@cs.cmu.edu>
2017-05-10 17:57:53 -0400

    Web UI: proof hints always at the top

--- keymaerax-webui/src/main/resources/js/keymaera-ui/sequent.js
+++ keymaerax-webui/src/main/resources/js/keymaera-ui/sequent.js
@@ -15,6 +15,8 @@ angular.module('sequent', ['ngSanitize', 'formula', 'ui.bootstrap', 'ngCookies',
             onApplyTwoPositionTactic: '&'
         },
         link: function(scope, elem, attr) {
+            scope.sequentSuggestions = [];
+
             derivationInfos.sequentSuggestionDerivationInfos(scope.userId, scope.proofId, scope.nodeId)
               .then(function(response) {
                 scope.sequentSuggestions = response.data;
