d21fd938f85a37ebd077206efdbcfa2404b2c89d
Nathan Fulton <gitcommit@nfulton.org>
2017-08-05 22:33:33 +0200

    Adds version management for Z3.
    Z3 is copied out of the JAR and into the ~/.keymaerax directory every
    time KeYmaera X is updated.

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/Z3Solver.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/Z3Solver.scala
@@ -7,7 +7,7 @@
   */
 package edu.cmu.cs.ls.keymaerax.tools
 
-import java.io.{File, FileOutputStream, FileWriter, InputStream}
+import java.io._
 import java.nio.channels.Channels
 import java.util.Locale
 
@@ -26,67 +26,106 @@ class Z3Solver extends SMTSolver {
 
   private val converter = DefaultSMTConverter
 
-  /** Get the absolute path to Z3 jar */
+  /** Get the absolute path to Z3 executable
+    * Copies Z3 out of the JAR if the KeYmaera X version has updated. */
   private val pathToZ3 : String = {
     val z3TempDir = System.getProperty("user.home") + File.separator + ".keymaerax"
     if(!new File(z3TempDir).exists) new File(z3TempDir).mkdirs
     val osName = System.getProperty("os.name").toLowerCase(Locale.ENGLISH)
-    if(osName.contains("windows") && new File(z3TempDir+"z3.exe").exists()) {
-      z3TempDir+"z3.exe"
-    } else if(new File(z3TempDir+"z3").exists()) {
-      z3TempDir+"z3"
-    } else {
-      val osArch = System.getProperty("os.arch")
-      var resource : InputStream = null
-      if(osName.contains("mac")) {
-        if(osArch.contains("64")) {
-          resource = this.getClass.getResourceAsStream("/z3/mac64/z3")
-        }
-      } else if(osName.contains("windows")) {
-        if(osArch.contains("64")) {
-          resource = this.getClass.getResourceAsStream("/z3/windows64/z3.exe")
-        } else {
-          resource = this.getClass.getResourceAsStream("/z3/windows32/z3.exe")
-        }
-      } else if(osName.contains("linux")) {
-        if(osArch.contains("64")) {
-          resource = this.getClass.getResourceAsStream("/z3/ubuntu64/z3")
-        } else {
-          resource = this.getClass.getResourceAsStream("/z3/ubuntu32/z3")
-        }
-      } else if(osName.contains("freebsd")) {
-        if(osArch.contains("64")) {
-          resource = this.getClass.getResourceAsStream("/z3/freebsd64/z3")
-        }
-      } else {
-        throw new Exception("Z3 solver is currently not supported in your operating system.")
+
+    val z3AbsPath =
+      if(needsUpdate(z3TempDir)) {
+        copyToDisk(osName, z3TempDir)
       }
-      if(resource == null)
-        throw new Exception("Could not find Z3 in classpath: " + System.getProperty("user.dir"))
-      val z3Source = Channels.newChannel(resource)
-      val z3Temp = {
-        if(osName.contains("windows")) {
-          new File(z3TempDir, "z3.exe")
-        } else {
-          new File(z3TempDir, "z3")
-        }
+      else if(osName.contains("windows") && new File(z3TempDir+"z3.exe").exists()) {
+        z3TempDir+"z3.exe"
+      }
+      else if(new File(z3TempDir+"z3").exists()) {
+        z3TempDir+"z3"
+      }
+      else {
+        copyToDisk(osName,z3TempDir)
       }
 
-      // Get a stream to the script in the resources dir
-      val z3Dest = new FileOutputStream(z3Temp)
-      // Copy file to temporary directory
-      z3Dest.getChannel.transferFrom(z3Source, 0, Long.MaxValue)
-      val z3AbsPath = z3Temp.getAbsolutePath
-      val permissionCmd =
-        if(osName.contains("windows")) "icacls " + z3AbsPath + " /e /p Everyone:F"
-        else "chmod u+x " + z3AbsPath
-      //@todo Could change to only modify permissions of freshly extracted files not from others that happen to preexist. It's in KeYmaera's internal folders, though.
-      Runtime.getRuntime.exec(permissionCmd)
-      z3Source.close()
-      z3Dest.close()
-      assert(new File(z3AbsPath).exists())
-      z3AbsPath
+    assert(new File(z3AbsPath).exists())
+    z3AbsPath
+  }
+
+  /** We store the last version of KeYmaera X that updated the Z3 binary, and copy over Z3 every time we notice
+    * a new version of KeYmaera X is installed.
+    * @todo We should probably check the Z3 version instead but... */
+  private def versionFile(z3TempDir:String) = new File(z3TempDir + File.separator + "z3v")
+
+  /** Returns the version of Z3 that the current system's JAR was updated from. */
+  private def needsUpdate(z3TempDir: String) = {
+    if(DEBUG) println("Updating Z3 binary...")
+    val versionWhenLastCopied =
+      if(versionFile(z3TempDir).exists()) {
+        scala.io.Source.fromFile(versionFile(z3TempDir)).mkString
+      }
+      else {
+        "Not A Version Number" //Return an invalid version number, forcing Z3 to be copied to disk.
+      }
+    //Update if the version stroed in the version file does not equal the current version.
+    !versionWhenLastCopied.equals(edu.cmu.cs.ls.keymaerax.core.VERSION)
+  }
+
+  /** Copies Z3 to the disk. */
+  private def copyToDisk(osName:String, z3TempDir:String) = {
+    //Update the version number.
+    new PrintWriter(versionFile(z3TempDir)) { write(edu.cmu.cs.ls.keymaerax.core.VERSION); close }
+    //Copy z3 binary to disk.
+    scala.io.Source.fromFile(versionFile(z3TempDir))
+    val osArch = System.getProperty("os.arch")
+    var resource : InputStream = null
+    if(osName.contains("mac")) {
+      if(osArch.contains("64")) {
+        resource = this.getClass.getResourceAsStream("/z3/mac64/z3")
+      }
+    } else if(osName.contains("windows")) {
+      if(osArch.contains("64")) {
+        resource = this.getClass.getResourceAsStream("/z3/windows64/z3.exe")
+      } else {
+        resource = this.getClass.getResourceAsStream("/z3/windows32/z3.exe")
+      }
+    } else if(osName.contains("linux")) {
+      if(osArch.contains("64")) {
+        resource = this.getClass.getResourceAsStream("/z3/ubuntu64/z3")
+      } else {
+        resource = this.getClass.getResourceAsStream("/z3/ubuntu32/z3")
+      }
+    } else if(osName.contains("freebsd")) {
+      if(osArch.contains("64")) {
+        resource = this.getClass.getResourceAsStream("/z3/freebsd64/z3")
+      }
+    } else {
+      throw new Exception("Z3 solver is currently not supported in your operating system.")
     }
+    if(resource == null)
+      throw new Exception("Could not find Z3 in classpath: " + System.getProperty("user.dir"))
+    val z3Source = Channels.newChannel(resource)
+    val z3Temp = {
+      if(osName.contains("windows")) {
+        new File(z3TempDir, "z3.exe")
+      } else {
+        new File(z3TempDir, "z3")
+      }
+    }
+
+    // Get a stream to the script in the resources dir
+    val z3Dest = new FileOutputStream(z3Temp)
+    // Copy file to temporary directory
+    z3Dest.getChannel.transferFrom(z3Source, 0, Long.MaxValue)
+    val z3AbsPath = z3Temp.getAbsolutePath
+    val permissionCmd =
+      if(osName.contains("windows")) "icacls " + z3AbsPath + " /e /p Everyone:F"
+      else "chmod u+x " + z3AbsPath
+    //@todo Could change to only modify permissions of freshly extracted files not from others that happen to preexist. It's in KeYmaera's internal folders, though.
+    Runtime.getRuntime.exec(permissionCmd)
+    z3Source.close()
+    z3Dest.close()
+    assert(new File(z3AbsPath).exists())
+    z3AbsPath
   }
 
   /** Return Z3 QE result and the proof evidence */
