c19a28e322d97730d7fb6bc1d208726425ca53c2
Brandon Bohrer <bbohrer@andrew.cmu.edu>
2017-08-23 15:34:46 -0400

    Switch to using arbitrary terms for geometry in CoasterX, removing floating point rounding grossness

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/coasterx/CoasterXSpec.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/coasterx/CoasterXSpec.scala
@@ -7,10 +7,8 @@ import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
 /* Generate dL specification for safety of CoasterX models from a file */
 object CoasterXSpec {
 
-  /* @TODO (Major) #1: Apply arbitrary amounts of rounding to model to make numbers less gross
-  *  @TODO (Major) #2: Maintain sanity of models by projecting all new straight line endpoints and arc control points
-  *    so they are tangent to the previous section
-  *  @TODO (Major) #3: Adopt arbitrary (closed) dL terms as number representation to enable exact projection.*/
+  /* @TODO (Major) #2: Maintain sanity of models by projecting all new straight line endpoints and arc control points
+  *    so they are tangent to the previous section*/
   def degToRad(theta:Number):Number = {
     Number(2*Math.PI*theta.value.doubleValue()/360.0)
   }
@@ -19,23 +17,23 @@ object CoasterXSpec {
     (Number(Math.cos(rad.value.doubleValue())), Number(Math.sin(rad.value.doubleValue())))
   }
 
-  def vecScale(xy:Point, scale:Number):Point = {
-    (Number(xy._1.value*scale.value), Number(xy._2.value*scale.value))
+  def vecScale(xy:(Term,Term), scale:Number):(Term,Term) = {
+    (Times(xy._1,scale), Times(xy._2,scale))
   }
 
-  def vecPlus(xy1:Point, xy2:Point):Point = {
-    (Number(xy1._1.value + xy2._1.value),Number(xy1._2.value + xy2._2.value))
+  def vecPlus(xy1:(Term,Term), xy2:(Term,Term)):(Term,Term) = {
+    (Plus(xy1._1,xy2._1),Plus(xy1._2, xy2._2))
   }
 
-  def min (x:Number,y:Number):Number = if (x.value < y.value) x else y
+  def min (x:Term,y:Term):Term = FuncOf(Function("min", domain = Tuple(Real,Real), interpreted = true, sort = Real), Pair(x,y))
 
-  def max (x:Number,y:Number):Number = if (x.value > y.value) x else y
+  def max (x:Term,y:Term):Term = FuncOf(Function("max", domain = Tuple(Real,Real), interpreted = true, sort = Real), Pair(x,y))
 
-  def boundingBox(points:List[Point]):(Point,Point) = {
+  def boundingBox(points:List[(Term,Term)]):((Term,Term),(Term,Term)) = {
     points.foldLeft((points.head,points.head))({case ((accbl,acctr),(x,y)) =>  ((min(accbl._1,x),min(accbl._2,y)),(max(acctr._1,x),max(acctr._2,y)))})
   }
 
-  def arcBounds(xy1:Point, xy2:Point, theta1:Number, theta2:Number):(Point,Point) = {
+  def arcBounds(xy1:Point, xy2:Point, theta1:Number, theta2:Number):((Term,Term),(Term,Term)) = {
     val rad1 = degToRad(theta1)
     val rad2 = degToRad(theta2)
     val (cx, cy) = arcCenter((xy1),(xy2))
@@ -50,8 +48,8 @@ object CoasterXSpec {
     box
   }
 
-  def arcCenter(xy1:Point, xy2:Point):Point = {
-    (Number((xy1._1.value+xy2._1.value)/2), Number((xy2._2.value+xy2._2.value)/2))
+  def arcCenter(xy1:Point, xy2:Point):(Term, Term) = {
+    (Divide(Plus(xy1._1,xy2._1),Number(2)), Divide(Plus(xy1._2,xy2._2),Number(2)))
   }
 
   def segmentOde(segBounds:(Section,(Point,Point))):Program = {
@@ -69,8 +67,9 @@ object CoasterXSpec {
         // val evol = And(LessEqual(x1,x),LessEqual(x,x2))
         ODESystem(sys, evol)
       case ArcSection(Some(ArcParam((x1,y1),(x2,y2),theta1,deltaTheta)), Some(gradient)) =>
-        val ((x3,y3),(x4,y4)) = arcBounds((x1,y1),(x2,y2),theta1,Number(theta1.value+deltaTheta.value))
-        val r = Number((x4.value-x3.value)/2)
+        //val ((x3,y3),(x4,y4)) = arcBounds((x1,y1),(x2,y2),theta1,Number(theta1.value+deltaTheta.value))
+        //val r = Number((x4.value-x3.value)/2)
+        val r = Divide(Minus(end._1,start._1),Number(2))
         val sysBase = "x' = dx*v, y' = dy*v, v' = -dy".asDifferentialProgram
         /* TODO: Set sign based on direction of arc */
         val sys = DifferentialProduct(sysBase,DifferentialProduct(AtomicODE(DifferentialSymbol(Variable("dx")), Divide("-dy*v".asTerm, r)),
@@ -89,12 +88,14 @@ object CoasterXSpec {
     Number(Math.sqrt(n.value.doubleValue()))
   }
 
-  def lineDir(xy1:Point, xy2:Point):Point = {
-    val mag = numSqrt(Number((xy2._1.value - xy1._1.value).pow(2) + (xy2._2.value - xy1._2.value).pow(2)))
-    (Number((xy2._1.value-xy1._1.value)/mag.value), Number((xy2._2.value-xy1._2.value)/mag.value))
+  def lineDir(xy1:Point, xy2:Point):(Term,Term) = {
+    val mag:Term = Power(Plus(Power(Minus(xy2._1,xy1._1),Number(2)),Power(Minus(xy2._2,xy1._2),Number(2))),Divide(Number(1),Number(2)))
+    //val mag = numSqrt(Number((xy2._1.value - xy1._1.value).pow(2) + (xy2._2.value - xy1._2.value).pow(2)))
+    (Divide(Minus(xy2._1,xy1._1),mag), Divide(Minus(xy2._2,xy1._2),mag))
   }
 
-  def segmentPre(seg:Section, v0:Number):Formula = {
+  def segmentPre(segBounds:(Section,(Point,Point)), v0:Number):Formula = {
+    val (seg, (start,end)) = segBounds
     seg match {
       case LineSection(Some(LineParam((x1,y1),(x2,y2))), Some(gradient)) =>
         /* TODO: Assert
@@ -130,7 +131,8 @@ object CoasterXSpec {
         (cx - xend)^2 + (cy - yend)^2 = r^2
         v0 > 0      /* positive velocity initially */
         */
-        val ((x3,y3),(x4,y4)) = arcBounds((x1,y1),(x2,y2),theta1,Number(theta1.value+deltaTheta.value))
+        // ((x1,y1),(x2,y2),theta1,Number(theta1.value+deltaTheta.value))
+        val ((x3,y3),(x4,y4)) = (start,end)
         val (cx, cy) = arcCenter((x1,y1),(x2,y2))
         val dx = Variable("dx")
         val dy = Variable("dy")
@@ -148,13 +150,13 @@ object CoasterXSpec {
     }
   }
 
-  def slope(xy1:Point, xy2:Point):Number = {
-    Number((xy1._2.value-xy2._2.value)/(xy1._1.value-xy2._1.value))
+  def slope(xy1:Point, xy2:Point):Term = {
+    Divide(Minus(xy1._2, xy2._2),Minus(xy1._1, xy2._1))
   }
 
   /* y-intercept of segment */
-  def yOffset(xy1:Point, xy2:Point):Number = {
-    Number(xy1._2.value - (slope(xy1,xy2).value*xy1._1.value))
+  def yOffset(xy1:Point, xy2:Point):Term = {
+    Minus(xy1._2,Times(slope(xy1,xy2), xy1._1))
   }
 
   def segmentPost(segBounds:(Section,(Point,Point))):Formula = {
@@ -168,11 +170,14 @@ object CoasterXSpec {
         val onTrack = Equal(Times(dxval, y), Plus(Times(dyval, x), yOffset((x1, y1), (x2, y2))))
         Imply(inRange, onTrack)
       case ArcSection(Some(ArcParam((x1, y1), (x2, y2), theta1, deltaTheta)), Some(gradient)) =>
-        val ((x3, y3), (x4, y4)) = arcBounds((x1, y1), (x2, y2), theta1, Number(theta1.value+deltaTheta.value))
+        val ((x3, y3), (x4, y4)) = (start,end)
+          //val ((x3, y3), (x4, y4)) =arcBounds((x1, y1), (x2, y2), theta1, Number(theta1.value+deltaTheta.value))
+
         val inRange = And(LessEqual(x3, x), LessEqual(x, x4))
-        val r = Number((x4.value - x3.value) / 2)
+        val r = Divide(Minus(end._1,start._1),Number(2))
+        //  val r = Number((x4.value - x3.value) / 2)
         val (cx, cy) = arcCenter((x1, y1), (x2, y2))
-        val centered = Equal(Plus(Power(Minus(cx, x), Number(2)), Power(Minus(cy, y), Number(2))), Number(r.value * r.value))
+        val centered = Equal(Plus(Power(Minus(cx, x), Number(2)), Power(Minus(cy, y), Number(2))), Power(r,Number(2)))
         val outRange = And(LessEqual(x, cx), Less(cy, y))
         /* (x0 + l <= x & x <= xend) -> ((cx - x)^2 + (cy - y)^2 = r^2 & x <= cx & cy < y))*/
         Imply(inRange, And(centered, outRange))
@@ -206,7 +211,7 @@ object CoasterXSpec {
     val (points, segments, v0, _) = fileRounded
     val nonemptySegs = segments.filter(!segmentEmpty(_))
     val withBounds = zipConsecutive(nonemptySegs,points)
-    val pre = segmentPre(nonemptySegs.head,v0)
+    val pre = segmentPre(withBounds.head,v0)
     val ode = withBounds.map(segmentOde).reduceRight[Program]({case(x,y) => Choice(x,y)})
     val energyConserved = "v^2 + 2*y = v0^2 + 2*y0".asFormula
     val globalPost = And("v > 0".asFormula, energyConserved)
