7a57b371efa47a3ca8a635512f383512784e33c6
Brandon Bohrer <bbohrer@andrew.cmu.edu>
2017-10-01 18:11:41 -0400

    Complete first draft of repeatability evaluation/command line for coasterx

--- keymaerax-webui/src/main/scala/edu/cmu/cs/ls/keymaerax/launcher/CoasterXMain.scala
+++ keymaerax-webui/src/main/scala/edu/cmu/cs/ls/keymaerax/launcher/CoasterXMain.scala
@@ -1,12 +1,11 @@
 package edu.cmu.cs.ls.keymaerax.launcher
 
-import edu.cmu.cs.ls.keymaerax.btactics.coasterx.CoasterXTestLib.doStats
+import edu.cmu.cs.ls.keymaerax.btactics.coasterx.CoasterXTestLib.{CoasterStats, ComponentStats, countVars, doStats}
 import edu.cmu.cs.ls.keymaerax.btactics.coasterx.{CoasterXParser, CoasterXProver, CoasterXSpec, CoasterXTestLib}
-import edu.cmu.cs.ls.keymaerax.btactics.coasterx.CoasterXTestLib.{countVars, doStats}
 import edu.cmu.cs.ls.keymaerax.parser.KeYmaeraXPrettyPrinter
 import edu.cmu.cs.ls.keymaerax.btactics._
 import edu.cmu.cs.ls.keymaerax.launcher._
-import edu.cmu.cs.ls.keymaerax.launcher.DefaultConfiguration
+import edu.cmu.cs.ls.keymaerax.pt.ProvableSig
 import edu.cmu.cs.ls.keymaerax.tools.Mathematica
 
 /** Command-line functionality of CoasterX, both for repeatability evaluation purposes and general usage
@@ -68,7 +67,49 @@ object CoasterXMain  {
   }
 
   def printTable2():Unit = {
+    val numRuns = 10
+    val coastersToTest:List[(String, Double)] = List(
+      (CoasterXTestLib.exampleFile1, 1.0),
+      (CoasterXTestLib.byrc, 0.08333333333),
+      (CoasterXTestLib.lilPhantom, 1.0),
+      (CoasterXTestLib.phantomsRevenge, 2.0),
+      (CoasterXTestLib.elToro,1.0)
+    )
+    var coasterStats:List[CoasterStats] = Nil
+    var componentStats:List[ComponentStats] = Nil
 
+    coastersToTest.foreach { case (coast:String, feetPerUnit) =>
+      val fileContents = scala.io.Source.fromFile(coast).getLines().mkString("\n")
+      val proof = CoasterXTestLib.prover(fileContents, "Coaster From Command Line", doFast = false, NUM_RUNS = numRuns,
+        feetPerUnit = feetPerUnit, velocity = None, doFormula = true, doStats = true, Some({case cs => coasterStats = cs :: coasterStats; cs.proof}))
+      assert(proof.isProved, "CoasterX prover did not prove model :'( ")
+    }
+    val spec = new CoasterXSpec(1.0)
+    val parsed = CoasterXParser.parseFile(CoasterXTestLib.straightLine).get
+    val (align,alignFml) = spec.prepareFile(parsed)
+    val env = spec.envelope(align)
+    val prFast = new CoasterXProver(spec,env, reuseComponents = false)
+    val componentsToTest:List[(String, (() => ProvableSig))] = List(
+      ("straight", () => prFast.straightProof),
+      ("Q1", () => prFast.arcProofQ1),
+      ("Q2", () => prFast.arcProofQ2),
+      ("Q3", () => prFast.arcProofQ3),
+      ("Q4", () => prFast.arcProofQ4)
+    )
+    componentsToTest.foreach({case (name, f) =>
+        doStats(name, f, doFormula = true, doTactic = true, willDoStats = true, numRuns = numRuns, Some({case cs => componentStats = cs :: componentStats}))
+    })
+
+    // Begin printing
+    println("Model\tSections\tVars\tTime(s)\tTime-NoReuse(s)\tSpeedup(%)\tSteps\tSize(KB)")
+    coasterStats.foreach{case CoasterStats(name,env,nSections,nVars,fastTimes,fastMean,slowTimes,slowMean,stepsFast,stepsSlow,speedupPercent,proof,size) =>
+      println(s"$name\t$nSections\t$nVars\t$fastMean\t$slowMean\t$speedupPercent\t$stepsFast\t$size")
+    }
+    println("")
+    println("Comp.\tVars\tTime(s)\tSteps\tSize(KB)")
+    componentStats.foreach{case ComponentStats(name,alltimes,meantime,steps,vars,size,tac,seq) =>
+      println(s"$name\t$vars\t$meantime\t$steps\t$size")
+    }
   }
 
   def main(options:Map[Symbol,Any]):Unit = {
@@ -89,11 +130,9 @@ object CoasterXMain  {
         val compareReuse = options.get('compareReuse).nonEmpty
         val velocity = options.get('velocity).asInstanceOf[Option[String]]
         val numRuns = options.getOrElse('numRuns, "1").asInstanceOf[String].toInt
-
         proveCoaster(fileName, doFormula = doFormula, doStats = doStats, compareReuse = compareReuse, feetPerUnit.toDouble, velocity.map(_.toDouble), numRuns)
       case "table2" => printTable2()
     }
   })
   }
-
 }
