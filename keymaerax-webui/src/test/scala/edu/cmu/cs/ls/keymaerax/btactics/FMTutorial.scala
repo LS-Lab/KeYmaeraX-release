e4c137eacaf5ac3468d9656e22403ebba2ab37f3
Stefan Mitsch <smitsch@cs.cmu.edu>
2017-12-08 16:58:23 -0500

    FM tutorial tests cleanup

--- keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/btactics/FMTutorial.scala
+++ keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/btactics/FMTutorial.scala
@@ -19,18 +19,12 @@ import scala.language.postfixOps
 @SlowTest
 class FMTutorial extends TacticTestBase {
 
-  "Example 1" should "be provable with master" in withMathematica { _ => withDatabase { db =>
+  "Example 1" should "be provable with master" in withQE { _ => withDatabase { db =>
     val modelContent = io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/00_stopsignsbfalse.kyx")).mkString
     db.proveBy(modelContent, master()) shouldBe 'proved
   }}
 
-  it should "be provable from parsed tactic" in withMathematica { _ => withDatabase { db =>
-    val modelContent = io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/00_stopsignsbfalse.kyx")).mkString
-    val tactic = BelleParser(io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/00_stopsignsbfalse.kyt")).mkString)
-    db.proveBy(modelContent, tactic) shouldBe 'proved
-  }}
-
-  it should "be provable from parsed tactic with Z3" in withZ3 { _ => withDatabase { db =>
+  it should "be provable from parsed tactic" in withQE { _ => withDatabase { db =>
     val modelContent = io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/00_stopsignsbfalse.kyx")).mkString
     val tactic = BelleParser(io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/00_stopsignsbfalse.kyt")).mkString)
     db.proveBy(modelContent, tactic) shouldBe 'proved
@@ -41,13 +35,11 @@ class FMTutorial extends TacticTestBase {
     val tactic = BelleParser(io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/01_stopsignabstractsb.kyt")).mkString)
     val result = db.proveBy(modelContent, tactic)
     result.subgoals should have size 2
-    result.subgoals.head.ante shouldBe empty
-    result.subgoals.head.succ should contain only "m-x>=A/2*ep^2+ep*v".asFormula
-    result.subgoals.last.ante shouldBe empty
-    result.subgoals.last.succ should contain only "m-x>=v^2/(2*b)".asFormula
+    result.subgoals(0) shouldBe "==> m-x>=A/2*ep^2+ep*v".asSequent
+    result.subgoals(1) shouldBe "==> m-x>=v^2/(2*b)".asSequent
   }}
 
-  "Example 3" should "be provable with master" in withMathematica { _ => withDatabase { db =>
+  "Example 3" should "be provable with master" in withQE { _ => withDatabase { db =>
     val modelContent = io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/02_stopsign.kyx")).mkString
     db.proveBy(modelContent, master()) shouldBe 'proved
   }}
@@ -58,36 +50,18 @@ class FMTutorial extends TacticTestBase {
     db.proveBy(modelContent, tactic) shouldBe 'proved
   }}
 
-  it should "be provable from parsed tactic with Z3" in withZ3 { _ => withDatabase { db =>
-    val modelContent = io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/02_stopsign.kyx")).mkString
-    val tactic = BelleParser(io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/02_stopsign.kyt")).mkString)
-    db.proveBy(modelContent, tactic) shouldBe 'proved
-  }}
-
-  "Example 4" should "be provable with master" in withMathematica { _ => withDatabase { db =>
+  "Example 4" should "be provable with master" in withQE { _ => withDatabase { db =>
     val modelContent = io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/03_increasinglydampedoscillator.kyx")).mkString
     db.proveBy(modelContent, master()) shouldBe 'proved
   }}
 
-  it should "be provable from parsed tactic" in withMathematica { _ => withDatabase { db =>
+  it should "be provable from parsed tactic" in withQE { _ => withDatabase { db =>
     val modelContent = io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/03_increasinglydampedoscillator.kyx")).mkString
     val tactic = BelleParser(io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/03_increasinglydampedoscillator.kyt")).mkString)
     db.proveBy(modelContent, tactic) shouldBe 'proved
   }}
 
-  it should "be provable from parsed tactic with Z3" in withZ3 { _ => withDatabase { db =>
-    val modelContent = io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/03_increasinglydampedoscillator.kyx")).mkString
-    val tactic = BelleParser(io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/03_increasinglydampedoscillator.kyt")).mkString)
-    db.proveBy(modelContent, tactic) shouldBe 'proved
-  }}
-
-  "Example 5" should "be provable from parsed tactic" in withMathematica { _ => withDatabase { db =>
-    val modelContent = io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/04_car.kyx")).mkString
-    val tactic = BelleParser(io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/04_car.kyt")).mkString)
-    db.proveBy(modelContent, tactic) shouldBe 'proved
-  }}
-
-  it should "be provable from parsed tactic with Z3" in withZ3 { _ => withDatabase { db =>
+  "Example 5" should "be provable from parsed tactic" in withQE { _ => withDatabase { db =>
     val modelContent = io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/04_car.kyx")).mkString
     val tactic = BelleParser(io.Source.fromInputStream(getClass.getResourceAsStream("/examples/tutorials/fm/04_car.kyt")).mkString)
     db.proveBy(modelContent, tactic) shouldBe 'proved
