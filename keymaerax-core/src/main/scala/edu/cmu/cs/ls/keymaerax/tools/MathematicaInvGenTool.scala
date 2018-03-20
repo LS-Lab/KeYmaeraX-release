45d4c03554ba8dd480a5c61623e25df9daef5227
Stefan Mitsch <smitsch@cs.cmu.edu>
2018-03-20 13:25:50 -0400

    Pegasus invariant generator logging

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/MathematicaInvGenTool.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/MathematicaInvGenTool.scala
@@ -1,32 +1,25 @@
 package edu.cmu.cs.ls.keymaerax.tools
 
-import com.wolfram.jlink.Expr
 import edu.cmu.cs.ls.keymaerax.core._
-import edu.cmu.cs.ls.keymaerax.parser.{KeYmaeraXParser, StringConverter}
-import edu.cmu.cs.ls.keymaerax.tools.MathematicaConversion.{KExpr, MExpr}
-
-import scala.collection.immutable
+import edu.cmu.cs.ls.keymaerax.tools.MathematicaConversion.KExpr
+import org.apache.logging.log4j.scala.Logging
 
 /**
  * A continuous invariant implementation using Mathematica over the JLink interface.
  * @author Andrew Sogokon, based on QETool by Nathan Fulton and Stefan Mitsch
  */
 class MathematicaInvGenTool(override val link: MathematicaLink)
-  extends BaseKeYmaeraMathematicaBridge[KExpr](link, KeYmaeraToMathematica, MathematicaToKeYmaera) with InvGenTool {
+  extends BaseKeYmaeraMathematicaBridge[KExpr](link, KeYmaeraToMathematica, MathematicaToKeYmaera)
+    with InvGenTool with Logging {
 
-  def invgen(problem:String): Formula = {
-
+  def invgen(problem: String): Formula = {
     try {
       val (output, result) = runUnchecked(problem)
-      print ("\nGenerated invariant: "+ result.toString + "\n")
-      //new StringConverter(output.toString).asFormula
+      logger.debug("\nGenerated invariant: "+ result.prettyString + "\n" + "from raw output " + output + "\n")
       result match {
-        case continuousInvariant: Formula =>
-          if (DEBUG) {
-            println(s"Invariant: " + continuousInvariant.prettyString)
-          }
-          continuousInvariant
-        case _ => throw ToolException("Expected a formula from Pegasus call but got a non-formula expression."+ "as")
+        case continuousInvariant: Formula => continuousInvariant
+        case _ => throw ToolException("Expected a formula from Pegasus call but got a non-formula expression: " +
+          result.prettyString)
       }
     } //finally { input.dispose() }
   }
