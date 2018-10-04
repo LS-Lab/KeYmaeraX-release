6d777a0149944cf5e3797cb5b8d31c0384978837
Stefan Mitsch <smitsch@cs.cmu.edu>
2018-10-04 09:39:12 -0400

    Fixes parser type analysis error message test

--- keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/parser/KeYmaeraXArchiveParserTests.scala
+++ keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/parser/KeYmaeraXArchiveParserTests.scala
@@ -1513,11 +1513,9 @@ class KeYmaeraXArchiveParserTests extends TacticTestBase {
         |  [{?true;}*@invariant(fg > 0)]true
         |End.""".stripMargin
     ) should have message """<somewhere> type analysis: undefined symbol fg with index None
-                            |Found:    undefined symbol
-                            |Expected: class edu.cmu.cs.ls.keymaerax.core.BaseVariable of sort Real
                             |Make sure to declare all variables in ProgramVariable and all symbols in Definitions block.
-                            |Found:    <unknown> at <somewhere>
-                            |Expected: <unknown>""".stripMargin
+                            |Found:    undefined symbol at <somewhere>
+                            |Expected: BaseVariable of sort Real""".stripMargin
   }
 
 
