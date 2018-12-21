4c0ca7213ef028d7c00ee58ecc1dbb225d567edb
Andre Platzer <aplatzer@cs.cmu.edu>
2018-12-21 15:26:06 -0500

    Split off hints in parser exceptions

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/ParserErrors.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/ParserErrors.scala
@@ -16,8 +16,9 @@ import edu.cmu.cs.ls.keymaerax.parser.KeYmaeraXParser.{ParseState, TokenStream}
  * @see [[ProverException.getContext]]
   * @todo make hint a new constructor argument
  */
-case class ParseException (msg: String, loc: Location, found: String/*Token*/, expect: String/**/, after: String/*ParseState*/, state: String/*ParseState*/, cause: Throwable = null)
-  extends ProverException(loc.begin + " " + msg + "\nFound:    " + found + " at " + loc + "\nExpected: " + expect, cause) {
+case class ParseException (msg: String, loc: Location, found: String/*Token*/, expect: String/**/, after: String/*ParseState*/, state: String/*ParseState*/,
+                           cause: Throwable = null, hint: String = "")
+  extends ProverException(loc.begin + " " + msg + "\nFound:    " + found + " at " + loc + "\nExpected: " + expect + (if (hint=="") "" else "\nHint: " + hint), cause) {
   /**
     * Add the input context information to this exception, returning the resulting exception to be thrown.
     * @param input textual description of the input in which this prover exception occurred.
@@ -50,6 +51,10 @@ case class ParseException (msg: String, loc: Location, found: String/*Token*/, e
 }
 
 object ParseException {
+//  def apply(msg: String, loc: Location, found: Token, expect: String/**/, after: ParseState, state: ParseState,
+//            cause: Throwable = null, hint: String = ""): ParseException =
+//    new ParseException(msg, loc, found=found + "", expect=expect, after=after + "", state=state + "", cause, hint)
+
   def apply(msg: String, state: ParseState /*, cause: Throwable = null*/): ParseException =
     new ParseException(msg, state.location, state.la.toString, "", state.topString, state.toString /*, cause*/)
 
@@ -83,17 +88,17 @@ object ParseException {
 
   /** Imbalanced parentheses parse errors: opening `unmatched` expects closing `expect` at the latest at location of current parse state (location is unmatched) */
   def imbalancedError(msg: String, unmatched: Token, expect: String, state: ParseState, hint: String = ""): ParseException = if (state.la.tok == EOF)
-    new ParseException(msg + (if(hint=="") "" else "\n" + hint), unmatched.loc, found=unmatched.toString, expect=expect, after=state.topString, state=state.toString /*, cause*/)
+    new ParseException(msg, unmatched.loc, found=unmatched.toString, expect=expect, after=state.topString, state=state.toString, null, hint=hint)
   else
-    new ParseException(msg + "\nunmatched: " + unmatched + " at " + unmatched.loc + "--" + state.location + (if(hint=="") "" else "\n" + hint), unmatched.loc--state.location, found=state.la.toString, expect=expect, after=state.topString, state.toString /*, cause*/)
+    new ParseException(msg + "\nunmatched: " + unmatched + " at " + unmatched.loc + "--" + state.location, unmatched.loc--state.location, found=state.la.toString, expect=expect, after=state.topString, state.toString, hint=hint /*, cause*/)
 
   /** Imbalanced parentheses parse errors needed here: opening `unmatched` expects closing `expect` exactly at the location of current parse state (location is state.location) */
   def imbalancedErrorHere(msg: String, unmatched: Token, expect: String, state: ParseState, hint: String = ""): ParseException =
-    new ParseException(msg + "\nunmatched: " + unmatched + " at " + unmatched.loc + "--" + state.location + (if(hint=="") "" else "\n" + hint), if (false) unmatched.loc--state.location else state.location, found=state.la.toString, expect=expect, after=state.topString, state.toString /*, cause*/)
+    new ParseException(msg + "\nunmatched: " + unmatched + " at " + unmatched.loc + "--" + state.location, if (false) unmatched.loc--state.location else state.location, found=state.la.toString, expect=expect, after=state.topString, state.toString, null, hint=hint)
 
   /** Type parse errors */
   private def typeException(msg: String, loc: Location, found: String, expect: String, hint: String = "", cause: Throwable = null) =
-    new ParseException("type analysis: " + msg + (if(hint=="") "" else "\n" + hint), loc, found, expect, "", "", cause)
+    new ParseException("type analysis: " + msg, loc, found, expect, "", "", cause, hint=hint)
 
   /** Type parse error with mismatch in found type illtyped and expected type */
   def typeError(msg: String, illtyped: Expression, expectedType: String, loc: Location, hint: String = ""): ParseException =
