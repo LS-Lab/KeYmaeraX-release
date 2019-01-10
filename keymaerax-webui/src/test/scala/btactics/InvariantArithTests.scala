d6a9e5b2bf45326915f701a3888c1566cfd7c713
Stefan Mitsch <smitsch@cs.cmu.edu>
2019-01-10 15:45:55 -0500

    Compile fix: marks slow arithmetic tests

--- keymaerax-webui/src/test/scala/btactics/InvariantArithTests.scala
+++ keymaerax-webui/src/test/scala/btactics/InvariantArithTests.scala
@@ -6,7 +6,6 @@ import edu.cmu.cs.ls.keymaerax.btactics.TactixLibrary._
 import edu.cmu.cs.ls.keymaerax.btactics._
 import edu.cmu.cs.ls.keymaerax.core._
 import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
-import edu.cmu.cs.ls.keymaerax.tags.SlowTest
 import testHelper.KeYmaeraXTestTags.{IgnoreInBuildTest, SlowTest}
 
 import scala.collection.immutable._
@@ -16,7 +15,7 @@ import scala.collection.immutable._
   * From Lectures 10/11
   * Created by yongkiat on 2/3/16.
   */
-@SlowTest
+@edu.cmu.cs.ls.keymaerax.tags.SlowTest
 class InvariantArithTests extends TacticTestBase {
 
 
