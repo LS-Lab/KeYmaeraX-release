d87fe8f0f2e1125a0d7ac2edde735da55f374aff
Yong Kiam <tanyongkiam@gmail.com>
2019-04-04 17:47:55 -0400

    Unify interface to barrier certificates a little
    - Minimize rounding inside the programs

--- keymaerax-core/src/main/resources/Pegasus/Primitives/BarrierCertificates.m
+++ keymaerax-core/src/main/resources/Pegasus/Primitives/BarrierCertificates.m
@@ -7,8 +7,8 @@ Needs["MATLink`"]
 BeginPackage["BarrierCertificates`"];
 
 
-SOSBarrier::usage="SOSBarrier[problem_List] uses an interface to Matlab (MatLink plugin required!) to compute barrier certificates.";
-Options[SOSBarrier]= {NPrecision -> 6, Lambda -> {}};
+SOSBarrierMATLAB::usage="SOSBarrierMATLAB[problem_List] uses an interface to Matlab (MatLink plugin required!) to compute barrier certificates.";
+Options[SOSBarrierMATLAB]= {Lambda -> {}, MaxDeg -> 10};
 
 
 BTemplate::usage="BTemplate[deg_, vars_List] Polynomial template with symbolic coefficients (of degree at most 'deg')";
@@ -19,9 +19,11 @@ LPGenVec::usage="LPGen returns an LP problem for vector barrier certificate sear
 
 
 LPBarrier::usage="LPBarrier[deg_, problem_List] Linear relaxation for barrier certificate search up to degree 'deg'";
+Options[LPBarrier]= {Lambda -> {}, MaxDeg -> 5};
 
 
 LPBarrierMATLAB::usage="LPBarrierMATLAB[deg_, problem_List] Linear relaxation for barrier certificate search up to degree 'deg'";
+Options[LPBarrierMATLAB]= {Lambda -> {}, MaxDeg -> 5};
 
 
 HandelmanRepEps::usage="HandelmanRepEps[BTemp_, vars_List, deg_Integer, constraints_List, eps_] Handelman representation of a template polynomial BTemp on a hyperbox given by constraints.";
@@ -39,7 +41,7 @@ ExtractPolys::usage="ExtractPolys[set_] extracts the LHS of inequalities in set"
 Begin["`Private`"];
 
 
-Pegasus`NPRECISION=6; (* Default precision for numericising rationals *)
+DEFAULTPRECISION = 10;
 
 
 (* Return all monomials of a given polynomial wrt the variables *)
@@ -51,7 +53,7 @@ MmaToMatlab[list_List]:="["<>StringRiffle[Map[MmaToMatlab, list],"; "]<>"]"
 MmaToMatlab[Power[trm_,exp_]]:=MmaToMatlab[trm]<>"^("<>MmaToMatlab[exp]<>")"
 MmaToMatlab[Times[product_]]:= "("<>StringRiffle[MmaToMatlab /@ (List @@ product), "*"]<>")"
 MmaToMatlab[product_Plus]:= "("<>StringRiffle[MmaToMatlab /@ (List @@ product), "+"]<>")"
-MmaToMatlab[rat_Rational]:=InputForm[N[rat, Pegasus`NPRECISION], NumberMarks->False]//ToString
+MmaToMatlab[rat_Rational]:=InputForm[N[rat, DEFAULTPRECISION], NumberMarks->False]//ToString
 MmaToMatlab[atom_?AtomQ]:=ToString[atom//InputForm]
 
 (* Relational symbols *)
@@ -92,7 +94,7 @@ If[ConjunctiveIneqSetQ[S],
 ]]
 
 
-HeuristicMonomials[vars_,vf_,degree_]:=Module[ {},
+HeuristicMonomials[vars_,vf_]:=Module[ {},
 DeleteDuplicates[Flatten[Function[x,monomialList[x,vars]]/@vf]]]
 
 
@@ -100,8 +102,8 @@ HeuristicLambdas[vars_,vf_]:=Module[ {},
 DeleteDuplicates[{-1,0,1,Div[vf,vars],-Div[vf,vars]}]]
 
 
-SOSBarrier[{ pre_, { vf_List, vars_List, evoConst_ }, post_}, opts:OptionsPattern[]]:=Catch[Module[
-{init,unsafe,Q,f, precision,sosprog,res,lines,B, link, barrierscript,heumons,heulambdas},
+SOSBarrierMATLAB[{ pre_, { vf_List, vars_List, evoConst_ }, post_}, opts:OptionsPattern[]]:=Catch[Module[
+{init,unsafe,Q,f,sosprog,res,lines,B, link, barrierscript,heumons,heulambdas},
 
 Print["Attempting to generate a barrier certificate with SOS Programming"];
 
@@ -112,12 +114,10 @@ Q=If[TrueQ[evoConst],{}, ExtractPolys[evoConst]];
 (* Open a link to Matlab *)
 link=MATLink`OpenMATLAB[];
 
-precision=OptionValue[NPrecision];
-
 f=MmaToMatlab[vf];
 
-heumons = HeuristicMonomials[vars,vf,2];
-heulambdas = If[OptionValue[Lambda]==={},HeuristicLambdas[vars,vf], {OptionValue[Lambda]}];
+heumons = HeuristicMonomials[vars,vf];
+heulambdas = If[OptionValue[Lambda]==={}, HeuristicLambdas[vars,vf], {OptionValue[Lambda]}];
 
 sosprog="
 % Exponential barrier certificates
@@ -143,7 +143,7 @@ dom = "<>MmaToMatlab[Q]<>" ;
 lambdas = "<>MmaToMatlab[heulambdas]<>";
 % min and max degree bounds (incremented by 1 each time)
 mindeg = 0;
-maxdeg = 10;
+maxdeg = "<>MmaToMatlab[OptionValue[MaxDeg]]<>";
 % Additional monomials to use for the barrier certificate
 monheu =  "<>MmaToMatlab[heumons]<>";
 % Additional SOS basis monomials for SOS-variables
@@ -230,13 +230,10 @@ barrierscript=MATLink`MScript["expbarrier",sosprog, "Overwrite" -> True];
 (* Print[sosprog]; *)
 res=MATLink`MEvaluate@barrierscript;
 lines=StringSplit[res,{"B2 =", "break"}];
-B=CoefficientRules[N[StringReplace[StringDelete[lines[[-1]], "\n" | "\r" |" "], {"e-"->"*10^-", "backtick"->"`"}]//ToExpression//Expand, 10],vars];
+B=N[StringReplace[StringDelete[lines[[-1]], "\n" | "\r" |" "], {"e-"->"*10^-", "backtick"->"`"}]//ToExpression//Expand, DEFAULTPRECISION];
 If[B=={},
-  Print["No feasible solution found by SOS programming."];
-  Throw[{}],
-  Throw[
-  {MapAt[Function[x,Rationalize[Round[x,1/10^precision]]],B,{All,2}]~FromCoefficientRules~vars}]
-  ];
+  Print["No feasible solution found by SOS programming."]];
+  Throw[{B}];
 ]]
 
 
@@ -277,8 +274,8 @@ prob = lambdaB
 
 (* Common implementation returning an LP problem *)
 LPGen[{ pre_, { vf_List, vars_List, evoConst_ }, post_},deg_,eps_,lambda_]:=Catch[Module[
-{init,unsafe,Q,ibox,ubox,qbox,B,LfB,BC1,BC2,BC3},
-Print["Setting up an LP problem for BC generation. Degree ",deg," Eps ",eps," Lambda ",lambda];
+{init,unsafe,Q,ibox,ubox,qbox,B,LfB,BC1,BC2,BC3,prob},
+(* Print["Setting up an LP problem for BC generation. Degree ",deg," Eps ",eps," Lambda ",lambda]; *)
 
 init=ExtractPolys[pre];
 unsafe=ExtractPolys[Not[post]];
@@ -306,14 +303,13 @@ Throw[{prob,B}]
 ]]
 
 
-LPBarrier[inp_, deg_, eps_, lambda_, tolerance_, method_]:=Catch[Module[
+LPBarrierStep[inp_, deg_, eps_, lambda_, tolerance_, method_]:=Catch[Module[
 (* Declare local variables *)
 {prob,B,equations,LPvars,
 Aeq,beq,const,obbjFn,LPres,objFn,LPsol,Binst},
 
 {prob,B} = LPGen[inp, deg,eps, lambda];
 
-Print["Solving LP problem in Mathematica"];
 (* Explicit LinearProgramming implementation *)
 equations=First/@Select[prob, Head[#]==Equal&];
 
@@ -335,7 +331,6 @@ LPres=LinearProgramming[
 
 (* If no feasible point is found, Mathematica returns the expression unevaluated. Replace this with a 0 and return. *)
 If[Head[LPres]===LinearProgramming,
-  Print["No feasible solution found by Linear Programming."];
   Throw[{}]];
  
 LPsol=Thread[LPvars -> LPres]; 
@@ -349,26 +344,12 @@ Throw[ {Binst} ]
 ]]
 
 
-(* Default wrapper *)
-LPBarrier[problem_List]:=LPBarrier[problem, 
-(*deg = *) 4,
-(*eps = *) 0.01,
-(*lambda = *) 1,
-(* tolerance = *) 0.001,
-(* Method = *) "InteriorPoint"];
-LPBarrierMATLAB[problem_List]:=LPBarrierMATLAB[problem, 
-(*deg = *) 4,
-(*eps = *) 0.01,
-(*lambda = *) 1];
-
-
-LPBarrierMATLAB[inp_, deg_, eps_, lambda_ ]:=Catch[Module[
+LPBarrierMATLABStep[inp_, deg_, eps_, lambda_ ]:=Catch[Module[
 (* Declare local variables *)
 {prob,B,equations,LPvars,Aeq,beq,lb,objFn,mSol,LPsol,Binst},
 
 {prob,B} = LPGen[inp, deg,eps,lambda];
 
-Print["Sending LP problem to MATLAB"];
 (* Explicit LinearProgramming implementation *)
 equations=First/@Select[prob, Head[#]==Equal&];
 
@@ -389,7 +370,8 @@ MATLink`MSet["aeq", Aeq];
 MATLink`MSet["beq", beq];
 MATLink`MSet["f", objFn];
 MATLink`MSet["lb", lb];
-MATLink`MEvaluate["lpsl = linprog(f,[],[],aeq,beq,lb,[])"];
+MATLink`MEvaluate["options = optimoptions('linprog','Algorithm','interior-point');
+lpsl = linprog(f,[],[],aeq,beq,lb,[],options)"];
 mSol=MATLink`MGet["lpsl"];
 (* If nothing was found, return 0 *)
 If[Length[mSol]==0, Throw[{}]];
@@ -401,41 +383,44 @@ Throw[ {Binst[[1]]} ]
 ]]
 
 
-(* Common implementation returning an LP problem *)
-LPGenVec[{ pre_, { vf_List, vars_List, evoConst_ }, post_},deg_,eps_,lambda_]:=Catch[Module[
-{init,unsafe,Q,ibox,ubox,qbox,B,B2,LfB,LfB2,BC1,BC21,BC22,BC31,BC32,BC33,BC34},
-Print["Setting up an LP problem for vectorial BC generation. Degree ",deg," Eps ",eps," Lambda ",lambda];
+(* Default looping wrapper *)
+LPBarrier[problem_List, opts:OptionsPattern[]]:=Catch[Module[{eps,tol,method,heulambdas,i,pre,vf,vars,evoConst,post,res},
+eps=0.001;
+tol=0.0001;
+method="InteriorPoint";
 
-init=ExtractPolys[pre];
-unsafe=ExtractPolys[Not[post]];
-Q=If[TrueQ[evoConst],{}, ExtractPolys[evoConst]];
+{pre, { vf, vars, evoConst}, post} = problem;
 
-(* compute axis-aligned bounding boxes *)
-ibox=BoundingBox[pre,vars];
-ubox=BoundingBox[Not[post],vars];
-qbox=BoundingBox[evoConst,vars];
+heulambdas = If[OptionValue[Lambda]==={}, HeuristicLambdas[vars,vf], {OptionValue[Lambda]}];
 
-(* Compute a symbolic template *)
-B=BTemplate[deg,vars];
-B2=BTemplate[deg,vars];
+Print["Solving LP problem in Mathematica"];
+For[i=1,i<=OptionValue[MaxDeg],i++,
+	Do[
+	res=LPBarrierStep[problem, i, eps, lambda, tol, method];
+	If[res==={},,Throw[res]];
+	,
+	{lambda,heulambdas}]];
+Print["No feasible solution found by Linear Programming."];
+Throw[{}]
+	]];
 
-(* Compute the Lie derivative along the vector field *)
-LfB=Primitives`Lf[B,vf,vars];
-LfB2=Primitives`Lf[B2,vf,vars];
+LPBarrierMATLAB[problem_List, opts:OptionsPattern[]]:=Catch[Module[{eps,heulambdas,i,pre,vf,vars,evoConst,post,res},
+eps=0.01;
 
-(* Compute Handelman representations for each of the barrier certificate conditions *)
-BC1=HandelmanRepEps[B2,vars,deg, Flatten[ubox], eps]; (* Ubox \[Rule] B2\[GreaterEqual]eps>0 *)
-BC21=HandelmanRepEps[-B,vars,deg, Flatten[ibox], 0]; (* Ibox \[Rule] B<=0 *)
-BC22=HandelmanRepEps[-B2,vars,deg, Flatten[ibox], 0]; (* Ibox \[Rule] B2<=0 *)
-(* qbox \[Rule] B'<=-B *)
-BC31=HandelmanRepEps[-LfB+lambda[[1]]*B+lambda[[2]]*B2,vars,Primitives`PolynomDegree[-LfB+lambda[[1]]*B+lambda[[2]]*B2,vars], Flatten[qbox], 0];
-BC32=HandelmanRepEps[-LfB2+lambda[[3]]*B+lambda[[4]]*B2,vars,Primitives`PolynomDegree[-LfB2+lambda[[3]]*B+lambda[[4]]*B2,vars], Flatten[qbox], 0];
+{pre, { vf, vars, evoConst}, post} = problem;
 
-(* Collect the systems of linear equations and inequalities into one *)
-prob=Union[BC1,BC21,BC22,BC31,BC32];
+heulambdas = If[OptionValue[Lambda]==={}, HeuristicLambdas[vars,vf], {OptionValue[Lambda]}];
 
-Throw[{prob,{B,B2}}]
-]]
+Print["Sending LP problem to MATLAB"];
+
+For[i=1,i<=OptionValue[MaxDeg],i++,
+	Do[
+	res=LPBarrierMATLABStep[problem, i, eps, lambda];
+	If[res==={},,Throw[res]];
+	,
+	{lambda,heulambdas}]];
+Print["No feasible solution found by Linear Programming."];
+Throw[{}]]];
 
 
 End[];
