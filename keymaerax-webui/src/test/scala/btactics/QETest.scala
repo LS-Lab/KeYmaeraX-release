956153c2ea9b56113dfae85298b3a0d75faef82f
Yong Kiam <tanyongkiam@gmail.com>
2019-05-17 09:31:24 -0400

    Add strange QE example

--- keymaerax-webui/src/test/scala/btactics/QETest.scala
+++ keymaerax-webui/src/test/scala/btactics/QETest.scala
@@ -2,6 +2,7 @@ package edu.cmu.cs.ls.keymaerax.btactics
 
 import edu.cmu.cs.ls.keymaerax.Configuration
 import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
+import testHelper.KeYmaeraXTestTags.IgnoreInBuildTest
 
 import scala.collection.immutable.Map
 
@@ -25,6 +26,13 @@ class QETest extends TacticTestBase {
   it should "prove abs(x)^2=x^2" in withMathematica  { qeTool =>
     withTemporaryConfig(Map(Configuration.Keys.QE_ALLOW_INTERPRETED_FNS -> "true")) {
       qeTool.qeEvidence("abs(x)^2=x^2".asFormula)
+    }
+  }
+
+  it should "not get stuck on a quantified arithmetic goal" taggedAs IgnoreInBuildTest in withMathematica  { qeTool =>
+    //Strange example that finishes immediately on Mathematica 12.0 but not earlier versions (11.2,11.3)
+    withTemporaryConfig(Map(Configuration.Keys.QE_ALLOW_INTERPRETED_FNS -> "true")) {
+      qeTool.qeEvidence("\\forall t \\forall v \\forall x \\forall y  (T()>0&A()>0&B()>0&rad()>0&ox()^2+oy()^2=rad()^2&(v>=0&t<=T())&x^2+y^2=rad()^2&t>=0->x-ox()-(v*(T()-t)+A()*(T()-t)^2/2+(v+A()*(T()-t))^2/(2*B()))>0->-y*v/rad()+v>=0)".asFormula)
     }
   }
 }
