be960cf1d30b2fa69d2410221124cb750167f50c
Yong Kiam <tanyongkiam@gmail.com>
2019-07-05 18:56:22 -0400

    Revert "Fix oops breakage"
    This reverts commit 6c57c6dfba50a0b4b88591819e64b1185f9e5188.

--- keymaerax-core/src/main/resources/Pegasus/InvariantExtractor.m
+++ keymaerax-core/src/main/resources/Pegasus/InvariantExtractor.m
@@ -98,7 +98,7 @@ Throw[{H0, cuts}]
 
 
 TimedDWC[precond_, postcond_, system_List, A0_List, opts:OptionsPattern[]]:=TimedDWC[precond, postcond, system, A0, cuts]=Catch[
-Module[{GT,EQ,LT,p,f=system[[1]],vars=system[[2]],H0=True,time,cuts},
+Module[{GT,EQ,LT,p,f=system[[1]],vars=system[[2]],H0=system[[3]],time,cuts},
 
 SetOptions[Reduce,WorkingPrecision-> OptionValue[WorkingPrecision]];
 
@@ -114,7 +114,6 @@ TimeConstrained[
 If[(TrueQ[Reduce[ForAll[vars,Implies[H0 && precond, p==0]],vars,Reals]]) && (TrueQ[LZZ`InvSFast[p==0, f, vars, H0]]),
 Print["DC on ", p==0];
 cuts = Join[cuts,{p==0}];
-H0=H0 && p==0;
 Continue[];
 ]
 
@@ -122,7 +121,6 @@ Continue[];
 If[(TrueQ[Reduce[ForAll[vars,Implies[H0 && precond, p>0]],vars,Reals]]) && (TrueQ[LZZ`InvSFast[p>0, f, vars, H0]]),
 Print["DC on ", p>0];
 cuts=Join[cuts,{p>0}];
-H0=H0 && p>0;
 Continue[];
 ]
 
@@ -130,7 +128,6 @@ Continue[];
 If[(TrueQ[Reduce[ForAll[vars,Implies[H0 && precond, p<0]],vars,Reals]]) && (TrueQ[LZZ`InvSFast[p<0, f, vars, H0]]),
 Print["DC on ", p<0];
 cuts=Join[cuts,{p<0}];
-H0=H0 && p<0;
 Continue[];
 ]
 
@@ -138,7 +135,6 @@ Continue[];
 If[(TrueQ[Reduce[ForAll[vars,Implies[H0 && precond, p>=0]],vars,Reals]]) && (TrueQ[LZZ`InvSFast[p>=0, f, vars, H0]]),
 Print["DC on ", p>=0];
 cuts=Join[cuts,{p>=0}];
-H0=H0 && p>=0;
 Continue[];
 ]
 
@@ -146,7 +142,6 @@ Continue[];
 If[(TrueQ[Reduce[ForAll[vars,Implies[H0 && precond, p<=0]],vars,Reals]]) && (TrueQ[LZZ`InvSFast[p<=0, f, vars, H0]]),
 Print["DC on ", p<=0];
 cuts=Join[cuts,{p<=0}];
-H0=H0 && p<=0;
 Continue[];
 ]
 
