2650b81746fd7254330ec64d116ac3c47441e5df
Andre Platzer <aplatzer@cs.cmu.edu>
2019-07-26 00:00:41 +0200

    Revert "Slightly better about reporting the reason for a BelleUnexpectedProofStateError at least in one place"
    This reverts commit 9d2d6b207de073fc41e36b7410fac77f800851c2.

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/TactixLibrary.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/TactixLibrary.scala
@@ -1042,16 +1042,10 @@ object TactixLibrary extends HilbertCalculus with SequentCalculus {
     */
   def proveBy(goal: ProvableSig, tactic: BelleExpr): ProvableSig = {
     val v = BelleProvable(goal)
-    try {
-      BelleInterpreter(tactic, v) match {
-        case BelleProvable(provable, _) => provable
-        //      //@note there is no other case at the moment
-        //      case r => throw BelleIllFormedError("Error in proveBy, goal\n" + goal + " was not provable but instead resulted in\n" + r)
-      }
-    } catch {
-      case ex: BelleUnexpectedProofStateError =>
-        println("BelleUnexpectedProofStateError\n" + ex.proofState)
-        throw ex
+    BelleInterpreter(tactic, v) match {
+      case BelleProvable(provable, _) => provable
+//      //@note there is no other case at the moment
+//      case r => throw BelleIllFormedError("Error in proveBy, goal\n" + goal + " was not provable but instead resulted in\n" + r)
     }
   }
 
