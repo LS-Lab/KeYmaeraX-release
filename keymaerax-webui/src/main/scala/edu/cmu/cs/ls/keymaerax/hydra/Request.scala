b7277859302501b942e1dd5c0c5c3554f5987a42
Stefan Mitsch <smitsch@cs.cmu.edu>
2019-07-28 06:41:39 -0400

    Web UI: ODE invariant search robustness to non-FOL post-condition

--- keymaerax-webui/src/main/scala/edu/cmu/cs/ls/keymaerax/hydra/Request.scala
+++ keymaerax-webui/src/main/scala/edu/cmu/cs/ls/keymaerax/hydra/Request.scala
@@ -326,12 +326,13 @@ class PegasusCandidatesRequest(db: DBAbstraction, userId: String, proofId: Strin
         try {
           node.goal match {
             case Some(sequent) => sequent.succ.find({ case Box(_: ODESystem, _) => true case _ => false }) match {
-              case Some(Box(ode: ODESystem, post)) => ToolProvider.invGenTool() match {
+              case Some(Box(ode: ODESystem, post)) if post.isFOL => ToolProvider.invGenTool() match {
                 case Some(tool) =>
                   val invs = tool.invgen(ode, sequent.ante, post)
                   new PegasusCandidatesResponse(invs) :: Nil
                 case None => new PegasusCandidatesResponse(Nil) :: Nil
               }
+              case Some(Box(_, post)) if !post.isFOL => new ErrorResponse("Post-condition in FOL is needed to search for invariants; please perform further proof steps until the post-condition of the ODE is a formula in first-order logic.") :: Nil
               case None => new ErrorResponse("ODE system needed to search for invariant candidates, but succedent does not contain an ODE system or ODE system may not be at top level. Please perform additional proof steps until ODE system is at top level.") :: Nil
             }
             case None => new ErrorResponse("ODE system needed to search for invariant candidates, but goal is empty.") :: Nil
