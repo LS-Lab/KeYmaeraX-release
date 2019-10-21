d383ab6590393f74887615283d9cc4fe1588c4d2
Andre Platzer <aplatzer@cs.cmu.edu>
2019-10-21 19:13:25 -0400

    Scaladoc

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/bellerophon/package.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/bellerophon/package.scala
@@ -7,12 +7,12 @@ import scala.collection.immutable
 /**
   * Bellerophon tactics language framework. This package includes
   *    - [[edu.cmu.cs.ls.keymaerax.bellerophon.BelleExpr tactic language expressions]]
-  *    - a [[edu.cmu.cs.ls.keymaerax.bellerophon.SequentialInterpreter sequential tactic interpreter]]
+  *    - [[edu.cmu.cs.ls.keymaerax.bellerophon.SequentialInterpreter sequential tactic interpreter]] for BelleEpxr
   *
   * All Bellerophon tactic expressions are of type [[edu.cmu.cs.ls.keymaerax.bellerophon.BelleExpr]],
   * which provides the following tactic combinators
   *
-  *   - `s & t` [[edu.cmu.cs.ls.keymaerax.bellerophon.SeqTactic sequential composition]] executes `t` on the output of `s`, failing if either fail.
+  *   - `s & t` alias `s ; t` [[edu.cmu.cs.ls.keymaerax.bellerophon.SeqTactic sequential composition]] executes t` on the output of `s`, failing if either fail.
   *   - `s | t` [[edu.cmu.cs.ls.keymaerax.bellerophon.EitherTactic alternative composition]] executes `t` if applying `s` fails, failing if both fail.
   *   - `t*` [[edu.cmu.cs.ls.keymaerax.bellerophon.SaturateTactic saturating repetition]] executes tactic `t` repeatedly to a fixpoint, casting result to type annotation,
   *     diverging if no fixpoint.
@@ -20,17 +20,15 @@ import scala.collection.immutable
   *   - `t+` saturating repetition executes tactic `t` to a fixpoint, requires at least one successful application.
   *   - `<(e1,...,en)` [[edu.cmu.cs.ls.keymaerax.bellerophon.BranchTactic branching]] to run tactic `ei` on branch `i`, failing if any of them fail or if there are not exactly `n` branches.
   *   - `case _ of {fi => ei}` [[edu.cmu.cs.ls.keymaerax.bellerophon.USubstPatternTactic uniform substitution case pattern]] applies the first `ei` such that
-  *
   *     `fi` uniformly substitutes to current provable for which `ei` does not fail, fails if the `ei` of all matching `fi` fail.`
-  *   - `t partial` [[edu.cmu.cs.ls.keymaerax.bellerophon.PartialTactic partial tactic marker]] marks that tactic `t` is allowed to not close all its goals.
   *
-  * [[edu.cmu.cs.ls.keymaerax.bellerophon.PositionTactic Positional tactics]] support flexible modes of identifying what position to apply them to via
+  * [[edu.cmu.cs.ls.keymaerax.bellerophon.PositionalTactic Positional tactics]] support flexible modes of identifying what position to apply them to via
   * [[edu.cmu.cs.ls.keymaerax.bellerophon.AtPosition]].
   * Applying a positional tactic `t` at a position supports many different ways of specifying the position:
   *
   *   - `t(1)` applied at the first [[edu.cmu.cs.ls.keymaerax.core.Sequent.succ succedent]] formula.
   *   - `t(-1)` applied at the first [[edu.cmu.cs.ls.keymaerax.core.Sequent.ante antecedent]] formula.
-  *   - `t(-4, 0::1::1::Nil)` applied at [[PosInExpr subexpression positioned at]] `.0.1.1` of the fourth antecedent formula,
+  *   - `t(-4, 0::1::1::Nil)` applied at [[edu.cmu.cs.ls.keymaerax.bellerophon.PosInExpr subexpression positioned at]] `.0.1.1` of the fourth antecedent formula,
   *     that is at the second child of the second child of the first child of the fourth antecedent formula in the sequent.
   *   - `t('L)` applied at the first applicable position in the [[edu.cmu.cs.ls.keymaerax.core.Sequent.ante antecedent]] (left side of the sequent).
   *   - `t('R)` applied at the first applicable position in the [[edu.cmu.cs.ls.keymaerax.core.Sequent.succ succedent]] (right side of the sequent).
@@ -47,8 +45,8 @@ import scala.collection.immutable
   *     ensuring that the formula `fml` is at that position.
   *   - `t(-2, fml)` applied at the second [[edu.cmu.cs.ls.keymaerax.core.Sequent.ante antecedent]] formula,
   *     ensuring that the formula `fml` is at that position.
-  *   - `t(5, 0::1::1::Nil, ex)` applied at [[PosInExpr subexpression positioned at]] `.0.1.1` of the fifth succedent formula,
-  *     that is at the second child of the second child of the first child of the fifth succcedent formula in the sequent,
+  *   - `t(5, 0::1::1::Nil, ex)` applied at [[edu.cmu.cs.ls.keymaerax.bellerophon.PosInExpr subexpression positioned at]] `.0.1.1` of the fifth succedent formula,
+  *     that is at the second child of the second child of the first child of the fifth succedent formula in the sequent,
   *     ensuring that the expression `ex` is at that position.
   *   - `t('L, fml)` applied at the antecedent position (left side of the sequent)
   *     where the expected formula `fml` can be found (on the top level).
