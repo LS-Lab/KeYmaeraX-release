e267c7b7576298faece049a28669e1a02ba9e695
Stefan Mitsch <smitsch@cs.cmu.edu>
2020-02-07 09:53:02 -0500

    Tool exceptions

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/Errors.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/Errors.scala
@@ -9,13 +9,20 @@ import edu.cmu.cs.ls.keymaerax.core.ProverException
 /** Arithmetic tool exceptions */
 case class ToolException(msg: String, cause: Throwable = null) extends ProverException(msg, cause)
 
+/** Reports errors converting to/from a tool. */
 class ConversionException(s: String) extends Exception(s)
+
+/** Reports Mathematica computation failures ($Failed). */
 class MathematicaComputationFailedException(msg: String) extends ConversionException(msg)
-/** Internal abort of computations (e.g., by TimeConstrained). */
+
+/** Internal abort of computations (e.g., by TimeConstrained, by $Abort). */
 class MathematicaComputationAbortedException(msg: String) extends ConversionException(msg)
+
 /** Externally triggered abort (e.g., by stopping from the UI). */
 class MathematicaComputationExternalAbortException(msg: String) extends ConversionException(msg)
 
+/** Reports errors converting to/from SMTLib format. */
 class SMTConversionException(s: String) extends ConversionException(s)
+
+/** Reports errors on QE. */
 class SMTQeException(s: String) extends Exception(s)
-class NoCountExException(s: String) extends Exception(s)
