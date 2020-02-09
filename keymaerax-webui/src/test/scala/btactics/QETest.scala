a5acbe9192f7b4b223e7a1472c8ff7b7ef379c5c
Stefan Mitsch <smitsch@cs.cmu.edu>
2020-02-09 13:10:51 -0500

    Fix QE tests

--- keymaerax-webui/src/test/scala/btactics/QETest.scala
+++ keymaerax-webui/src/test/scala/btactics/QETest.scala
@@ -13,26 +13,27 @@ class QETest extends TacticTestBase {
 
   "Standalone Mathematica QE tool" should "prove abs(5)=5" in withMathematica { qeTool =>
     withTemporaryConfig(Map(Configuration.Keys.QE_ALLOW_INTERPRETED_FNS -> "true")) {
-      qeTool.qe("abs(5)=5".asFormula).fact.conclusion shouldBe "==> true".asSequent
+      qeTool.qe("abs(5)=5".asFormula).fact.conclusion shouldBe "==> abs(5)=5 <-> true".asSequent
     }
   }
 
   it should "prove abs(-5)=5" in withMathematica { qeTool =>
     withTemporaryConfig(Map(Configuration.Keys.QE_ALLOW_INTERPRETED_FNS -> "true")) {
-      qeTool.qe("abs(-5)=5".asFormula).fact.conclusion shouldBe "==> true".asSequent
+      qeTool.qe("abs(-5)=5".asFormula).fact.conclusion shouldBe "==> abs(-5)=5 <-> true".asSequent
     }
   }
 
   it should "prove abs(x)^2=x^2" in withMathematica  { qeTool =>
     withTemporaryConfig(Map(Configuration.Keys.QE_ALLOW_INTERPRETED_FNS -> "true")) {
-      qeTool.qe("abs(x)^2=x^2".asFormula).fact.conclusion shouldBe "==> true".asSequent
+      qeTool.qe("abs(x)^2=x^2".asFormula).fact.conclusion shouldBe "==> abs(x)^2=x^2 <-> true".asSequent
     }
   }
 
   it should "not get stuck on a quantified arithmetic goal" taggedAs IgnoreInBuildTest in withMathematica  { qeTool =>
     //Strange example that finishes immediately on Mathematica 12.0 but not earlier versions (11.2,11.3)
     withTemporaryConfig(Map(Configuration.Keys.QE_ALLOW_INTERPRETED_FNS -> "true")) {
-      qeTool.qe("\\forall t \\forall v \\forall x \\forall y  (T()>0&A()>0&B()>0&rad()>0&ox()^2+oy()^2=rad()^2&(v>=0&t<=T())&x^2+y^2=rad()^2&t>=0->x-ox()-(v*(T()-t)+A()*(T()-t)^2/2+(v+A()*(T()-t))^2/(2*B()))>0->-y*v/rad()+v>=0)".asFormula).fact.conclusion shouldBe "==> true".asSequent
+      val formula = "\\forall t \\forall v \\forall x \\forall y  (T()>0&A()>0&B()>0&rad()>0&ox()^2+oy()^2=rad()^2&(v>=0&t<=T())&x^2+y^2=rad()^2&t>=0->x-ox()-(v*(T()-t)+A()*(T()-t)^2/2+(v+A()*(T()-t))^2/(2*B()))>0->-y*v/rad()+v>=0)".asFormula
+      qeTool.qe(formula).fact.conclusion shouldBe s"==> ${formula.prettyString} <-> true".asSequent
     }
   }
 }
