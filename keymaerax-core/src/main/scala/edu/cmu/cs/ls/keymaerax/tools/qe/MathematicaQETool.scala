26015fc8ad2fbf6f72b61094513220ffb818baf2
Stefan Mitsch <smitsch@cs.cmu.edu>
2020-02-17 20:27:55 -0500

    Mathematica QE tool cleanup: produce formula without evidence

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/qe/MathematicaQETool.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/qe/MathematicaQETool.scala
@@ -9,10 +9,7 @@ package edu.cmu.cs.ls.keymaerax.tools.qe
 
 import edu.cmu.cs.ls.keymaerax.Configuration
 import edu.cmu.cs.ls.keymaerax.core._
-import edu.cmu.cs.ls.keymaerax.lemma.Evidence
 import edu.cmu.cs.ls.keymaerax.tools._
-
-import scala.collection.immutable
 
 /**
   * A QE tool implementation using the provided link to Mathematica/Wolfram Engine.
@@ -23,28 +20,23 @@ import scala.collection.immutable
 class MathematicaQETool(val link: MathematicaCommandRunner) extends QETool {
 
   /** @inheritdoc */
-  override def quantifierElimination(formula: Formula): Formula = qeEvidence(formula)._1
-
-  /** @inheritdoc */
-  def qeEvidence(originalFormula: Formula): (Formula, Evidence) = {
+  override def quantifierElimination(formula: Formula): Formula = {
     val f = try {
-      KeYmaeraToMathematica(originalFormula)
+      KeYmaeraToMathematica(formula)
     } catch {
       case ex: ConversionException => throw ex
-      case ex: Throwable => throw ConversionException("Error converting to Mathematica: " + originalFormula.prettyString, ex)
+      case ex: Throwable => throw ConversionException("Error converting to Mathematica: " + formula.prettyString, ex)
     }
     val method = Configuration.getOption(Configuration.Keys.MATHEMATICA_QE_METHOD).getOrElse("Reduce") match {
       case "Reduce" => MathematicaOpSpec.reduce
       case "Resolve" => MathematicaOpSpec.resolve
-      case m => throw new IllegalStateException("Unknown Mathematica QE method '" + m + "'. Please configure either 'Reduce' or 'Resolve'.")
+      case m => throw ToolCommunicationException("Unknown Mathematica QE method '" + m + "'. Please configure either 'Reduce' or 'Resolve'.")
     }
     val input = method(f, MathematicaOpSpec.list(), MathematicaOpSpec.reals.op)
-    //@todo move evidence into lemma/tactic mechanism
     try {
-      val (output, result) = link.run(input, MathematicaToKeYmaera)
+      val (_, result) = link.run(input, MathematicaToKeYmaera)
       result match {
-        case resultingQeFormula: Formula =>
-          (resultingQeFormula, ToolEvidence(immutable.List("input" -> input.toString, "output" -> output)))
+        case resultFormula: Formula => resultFormula
         case _ => throw ConversionException("Expected a formula from Reduce call but got a non-formula expression.")
       }
     } finally { input.dispose() }
