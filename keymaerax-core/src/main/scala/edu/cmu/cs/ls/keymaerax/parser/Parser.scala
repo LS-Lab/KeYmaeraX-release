a064e0a7c166758e228c9d0bba7bca63ff7d35ef
Andre Platzer <aplatzer@cs.cmu.edu>
2020-02-21 16:05:09 -0500

    Documentation

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/Parser.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/parser/Parser.scala
@@ -3,25 +3,33 @@
 * See LICENSE.txt for the conditions of this license.
 */
 /**
- * Differential Dynamic Logic parser for concrete KeYmaera X notation.
- * @author Andre Platzer
- * @see "Andre Platzer. A uniform substitution calculus for differential dynamic logic.  arXiv 1503.01981, 2015."
- */
+  * Differential Dynamic Logic parser for concrete KeYmaera X notation.
+  * @author Andre Platzer
+  * @see Andre Platzer. [[https://doi.org/10.1007/s10817-016-9385-1 A complete uniform substitution calculus for differential dynamic logic]]. Journal of Automated Reasoning, 59(2), pp. 219-266, 2017.
+  */
 package edu.cmu.cs.ls.keymaerax.parser
 
 import edu.cmu.cs.ls.keymaerax.core._
 
 /**
  * Parser interface for KeYmaera X.
- * Provides a parser to read string and file inputs with differential dynamic logic.
- * A parser is essentially a function from input string to differential dynamic logic [[edu.cmu.cs.ls.keymaerax.core.Expression expressions]].
+ * Provides a parser to read string inputs as differential dynamic logic.
+ * A parser is a function from input strings to differential dynamic logic [[edu.cmu.cs.ls.keymaerax.core.Expression expressions]].
  * {{{
  *     Parser: String => Expression
  * }}}
+ * Parsers are adjoint to printers, i.e., they reparse printed expressions as the original expressions
+ * but fail to parse syntactically ill-formed strings.
  * @author Andre Platzer
+ * @see [[TokenParser]]
+* @see Andre Platzer. [[https://doi.org/10.1007/s10817-016-9385-1 A complete uniform substitution calculus for differential dynamic logic]]. Journal of Automated Reasoning, 59(2), pp. 219-266, 2017.
  */
 trait Parser extends (String => Expression) {
-  /** Parse the input string in the concrete syntax as a differential dynamic logic expression */
+  /** Parse the input string in the concrete syntax as a differential dynamic logic expression
+    * @param input the string to parse as a dL formula, dL term, or dL program.
+    * @ensures apply(printer(\result)) == \result
+    * @throws ParseException if `input` is not a well-formed expression of differential dynamic logic or differential game logic.
+    */
   def apply(input: String): Expression
 
   /** Parse the input string in the concrete syntax as a differential dynamic logic term */
@@ -36,16 +44,31 @@ trait Parser extends (String => Expression) {
   /** Parse the input string in the concrete syntax as a differential dynamic logic differential program */
   val differentialProgramParser: (String => DifferentialProgram)
 
-  /** A pretty-printer that can write the output that this parser reads */
+  /** A pretty-printer that can write the output that this parser reads
+    * @ensures \forall e: apply(printer(e)) == e
+    */
   val printer: PrettyPrinter
 
 }
 
+/**
+ * Parser interface for KeYmaera X reading token streams instead of strings.
+ * Provides a parser to read token streams with differential dynamic logic.
+ * A token parser is a function from token sequences to differential dynamic logic [[edu.cmu.cs.ls.keymaerax.core.Expression expressions]].
+ * {{{
+ *     TokenParser: TokenStream => Expression
+ * }}}
+ * @author Stefan Mitsch
+ * @see [[Parser]]
+ */
 trait TokenParser {
   /** Lexer's token stream with first token at head. */
   type TokenStream = KeYmaeraXLexer.TokenStream
 
-  /** Parse the input tokens in the concrete syntax as a differential dynamic logic expression */
+  /** Parse the input tokens in the concrete syntax as a differential dynamic logic expression
+    * @param input the token stream to parse as a dL formula, dL term, or dL program.
+    * @throws ParseException if `input` is not a well-formed expression of differential dynamic logic or differential game logic.
+    */
   def parse(input: TokenStream): Expression
 
   /** Parse the input tokens in the concrete syntax as a differential dynamic logic term */
