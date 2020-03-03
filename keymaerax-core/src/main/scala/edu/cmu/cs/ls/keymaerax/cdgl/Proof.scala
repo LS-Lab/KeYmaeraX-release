3060bcc604e96420f1cd3fe5194881f93d7b7acc
Brandon Bohrer <bbohrer@andrew.cmu.edu>
2020-03-03 11:38:01 -0500

    Begin sketching out implementation for cdgl proof terms

---
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/cdgl/Proof.scala
@@ -0,0 +1,412 @@
+package edu.cmu.cs.ls.keymaerax.cdgl
+
+import edu.cmu.cs.ls.keymaerax.cdgl.Proof._
+import edu.cmu.cs.ls.keymaerax.core._
+import scala.collection.immutable
+
+
+
+object Proof {
+  type ProofVariable = Int
+}
+
+final case class Context(c:List[Formula]) {
+  def asIndexedSeq: immutable.IndexedSeq[Formula] =
+    c.to[immutable.IndexedSeq]
+  def asSequent: Sequent = Sequent(this.asIndexedSeq, immutable.IndexedSeq())
+  def entails(f:Formula): Sequent = {
+    new Sequent(c.toIndexedSeq, immutable.IndexedSeq(f))
+  }
+  def rename(what: Variable, repl: Variable): Context = {
+    Context.ofSequent(UniformRenaming(what,repl)(this.asSequent).head)
+  }
+  def extend(p: ProofVariable, P:Formula): Context = {
+    Context(P :: c)
+  }
+  def apply(p:ProofVariable): Formula = c(p)
+}
+object Context {
+  def ofSequent(seq: Sequent): Context = {
+    Context(seq.ante.toList)
+  }
+}
+
+final case class Judgement(ante: Context, succ: Formula) {}
+
+sealed trait Proof {}
+
+/* G |- M: P   G |- N: Q
+ * ----------------------------------------------
+ * G |- <M,N>: <?P>Q
+ */
+case class DTestI(left: Proof, right: Proof) extends Proof {}
+
+/* G |- M: <?P>Q
+ * ----------------------------------------------
+ * G |- piL(M): P
+ */
+case class DTestEL(child: Proof) extends Proof {}
+
+/* G |- M: <?P>Q
+ * ----------------------------------------------
+ * G |- piR(M): Q
+ */
+case class DTestER(child: Proof) extends Proof {}
+
+/* G_x^y, p:(x=f_x^y) |- M: P
+ * ----------------------------------------------
+ * G |- <x:=f_x^y in p. M> : <x:=f>P
+ */
+case class DAssignI(e:Assign, y:Variable, p:ProofVariable, child: Proof) extends Proof {}
+
+/* G |- M: <x:=f>p(x)
+ * ----------------------------------------------
+ * G |- <:=^-1>(M): p(f)
+ */
+case class DAssignE(e:Assign, child: Proof) extends Proof {}
+
+/* G_x^y, p:(x=f_x^y) |- M: P
+ * ----------------------------------------------
+ * G |- <x :* f_x^y in p. M> : <x:=*>P
+ */
+case class DRandomI(e:Assign, y:Variable, p:ProofVariable, child: Proof) extends Proof {}
+
+/* G |- M: <x:=f>p(x)   G_x^y, p(x) |- N: Q
+ * ----------------------------------------------
+ * G |-  (unpack_x^y = M in N) : Q
+ */
+case class DRandomE(x:Variable, y:Variable, p:ProofVariable, left: Proof, right: Proof) extends Proof {}
+
+/* G |- M: <a><b>P
+ * ----------------------------------------------
+ * G |- <i M>: <a;b>P
+ */
+case class DComposeI(child: Proof) extends Proof {}
+
+/* G |- M: <a;b>P
+ * ----------------------------------------------
+ * G |- <i^-1 M>: <a><b>P
+ */
+case class DComposeE(child: Proof) extends Proof {}
+
+/* G |- M: <a>P
+ * ----------------------------------------------
+ * G |- injL[b](M): <a++b>P
+ */
+case class DChoiceIL(other:Program, child: Proof) extends Proof {}
+
+/* G |- M: <b>P
+ * ----------------------------------------------
+ * G |- injR[a](M): <a++b>P
+ */
+case class DChoiceIR(other:Program, child: Proof) extends Proof {}
+
+/* G |- A: <a++b>P   G,l:<a>P |- B:Q   G,r:<b>P |- C:Q
+ * ----------------------------------------------
+ * G |- (case A of l=>B | r=>C): Q
+ */
+case class DChoiceE(l:ProofVariable, r:ProofVariable, child:Proof, left:Proof, right:Proof) extends Proof {}
+
+/* G |- M: P
+ * ----------------------------------------------
+ * G |- stop[a](M): <a*>P
+ */
+case class DStop(a:Program,child: Proof) extends Proof {}
+
+/* G |- M: <a><a*>P
+ * ----------------------------------------------
+ * G |- (go M): <a*>P
+ */
+case class DGo(child: Proof) extends Proof {}
+// TODO: Less args. Variant unnecessary
+
+/* G |- A:J   m:met0=met>metz,v:J |- <a>(J & met<metz)  m:met=metz,v:J |- Q
+ * ------------------------------------------------------------------------
+ * G |- for_{met,metz}(A;mv.B;C): <a*>Q
+ */
+case class DRepeatI(metric: Term, metz:Term, mx:BaseVariable, variant: Formula, m:ProofVariable, v:ProofVariable, init: Proof, step: Proof, post: Proof) extends Proof {}
+// FP
+
+/* G |- A: <a*>P   x:P |- B: Q   x:<a>Q |- C:Q
+ * ----------------------------------------------
+ * G |- FP(A,x.B,x.C): Q
+ */
+case class DRepeatE(child: Proof, post: Proof, step: Proof) extends Proof {}
+
+/* G |- M: [a]P
+ * ----------------------------------------------
+ * G |- yield M: <a^d>P
+ */
+case class DDualI(child: Proof) extends Proof {}
+
+/* G |- M: <a^d>P
+ * ----------------------------------------------
+ * G |- (yield^-1 M): [a]P
+ */
+case class DDualE(child: Proof) extends Proof {}
+
+/* G_xs^ys, 0<=s<=dur |- M: Q_xs^sol(s)   G_xs^ys, 0<=dur |- N: P_xs^sol(dur)
+ * ----------------------------------------------  sol solves x'=f&Q on [0,dur], s and t fresh
+ * G |- DS[x'=f&Q,ys,sol,dur,s](M,N): <x'=f&Q>P
+ */
+case class DSolve(ode:ODESystem, ys:List[Variable],sol:List[Term], dur:Term, s:Variable, t:Variable, dc:Proof, post:Proof) extends Proof {}
+
+/* G |- A: d>0 & v>0 & f-g >= -dv
+ * G |- B: <t:=0><x'=f,t'=1&Q>t>=d
+ * G |- C: [x'=f&Q](f'-g' >= eps)
+ * f>= g |- D: P
+ * ---------------------------------------------- d,v constant
+ * G |- DV[f,g,d,eps,v](A,B,C,D): <x'=f&Q>P
+ */
+case class DV(f:Term, g:Term, d:Term, const:Proof, dur:Proof, rate:Proof, post:Proof) extends Proof {}
+
+/* G, x:P |- M:Q
+ * ----------------------------------------------
+ * G |- (fn x:P => M): [?P]Q
+ */
+case class BTestI(p:ProofVariable, fml:Formula, child:Proof) extends Proof {}
+
+/* G |- M: [?P]Q   G|- N:P
+ * ----------------------------------------------
+ * G |- (M N): Q
+ */
+case class BTestE(left:Proof, right:Proof) extends Proof {}
+
+/* G_x^y, p:(x=f_x^y) |- M: P
+ * ----------------------------------------------
+ * G |- [x:=f_x^y in p. M] : [x:=f]P
+ */
+case class BAssignI(e:Assign, y:Variable, p:ProofVariable, child: Proof) extends Proof {}
+
+/* G |- M: [x:=f]p(x)
+ * ----------------------------------------------
+ * G |- [:=^-1](M): p(f)
+ */
+case class BAssignE(e:Assign, child: Proof) extends Proof {}
+
+/* G |- G_x^y |- M:P
+ * ----------------------------------------------
+ * G |- (fn x^y => M): [x:=*]P
+ */
+case class BRandomI(x:Variable, y:Variable, child: Proof) extends Proof {}
+
+/* G |- M: [x:=*]p(x)
+ * ----------------------------------------------
+ * G |- (M f): p(f)
+ */
+case class BRandomE(child: Proof, f:Term) extends Proof {}
+
+/* G |- M: [a][b]P
+ * ----------------------------------------------
+ * G |- [i M]: [a;b]P
+ */
+case class BComposeI(child: Proof) extends Proof {}
+
+/* G |- M: [a;b]P
+ * ----------------------------------------------
+ * G |- [i M]: [a][b]P
+ */
+case class BComposeE(child: Proof) extends Proof {}
+
+/* G |- M: [a]P   G |- N: [b]P
+ * ----------------------------------------------
+ * G |- [M,N]: [a++b]P
+ */
+case class BChoiceI(left: Proof, right: Proof) extends Proof {}
+
+/* G |- M: [a++b]P
+ * ----------------------------------------------
+ * G |- projL(M): [a]P
+ */
+case class BChoiceEL(child: Proof) extends Proof {}
+
+/* G |- M: [a++b]P
+ * ----------------------------------------------
+ * G |- projR(M): [b]P
+ */
+case class BChoiceER(child: Proof) extends Proof {}
+
+/* G |- A: J   G_xs^ys,x:J |- B: [a]J   G_xs^ys,x:J |- C:Q
+ * ---------------------------------------------------------
+ * G |- (A rep[ys] x:J. B in C): [a*]Q
+ */
+case class BRepeatI(J:ProofVariable, pre: Proof, step: Proof, post: Proof) extends Proof {}
+
+/* G |- M: [a*]P
+ * ----------------------------------------------
+ * G |- projL(M): P
+ */
+case class BRepeatEL(child: Proof) extends Proof {}
+
+/* G |- M: [a*]P
+ * ----------------------------------------------
+ * G |- projR(M): [a][a*]P
+ */
+case class BRepeatER(child: Proof) extends Proof {}
+
+/* G |- M: <a>P
+ * ----------------------------------------------
+ * G |- [yield M]: [a^d]P
+ */
+case class BDualI(child: Proof) extends Proof {}
+
+/* G |- M: [a^d]P
+ * ----------------------------------------------
+ * G |- [yield^-1 M]: <a>P
+ */
+case class BDualE(child: Proof) extends Proof {}
+
+/*  G_xs^ys, 0<=t, (\forall 0<=s<=t, Q_xs^sol(dur)) |- N: P_xs^sol(dur)
+ * --------------------------------------------------------------------  sol solves x'=f&Q on [0,dur], s and t fresh
+ * G |- BS[x'=f&Q,ys,sol,t,s](M,N): [x'=f&Q]P
+ */
+case class BSolve(ode:ODESystem, ys:List[Variable], sols:List[Term], s:Variable, t:Variable, dc:ProofVariable, post:Proof) extends Proof {}
+
+
+/* G_xs^ys, x:Q |- M: P
+ * ----------------------------------------------
+ * G |- DW[ys](x.M): [x'=f & Q]P
+ */
+case class DW(ode:ODESystem, p:ProofVariable, child: Proof) extends Proof {}
+
+/* G |- M:[x'=f&Q]R   G |- N:[x'=f&(Q&R)]P
+ * ----------------------------------------------
+ * G |- DC(M,N): [x'=f&Q]P
+ */
+case class DC(inv: Formula, left: Proof, right: Proof) extends Proof {}
+
+/* G |- M: P   G_xs^ys |- N: (P)'_x'^f
+ * ----------------------------------------------
+ * G |- DI(M,N): [x'=f & Q]P
+ */
+case class DI(pre: Proof, step: Proof) extends Proof {}
+
+/* G, y=y0 |- M: [x'=f,y'=a(y)+b&Q]P
+ * ----------------------------------------------
+ * G |- DG[y,y0,a,b](M): [x'=f]P
+ */
+case class DG(y: Variable, y0: Term, a: Term, b:Term, child: Proof) extends Proof {}
+
+
+
+/* G_x^y |- M:P
+ * ----------------------------------------------
+ * G |- (fn x^y => M): (\forall x, P)
+ */
+case class ForallI(x:Variable, y:Variable, child: Proof) extends Proof {}
+
+/* G |- M: (\forall x, p(x))
+ * ----------------------------------------------
+ * G |- (M f): p(f)
+ */
+case class ForallE(child: Proof, f:Term) extends Proof {}
+
+
+/* G_x^y, p:(x=f_x^y) |- M: P
+ * ----------------------------------------------
+ * G |- <x = f_x^y in p. M> : \exists x, P
+ */
+case class ExistsI(e:Assign, y:Variable, p:ProofVariable, child: Proof) extends Proof {}
+
+/* G |- M: (exists x, p(x))   G_x^y, p(x) |- N: Q
+ * ----------------------------------------------
+ * G |- (unpack_x^y = M in N) : Q
+ */
+case class ExistsE(x:Variable, y:Variable, p:ProofVariable, left: Proof, right: Proof) extends Proof {}
+
+/* G |- M: P   G |- N:Q
+ * ----------------------------------------------
+ * G |- (M,N): P & Q
+ */
+case class AndI(left: Proof, right: Proof) extends Proof {}
+
+/* G |- M: P & Q
+ * ----------------------------------------------
+ * G |- projL(M): P
+ */
+case class AndEL(child: Proof) extends Proof {}
+
+/* G |- M: P & Q
+ * ----------------------------------------------
+ * G |- projR(M): Q
+ */
+case class AndER(child: Proof) extends Proof {}
+
+/* G |- M: P
+ * ----------------------------------------------
+ * G |- injL[Q](M): P | Q
+ */
+case class OrIL(other: Formula, child: Proof) extends Proof {}
+
+/* G |- M: Q
+ * ----------------------------------------------
+ * G |- injR[P](M): P | Q
+ */
+case class OrIR(other: Formula, child: Proof) extends Proof {}
+
+/* G |- A:(P|Q)   G,l:P |- B:R    G,r:Q |- C:R
+ * ----------------------------------------------
+ * G |- (case A of l=>B | r=>C): R
+ */
+case class OrE(l:ProofVariable, r:ProofVariable, child:Proof, left:Proof, right:Proof) extends Proof {}
+
+/* G,x:P |- M:Q
+ * ----------------------------------------------
+ * G |- (fn x:P => M): (P -> Q)
+ */
+case class ImplyI(p:ProofVariable, fml:Formula, child:Proof) extends Proof {}
+
+/* G |- M: (P -> Q)   G |- N: P
+ * ----------------------------------------------
+ * G |- (M N): Q
+ */
+case class ImplyE(left:Proof, right:Proof) extends Proof {}
+
+/* G,x:P |- M: Q   G,x:Q |- N:P
+ * ----------------------------------------------
+ * G |- (M,N)[x:P,Q]
+ */
+case class EquivI(vx:ProofVariable, p:Formula, q:Formula, left:Proof, right:Proof) extends Proof {}
+
+/* G |- M: (P <-> Q)   G |- N: P
+ * ----------------------------------------------
+ * G |- (projL(M) N): Q
+ */
+case class EquivEL(child:Proof, assump:Proof) extends Proof {}
+
+/* G |- M: (P <-> Q)   G |- N: Q
+ * ----------------------------------------------
+ * G |- (projR(M) N): P
+ */
+case class EquivER(child:Proof, assump:Proof) extends Proof {}
+
+/* G, x:P |- M: False
+ * ----------------------------------------------
+ * G |- (fn x:P => M): !P
+ */
+case class NotI(vx:ProofVariable, p:Formula, child:Proof) extends Proof {}
+
+/* G |- M: !P   G |- N: P
+ * ----------------------------------------------
+ * G |- (M N): False
+ */
+case class NotE(left:Proof, right:Proof) extends Proof {}
+
+/*       *
+ * ----------------------------------------------
+ * G,x:P |- x:P
+ */
+case class Hyp(p:ProofVariable) extends Proof {}
+
+
+/* G |- M: [a]J   G_xs^ys, x:J |- N:Q
+ * ----------------------------------------------
+ * G |-  (M o x:J. N)[ys] : [a]Q
+ */
+case class Mon(left: Proof, vx: ProofVariable, right: Proof) extends Proof {}
+
+/* G |- M: Q
+ * ---------------------------------------------- (Q -> P is valid first-order arithmetic)
+ * G |- QE[P](M) : P
+ */
+case class QE(p:Formula, child: Proof) extends Proof {}
---
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/cdgl/ProofChecker.scala
@@ -0,0 +1,282 @@
+package edu.cmu.cs.ls.keymaerax.cdgl
+
+import edu.cmu.cs.ls.keymaerax.core._
+import edu.cmu.cs.ls.keymaerax.cdgl.Proof._
+import edu.cmu.cs.ls.keymaerax.infrastruct.SubstitutionHelper
+
+case class ProofException(msg: String) extends Exception {}
+
+object ProofChecker {
+
+  private def deriveFormula(p:Formula): Formula = {
+    ???
+  }
+
+  private def qeValid(p:Formula): Boolean = false
+
+  //def variable
+  def apply(G: Context, M: Proof): Formula = {
+    val Context(con) = G
+     M match {
+      case DTestI(left, right) =>
+        val P = apply(G, left)
+        val Q = apply(G, right)
+        Diamond(Test(P),Q)
+      case DTestEL(child) =>
+        val Diamond(Test(p),_) = apply(G,child)
+        p
+      case DTestER(child) =>
+        val Diamond(Test(_),q) = apply(G,child)
+        q
+      case DAssignI(Assign(x,f), y, p, child) =>
+        val ren = URename(x,y)
+        val g = ren(f)
+        val d = G.rename(x,y).extend(p, Equal(x,g))
+        val P = apply(d, child)
+        Diamond(Assign(x,f), P)
+      case DAssignE(Assign(x,f), child) =>
+        // TODO: Make sound
+        val Diamond(Assign(xx,ff),pp) = apply(G, child)
+        assert(xx == x && ff == f)
+        SubstitutionHelper.replaceFree(pp)(x,f)
+      case DRandomI(Assign(x,f), y, p, child) =>
+        val ren = URename(x,y)
+        val g = ren(f)
+        val d = G.rename(x,y).extend(p, Equal(x,g))
+        val P = apply(d, child)
+        Diamond(AssignAny(x), P)
+      case DRandomE(x, y, p, left, right) =>
+        //TODO: Assert y fresh and doesnt escape scope
+        val Exists(List(x),q) = apply(G,left)
+        val P = apply(G.rename(x,y).extend(p,q),right)
+        P
+      case ExistsI(Assign(x,f), y:Variable, p:ProofVariable, child: Proof) =>
+        val ren = URename(x,y)
+        val g = ren(f)
+        val d = G.rename(x,y).extend(p, Equal(x,g))
+        val P = apply(d, child)
+        Exists(List(x), P)
+      case  ExistsE(x:Variable, y:Variable, p:ProofVariable, left: Proof, right: Proof)  =>
+        //TODO: Assert y fresh and doesnt escape scope
+        val Diamond(AssignAny(x),q) = apply(G,left)
+        val P = apply(G.rename(x,y).extend(p,q),right)
+        P
+      case DComposeI(child) =>
+        val Diamond(a,Diamond(b,p)) = apply(G,child)
+        Diamond(Compose(a,b),p)
+      case DComposeE(child) =>
+        val Diamond(Compose(a,b),p) = apply(G,child)
+        Diamond(a,Diamond(b,p))
+      case DChoiceIL(b,child) =>
+        val Diamond(a,p) = apply(G,child)
+        Diamond(Choice(a,b),p)
+      case DChoiceIR(a,child) =>
+        val Diamond(b,p) = apply(G,child)
+        Diamond(Choice(a,b),p)
+      case DChoiceE(l, r, child, left, right) =>
+        val Diamond(Choice(a,b),p) = apply(G,child)
+        val pp = apply(G.extend(l,Diamond(a,p)),left)
+        val qq = apply(G.extend(r,Diamond(b,p)),right)
+        require(pp == qq)
+        pp
+      case DStop(a,child) =>
+        val p = apply(G,child)
+        Diamond(Loop(a),p)
+      case DGo(child) =>
+        val Diamond(a,Diamond(Loop(b),p)) = apply(G,child)
+        assert(a == b)
+        Diamond(Loop(a),p)
+      case DRepeatI(metric, metz, mx, variant, m, v, init, step, post) =>
+        //val met0 = BaseVariable(mx.name, Some(mx.index.map(i => i+ 1).getOrElse(0)))
+        val p1 = apply(G,init)
+        val Diamond(a,p2) = apply(Context(List(And(Equal(mx,metric), Greater(metric,metz)), variant)), step)
+        val p3 = apply(Context(List(Equal(metric,metz),variant)),post)
+        assert(p1 == variant)
+        assert(p2 == And(variant, Greater(metz, metric)))
+        Diamond(Loop(a), p3)
+      case DRepeatE(child, post, step) =>
+        val Diamond(Loop(a),p) = apply(G,child)
+        val p1 = apply(Context(List(p)),post)
+        val p2 = apply(Context(List(Diamond(a,p1))),step)
+        assert(p2 == p1)
+        p1
+      case DDualI(child) =>
+        val Box(a,p) = apply(G,child)
+        Diamond(Dual(a),p)
+      case DDualE(child) =>
+        val Diamond(Dual(a),p) = apply(G,child)
+        Box(a,p)
+      case DSolve(ode, ys, sol, dur, s, t, dc, post) =>
+        ???
+      case DV(f, g, d, const, dur, rate, post) =>
+        ???
+
+      case BTestI(p, fml, child) =>
+        val p1 = apply(G.extend(p,fml),child)
+        Box(Test(fml),p1)
+      case BTestE(left, right) =>
+        val Box(Test(p),q) = apply(G,left)
+        val p2 = apply(G,right)
+        assert(p == p2)
+        q
+      case BAssignI(Assign(x,f), y, p, child) =>
+        val ren = URename(x,y)
+        val g = ren(f)
+        val d = G.rename(x,y).extend(p, Equal(x,g))
+        val P = apply(d, child)
+        Box(Assign(x,f), P)
+      case BAssignE(Assign(x,f), child) =>
+        // TODO: Make sound
+        val Box(Assign(xx,ff),pp) = apply(G, child)
+        assert(xx == x && ff == f)
+        SubstitutionHelper.replaceFree(pp)(x,f)
+      case BRandomI(x, y, child) =>
+        val pp = apply(G.rename(x,y), child)
+        Box(AssignAny(x),pp)
+      case BRandomE(child, f) =>
+        val Box(AssignAny(x),pp) = apply(G,child)
+        SubstitutionHelper.replaceFree(pp)(x,f)
+      case ForallI(x:Variable, y:Variable, child: Proof) =>
+        val pp = apply(G.rename(x,y), child)
+        Forall(List(x),pp)
+      case ForallE(child: Proof, f:Term) =>
+        val Forall(List(x),pp) = apply(G,child)
+        SubstitutionHelper.replaceFree(pp)(x,f)
+      case BComposeI(child) =>
+        val Box(a,Box(b,p)) = apply(G,child)
+        Box(Compose(a,b),p)
+      case BComposeE(child) =>
+        val Box(Compose(a,b),p) = apply(G,child)
+        Box(a,Box(b,p))
+      case BChoiceI(left, right) =>
+        val Box(a,p) = apply(G,left)
+        val Box(b,q) = apply(G,right)
+        assert(p == q)
+        Box(Choice(a,b),p)
+      case BChoiceEL(child) =>
+        val Box(Choice(a,b),p) = apply(G,child)
+        Box(a,p)
+      case BChoiceER(child) =>
+        val Box(Choice(a,b),p) = apply(G,child)
+        Box(b,p)
+        // TODO: Better consistency in rename across rules
+      case BRepeatI(j, pre, step, post) =>
+        val j1 = apply(G,pre)
+        val Box(a,j2) = apply(Context(List(j1)), step)
+        assert(j1 == j2)
+        apply(Context(List(j1)), post)
+      case BRepeatEL(child) =>
+        val Box(Loop(_),p) = apply(G, child)
+        p
+      case BRepeatER(child) =>
+        val Box(Loop(a),p) = apply(G,child)
+        Box(a,Box(Loop(a),p))
+      case BDualI(child) =>
+        val Diamond(a,p) = apply(G,child)
+        Box(Dual(a),p)
+      case BDualE(child) =>
+        val Box(Dual(a),p) = apply(G,child)
+        Diamond(a,p)
+      case BSolve(ode, ys, sol, s, t, dc, post) =>
+        // TODO: assert solution
+        // TODO: assert freshness
+        val xs = StaticSemantics(ode).bv.toSet.toList.filter({case _ : BaseVariable => true case _ => false}: (Variable => Boolean))
+        val xys = xs.zip(ys)
+        val sols = xs.zip(sol)
+        val asgn = sols.map({case (x,e) => Assign(x,e)}).fold[Program](Test(True))(Compose(_,_))
+        val tasgn = Compose(Assign(t,s), asgn)
+        val g = xys.foldLeft(G)({case (acc, (x,y)) => acc.rename(x,y)})
+        val dcFml = Forall(List(s),Imply(And(LessEqual(Number(0),s),LessEqual(s,t)),Diamond(asgn,ode.constraint)))
+        val Diamond(asgns,p1) = apply(g.extend(dc, And(GreaterEqual(t,Number(0)),dcFml)), post)
+        assert(asgn == tasgn)
+        Box(ode,p1)
+      case DW(ode, p, child) =>
+        val p1 = apply(G.extend(p,ode.constraint),child)
+        Box(ode,p1)
+      case DC(inv, left, right) =>
+        val Box(ODESystem(ode,con),p) = apply(G,left)
+        val Box(ODESystem(ode1,And(con1,p1)),q) = apply(G,right)
+        assert(ode == ode1 && con1 == con && p1 == p)
+        Box(ODESystem(ode,con),q)
+      case DI(pre, step) =>
+        val p = apply(G,pre)
+        // TODO: Context
+        val Box(ode,dp) = apply(G,step)
+        assert(dp == deriveFormula(p))
+          ???
+      case DG(y, y0, a, b, child) =>
+        //@TODO: freshness
+        val Box(Compose(Assign(xy,e),ODESystem(DifferentialProduct(AtomicODE(xp,xe),c),con)),pp) = apply(G,child)
+        assert(y == xy && xp == DifferentialSymbol(xy) && e == y0 && xe == Plus(Times(a,xy),b))
+        Box(ODESystem(c,con),pp)
+      case AndI(left, right) =>
+        val p = apply(G,left)
+        val q = apply(G,right)
+        And(p,q)
+      case AndEL(child) =>
+        val And(p,q) = apply(G,child)
+        p
+      case AndER(child) =>
+        val And(p,q) = apply(G,child)
+        q
+      case OrIL(other, child) =>
+        val p = apply(G,child)
+        Or(p,other)
+      case OrIR(other,child) =>
+        val q = apply(G,child)
+        Or(other,q)
+      case OrE(l, r, child, left, right) =>
+        val Or(p,q) = apply(G,child)
+        val r1 = apply(G.extend(l,p),left)
+        val r2 = apply(G.extend(r,q),right)
+        assert(r1 == r2)
+        r1
+      case ImplyI(p, fml, child) =>
+        val q = apply(G.extend(p,fml),child)
+        Imply(fml,q)
+      case ImplyE(left, right) =>
+        val Imply(p1,q) = apply(G,left)
+        val p2 = apply(G,right)
+        assert(p1 == p2)
+        q
+      case EquivI(vx, p, q, left, right) =>
+        val q1 = apply(G.extend(vx,p),left)
+        val p1 = apply(G.extend(vx,q),right)
+        assert(p1 == p && q1 == q)
+        Equiv(p,q)
+      case EquivEL(child, assump) =>
+        val Equiv(p,q) = apply(G,child)
+        val pp = apply(G,assump)
+        assert(p == pp)
+        q
+      case EquivER(child, assump) =>
+        val Equiv(p,q) = apply(G,child)
+        val qq = apply(G,assump)
+        assert(q == q)
+        p
+      case NotI(pv, p, child) =>
+        val False = apply(G.extend(pv,p),child)
+        Not(p)
+      case NotE(left, right) =>
+        val Not(p) = apply(G,left)
+        val pp = apply(G,right)
+        assert(p == pp)
+        False
+      case Hyp(p) => G(p)
+      case Mon(left,vx,right) =>
+        // @TODO: Better context extending
+        apply(G,left) match {
+          case Box(a,p) =>
+            val q = apply(Context(List(p)), right)
+            Box(a,q)
+          case Diamond(a,p) =>
+            val q = apply(Context(List(p : Formula)), right)
+            Diamond(a,q)
+        }
+      case QE(q, child) =>
+         val p = apply(G,child)
+         assert(qeValid(Imply(p,q)))
+         q
+    }
+  }
+}
