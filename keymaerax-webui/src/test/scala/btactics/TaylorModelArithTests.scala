9d9ed93511551dcd4e4cf34c397a8249a67d68fe
immler <immler@in.tum.de>
2020-04-15 12:46:34 +0200

    streamlined tests

--- keymaerax-webui/src/test/scala/btactics/TaylorModelArithTests.scala
+++ keymaerax-webui/src/test/scala/btactics/TaylorModelArithTests.scala
@@ -19,92 +19,55 @@ import org.scalatest.LoneElement._
   */
 class TaylorModelArithTests extends TacticTestBase {
 
+  import PolynomialArithV2Helpers._
+
   val context3 = ("-1 <= x0(), x0() <= 1, -1 <= y0(), y0() <= 1, -1 <= z0(), z0() <= 1," +
     "x = x0() + y0() + rx, -0.01 <= rx, rx <= 0.02," +
     "y = 0.5*x0() - y0() + ry, 0 <= ry, ry <= 0.1").split(',').map(_.asFormula).toIndexedSeq
   lazy val ta3 = new TaylorModelArith(context3, "x0(),y0(),z0(),rx,ry".split(',').map(_.asTerm).toIndexedSeq, 10, 4)
+  lazy val x0 = ta3.polynomialRing.ofTerm("x0()".asTerm)
+  lazy val y0 = ta3.polynomialRing.ofTerm("y0()".asTerm)
+  lazy val tm1 = ta3.TM("x".asTerm, x0 + y0, "-0.01".asTerm, "0.02".asTerm, QE)
+  lazy val tm2 = ta3.TM("y".asTerm, ta3.polynomialRing.Const(BigDecimal("0.5")) * x0 - y0, "0".asTerm, "0.1".asTerm, QE)
 
   "Taylor models" should "add exactly" in withMathematica { qeTool =>
-    import ta3._
-    import ta3.polynomialRing._
-    val x0 = ofTerm("x0()".asTerm)
-    val y0 = ofTerm("y0()".asTerm)
-    val tm1 = TM("x".asTerm, x0 + y0, "-0.01".asTerm, "0.02".asTerm, QE)
-    val tm2 = TM("y".asTerm, Const(BigDecimal("0.5")) * x0 - y0, "0".asTerm, "0.1".asTerm, QE)
     println(tm1)
     println(tm2)
     println(tm1 +! tm2)
   }
 
   it should "subtract exactly" in withMathematica { qeTool =>
-    import ta3._
-    import ta3.polynomialRing._
-    val x0 = ofTerm("x0()".asTerm)
-    val y0 = ofTerm("y0()".asTerm)
-    val tm1 = TM("x".asTerm, x0 + y0, "-0.01".asTerm, "0.02".asTerm, QE)
-    val tm2 = TM("y".asTerm, Const(BigDecimal("0.5")) * x0 - y0, "0".asTerm, "0.1".asTerm, QE)
     println(tm1)
     println(tm2)
     println(tm1 -! tm2)
   }
 
   it should "multiply exactly" in withMathematica { qeTool =>
-    import ta3._
-    import ta3.polynomialRing._
-    val x0 = ofTerm("x0()".asTerm)
-    val y0 = ofTerm("y0()".asTerm)
-    val tm1 = TM("x".asTerm, x0 + y0, "-0.01".asTerm, "0.02".asTerm, QE)
-    val tm2 = TM("y".asTerm, Const(BigDecimal("0.5")) * x0 - y0, "0".asTerm, "0.1".asTerm, QE)
     println(tm1)
     println(tm2)
     println(tm1 *! tm2)
   }
 
   it should "negate" in withMathematica { qeTool =>
-    import ta3._
-    import ta3.polynomialRing._
-    val x0 = ofTerm("x0()".asTerm)
-    val y0 = ofTerm("y0()".asTerm)
-    val tm1 = TM("x".asTerm, x0 + y0, "-0.01".asTerm, "0.02".asTerm, QE)
     println(tm1)
     println(-tm1)
   }
 
   it should "square exactly" in withMathematica { qeTool =>
-    import ta3._
-    import ta3.polynomialRing._
-    val x0 = ofTerm("x0()".asTerm)
-    val y0 = ofTerm("y0()".asTerm)
-    val tm1 = TM("x".asTerm, x0 + y0, "-0.01".asTerm, "0.02".asTerm, QE)
     println(tm1)
     println(tm1.squareExact)
   }
 
   it should "^1" in withMathematica { qeTool =>
-    import ta3._
-    import ta3.polynomialRing._
-    val x0 = ofTerm("x0()".asTerm)
-    val y0 = ofTerm("y0()".asTerm)
-    val tm1 = TM("x".asTerm, x0 + y0, "-0.01".asTerm, "0.02".asTerm, QE)
     println(tm1)
     println(tm1^1)
   }
 
   it should "^(2*n)" in withMathematica { qeTool =>
-    import ta3._
-    import ta3.polynomialRing._
-    val x0 = ofTerm("x0()".asTerm)
-    val y0 = ofTerm("y0()".asTerm)
-    val tm1 = TM("x".asTerm, x0 + y0, "-0.01".asTerm, "0.02".asTerm, QE)
     println(tm1)
     println(tm1^4)
   }
   it should "^(2*n + 1)" in withMathematica { qeTool =>
-    import ta3._
-    import ta3.polynomialRing._
-    val x0 = ofTerm("x0()".asTerm)
-    val y0 = ofTerm("y0()".asTerm)
-    val tm1 = TM("x".asTerm, x0 + y0, "-0.01".asTerm, "0.02".asTerm, QE)
     println(tm1)
     println(tm1^3)
   }
@@ -125,13 +88,9 @@ class TaylorModelArithTests extends TacticTestBase {
 
   it should "approx" in withMathematica { qeTool =>
     import ta3._
-    import ta3.polynomialRing._
-    import PolynomialArithV2Helpers._
-    val x0 = ofTerm("x0()".asTerm)
-    val y0 = ofTerm("y0()".asTerm)
-    val tm1 = Exact(ofTerm("1/3".asTerm)) *! TM("x".asTerm, x0 + y0, "-0.01".asTerm, "0.02".asTerm, QE)
-    val tm2 = TM("y".asTerm, Const(BigDecimal("0.5")) * x0 - y0, "0".asTerm, "0.1".asTerm, QE)
-    val tm = (tm1 +! tm2).squareExact
+    import polynomialRing._
+    val tm3 = Exact(ofTerm("1/3".asTerm)) *! TM("x".asTerm, x0 + y0, "-0.01".asTerm, "0.02".asTerm, QE)
+    val tm = (tm3 +! tm2).squareExact
     val tmA = tm.approx(5)
     tmA.elem shouldBe "(1/3*x+y)^2".asTerm
     rhsOf(tmA.poly.prettyRepresentation) shouldBe "0.6944*x0()^2+- 1.112*x0()*y0()+0.4444*y0()^2".asTerm
