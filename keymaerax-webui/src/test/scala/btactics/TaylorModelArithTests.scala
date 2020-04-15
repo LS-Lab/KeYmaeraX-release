318aca4f26a524f66979e66ecc761d8bb029cd96
immler <immler@in.tum.de>
2020-04-15 16:02:57 +0200

    tests for approximate operations

--- keymaerax-webui/src/test/scala/btactics/TaylorModelArithTests.scala
+++ keymaerax-webui/src/test/scala/btactics/TaylorModelArithTests.scala
@@ -30,10 +30,19 @@ class TaylorModelArithTests extends TacticTestBase {
   lazy val tm1 = ta3.TM("x".asTerm, x0 + y0, "-0.01".asTerm, "0.02".asTerm, QE)
   lazy val tm2 = ta3.TM("y".asTerm, ta3.polynomialRing.Const(BigDecimal("0.5")) * x0 - y0, "0".asTerm, "0.1".asTerm, QE)
   lazy val tm3 = ta3.Exact(ta3.polynomialRing.ofTerm("1/3".asTerm)) *! tm1
+  lazy val tm100000 = ta3.Exact(ta3.polynomialRing.ofTerm("0.000001".asTerm)) *! tm1
+  lazy val tm1234 = ta3.Exact(ta3.polynomialRing.ofTerm("12.34".asTerm)) *! tm2
 
   "Taylor models" should "add exactly" in withMathematica { qeTool =>
     (tm1 +! tm2).prettyPrv.conclusion.succ(0) shouldBe
       "\\exists err_ (x+y=1.5*x0()+err_&-0.01<=err_&err_<=0.12)".asFormula
+  }
+
+  it should "add approximately" in withMathematica { qeTool =>
+    (tm1 +! tm100000).prettyPrv.conclusion.succ(0) shouldBe
+      "\\exists err_ (x+0.000001*x=1.000001*x0()+1.000001*y0()+err_&-0.010001<=err_&err_<=0.020001)".asFormula
+    (tm1 + tm100000).prettyPrv.conclusion.succ(0) shouldBe
+      "\\exists err_ (x+0.000001*x=x0()+1*y0()+err_&-0.010003<=err_&err_<=0.020003)".asFormula
   }
 
   it should "subtract exactly" in withMathematica { qeTool =>
@@ -41,9 +50,23 @@ class TaylorModelArithTests extends TacticTestBase {
       "\\exists err_ (x-y=0.5*x0()+2*y0()+err_&-0.11<=err_&err_<=0.02)".asFormula
   }
 
+  it should "subtract approximately" in withMathematica { qeTool =>
+    (tm1 -! tm100000).prettyPrv.conclusion.succ(0) shouldBe
+      "\\exists err_ (x-0.000001*x=0.999999*x0()+0.999999*y0()+err_&-0.010001<=err_&err_<=0.020001)".asFormula
+    (tm1 - tm100000).prettyPrv.conclusion.succ(0) shouldBe
+      "\\exists err_ (x-0.000001*x=0.9999*x0()+0.9999*y0()+err_&-0.010199<=err_&err_<=0.020199)".asFormula
+  }
+
   it should "multiply exactly" in withMathematica { qeTool =>
     (tm1 *! tm2).prettyPrv.conclusion.succ(0) shouldBe
       "\\exists err_ (x*y=0.5*x0()^2+- 0.5*x0()*y0()+- 1*y0()^2+err_&-0.231<=err_&err_<=0.232)".asFormula
+  }
+
+  it should "multiply approximately" in withMathematica { qeTool =>
+    (tm1234 *! tm1234).prettyPrv.conclusion.succ(0) shouldBe
+      "\\exists err_ (12.34*y*(12.34*y)=38.068900*x0()^2+- 152.27560*x0()*y0()+152.2756*y0()^2+err_&-45.684<=err_&err_<=47.207)".asFormula
+    (tm1234 * tm1234).prettyPrv.conclusion.succ(0) shouldBe
+      "\\exists err_ (12.34*y*(12.34*y)=38.06*x0()^2+- 152.3*x0()*y0()+152.2*y0()^2+err_&-45.793<=err_&err_<=47.316)".asFormula
   }
 
   it should "negate" in withMathematica { qeTool =>
@@ -54,6 +77,13 @@ class TaylorModelArithTests extends TacticTestBase {
   it should "square exactly" in withMathematica { qeTool =>
     tm1.squareExact.prettyPrv.conclusion.succ(0) shouldBe
       "\\exists err_ (x^2=x0()^2+2*x0()*y0()+1*y0()^2+err_&-0.08<=err_&err_<=0.0804)".asFormula
+  }
+
+  it should "square approximately" in withMathematica { qeTool =>
+    tm1234.squareExact.prettyPrv.conclusion.succ(0) shouldBe
+      "\\exists err_ ((12.34*y)^2=38.068900*x0()^2+- 152.27560*x0()*y0()+152.2756*y0()^2+err_&-45.683<=err_&err_<=47.206)".asFormula
+    tm1234.square.prettyPrv.conclusion.succ(0) shouldBe
+      "\\exists err_ ((12.34*y)^2=38.06*x0()^2+- 152.3*x0()*y0()+152.2*y0()^2+err_&-45.792<=err_&err_<=47.315)".asFormula
   }
 
   it should "^1" in withMathematica { qeTool =>
@@ -68,6 +98,11 @@ class TaylorModelArithTests extends TacticTestBase {
   it should "^(2*n + 1)" in withMathematica { qeTool =>
     (tm1^3).prettyPrv.conclusion.succ(0) shouldBe
       "\\exists err_ (x^3=x0()^3+3*x0()^2*y0()+3*x0()*y0()^2+1*y0()^3+err_&-0.2024<=err_&err_<=0.24241)".asFormula
+  }
+
+  it should "exponentiate approximately" in withMathematica { qeTool =>
+    (tm1234^3).prettyPrv.conclusion.succ(0) shouldBe
+      "\\exists err_ ((12.34*y)^3=234.8*x0()^3+- 1410*x0()^2*y0()+2818*x0()*y0()^2+- 1879*y0()^3+err_&-1122.4<=err_&err_<=1359.0)".asFormula
   }
 
   it should "exact" in withMathematica { qeTool =>
