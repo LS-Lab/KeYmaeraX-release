6afb68f9f721fc1608f10ca27453fef8b0fa600c
immler <immler@in.tum.de>
2020-04-30 15:30:04 +0200

    tactics to prove SOSsolve witnesses

---
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/SOSSolve.scala
@@ -0,0 +1,78 @@
+package edu.cmu.cs.ls.keymaerax.btactics
+
+import edu.cmu.cs.ls.keymaerax.bellerophon._
+import edu.cmu.cs.ls.keymaerax.btactics.TactixLibrary._
+import edu.cmu.cs.ls.keymaerax.btactics.TacticFactory._
+import edu.cmu.cs.ls.keymaerax.core._
+import edu.cmu.cs.ls.keymaerax.infrastruct._
+import edu.cmu.cs.ls.keymaerax.infrastruct.Augmentors._
+import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
+import edu.cmu.cs.ls.keymaerax.pt.ProvableSig
+import edu.cmu.cs.ls.keymaerax.tools.ext.BigDecimalTool
+import edu.cmu.cs.ls.keymaerax.tools.qe.BigDecimalQETool
+
+import scala.collection.immutable._
+
+/** tactics to prove SOSsolve witnesses */
+object SOSSolve {
+
+  private lazy val zeroGtOne = AnonymousLemmas.remember("1 > 0".asFormula, QE & done)
+  private lazy val sosPosIntro = AnonymousLemmas.remember("x_() > 0 -> x_() + y_()^2 > 0".asFormula, QE & done)
+  private lazy val sosPosCoeffIntro = AnonymousLemmas.remember("((c_() > 0<->true) & x_() > 0) -> x_() + c_()*y_()^2 > 0".asFormula, QE & done)
+  private def sosPosTac : DependentTactic = "sosPosTac" by { (seq: Sequent) =>
+    require(seq.succ.length==1, "sosPosTac requires exactly one succedent")
+    require(seq.ante.isEmpty, "sosPosTac requires empty antecedent")
+    seq.succ(0) match {
+      case Greater(Number(m), Number(n)) if m.compareTo(1) == 0 && n.compareTo(0) == 0 =>
+        by(zeroGtOne)
+      case Greater(Plus(x, Power(y, Number(p))), Number(n)) if p.compareTo(2) == 0 && n.compareTo(0) == 0 =>
+        useAt(sosPosIntro, PosInExpr(1::Nil))(1) & sosPosTac
+      case Greater(Plus(x, Times(c, Power(y, Number(p)))), Number(n)) if p.compareTo(2) == 0 && n.compareTo(0) == 0  =>
+        val cPrv = ProvableSig.proveArithmetic(BigDecimalQETool, Greater(c, Number(0)))
+        useAt(sosPosCoeffIntro, PosInExpr(1::Nil))(1) & andR(1) & Idioms.<(by(cPrv), sosPosTac)
+      case fml => throw new IllegalArgumentException("sosPosTac must be applied to 1 > 0, x + y^2 > 0, x + c*y^2 > 0 but got " + fml)
+    }
+  }
+
+  private lazy val plusEqZeroIntro = AnonymousLemmas.remember("(p_() = 0 & q_() = 0) -> p_() + q_() = 0".asFormula, QE & done)
+  private lazy val timesEqZeroIntro = AnonymousLemmas.remember("p_() = 0 -> c_()*p_() = 0".asFormula, QE & done)
+  private def eqZeroTac : DependentTactic = "eqZeroTac" by { (seq: Sequent) =>
+    require(seq.succ.length==1, "eqZeroTac requires exactly one succedent")
+    seq.succ(0) match {
+      case Equal(Plus(p, q), Number(n)) if n.compareTo(0) == 0 =>
+        useAt(plusEqZeroIntro, PosInExpr(1::Nil))(1) & andR(1) & Idioms.<(eqZeroTac, eqZeroTac)
+      case Equal(Times(c, p), Number(n)) if n.compareTo(0) == 0 =>
+        useAt(timesEqZeroIntro, PosInExpr(1::Nil))(1) & closeId
+      case fml => throw new IllegalArgumentException("sosPos must be applied to 1 > 0 or x + y^2 > 0 but got " + fml)
+    }
+  }
+
+  lazy val witnessSOSLemma = AnonymousLemmas.remember("(sos_() > 0 & sos_() = comb_() & comb_() = 0) -> false".asFormula, QE & done)
+  private def witnessSOS(degree: Int) : DependentTactic = {
+    val name = "witnessSOS"
+    name by { (seq: Sequent) =>
+      require(seq.succ.isEmpty, name + " requires succedent to be empty")
+      val polys = seq.ante.map{
+        case Equal(p, Number(n)) if n.compareTo(0) == 0 => p
+        case fml => throw new IllegalArgumentException(name + " requires only formulas of the form 'poly = 0' in the antecedent but got " + fml)
+      }.toList
+      val vars = polys.flatMap(StaticSemantics.freeVars(_).toSet).distinct
+      val sosSolveTool = ToolProvider.sosSolveTool().getOrElse(throw new RuntimeException("no SOSSolveTool configured"))
+      val (sos, cofactors) = sosSolveTool.sosSolve(polys, vars, degree)
+      val sosPos = proveBy(Greater(sos, Number(0)), sosPosTac & done)
+      val combination = (cofactors, polys).zipped.map(Times).reduceLeft(Plus)
+      val witnessPrv = proveBy(Equal(sos, combination), PolynomialArithV2.equate(1))
+      val zeroPrv = proveBy(Sequent(seq.ante, IndexedSeq(Equal(combination, Number(0)))), eqZeroTac & done)
+      cut(False) & Idioms.<(
+        closeF,
+        useAt(witnessSOSLemma.fact(USubst(Seq(SubstitutionPair("sos_()".asTerm, sos), SubstitutionPair("comb_()".asTerm, combination)))), PosInExpr(1::Nil))(1) &
+          andR(1) & Idioms.<(cohideR(1) & by(sosPos), andR(1) & Idioms.<(cohideR(1) & by(witnessPrv), by(zeroPrv))))
+    }
+  }
+
+  def sossolve(degree: Int) : BelleExpr = "ANON" by {
+    prop &
+      PolynomialArith.prepareArith &
+      witnessSOS(degree)
+  }
+}
--- keymaerax-webui/src/test/scala/btactics/SOSsolveTests.scala
+++ keymaerax-webui/src/test/scala/btactics/SOSsolveTests.scala
@@ -1,10 +1,25 @@
 package btactics
 
 import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
-import edu.cmu.cs.ls.keymaerax.btactics.{TacticTestBase, ToolProvider}
+import edu.cmu.cs.ls.keymaerax.btactics.{SOSSolve, TacticTestBase, ToolProvider}
+import edu.cmu.cs.ls.keymaerax.btactics.TactixLibrary._
+import edu.cmu.cs.ls.keymaerax.parser.KeYmaeraXPrettierPrinter
 import org.scalatest.PrivateMethodTester
 
 class SOSsolveTests extends TacticTestBase with PrivateMethodTester {
+  val prob1 = ("!(-200 + 10 * t1uscore0dollarskuscore0 + vuscore2dollarskuscore0 = 0 &" +
+    "t1uscore0dollarskuscore0 >= 0 &" +
+    "stuscore2dollarskuscore0 = 0 &" +
+    "-2000 * stuscore2dollarskuscore0 - 4000 * suscore2dollarskuscore0 -" +
+    "  200 * stuscore2dollarskuscore0 * tuscore2dollarskuscore0 - 5 * tuscore2dollarskuscore0^2 +" +
+    "  10 * stuscore2dollarskuscore0 * tuscore2dollarskuscore0^2 + zuscore2dollarskuscore0 = 0 &" +
+    "tuscore2dollarskuscore0 >= 0 &" +
+    "suscore2dollarskuscore0 >= 0 &" +
+    "vuscore2dollarskuscore0 >= 0 &" +
+    "zuscore2dollarskuscore0 >= 0 &" +
+    "-200 * stuscore2dollarskuscore0 - 10 * tuscore2dollarskuscore0 +" +
+    "  20 * stuscore2dollarskuscore0 * tuscore2dollarskuscore0 + vuscore2dollarskuscore0 = 0 &" +
+    "t1uscore0dollarskuscore0 + tuscore2dollarskuscore0 < 0)").asFormula
   val polys1 = (
     "stuscore2dollarskuscore0," +
       "-200 + 10 * t1uscore0dollarskuscore0 + vuscore2dollarskuscore0," +
@@ -20,10 +35,16 @@ class SOSsolveTests extends TacticTestBase with PrivateMethodTester {
   val vars1 = ("vuscore2dollarskuscore0, t1uscore0dollarskuscore0, stuscore2dollarskuscore0, zuscore2dollarskuscore0," +
     "tuscore2dollarskuscore0, suscore2dollarskuscore0, GEQ11, GEQ12, GEQ13, GEQ14, GEQ15, GT16").split(',').map(_.asTerm).toList
 
-  "SOSsolve" should "return the certificate" in withMathematica { _ =>
+  "sosSolveTool" should "return the certificate" in withMathematica { _ =>
     val sosSolveTool = ToolProvider.sosSolveTool().getOrElse(throw new RuntimeException("no SOSSolveTool configured"))
     sosSolveTool.sosSolve(polys1, vars1, 1) shouldBe
       ("1+20*GT16^2".asTerm,
         "1/10*(200*GT16^2+-20*GT16^2*tuscore2dollarskuscore0), -1/10*GT16^2, 1/10*GT16^2, 0, 0, 0, 0, 0, 0, -1".split(',').map(_.asTerm).toList)
   } /* to verify: cofactors * polys = sos */
+
+  "SOSSolve" should "prove using the certificate" in withMathematica { _ =>
+    val pp = new KeYmaeraXPrettierPrinter(100)
+    proveBy(prob1, SOSSolve.sossolve(1)) shouldBe 'proved
+  }
+
 }
