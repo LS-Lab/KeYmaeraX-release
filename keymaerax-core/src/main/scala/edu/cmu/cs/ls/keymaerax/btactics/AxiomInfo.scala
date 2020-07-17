cdd35c5f72d384804a71f1964eaa922d14707e2d
Brandon Bohrer <bbohrer@cs.cmu.edu>
2020-07-17 16:36:56 -0400

    Add main TacticInfo initialization algorithm

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/AxiomInfo.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/AxiomInfo.scala
@@ -4,6 +4,8 @@
   */
 package edu.cmu.cs.ls.keymaerax.btactics
 
+import java.lang.reflect.Method
+
 import edu.cmu.cs.ls.keymaerax.bellerophon._
 import edu.cmu.cs.ls.keymaerax.bellerophon.parser.BelleParser
 import edu.cmu.cs.ls.keymaerax.macros._
@@ -16,12 +18,17 @@ import edu.cmu.cs.ls.keymaerax.infrastruct._
 import edu.cmu.cs.ls.keymaerax.lemma.Lemma
 import edu.cmu.cs.ls.keymaerax.parser.StringConverter._
 import edu.cmu.cs.ls.keymaerax.pt.ProvableSig
+import org.apache.logging.log4j.scala.Logging
 
 import scala.collection.immutable.HashMap
 import scala.language.implicitConversions
 import DerivationInfoAugmentors._
+import edu.cmu.cs.ls.keymaerax.btactics
+
+import scala.collection.mutable
 import scala.reflect.macros
 import scala.reflect.runtime.universe.TypeTag
+import scala.reflect.runtime.{universe => ru}
 import scala.util.Try
 
 /**
@@ -48,7 +55,7 @@ import scala.util.Try
   * Central list of all derivation steps (axioms, derived axioms, proof rules, tactics)
   * with meta information of relevant names and display names and visualizations for the user interface.
   */
-object DerivationInfoRegistry {
+object DerivationInfoRegistry extends Logging {
   import scala.language.implicitConversions
   //implicit def DerivationInfoToDerivationInfoConverter(s: DerivationInfo): DerivationInfoConverter = new DerivationInfoConverter(s)
 
@@ -880,9 +887,167 @@ object DerivationInfoRegistry {
     differentialInfos ++ foInfos ++ miscInfos ++ derivedAxiomsInfos ++ sequentCalculusInfos) ensures (
     consistentInfo _, "meta-information on AxiomInfo is consistent with actual (derived) axioms etc.")
 
-  def init: Unit = {
-    if(DerivationInfo._allInfo.isEmpty)
-      DerivationInfo._allInfo = DerivationInfo._allInfo ++ allInfo
+  /** We need to force the right-hand side of every tactic definition to evaluate, which requires passing in arguments
+    * to every "def" of a tactic. Subtly, it's ok for these arguments to be dummies, because we're not actually
+    * interpreting the tactic on a Provable, just creating a BelleExpr without interpreting that BelleExpr */
+  private def instantiateArg(ai: String): Any = {
+    ai match {
+      case "Number" | "Term" | "Expression" => Number(0)
+      case "String" => ""
+      case "Variable" => Variable("x")
+      case "Formula" => True
+      case "Generator" => FixedGenerator(Nil)
+      case "SubstitutionPair"  =>  SubstitutionPair(Nothing, Nothing)
+      case "PosInExpr" => PosInExpr()
+      case "Option" => None
+      case "List" => Nil
+    }
+  }
+
+  /* Apply a reflected method with well-typed dummy arguments (for its side effects)*/
+  private def applyMirror(m: ru.MethodMirror, argSyms: List[ru.Symbol]): Unit = {
+    val args = argSyms.map((theSymbol: ru.Symbol) => theSymbol.info.typeConstructor.toString.split('.').last)
+    args match {
+      // If [[m]] is a tactic, determine and instantiate correct argument types
+      case Nil => m()
+      case _ =>
+        val arguments = args.map(instantiateArg)
+        m(arguments:_*)
+    }
+  }
+
+
+  /* Initialize TacticInfo for all @Tactic annotations in given class [[cl]]*/
+  private def initClass(cl: Class[_], tpe: ru.Type): Unit = {
+    // @TODO: Reduce code duplication
+    /* Collect fields and methods of class */
+    val fields  = cl.getDeclaredFields.filter(f => classOf[BelleExpr].isAssignableFrom(f.getType))
+    val methods = cl.getDeclaredMethods.filter(f => classOf[BelleExpr].isAssignableFrom(f.getReturnType))
+    val mirror = ru.runtimeMirror(getClass.getClassLoader)
+    // access the singleton object
+    val moduleMirror = mirror.reflectModule(tpe.termSymbol.asModule)
+    val im = mirror.reflect(moduleMirror.instance)
+
+    /*  @Tactic and friends disappear during compilation, but they replace themselves with @InternalAnnotation
+     *  which we use here to identify annotated fields, which must be executed in order to initialize TacticInfos*/
+    //@note lazy vals have a "hidden" getter method that does the initialization
+    val keptFields = fields.filter(fn => { tpe.member(ru.TermName(fn.getName)).alternatives.flatMap(_.annotations).exists(_.tree.tpe.typeSymbol.toString == "InternalAnnotation")})
+    val fieldFields = keptFields.map(fn => (fn, tpe.member(ru.TermName(fn.getName)).asMethod.getter.asMethod))
+    val methodFields: mutable.ArraySeq[(String, ru.MethodSymbol)] = methods.flatMap(fn => {
+      val mem = tpe.member(ru.TermName(fn.getName))
+      /* Overoaded values are considered terms (not methods), and have a list of alternatives, which might be annotated.
+      * In this case, we are only interested in the alternative that was actually annotated, not the whole method*/
+      if (mem.isTerm && mem.asTerm.isOverloaded) {
+        mem.asTerm.alternatives.filter(_.annotations.exists(_.tree.tpe.typeSymbol.name.toString == "InternalAnnotation"))
+          .map(c => (fn.getName, c.asMethod))
+      } else if (mem.annotations.exists(_.tree.tpe.typeSymbol.name.toString == "InternalAnnotation")){
+        mutable.ArraySeq((fn.getName, mem.asMethod))
+      } else {
+        mutable.ArraySeq()
+      }})
+    val fieldMirrors = fieldFields.map({case (x, y) => (x, im.reflectMethod(y))})
+    val methodMirrors = methodFields.map({case (x, y) => (x, im.reflectMethod(y))})
+    var failures: mutable.Buffer[(String,Throwable)] = mutable.Buffer()
+    methodMirrors.indices.foreach(idx => {
+      try {
+        val (fn, fm) = methodMirrors(idx)
+        /* Type signature of function needed in order to generate well-typed arguments */
+        val argSyms = fm.symbol.typeSignature.paramLists.headOption.getOrElse(Nil)
+        // NB: allInfo gets updated automatically when touching each field/method
+        applyMirror(fm, argSyms)
+      } catch {
+        case e: Throwable =>
+          failures += (methodFields(idx)._1 -> e)
+          logger.warn("WARNING: Failed to initialize @Tactic.", e)
+      }
+    })
+    if (failures.nonEmpty)
+      throw new Exception(s"WARNING: Encountered ${failures} method failures when trying to initialize @Tactic in ${cl.getName}. Unable to initialize:\n" + failures.map(_._1).mkString("\n"), failures.head._2)
+    fieldMirrors.indices.foreach(idx => {
+      try {
+        val (fn, fm) = fieldMirrors(idx)
+        val argSyms = tpe.member(ru.TermName(fn.getName)).asMethod.typeSignature.paramLists.headOption.getOrElse(Nil)
+        applyMirror(fm, argSyms)
+      } catch {
+        case e: Throwable =>
+          failures += (keptFields(idx).getName -> e)
+          logger.warn("WARNING: Failed to initialize @Tactic.", e)
+      }
+    })
+    if (failures.nonEmpty)
+      throw new Exception(s"WARNING: Encountered ${failures} field failures when trying to initialize @Tactic in ${cl.getName}. Unable to initialize:\n" + failures.map(_._1).mkString("\n"), failures.head._2)
+  }
+
+  /* Has the global DerivationInfo list been initialized? */
+  def isInit: Boolean = DerivationInfo._allInfo.nonEmpty
+  /* Initialize global DerivationInfo list. This is surprisingly tricky because DerivationInfo is attached to tactic
+  * (and axiom and rule) definitions, which are scattered throughout the prover. Here we maintain a global list of files
+  * that can contain @Tactic definitions. Those classes are reflected and all annotated definitions are evaluated.
+  * Having been modified by the @Tactic macro, those definitions all contain a function call which adds their TacticInfo
+  * object to the global list. While this process does not require evaluating tactics in full, it does require loading
+  * a number of classes, which triggers their static initializers and thus requires some attention to initialization order.
+  *
+  * Sanity checks ensure a runtime error is raised if @Tactic is used outside the allowed classes.
+  * */
+  def init(tool: String = "mathematica", initLibrary: Boolean = true): Unit = {
+    /* Initialization is relatively slow, so only initialize once*/
+    if(isInit) return
+    // Remember that initialization is in progress,
+    DerivationInfo._initStatus = DerivationInfo.InitInProgress
+    if(!initLibrary) // Advanced use - user takes over in-progress initialization
+      return
+    // Initialize derived axioms and rules, which automatically initializes their AxiomInfo and RuleInfo too
+    Ax.prepopulateDerivedLemmaDatabase()
+    /* Global list of library files which are permitted to use @Tactic. When adding new files to the tactics library,
+    * it should suffice to update this list. */
+    val objects: List[(Class[_], ru.Type)] = List(
+      (Approximator.getClass, ru.typeOf[Approximator.type]),
+      (ArithmeticSimplification.getClass, ru.typeOf[ArithmeticSimplification.type]),
+      (ArithmeticSpeculativeSimplification.getClass, ru.typeOf[ArithmeticSpeculativeSimplification.type]),
+      (ComponentSystem.getClass, ru.typeOf[ComponentSystem.type]),
+      (DebuggingTactics.getClass, ru.typeOf[DebuggingTactics.type]),
+      (DifferentialEquationCalculus.getClass, ru.typeOf[btactics.DifferentialEquationCalculus.type]),
+      (DifferentialTactics.getClass, ru.typeOf[DifferentialTactics.type]),
+      (DLBySubst.getClass, ru.typeOf[DLBySubst.type]),
+      (EqualityTactics.getClass, ru.typeOf[EqualityTactics.type]),
+      (FOQuantifierTactics.getClass, ru.typeOf[FOQuantifierTactics.type]),
+      (HilbertCalculus.getClass, ru.typeOf[HilbertCalculus.type]),
+      (HybridProgramTactics.getClass, ru.typeOf[HybridProgramTactics.type]),
+      (IntervalArithmeticV2.getClass, ru.typeOf[IntervalArithmeticV2.type]),
+      (ODEInvariance.getClass, ru.typeOf[ODEInvariance.type]),
+      (PropositionalTactics.getClass, ru.typeOf[PropositionalTactics.type]),
+      (ProofRuleTactics.getClass, ru.typeOf[ProofRuleTactics.type]),
+      (SequentCalculus.getClass, ru.typeOf[SequentCalculus.type]),
+      (SimplifierV3.getClass, ru.typeOf[SimplifierV3.type]),
+      (TactixLibrary.getClass, ru.typeOf[TactixLibrary.type]),
+      (ToolTactics.getClass, ru.typeOf[ToolTactics.type]),
+      (Transitivity.getClass, ru.typeOf[Transitivity.type])
+    )
+    objects.foreach({case (cl, ct) => initClass(cl, ct)})
+    /* Check that the list of annotated tactics we processed matches the list of named BelleExprs which have been
+     * constructed so far. This can catch some named tactics that were never annotated, or catch some annotated files
+     * that were forgotten in our list of files.
+    */
+    // NB: This check used to be an assertion in NamedTactic, but that doesn't work if a tactic is mentioned before registration.
+    // Instead, check the names after everything is initialized.
+    var overimplemented: Set[String] = Set()
+    DerivationInfo._seenNames.foreach((n:String) => {
+      if(!DerivationInfo.hasCodeName(n))
+        overimplemented = overimplemented.+(n)
+    })
+    assert(overimplemented.isEmpty, s"@Tactic init failed: NamedBelleExpr(s) named ${overimplemented.toList.mkString(", ")} but this name does not appear in DerivationInfo's list of codeNames.")
+    var unimplemented: Set[String] = Set()
+    DerivationInfo._allInfo.foreach((di: DerivationInfo) => {
+      if(!DerivationInfo._seenNames.contains(di.codeName)) {
+        di match {
+          // Axioms and rules are not tracked
+          case _: CoreAxiomInfo | _: AxiomaticRuleInfo | _: DerivedAxiomInfo => ()
+          case _ => unimplemented = unimplemented.+(di.codeName)
+        }
+      }
+    })
+    assert(unimplemented.isEmpty, s"@Tactic init failed: Following DerivationInfo never implemented as @Tactic: " + unimplemented.toList.mkString(", "))
+    DerivationInfo._initStatus = DerivationInfo.InitComplete
   }
 
   ////////////////////////////////////////////////////////
