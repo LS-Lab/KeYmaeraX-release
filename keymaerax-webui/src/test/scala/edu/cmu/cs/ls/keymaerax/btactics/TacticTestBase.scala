96cf96f14c9c08125c82390c3365c99b57b0b347
Stefan Mitsch <smitsch@cs.cmu.edu>
2020-07-23 18:29:07 -0400

    Fix withTactics test wrapper: skip only initialization, but execute tests

--- keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/btactics/TacticTestBase.scala
+++ keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/btactics/TacticTestBase.scala
@@ -129,30 +129,31 @@ class TacticTestBase extends FlatSpec with Matchers with BeforeAndAfterEach with
   def withDatabase(testcode: TempDBTools => Any): Unit = testcode(dbTester())
 
   def withTactics(testcode: => Any): Unit = {
-    if (DerivationInfoRegistry.isInit) return
-    val mathLinkTcp = System.getProperty(Configuration.Keys.MATH_LINK_TCPIP, Configuration(Configuration.Keys.MATH_LINK_TCPIP)) // JVM parameter -DMATH_LINK_TCPIP=[true,false]
-    val common = Map(
-      Configuration.Keys.MATH_LINK_TCPIP -> mathLinkTcp,
-      Configuration.Keys.QE_TOOL -> "mathematica")
-    val interp = common.+(Configuration.Keys.QE_ALLOW_INTERPRETED_FNS -> "true")
-    val oldProvider = ToolProvider.provider
-    val provider = mathematicaProvider()
-    ToolProvider.setProvider(provider)
-    val tool = provider.defaultTool() match {
-      case Some(m: Mathematica) => m
-      case _ => fail("Illegal Wolfram tool, please use one of 'Mathematica' or 'Wolfram Engine' in test setup")
+    if (!DerivationInfoRegistry.isInit) {
+      val mathLinkTcp = System.getProperty(Configuration.Keys.MATH_LINK_TCPIP, Configuration(Configuration.Keys.MATH_LINK_TCPIP)) // JVM parameter -DMATH_LINK_TCPIP=[true,false]
+      val common = Map(
+        Configuration.Keys.MATH_LINK_TCPIP -> mathLinkTcp,
+        Configuration.Keys.QE_TOOL -> "mathematica")
+      val interp = common.+(Configuration.Keys.QE_ALLOW_INTERPRETED_FNS -> "true")
+      val oldProvider = ToolProvider.provider
+      val provider = mathematicaProvider()
+      ToolProvider.setProvider(provider)
+      val tool = provider.defaultTool() match {
+        case Some(m: Mathematica) => m
+        case _ => fail("Illegal Wolfram tool, please use one of 'Mathematica' or 'Wolfram Engine' in test setup")
+      }
+      withTemporaryConfig(interp) {
+        DerivationInfoRegistry.init()
+      }
+      tool.cancel()
+      tool.shutdown() // let testcode know it should stop (forEvery catches all exceptions)
+      mathematicaProvider.synchronized {
+        mathematicaProvider().doShutdown() //@note see [[afterAll]]
+        provider.shutdown()
+        mathematicaProvider = new Lazy(new DelayedShutdownToolProvider(new MathematicaToolProvider(configFileMathematicaConfig)))
+      }
+      ToolProvider.setProvider(oldProvider)
     }
-    withTemporaryConfig(interp) {
-      DerivationInfoRegistry.init()
-    }
-    tool.cancel()
-    tool.shutdown() // let testcode know it should stop (forEvery catches all exceptions)
-    mathematicaProvider.synchronized {
-      mathematicaProvider().doShutdown() //@note see [[afterAll]]
-      provider.shutdown()
-      mathematicaProvider = new Lazy(new DelayedShutdownToolProvider(new MathematicaToolProvider(configFileMathematicaConfig)))
-    }
-    ToolProvider.setProvider(oldProvider)
     testcode
   }
 
