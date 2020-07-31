37e75756747b9f60ce7ac84502469b7f2cb1da96
Brandon Bohrer <bbohrer@cs.cmu.edu>
2020-07-31 16:25:16 -0400

    pretty up VariableSets.scala

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/cdgl/kaisar/VariableSets.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/cdgl/kaisar/VariableSets.scala
@@ -1,28 +1,62 @@
+/**
+  * Copyright (c) Carnegie Mellon University.
+  * See LICENSE.txt for the conditions of this license.
+  */
+/**
+  * Syntactic compute variable sets for static semantics of Kaisar proofs
+  * @author Brandon Bohrer
+  */
 package edu.cmu.cs.ls.keymaerax.cdgl.kaisar
 
-import edu.cmu.cs.ls.keymaerax.cdgl.kaisar.Context
-import edu.cmu.cs.ls.keymaerax.cdgl.kaisar.Context.Context
 import edu.cmu.cs.ls.keymaerax.cdgl.kaisar.KaisarProof._
 import edu.cmu.cs.ls.keymaerax.core._
 
-case class VariableSets (boundVars: Set[Variable], tabooVars: Set[Variable], functions: Set[Ident], facts: Set[Ident]) {
-  def addTabooVars(v: Set[Variable]): VariableSets = VariableSets(boundVars, tabooVars.++(v), functions, facts)
-  def addTabooFuncs(f: Set[Ident]): VariableSets = VariableSets(boundVars, tabooVars, functions.++(f), facts)
-  def addTabooFacts(p: Set[Ident]): VariableSets = VariableSets(boundVars, tabooVars, functions, facts.++(p))
+/**
+  *
+  * @param boundVars All program variables which are bound at some point in the proof
+  * @param tabooVars All program variables which are taboo (not allowed to be referenced), usually because they are ghosts
+  * @param tabooFunctions All function symbols which are taboo to reference, usually because they are inverse ghosts
+  * @param tabooFacts All fact variables which are taboo to reference, usually because they are inverse ghosts
+  */
+case class VariableSets (boundVars: Set[Variable], tabooVars: Set[Variable], tabooFunctions: Set[Ident], tabooFacts: Set[Ident]) {
+  def addTabooVars(v: Set[Variable]): VariableSets = VariableSets(boundVars, tabooVars.++(v), tabooFunctions, tabooFacts)
+  def addTabooFuncs(f: Set[Ident]): VariableSets = VariableSets(boundVars, tabooVars, tabooFunctions.++(f), tabooFacts)
+  def addTabooFacts(p: Set[Ident]): VariableSets = VariableSets(boundVars, tabooVars, tabooFunctions, tabooFacts.++(p))
+  /** Set union  */
   def ++(other: VariableSets): VariableSets =
-    VariableSets(boundVars.++(other.boundVars), tabooVars.++(other.tabooVars), functions.++(other.functions), facts.++(other.facts))
+    VariableSets(boundVars.++(other.boundVars), tabooVars.++(other.tabooVars), tabooFunctions.++(other.tabooFunctions), tabooFacts.++(other.tabooFacts))
 }
 
 object VariableSets {
   val empty: VariableSets = VariableSets(Set(), Set(), Set(), Set())
+
+  /** Set of taboo fact variables
+    * @param isInverseGhost Was the context where the facts were defined enclosed by an inverse-ghost?
+    * */
   private def ofFacts(vars: Set[Variable], isInverseGhost: Boolean) =
-    VariableSets(boundVars = Set(), tabooVars = Set(), functions = Set(), facts = if (isInverseGhost) vars else Set())
+    VariableSets(boundVars = Set(), tabooVars = Set(), tabooFunctions = Set(), tabooFacts = if (isInverseGhost) vars else Set())
+
+  /** Set of bound program variables
+    * @param isGhost Was the context where the program variables were defined enclosed by a ghost?
+    * */
   private def ofBound(vars: Set[Variable], isGhost: Boolean) =
     VariableSets(boundVars = vars, tabooVars = if(isGhost) vars else Set(), Set(), Set())
-  private def ofFunc(vars: Set[Variable], isInverseGhost: Boolean) =
-    VariableSets(boundVars = Set(), tabooVars = Set(), functions = if (isInverseGhost) vars else Set(), facts = Set())
 
-  def apply(con: Context, isGhost: Boolean = false, isInverseGhost: Boolean = false): VariableSets = con match {
+  /** Set of taboo function symbols
+    * @param isInverseGhost Was the context where the functions were defined enclosed by an inverse-ghost?
+    * */
+  private def ofFunc(vars: Set[Variable], isInverseGhost: Boolean) =
+    VariableSets(boundVars = Set(), tabooVars = Set(), tabooFunctions = if (isInverseGhost) vars else Set(), tabooFacts = Set())
+
+  /** Compute static semantic variables sets for a given Kaisar [[statement]], depending on the context in which
+    * [[statement]] appears
+    *
+    * @param statement The Kaisar statement whose variable sets should be computed
+    * @param isGhost Does [[statement]] appear under the [[Ghost]] constructor?
+    * @param isInverseGhost Does [[statement]] appear under the [[InverseGhost]] constructor
+    * @return All bound and taboo variables of [[statement]]
+    */
+  def apply(statement: Statement, isGhost: Boolean = false, isInverseGhost: Boolean = false): VariableSets = statement match {
     case Triv() | Label(_, _) => VariableSets.empty
     case Ghost(s) => apply(s, isGhost = true, isInverseGhost = false)
     case InverseGhost(s) => apply(s, isGhost = false, isInverseGhost = true)
@@ -46,14 +80,30 @@ object VariableSets {
     case m: MetaNode => m.children.map(apply(_, isGhost, isInverseGhost)).foldLeft(VariableSets.empty)(_.++(_))
   }
 
-  def apply(con: DiffStatement, isGhost: Boolean, isInverseGhost: Boolean): VariableSets = con match {
+  /** Compute static semantic variables sets for a given Kaisar differential statement, depending on the context in
+    * which it appears
+    *
+    * @param diffStatement The differential Kaisar statement whose variable sets should be computed
+    * @param isGhost Does statement appear under the [[Ghost]] constructor?
+    * @param isInverseGhost Does statement appear under the [[InverseGhost]] constructor
+    * @return All bound and taboo variables of statement
+    */
+  def apply(diffStatement: DiffStatement, isGhost: Boolean, isInverseGhost: Boolean): VariableSets = diffStatement match {
     case AtomicODEStatement(dp) => ofBound(Set(dp.xp.x), isGhost)
     case DiffProductStatement(l, r) => apply(l, isGhost, isInverseGhost).++(apply(r, isGhost, isInverseGhost))
     case DiffGhostStatement(ds) => apply(ds, isGhost = true, isInverseGhost = false)
     case InverseDiffGhostStatement(ds) => apply(ds, isGhost = false, isInverseGhost = true)
   }
 
-  def apply(con: DomainStatement, isGhost: Boolean, isInverseGhost: Boolean): VariableSets = con match {
+  /** Compute static semantic variables sets for a given Kaisar domain statement, depending on the context in
+    * which it appears
+    *
+    * @param domainStatement The Kaisar domain statement whose variable sets should be computed
+    * @param isGhost Does statement appear under the [[Ghost]] constructor?
+    * @param isInverseGhost Does statement appear under the [[InverseGhost]] constructor
+    * @return All bound and taboo variables of statement
+    */
+  def apply(domainStatement: DomainStatement, isGhost: Boolean, isInverseGhost: Boolean): VariableSets = domainStatement match {
     case DomAssume(x: Term, f) => ofFacts(StaticSemantics(x).toSet, isInverseGhost)
     case DomAssert(x: Term, f, child) => ofFacts(StaticSemantics(x).toSet, isInverseGhost)
     case DomWeak(dc: DomainStatement) => apply(dc, isGhost = false, isInverseGhost = true)
