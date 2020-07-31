33be4a6c0933f28da4e455fc6ba934f458499303
Brandon Bohrer <bbohrer@cs.cmu.edu>
2020-07-31 16:00:57 -0400

    clean up variable set computation code

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/cdgl/kaisar/VariableSets.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/cdgl/kaisar/VariableSets.scala
@@ -9,21 +9,29 @@ case class VariableSets (boundVars: Set[Variable], tabooVars: Set[Variable], fun
   def addTabooVars(v: Set[Variable]): VariableSets = VariableSets(boundVars, tabooVars.++(v), functions, facts)
   def addTabooFuncs(f: Set[Ident]): VariableSets = VariableSets(boundVars, tabooVars, functions.++(f), facts)
   def addTabooFacts(p: Set[Ident]): VariableSets = VariableSets(boundVars, tabooVars, functions, facts.++(p))
-  def ++(other: VariableSets): VariableSets = VariableSets(boundVars.++(other.boundVars), tabooVars.++(other.tabooVars), functions.++(other.functions), facts.++(other.facts))
+  def ++(other: VariableSets): VariableSets =
+    VariableSets(boundVars.++(other.boundVars), tabooVars.++(other.tabooVars), functions.++(other.functions), facts.++(other.facts))
 }
 
 object VariableSets {
   val empty: VariableSets = VariableSets(Set(), Set(), Set(), Set())
+  private def ofFacts(vars: Set[Variable], isInverseGhost: Boolean) =
+    VariableSets(boundVars = Set(), tabooVars = Set(), functions = Set(), facts = if (isInverseGhost) vars else Set())
+  private def ofBound(vars: Set[Variable], isGhost: Boolean) =
+    VariableSets(boundVars = vars, tabooVars = if(isGhost) vars else Set(), Set(), Set())
+  private def ofFunc(vars: Set[Variable], isInverseGhost: Boolean) =
+    VariableSets(boundVars = Set(), tabooVars = Set(), functions = if (isInverseGhost) vars else Set(), facts = Set())
+
   def apply(con: Context, isGhost: Boolean = false, isInverseGhost: Boolean = false): VariableSets = con match {
     case Triv() | Label(_, _) => VariableSets.empty
     case Ghost(s) => apply(s, isGhost = true, isInverseGhost = false)
     case InverseGhost(s) => apply(s, isGhost = false, isInverseGhost = true)
-    case Assume(pat: Term, f) => VariableSets(boundVars = Set(), tabooVars = Set(), functions = Set(), facts = if (isInverseGhost) StaticSemantics(pat).toSet else Set())
-    case Assert(pat: Term, f, m) => VariableSets(boundVars = Set(), tabooVars = Set(), functions = Set(), facts = if (isInverseGhost) StaticSemantics(pat).toSet else Set())
-    case Modify(pat: AsgnPat, _) => VariableSets(boundVars = pat.boundVars, tabooVars = if (isGhost) pat.boundVars else Set(), functions = Set(), facts = if (isInverseGhost) pat.boundFacts else Set())
-    case Note(x, proof, ann) => VariableSets(boundVars = Set(), tabooVars = Set(), functions = Set(), facts = if (isInverseGhost) Set(x) else Set())
-    case LetFun(f, args, e) => VariableSets(boundVars = Set(), tabooVars = Set(), functions = if (isInverseGhost) Set(f) else Set(), facts = Set())
-    case Match(pat: Term, e) => VariableSets(boundVars = StaticSemantics(pat).toSet, tabooVars = if (isGhost) StaticSemantics(pat).toSet else Set(), functions = Set(), facts = Set())
+    case Assume(pat: Term, f) => ofFacts(StaticSemantics(pat).toSet, isInverseGhost)
+    case Assert(pat: Term, f, m) => ofFacts(StaticSemantics(pat).toSet, isInverseGhost)
+    case Modify(pat: AsgnPat, _) => ofBound(pat.boundVars, isGhost).++(ofFacts(pat.boundFacts, isInverseGhost))
+    case Note(x, proof, ann) => ofFacts(Set(x), isInverseGhost)
+    case LetFun(f, args, e) => ofFunc(Set(f), isInverseGhost)
+    case Match(pat: Term, e) => ofBound(StaticSemantics(pat).toSet, isGhost)
     case Block(ss) => ss.map(apply(_, isGhost, isInverseGhost)).foldLeft(VariableSets.empty)((l, r) => l.++(r))
     case Switch(scrutinee, pats) =>
       pats.map({ case (x: Term, fml, s) => {
@@ -39,17 +47,17 @@ object VariableSets {
   }
 
   def apply(con: DiffStatement, isGhost: Boolean, isInverseGhost: Boolean): VariableSets = con match {
-    case AtomicODEStatement(dp) => VariableSets(boundVars = Set(dp.xp.x), tabooVars = if(isGhost) Set(dp.xp.x) else Set(), Set(), Set())
+    case AtomicODEStatement(dp) => ofBound(Set(dp.xp.x), isGhost)
     case DiffProductStatement(l, r) => apply(l, isGhost, isInverseGhost).++(apply(r, isGhost, isInverseGhost))
     case DiffGhostStatement(ds) => apply(ds, isGhost = true, isInverseGhost = false)
     case InverseDiffGhostStatement(ds) => apply(ds, isGhost = false, isInverseGhost = true)
   }
 
   def apply(con: DomainStatement, isGhost: Boolean, isInverseGhost: Boolean): VariableSets = con match {
-    case DomAssume(x: Term, f) => VariableSets(Set(), Set(), Set(), if(isInverseGhost) StaticSemantics(x).toSet else Set())
-    case DomAssert(x: Term, f, child) => VariableSets(Set(), if(isGhost) StaticSemantics(x).toSet else Set(), Set(), Set())
+    case DomAssume(x: Term, f) => ofFacts(StaticSemantics(x).toSet, isInverseGhost)
+    case DomAssert(x: Term, f, child) => ofFacts(StaticSemantics(x).toSet, isInverseGhost)
     case DomWeak(dc: DomainStatement) => apply(dc, isGhost = false, isInverseGhost = true)
-    case DomModify(x: AsgnPat, f: Term) => VariableSets(x.boundVars, if(isGhost) x.boundVars else Set(), Set(), if(isInverseGhost) x.boundFacts else Set())
+    case DomModify(x: AsgnPat, f: Term) => ofBound(x.boundVars, isGhost).++(ofFacts(x.boundFacts, isInverseGhost))
     case DomAnd(l, r) => apply(l, isGhost, isInverseGhost).++(apply(r, isGhost, isInverseGhost))
   }
 
