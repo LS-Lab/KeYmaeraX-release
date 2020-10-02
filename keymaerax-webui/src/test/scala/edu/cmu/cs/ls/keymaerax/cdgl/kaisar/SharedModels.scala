d3dc46d3039d6e9746388363908b3b5165f48dfd
Brandon Bohrer <bbohrer@cs.cmu.edu>
2020-10-02 16:10:07 -0400

    somewhat optimize ijrr example proof

--- keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/cdgl/kaisar/SharedModels.scala
+++ keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/cdgl/kaisar/SharedModels.scala
@@ -276,62 +276,62 @@ object SharedModels {
       |let norm(x, y) = (x^2 + y^2)^(1/2);
       |let dist(xl, xr, yl, yr) = norm (xl - xr, yl - yr);
       |let initialState() <-> (v = 0 & dist(x,y,ox,oy) > 0 & norm(dx, dy) = 1);
-      |let assumptions() <-> (bounds() & initialState());
       |let infdist(xl, yl, xr, yr) = max(abs(xl - xr), abs(yl - yr));
-      |let loopinv() <-> (v >= 0 & norm(dx, dy) = 1 & infdist(x, y, xo, yo) >= stopDist(v));
       |let goal() <-> dist(x, y, xo, yo) > 0;
-      |?assump:(assumptions());
-      |!inv:(loopinv()) using assump by auto;
+      |?(bnds, st):(bounds() & initialState());
+      |!(vSign, dxyNorm, safeDist):(v >= 0 & norm(dx, dy) = 1 & infdist(x, y, xo, yo) >= stopDist(v)) using bnds st by auto;
       |{body:
       |  {
       |    {
-      |      { a := -b; t := 0;
+      |      { ?aB:(a := -b); ?tZ:(t := 0);
       |        { x' = v * dx, y' = v * dy, v' = a,
       |          dx' = -w * dy, dy' = w * dx, w' = a/r,
       |          t' = 1 & ?dc:(t <= T & v >= 0);
-      |         & !tSign:(t >= 0) using t by induction;
-      |         & !dir:(norm(dx, dy) = 1) using inv by induction;
-      |         & !vSol:(v = v@body - b*t) using a by induction;
+      |         & !tSign:(t >= 0) using tZ by induction;
+      |         & !dir:(norm(dx, dy) = 1) using dxyNorm by induction;
+      |         & !vSol:(v = v@body - b*t) using aB by induction;
       |         & !xBound:(-t * v <= x - x@body & x - x@body <= t * v) using vSol dir tSign dc by induction;
       |         & !yBound:(-t * v <= y - y@body & y - y@body <= t * v) using vSol dir tSign dc by induction;
       |        };
       |      }
       |        ++
-      |      { ?vZ:(v = 0); a := 0; w := 0; t := 0;
+      |      { ?vZ:(v = 0); ?aZ:(a := 0); w := 0; ?tZ(t := 0);
       |        { x' = v * dx, y' = v * dy, v' = a,        /* accelerate/decelerate and move */
       |          dx' = -w * dy, dy' = w * dx, w' = a/r,   /* follow curve */
       |          t' = 1 & ?dc:(t <= T & v >= 0);
-      |         & !tSign:(t >= 0) using t by induction;
-      |         & !dir:(norm(dx, dy) = 1) using inv by induction;
-      |         & !vSol:(v = v@body) using a by induction;
+      |         & !tSign:(t >= 0) using tZ by induction;
+      |         & !dir:(norm(dx, dy) = 1) using dxyNorm by induction;
+      |         & !vSol:(v = v@body) using aZ by induction;
       |         & !xSol:(x = x@body) using vZ vSol dir tSign dc by induction;
       |         & !ySol:(y = y@body) using vZ vSol dir tSign dc by induction;
       |         };
       |      }
       |        ++
       |        /* or choose a new safe curve */
-      |      { a := A;
+      |      { ?aA:(a := A);
       |        w := *; ?(-W<=w & w<=W);
       |        r := *;
       |        xo := *; yo := *;
       |        ?(r!=0 & r*w = v);
       |        ?(infdist(x, y, xo, yo) > admissibleSeparation(v));
-      |        t := 0;
+      |        ?tZ:(t := 0);
       |        { x' = v * dx, y' = v * dy, v' = a,        /* accelerate/decelerate and move */
       |          dx' = -w * dy, dy' = w * dx, w' = a/r,   /* follow curve */
       |          t' = 1 & ?dc:(t <= T & v >= 0);
-      |         & !tSign:(t >= 0) using t by induction;
-      |         & !dir:(norm(dx, dy) = 1) using inv by induction;
-      |         & !vSol:(v = v@body + A*t) using a by induction;
+      |         & !tSign:(t >= 0) using tZ aA by induction;
+      |         & !dir:(norm(dx, dy) = 1) using dxyNorm by induction;
+      |         & !vSol:(v = v@body + A*t) using aA by induction;
       |         & !xBound:(-t * v <= x - x@body & x - x@body <= t * v) using vSol dir tSign dc by induction; /* got here after 20 mins */
       |         & !xBound:(-t * v <= y - y@body & y - y@body <= t * v) using vSol dir tSign dc by induction;
       |        };
       |      }
       |    }
       |  }
-      |  !(loopinv()) using assump by auto;
+      |  !vInv: (v >= 0) using vSol vSign by auto;
+      |  !infInd: (infdist(x, y, xo, yo) >= stopDist(v)) using safeDist bnds by auto;
+      |  note indStep = andI(andI(vInv, dir), infInd);
       |}*
-      |!(goal());
+      |!(goal()) using safeDist bnds by auto;
       |""".stripMargin
 
   val ijrrStaticSafetySimplified: String =
