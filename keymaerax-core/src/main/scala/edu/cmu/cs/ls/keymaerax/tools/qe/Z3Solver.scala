2ad850ca58f5603698d471cc45eee9b39daac7e1
Stefan Mitsch <smitsch@cs.cmu.edu>
2020-10-13 10:05:27 -0400

    Configuration: allow user-specified Z3 version

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/qe/Z3Solver.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/qe/Z3Solver.scala
@@ -9,6 +9,7 @@ package edu.cmu.cs.ls.keymaerax.tools.qe
 
 import java.io._
 
+import edu.cmu.cs.ls.keymaerax.Configuration
 import edu.cmu.cs.ls.keymaerax.core._
 import edu.cmu.cs.ls.keymaerax.tools._
 import org.apache.logging.log4j.scala.Logging
@@ -35,16 +36,20 @@ class Z3Solver(val z3Path: String, val converter: SMTConverter) extends ToolOper
 
   /** The expected version and hash code */
   private val version :: hash :: Nil = {
-    val resource = getClass.getResourceAsStream("/z3/VERSION")
-    if (resource != null) scala.io.Source.fromInputStream(resource).getLines().toList
-    else "-1" :: "-1" :: Nil //@note ignore version check if KeYmaera X jar does not ship Z3
+    Configuration.get[String](Configuration.Keys.Z3_ALLOW_VERSION) match {
+      case Some(v) => v :: "-1" :: Nil
+      case None =>
+        val resource = getClass.getResourceAsStream("/z3/VERSION")
+        if (resource != null) scala.io.Source.fromInputStream(resource).getLines().toList
+        else "-1" :: "-1" :: Nil //@note ignore version check if KeYmaera X jar does not ship Z3
+    }
   }
 
   /** Lightweight check that we are communicating with Z3 in the expected version and build hash. */
   insist(version == "-1" && hash == "-1" || {
     val versionOutput = runZ3(z3Path + " -version", -1)
     versionOutput.startsWith("Z3 version " + version) && (hash == "-1" || versionOutput.endsWith(hash))
-  }, "Z3 not of the expected version and build hash")
+  }, "Z3 not of the expected version " + version)
 
   /** Return Z3 QE result and the proof evidence */
   def qe(f: Formula): Formula = {
