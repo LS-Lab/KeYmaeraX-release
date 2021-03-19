b9e8384233dfd809a89b26689d49dfa6873bb386
Stefan Mitsch <smitsch@andrew.cmu.edu>
2021-03-19 15:01:33 +0200

    Interpreter: fix listener race condition when cancelling tactic after timout

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/bellerophon/SequentialInterpreter.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/bellerophon/SequentialInterpreter.scala
@@ -54,15 +54,18 @@ abstract class BelleBaseInterpreter(val listeners: scala.collection.immutable.Se
       listeners.foreach(_.end(v, expr, Left(result)))
       result
     } catch {
-      case e: Throwable =>
+      case e: Throwable => throw try {
         listeners.foreach(_.end(v, expr, Right(e)))
-        throw e
+        e
+      } catch {
+        case ex: Throwable => ex.initCause(e)
+      }
     }
   }
 
   override def kill(): Unit = {
     isDead = true
-    listeners.foreach(_.kill())
+    listeners.synchronized(listeners.foreach(_.kill()))
   }
 
   /** Adjusts the number of labels to match the number of subgoals. */
@@ -240,7 +243,7 @@ abstract class BelleBaseInterpreter(val listeners: scala.collection.immutable.Se
 
     case TimeoutAlternatives(alternatives, timeout) => alternatives.headOption match {
       case Some(tactic) =>
-        val c = Cancellable(apply(tactic, v))
+        val c = Cancellable(listeners.synchronized(apply(tactic, v)))
         try {
           Await.result(c.future, Duration(timeout, MILLISECONDS))
         } catch {
@@ -251,7 +254,8 @@ abstract class BelleBaseInterpreter(val listeners: scala.collection.immutable.Se
           }
           case ex: TimeoutException =>
             c.cancel()
-            throw new BelleNoProgress("Alternative timed out", ex)
+            //@note otherwise race condition between tactic exceptions (caused by cancel) and BelleNoProgress in notifying listeners
+            listeners.synchronized(throw new BelleNoProgress("Alternative timed out", ex))
         }
       case None => throw new BelleNoProgress("Exhausted all timeout alternatives")
     }
