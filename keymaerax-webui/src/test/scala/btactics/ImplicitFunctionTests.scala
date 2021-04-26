8a8124e1acd6f6dc4e7508cc3edef71f22d5030d
Yong Kiam <tanyongkiam@gmail.com>
2021-04-26 09:11:40 -0400

    some examples with exp/trig

--- keymaerax-webui/src/test/scala/btactics/ImplicitFunctionTests.scala
+++ keymaerax-webui/src/test/scala/btactics/ImplicitFunctionTests.scala
@@ -62,4 +62,80 @@ class ImplicitFunctionTests extends TacticTestBase {
     proof shouldBe 'proved
   }
 
+  "examples" should "allow arithmetic over implicit functions" in withMathematica { _ =>
+    val arith1 = "exp(x) > 1".asFormula
+    val arith2 = "sin(x)^2 + cos(x)^2 = 1".asFormula
+
+    val pr1 = proveBy(arith1, skip)
+    val pr2 = proveBy(arith2, skip)
+    // Ideally: prove by QE
+
+    println(pr1)
+    println(pr2)
+  }
+
+  it should "work under assignments and nesting" in withMathematica { _ =>
+    val fml = "x =1 -> [x := exp(exp(x)); x:=1; ++ x:= exp(x); ] x > 0".asFormula
+    val pr = proveBy(fml, skip)
+    // Ideally: prove by ODE(1), dI('full)(1), etc.
+
+    println(pr)
+  }
+
+  it should "be usable as a loop invariant" in withMathematica { _ =>
+    val fml = "x = 0 -> [ {x := x + 1;}* ] exp(x) >= 1".asFormula
+    val pr = proveBy(fml, skip)
+    // Ideally: prove using loop with loop invariants like x>=0 or exp(x)>=1
+
+    println(pr)
+  }
+
+  it should "work with DI (1)" in withMathematica { _ =>
+    val fml = "x =1 -> [{x' = exp(x)}] x > 0".asFormula
+    val pr = proveBy(fml, skip)
+    // Ideally: prove by dI('full)(1), etc.
+
+    println(pr)
+  }
+
+  it should "work with DI (2)" in withMathematica { _ =>
+    val fml = "x>=0&y>=0&z>=0 -> [{x' = exp(y), y' = exp(z), z'=1}] x+y+z>=0".asFormula
+    val pr = proveBy(fml, skip)
+    // Ideally: prove by dI('full)(1), etc.
+
+    println(pr)
+  }
+
+  it should "work with DI (3)" in withMathematica { _ =>
+    val fml = "x=5 -> [{x' = sin(x) + 1}] x>=0".asFormula
+    val pr = proveBy(fml, skip)
+    // Ideally: prove by dI('full)(1), etc.
+
+    println(pr)
+  }
+
+  it should "prove an exponential solution" in withMathematica { _ =>
+    val fml = "x=x0 & t=0 -> [{x'=x, t' =1}] x = x0*exp(t)".asFormula
+    val pr = proveBy(fml, skip)
+    // may be provable using ODE(1) or dbx
+
+    println(pr)
+  }
+
+  it should "prove a trig solution" in withMathematica { _ =>
+    val fml = "c=1 & s = 0 & t=0 -> [{c'=-s, s'=c, t' =1}] (c=cos(t) & s = sin(t))".asFormula
+    val pr = proveBy(fml, skip)
+    // may be provable using ODE(1) or dbx
+
+    println(pr)
+  }
+
+  it should "model a pendulum" in withMathematica { _ =>
+    val fml = "a > 0 & b > 0 & x1 = 1 & x2 = 1 -> [{x1' = x2, x2'= -a*sin(x1) - b*x2}] a*(1-cos(x1))+1/2*x2^2 <= 1/2".asFormula
+    val pr = proveBy(fml, skip)
+    // end goal: prove something like this or more
+
+    println(pr)
+  }
+
 }
