dc373d989d730abde309638fdbf647d51920faca
Stefan Mitsch <smitsch@andrew.cmu.edu>
2022-03-01 06:50:15 -0500

    Expand min/max/abs: expand inside out

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/EqualityTactics.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/EqualityTactics.scala
@@ -337,10 +337,13 @@ private object EqualityTactics {
     ProofRuleTactics.requireOneSubgoal(provable, "abs")
     val sequent = provable.subgoals.head
     sequent.at(pos) match {
-    case (_, _: Formula) => mapSubpositions(pos, sequent, {
-      case (FuncOf(InterpretedSymbols.absF, _), pp) => Some(?(abs(pp).computeResult _)(_))
-      case _ => None
-    }).foldLeft(provable)({ (pr, r) => pr(r, 0) })
+    case (_, _: Formula) =>
+      val positions = mapSubpositions(pos, sequent, {
+        case (t@FuncOf(InterpretedSymbols.absF, _), pp) => Some((t, (pp, (abs(pp).computeResult _)(_))))
+        case _ => None
+      })
+      //@note take only the first position, because minmax itself abbreviates before expanding and so expands other positions with it; then reverse-sort by "posInExpr" length to work inside out
+      positions.groupBy(_._1).map(_._2.head._2).toList.sortBy(_._1.inExpr.pos.length).reverseMap(_._2).foldLeft(provable)({ (pr, r) => pr(r, 0) })
     case (ctx, abs@FuncOf(fn@InterpretedSymbols.absF, t)) =>
       if (StaticSemantics.boundVars(ctx.ctx).intersect(StaticSemantics.freeVars(t)).isEmpty) {
         val freshAbsIdx = TacticHelper.freshIndexInSequent(fn.name + "_", sequent)
@@ -475,12 +478,15 @@ private object EqualityTactics {
     ProofRuleTactics.requireOneSubgoal(provable, "minmax")
     val sequent = provable.subgoals.head
     sequent.at(pos) match {
-      case (_, _: Formula) => mapSubpositions(pos, sequent, {
-        case (FuncOf(Function(fn, None, Tuple(Real, Real), Real, true), _), pp) =>
-          if (fn == InterpretedSymbols.minF.name || fn == InterpretedSymbols.maxF.name) Some(?(minmax(pp).computeResult _)(_))
-          else None
-        case _ => None
-      }).foldLeft(provable)({ (pr, r) => pr(r, 0) })
+      case (_, _: Formula) =>
+        val positions = mapSubpositions(pos, sequent, {
+          case (t@FuncOf(Function(fn, None, Tuple(Real, Real), Real, true), _), pp) =>
+            if (fn == InterpretedSymbols.minF.name || fn == InterpretedSymbols.maxF.name) Some((t, (pp, (minmax(pp).computeResult _)(_))))
+            else None
+          case _ => None
+        })
+        //@note take only the first position, because minmax itself abbreviates before expanding and so expands other positions with it; then reverse-sort by "posInExpr" length to work inside out
+        positions.groupBy(_._1).map(_._2.head._2).toList.sortBy(_._1.inExpr.pos.length).reverseMap(_._2).foldLeft(provable)({ (pr, r) => pr(r, 0) })
       case (ctx, minmax@FuncOf(Function(fn, None, Tuple(Real, Real), Real, true), t: Pair))
           if fn == InterpretedSymbols.minF.name || fn == InterpretedSymbols.maxF.name =>
         if (StaticSemantics.boundVars(ctx.ctx).intersect(StaticSemantics.freeVars(t)).isEmpty) {
