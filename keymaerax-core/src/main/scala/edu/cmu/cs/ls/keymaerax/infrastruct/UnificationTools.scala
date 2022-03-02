5716a7244c23e6dd63161e6d97551b8f81fe703e
Stefan Mitsch <smitsch@andrew.cmu.edu>
2022-03-02 05:25:27 -0500

    Collect substitutions according to symbol dependencies when merging provables

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/infrastruct/UnificationTools.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/infrastruct/UnificationTools.scala
@@ -4,33 +4,37 @@
   */
 package edu.cmu.cs.ls.keymaerax.infrastruct
 
-import edu.cmu.cs.ls.keymaerax.core.{NamedSymbol, Provable, StaticSemantics, SubstitutionPair, USubst}
+import edu.cmu.cs.ls.keymaerax.core.{DotTerm, Expression, NamedSymbol, Provable, StaticSemantics, SubstitutionPair, USubst, Variable}
 import edu.cmu.cs.ls.keymaerax.infrastruct.ProvableHelper.exhaustiveSubst
-import edu.cmu.cs.ls.keymaerax.parser.Declaration
+
 
 object UnificationTools {
+  private val expansible = (t: Expression) => t match { case _: DotTerm => false case _: Variable => false case _ => true }
   /** Collects substitutions (of `defs`) that are needed to make `sub` fit the `i`-th subgoal of `goal`. */
   def collectSubst(goal: Provable, i: Int, sub: Provable, substs: List[SubstitutionPair]): USubst = {
-    //@note prefer to expand definitions from goal towards sub (in analogy to what happens in the proof)
-    //@todo performance will degrade with depth of nested definitions
+    if (goal.subgoals(i) == sub.conclusion) USubst(List.empty)
+    else {
+      // order substitutions by dependency
+      val adj = substs.map(sp =>
+        StaticSemantics.symbols(sp.what).filter(expansible).head -> StaticSemantics.symbols(sp.repl).filter(expansible)
+      ).groupBy(_._1).map({ case (v, e) => v -> e.flatMap(_._2).toSet })
+      val symbolDeps = DependencyAnalysis.dfs(adj)
+
+      collectSubstOrdered(goal, i, sub, substs, symbolDeps)
+    }
+  }
+
+  /** Collects substitutions (of `defs`) that are needed to make `sub` fit the `i`-th subgoal of `goal`. Uses `symbolDeps` to determine in which order to expand symbols. */
+  private def collectSubstOrdered(goal: Provable, i: Int, sub: Provable, substs: List[SubstitutionPair], symbolDeps: List[NamedSymbol]): USubst = {
     if (goal.subgoals(i) == sub.conclusion) USubst(List.empty)
     else {
       val (sg, ss, _) = FormulaTools.symbolsDiff(goal.subgoals(i).ante ++ goal.subgoals(i).succ, sub.conclusion.ante ++ sub.conclusion.succ)
-      val downSubst = USubst(substs.filter({ case SubstitutionPair(what, _) => sg.intersect(StaticSemantics.symbols(what)).nonEmpty }))
-      val downSubstGoal = exhaustiveSubst(goal, downSubst)
-      val downSubstSub = exhaustiveSubst(sub, downSubst)
-      if (downSubstGoal.subgoals(i) == downSubstSub.conclusion) downSubst
-      else if (downSubst.subsDefsInput.nonEmpty) downSubst ++ collectSubst(downSubstGoal, i, downSubstSub, substs) // expand nested definitions
-      else if (ss.nonEmpty) {
-        //@note if even after expanding all nested definitions from goal towards sub there are still differences,
-        // then goal may have a symbol expanded but sub hasn't; can happen when applying a general lemma
-        val upSubst = USubst(substs.filter({ case SubstitutionPair(what, _) => ss.intersect(StaticSemantics.symbols(what)).nonEmpty }))
-        val upSubstGoal = exhaustiveSubst(downSubstGoal, upSubst)
-        val upSubstSub = exhaustiveSubst(downSubstSub, upSubst)
-        if (upSubstGoal.subgoals(i) == upSubstSub.conclusion) downSubst ++ upSubst
-        else if (upSubst.subsDefsInput.nonEmpty) downSubst ++ upSubst ++ collectSubst(upSubstGoal, i, upSubstSub, substs) // expand nested definitions
-        else downSubst ++ upSubst ++ RestrictedBiDiUnificationMatch(upSubstGoal.subgoals(i), upSubstSub.conclusion).usubst
-      } else downSubst ++ RestrictedBiDiUnificationMatch(downSubstGoal.subgoals(i), downSubstSub.conclusion).usubst
+      val expand = (sg ++ ss).filter(expansible).toList.minBy(symbolDeps.indexOf)
+      val subst = USubst(substs.find({ case SubstitutionPair(what, _) => StaticSemantics.symbols(what).contains(expand) }).toList)
+
+      val substGoal = exhaustiveSubst(goal, subst)
+      val substSub = exhaustiveSubst(sub, subst)
+      subst ++ collectSubstOrdered(substGoal, i, substSub, substs.diff(subst.subsDefsInput), symbolDeps)
     }
   }
 }
---
+++ keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/infrastruct/UnificationToolsTests.scala
@@ -0,0 +1,89 @@
+package edu.cmu.cs.ls.keymaerax.infrastruct
+
+import edu.cmu.cs.ls.keymaerax.{Configuration, FileConfiguration}
+import edu.cmu.cs.ls.keymaerax.core._
+import edu.cmu.cs.ls.keymaerax.parser.KeYmaeraXPrettyPrinter
+import edu.cmu.cs.ls.keymaerax.parser.StringConverter.StringToStringConverter
+import org.scalatest.{BeforeAndAfterEach, FlatSpec, Matchers}
+
+class UnificationToolsTests extends FlatSpec with Matchers with BeforeAndAfterEach {
+
+  override protected def beforeEach(): Unit = {
+    Configuration.setConfiguration(FileConfiguration)
+    PrettyPrinter.setPrinter(KeYmaeraXPrettyPrinter.pp)
+  }
+
+  "Collect substitutions" should "collect upwards" in {
+    val defs = "P(x) ~> x>=0 :: nil".asDeclaration
+    UnificationTools.collectSubst(
+      Provable.startProof("P(x)".asFormula), 0,
+      Provable.startProof("x>=0".asFormula), defs.substs).subsDefsInput should contain theSameElementsAs List(
+      "P(x) ~> x>=0".asSubstitutionPair
+    )
+  }
+
+  it should "collect downwards" in {
+    val defs = "P(x) ~> x>=0 :: nil".asDeclaration
+    UnificationTools.collectSubst(
+      Provable.startProof("x>=0".asFormula), 0,
+      Provable.startProof("P(x)".asFormula), defs.substs).subsDefsInput should contain theSameElementsAs List(
+      "P(x) ~> x>=0".asSubstitutionPair
+    )
+  }
+
+  it should "collect nested" in {
+    val defs = "P(x) ~> x>=0 & Q(x) :: Q(x) ~> x<=0 :: nil".asDeclaration
+    UnificationTools.collectSubst(
+      Provable.startProof("P(x)".asFormula), 0,
+      Provable.startProof("x>=0 & x<=0".asFormula), defs.substs).subsDefsInput should contain theSameElementsAs List(
+      "P(x) ~> x>=0 & Q(x)".asSubstitutionPair, "Q(x) ~> x<=0".asSubstitutionPair
+    )
+  }
+
+  it should "collect minimal nested definitions upwards" in {
+    val defs = "prg{|^@|}; ~> ?Q(x); :: Q(x) ~> x>=0 :: nil".asDeclaration
+    UnificationTools.collectSubst(
+      Provable.startProof("[prg{|^@|};]P(x)".asFormula), 0,
+      Provable.startProof("[?Q(x);]P(x)".asFormula), defs.substs).subsDefsInput should contain theSameElementsAs List(
+      "prg{|^@|}; ~> ?Q(x);".asSubstitutionPair
+    )
+  }
+
+  it should "collect minimal nested definitions downwards" in {
+    //@note loop tactic creates several subgoals with cuts, often of the shape
+    //      J(x) |- [prg;]J(x)     const(P(x)) |- [expanded(prg);]const(P(x))
+    //      -----------------------------------------------------------------
+    //      P(x) |- [{prg;}*]Q(x)
+    // where expanded(prg) has nested definitions (predicates) that are not expanded. The first pass (on the right
+    // branch) finds prg expanded and substitutes in the conclusion, the next pass (on the left branch) then has
+    // conclusion P(x) |- [{expanded(prg);}*] and so should start by searching downwards from the premise
+    val defs = "prg{|^@|}; ~> ?Q(x); :: Q(x) ~> x>=0 :: nil".asDeclaration
+    UnificationTools.collectSubst(
+      Provable.startProof("[?Q(x);]P(x)".asFormula), 0,
+      Provable.startProof("[prg{|^@|};]P(x)".asFormula), defs.substs).subsDefsInput should contain theSameElementsAs List(
+      "prg{|^@|}; ~> ?Q(x);".asSubstitutionPair
+    )
+  }
+
+  it should "collect minimal nested definitions in any direction" in {
+    val defs = "prg{|^@|}; ~> left{|^@|}; ++ right{|^@|}; :: left{|^@|}; ~> ?P(x); :: right{|^@|}; ~> ?Q(x); :: P(x) ~> x<=0 :: Q(x) ~> x>=0 :: nil".asDeclaration
+    UnificationTools.collectSubst(
+      Provable.startProof("[prg{|^@|};]x<=0".asFormula), 0,
+      Provable.startProof("[left{|^@|}; ++ ?Q(x);]P(x)".asFormula), defs.substs).subsDefsInput should contain theSameElementsAs List(
+      "prg{|^@|}; ~> left{|^@|}; ++ right{|^@|};".asSubstitutionPair,
+      "right{|^@|};~>?Q(x);".asSubstitutionPair,
+      "P(x) ~> x<=0".asSubstitutionPair
+    )
+  }
+
+  it should "focus on expansible symbols" in {
+    val defs = "prg{|^@|}; ~> a:=0;?Q(x); :: P(x) ~> x<=0 :: Q(x) ~> x>=0 :: nil".asDeclaration
+    UnificationTools.collectSubst(
+      Provable.startProof("[prg{|^@|};]x<=0".asFormula), 0,
+      Provable.startProof("[a:=0;?Q(x);]P(x)".asFormula), defs.substs).subsDefsInput should contain theSameElementsAs List(
+      "prg{|^@|}; ~> a:=0;?Q(x);".asSubstitutionPair,
+      "P(x) ~> x<=0".asSubstitutionPair
+    )
+  }
+
+}
