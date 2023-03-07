58e99b10e56c5015400bc1634c27c2f3353b9a93
Andre Platzer <platzer@kit.edu>
2023-03-07 14:26:25 +0100

    nilpotentSolve added to browse and menu since it's the only one able to solve x'=v,v'=0*v+0*x etc.

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/ODEInvariance.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/ODEInvariance.scala
@@ -1893,8 +1893,15 @@ object ODEInvariance {
     ).reduce(vecvec_sum)
   }
 
+  @Tactic("[']n", longDisplayName = "nilsolve",
+    premises = "Γ,x=x0,t=0 |- [x'=f(x),t'=1&q(x)&x=y(x0,t)]p(x), Δ",
+    conclusion = "Γ |- [x'=f(x)&q(x)]p(x), Δ",
+    revealInternalSteps = false, displayLevel = "menu")
+  def nilpotentSolve : DependentPositionTactic = nilpotentSolve(false)
+
+
   /** Given a top-level succedent position corresponding to [x'=f(x)&Q]P
-    * with x'=f(x) a linear, nilpotent ODE
+    * with x'=f(x) a linear, nilpotent ODE, solve the differential equation.
     *
     * Adds the (polynomial) solution x=Phi(x_0,t) of that ODE to the domain constraint
     *
@@ -1905,7 +1912,7 @@ object ODEInvariance {
     * --------------- (nilpotent solve)
     * G |- [x'=f(x)&Q]P
     *
-    * @param solveEnd whether to continue with weaken and QE (see rule rendition above)
+    * @param solveEnd whether to continue with differential weaken and QE (see rule rendition above)
     * @return See the rule rendition above
     *         Special failure cases:
     *         1) Linearity heuristic checks fail e.g.: x'=1+x^2-x^2 will be treated as non-linear even though it is really linear
