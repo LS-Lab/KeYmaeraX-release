8a2caa3863f03fabada716eb692ca2358cd8bd65
E. Prebet <enguerrand.prebet@kit.edu>
2023-10-17 10:52:55 +0200

    Copyright

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/infrastruct/Context.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/infrastruct/Context.scala
@@ -1,5 +1,5 @@
 /**
-* Copyright (c) Carnegie Mellon University.
+* Copyright (c) Carnegie Mellon University, Karlsruhe Institute of Technology.
 * See LICENSE.txt for the conditions of this license.
 */
 package edu.cmu.cs.ls.keymaerax.infrastruct
