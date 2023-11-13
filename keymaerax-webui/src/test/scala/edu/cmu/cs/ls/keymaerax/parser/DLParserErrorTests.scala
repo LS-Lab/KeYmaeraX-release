f12ce64ee23f34ae6b316b1225a1b27184299be4
Tobias Erthal <tobsbot@mailbox.org>
2023-11-13 22:53:49 +0100

    Update copyright notice

--- keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/parser/DLParserErrorTests.scala
+++ keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/parser/DLParserErrorTests.scala
@@ -1,7 +1,7 @@
-/**
-  * Copyright (c) Carnegie Mellon University.
-  * See LICENSE.txt for the conditions of this license.
-  */
+/*
+ * Copyright (c) Carnegie Mellon University, Karlsruhe Institute of Technology.
+ * See LICENSE.txt for the conditions of this license.
+ */
 package edu.cmu.cs.ls.keymaerax.parser
 
 import edu.cmu.cs.ls.keymaerax.bellerophon.{Cancellable, LazySequentialInterpreter, ReflectiveExpressionBuilder}
