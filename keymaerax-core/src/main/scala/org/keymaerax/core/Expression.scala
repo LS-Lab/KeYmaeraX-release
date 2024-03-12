0be9d8d6862dc780ee4b4c565c19cfd24a9bbb46
E. Prebet <enguerrand.prebet@kit.edu>
2024-03-12 15:18:17 +0100

    Extend core to dRL w/o axioms

--- keymaerax-core/src/main/scala/org/keymaerax/core/Expression.scala
+++ keymaerax-core/src/main/scala/org/keymaerax/core/Expression.scala
@@ -686,6 +686,8 @@ final case class Pair(left: Term, right: Term) extends BinaryCompositeTerm {
  *   - [[ComparisonFormula]] are [[AtomicFormula]] composed of two terms but not composed of formulas
  *     - `e>=d` comparisons as [[GreaterEqual]]([[Term]],[[Term]]) and likewise [[Equal]], [[NotEqual]], [[Greater]],
  *       [[LessEqual]], [[Less]]
+ *   - [[ProgramComparison]] are [[AtomicFormula]] composed of two programs
+ *     - `a<=b` comparisons as [[Refinement]]([[Program]],[[Program]]) and likewise [[ProgramEquivalence]]
  *   - [[ApplicationOf]] predicate applications
  *     - `p(e)` predicate application as [[PredOf]]([[Function]], [[Term]])
  *     - `P{Q}` predicational application or quantifier symbol as [[PredicationalOf]]([[Function]], [[Formula]])
@@ -940,6 +942,23 @@ final case class Diamond(program: Program, child: Formula) extends Modal {
  * used in the form (e>=k)' which is (e)'>=(k)'. In practice, derived forms are useful.
  */
 final case class DifferentialFormula(child: Formula) extends UnaryCompositeFormula {
+  def reapply = copy
+}
+
+/** Atomic comparison of two programs */
+sealed trait ProgramComparison extends AtomicFormula with BinaryComposite {
+  def reapply: (Program, Program) => Formula
+  val left: Program
+  val right: Program
+}
+
+/** Refinements of programs: left <= right */
+case class Refinement(left: Program, right: Program) extends ProgramComparison {
+  def reapply = copy
+}
+
+/** Equivalence of programs: left == right */
+case class ProgramEquivalence(left: Program, right: Program) extends ProgramComparison {
   def reapply = copy
 }
 
