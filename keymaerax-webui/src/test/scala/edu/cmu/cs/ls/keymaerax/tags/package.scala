b808a4ed3c716c3c01cc84481badbbf1d30da0fb
Joscha Mennicken <keymaerax@plugh.de>
2024-03-19 21:56:28 +0100

    Move test tag objects next to tag annotations
    As documented in edu.cmu.cs.ls.keymaerax.tagobjects, there are two ways
    to tag tests: With Java annotations and with Scala objects. There should
    be a one-to-one correspondence between the objects and annotations.
    
    At the moment, keymaerax's annotations and tags are in two completely
    different places and formats. This commit starts moveing them closer
    together.
    
    The move is inspired by the way scaladoc organizes its own tags:
    - Java annotations are at `org.scalatest.tags`, one per file
    - Scala objects are at `org.scalatest.tagobjects`, one per file
    
    During the refactoring, there were cases where both an annotation and
    the corresponding object were used in the same class. Since the
    annotation tags all tests in a class, the objects were redundant. Hence,
    they were removed.

---
+++ keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/tags/package.scala
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) Carnegie Mellon University, Karlsruhe Institute of Technology.
+ * See LICENSE.txt for the conditions of this license.
+ */
+
+package edu.cmu.cs.ls.keymaerax
+
+/** See [[edu.cmu.cs.ls.keymaerax.tagobjects]] for more details. */
+package object tags {}
