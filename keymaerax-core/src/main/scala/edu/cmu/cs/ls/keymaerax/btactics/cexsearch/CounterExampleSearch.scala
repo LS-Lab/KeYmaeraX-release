3f2fdc975a9f1fa7bfc6faa973dd963a7c9bd6c8
Joscha Mennicken <keymaerax@plugh.de>
2024-04-10 16:41:06 +0200

    Remove unused classes and objects

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/btactics/cexsearch/CounterExampleSearch.scala
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) Carnegie Mellon University, Karlsruhe Institute of Technology.
- * See LICENSE.txt for the conditions of this license.
- */
-
-package edu.cmu.cs.ls.keymaerax.btactics.cexsearch
-
-/** Created by bbohrer on 4/24/16. */
-object CounterExampleSearch {}
