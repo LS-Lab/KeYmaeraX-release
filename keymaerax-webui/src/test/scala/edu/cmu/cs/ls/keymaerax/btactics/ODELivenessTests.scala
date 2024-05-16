c22b837820974723ecf11d1531576174a8360a6e
Joscha Mennicken <keymaerax@plugh.de>
2024-05-16 17:17:12 +0200

    Reorder ODEs in test
    They are identical up to reordering, so the test probably shouldn't
    fail.

--- keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/btactics/ODELivenessTests.scala
+++ keymaerax-webui/src/test/scala/edu/cmu/cs/ls/keymaerax/btactics/ODELivenessTests.scala
@@ -192,7 +192,7 @@ class ODELivenessTests extends TacticTestBase {
     val pr = proveBy(seq, odeReduce(strict = true, Nil)(2))
 
     pr.subgoals.length shouldBe 1
-    pr.subgoals(0) shouldBe "d>0, 1+1=2  ==> 1+1=3, <{x'=x+1,c'=d+e_(),v'=2&c<=5}>x+v<=5, 2+2=1".asSequent
+    pr.subgoals(0) shouldBe "d>0, 1+1=2  ==> 1+1=3, <{c'=d+e_(),x'=x+1,v'=2&c<=5}>x+v<=5, 2+2=1".asSequent
   }
 
   it should "throw a helpful error when it gets stuck" in withQE { _ =>
