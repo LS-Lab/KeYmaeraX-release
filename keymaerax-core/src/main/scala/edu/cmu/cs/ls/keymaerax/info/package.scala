17159ea08872c363ab31003251e2ab44e1de54cf
Joscha Mennicken <keymaerax@plugh.de>
2024-05-16 18:51:35 +0200

    Add more kinds of project info constants
    These should be used whenever writing new code or overhauling old code.

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/info/package.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/info/package.scala
@@ -5,6 +5,8 @@
 
 package edu.cmu.cs.ls.keymaerax
 
+import java.net.URI
+
 /**
  * Information about the KeYmaera X project.
  *
@@ -14,6 +16,25 @@ package edu.cmu.cs.ls.keymaerax
  */
 package object info {
 
+  /** The full project name, including correct case and whitespace. */
+  val FullName = "KeYmaera X"
+
+  /** A version of the name that is safe to use in technical contexts (e.g. file names). */
+  val TechnicalName = "keymaerax"
+
   /** The current project version. */
   val Version: VersionNumber = VersionNumber.parse(BuildInfo.version)
+
+  /** The full name and version of the project. */
+  val FullNameAndVersion = s"$FullName $Version"
+
+  val Copyright = "Copyright (c) Carnegie Mellon University, Karlsruhe Institute of Technology"
+
+  val Website = new URI("https://keymaerax.org/")
+
+  /** Domain used to uniquely identify the project. */
+  // TODO Switch to keymaerax.org and move all source files as well
+  val Domain = "keymaerax.ls.cs.cmu.edu"
+
+  val DomainReversed = Domain.split('.').reverseIterator.mkString(".")
 }
