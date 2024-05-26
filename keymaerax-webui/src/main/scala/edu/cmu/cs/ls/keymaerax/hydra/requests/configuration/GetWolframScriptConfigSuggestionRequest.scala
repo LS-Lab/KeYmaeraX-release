31d680771f53d114da7951010526013c8a0497f8
Joscha Mennicken <keymaerax@plugh.de>
2024-05-26 20:31:50 +0200

    Fix tool configuration web ui
    The "Use Default"/"Reset to <os> Defaults" button now uses the
    suggestions by the ToolPathFinder.

--- keymaerax-webui/src/main/scala/edu/cmu/cs/ls/keymaerax/hydra/requests/configuration/GetWolframScriptConfigSuggestionRequest.scala
+++ keymaerax-webui/src/main/scala/edu/cmu/cs/ls/keymaerax/hydra/requests/configuration/GetWolframScriptConfigSuggestionRequest.scala
@@ -8,31 +8,28 @@ package edu.cmu.cs.ls.keymaerax.hydra.requests.configuration
 import edu.cmu.cs.ls.keymaerax.hydra.responses.configuration.MathematicaConfigSuggestionResponse
 import edu.cmu.cs.ls.keymaerax.hydra.{LocalhostOnlyRequest, ReadRequest, Response}
 import edu.cmu.cs.ls.keymaerax.info.Os
+import edu.cmu.cs.ls.keymaerax.tools.ToolPathFinder
 import edu.cmu.cs.ls.keymaerax.tools.ext.WolframScript
-import edu.cmu.cs.ls.keymaerax.tools.install.ToolConfiguration
 
+import java.nio.file.Paths
 import scala.collection.immutable.{List, Nil}
 
 class GetWolframScriptConfigSuggestionRequest extends LocalhostOnlyRequest with ReadRequest {
   override def resultingResponses(): List[Response] = {
     try {
       val we = new WolframScript()
-      val version = we.getVersion
       we.shutdown()
       new MathematicaConfigSuggestionResponse(
         Os.Name,
         true,
-        ToolConfiguration
-          .ConfigSuggestion(version.major + "." + version.minor + "." + version.revision, "", "", "", ""),
-        Nil,
+        ToolPathFinder.MathematicaPaths(mathKernel = Paths.get(""), jlinkLib = Paths.get("")),
       ) :: Nil
     } catch {
       case _: Throwable =>
         new MathematicaConfigSuggestionResponse(
           Os.Name,
           false,
-          ToolConfiguration.ConfigSuggestion("", "", "", "", ""),
-          Nil,
+          ToolPathFinder.MathematicaPaths(mathKernel = Paths.get(""), jlinkLib = Paths.get("")),
         ) :: Nil
     }
   }
