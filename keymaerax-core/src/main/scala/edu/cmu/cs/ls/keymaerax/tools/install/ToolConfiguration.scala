0d1872ea4ddc8fde4797771b589cedcf0a4ab0ce
Joscha Mennicken <keymaerax@plugh.de>
2024-05-26 20:49:31 +0200

    Remove now-unnecessary hardcoded paths

--- keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/install/ToolConfiguration.scala
+++ keymaerax-core/src/main/scala/edu/cmu/cs/ls/keymaerax/tools/install/ToolConfiguration.scala
@@ -6,13 +6,7 @@
 package edu.cmu.cs.ls.keymaerax.tools.install
 
 import edu.cmu.cs.ls.keymaerax.Configuration
-import edu.cmu.cs.ls.keymaerax.info.{Os, OsType}
-import spray.json.DefaultJsonProtocol._
-import spray.json._
 
-import java.io.File
-import scala.collection.immutable.{List, Map}
-import scala.io.Source
 import scala.util.Try
 
 /**
@@ -22,15 +16,6 @@ import scala.util.Try
  *   Stefan Mitsch
  */
 object ToolConfiguration {
-
-  /** Configuration suggestions. */
-  case class ConfigSuggestion(
-      version: String,
-      kernelPath: String,
-      kernelName: String,
-      jlinkPath: String,
-      jlinkName: String,
-  )
 
   /** Returns the Mathematica configuration. */
   def mathematicaConfig(preferred: Map[String, String]): Map[String, String] = {
@@ -103,45 +88,5 @@ object ToolConfiguration {
     case "wolframscript" => Map("tool" -> "wolframscript") ++ ToolConfiguration.wolframScriptConfig(preferred)
     case "z3" => Map("tool" -> "z3") ++ ToolConfiguration.z3Config(preferred)
     case t => throw new Exception("Unknown tool '" + t + "'")
-  }
-
-  /** Returns suggestions for Mathematica configuration. */
-  def mathematicaSuggestion(): List[ConfigSuggestion] = parseSuggestions("/config/potentialMathematicaPaths.json")
-
-  /** Returns suggestions for Wolfram Engine configuration */
-  def wolframEngineSuggestion(): List[ConfigSuggestion] = parseSuggestions("/config/potentialWolframEnginePaths.json")
-
-  /** Parses a suggestions JSON file. */
-  private def parseSuggestions(suggestionsFile: String): List[ConfigSuggestion] = {
-    val reader = this.getClass.getResourceAsStream(suggestionsFile)
-    val contents: String = Source.fromInputStream(reader).mkString
-    val source: JsArray = contents.parseJson.asInstanceOf[JsArray]
-
-    // TODO provide classes and spray JSON protocol to convert
-    val osKey = Os.Type match {
-      case OsType.Windows => "Windows"
-      case OsType.Linux => "Unix"
-      case OsType.MacOs => "MacOS"
-      case OsType.Unknown => "Unknown"
-    }
-    val osPathGuesses =
-      source.elements.find(osCfg => osCfg.asJsObject.getFields("os").head.convertTo[String] == osKey) match {
-        case Some(opg) => opg.asJsObject.getFields("paths").head.convertTo[List[JsObject]]
-        case None => throw new IllegalStateException("No default configuration for Unknown OS")
-      }
-
-    osPathGuesses
-      .map(osPath =>
-        (
-          osPath.getFields("version").head.convertTo[String],
-          osPath.getFields("kernelPath").head.convertTo[List[String]],
-          osPath.getFields("kernelName").head.convertTo[String],
-          osPath.getFields("jlinkPath").head.convertTo[List[String]].map(p => p + File.separator),
-          osPath.getFields("jlinkName").head.convertTo[String],
-        )
-      )
-      .flatMap({ case (p1, p2, p3, p4, p5) =>
-        p2.zipWithIndex.map({ case (p, i) => ConfigSuggestion(p1, p, p3, p4(i), p5) })
-      })
   }
 }
