715be89712a616a7c4047a6d25d3e91991cdd6fc
Joscha Mennicken <keymaerax@plugh.de>
2024-08-08 17:02:03 +0200

    Move Generator into InvariantGenerator.scala
    In preparation for specializing Generator into InvariantGenerator.

--- keymaerax-core/src/main/scala/org/keymaerax/btactics/InvariantGenerator.scala
+++ keymaerax-core/src/main/scala/org/keymaerax/btactics/InvariantGenerator.scala
@@ -7,12 +7,148 @@ package org.keymaerax.btactics
 
 import org.keymaerax.Logging
 import org.keymaerax.bellerophon.{BelleExpr, BelleNoProgress, ProverSetupException}
+import org.keymaerax.btactics.helpers.DifferentialHelper
 import org.keymaerax.core._
 import org.keymaerax.infrastruct.Augmentors._
-import org.keymaerax.infrastruct.{DependencyAnalysis, FormulaTools, StaticSemanticsTools}
+import org.keymaerax.infrastruct.{
+  DependencyAnalysis,
+  FormulaTools,
+  NonSubstUnificationMatch,
+  Position,
+  StaticSemanticsTools,
+  UnificationMatch,
+}
+import org.keymaerax.parser.Declaration
 import org.keymaerax.tools.ToolException
 
 import scala.annotation.nowarn
+import scala.util.Try
+
+/**
+ * Invariant generator.
+ *
+ * @tparam A
+ *   The type of results that are being generated.
+ * @author
+ *   Stefan Mitsch
+ */
+trait Generator[+A] {
+
+  /**
+   * Generate multiple objects of type `A` to try. Results do not necessarily have to be deterministic.
+   *
+   * @author
+   *   Stefan Mitsch
+   */
+  def generate(sequent: Sequent, position: Position, declaration: Declaration): LazyList[A]
+}
+
+/** Generator always providing a fixed list as output. */
+case class FixedGenerator[A](list: List[A]) extends Generator[A] {
+  override def generate(sequent: Sequent, position: Position, declaration: Declaration): LazyList[A] = list.to(LazyList)
+}
+
+object ConfigurableGenerator {
+
+  /** Creates a generator that has `products` in verbatim form and fully expanded according to defs`. */
+  def create[A](products: Map[Expression, Seq[A]], defs: Declaration): ConfigurableGenerator[A] =
+    new ConfigurableGenerator[A](
+      products
+        .map({ case (k, v) =>
+          defs.elaborateToSystemConsts(defs.elaborateToFunctions(k)) ->
+            v.map({
+                case (f: Expression, h) => defs.elaborateToSystemConsts(defs.elaborateToFunctions(f)) -> h
+                case v => v
+              })
+              .distinct
+        })
+        .asInstanceOf[Map[Expression, Seq[A]]] ++
+        products
+          .map({ case (k, v) =>
+            defs.exhaustiveSubst(defs.elaborateToSystemConsts(defs.elaborateToFunctions(k))) ->
+              v.map({
+                  case (f: Expression, h) =>
+                    defs.exhaustiveSubst(defs.elaborateToSystemConsts(defs.elaborateToFunctions(f))) -> h
+                  case v => v
+                })
+                .distinct
+          })
+          .asInstanceOf[Map[Expression, Seq[A]]]
+    )
+}
+
+/**
+ * Map-based generator providing output according to the fixed map `products` according to its program or whole formula.
+ * @author
+ *   Stefan Mitsch
+ */
+class ConfigurableGenerator[A](var products: Map[Expression, Seq[A]] = Map[Expression, Seq[A]]()) extends Generator[A] {
+  override def generate(sequent: Sequent, position: Position, declaration: Declaration): LazyList[A] =
+    sequent.sub(position) match {
+      case Some(Box(prg, _)) => findPrgProducts(prg)
+      case Some(Diamond(prg, _)) => findPrgProducts(prg)
+      case Some(f) => products.getOrElse(f, Nil).distinct.to(LazyList)
+      case None => Nil.to(LazyList)
+    }
+
+  /**
+   * Finds products that match the program `prg` either literally, or if ODE then without evolution domain constraint.
+   */
+  private def findPrgProducts(prg: Program): LazyList[A] = prg match {
+    case sys @ ODESystem(ode, _) =>
+      val odeProducts = products.find({
+        case (ODESystem(key, _), _) => ode == key
+        case _ => false
+      })
+      val extractedConditionalProducts = odeProducts.map(p =>
+        (
+          p._1,
+          p._2
+            .map(extractConditionalDiffInv(DifferentialHelper.atomicOdes(p._1.asInstanceOf[ODESystem]), _))
+            .filter(_.isDefined)
+            .flatten,
+        )
+      )
+      extractedConditionalProducts.getOrElse(() -> findConditionalDiffInv(sys))._2.distinct.to(LazyList)
+    case _ => products.get(prg) match {
+        case Some(products) => products.distinct.to(LazyList)
+        case None => products.find({ case (k, _) => Try(!UnificationMatch(k, prg).isEmpty).getOrElse(false) }) match {
+            case Some((_, products)) => products.distinct.to(LazyList)
+            case None => Nil.to(LazyList)
+          }
+      }
+  }
+
+  /**
+   * Finds products that match the ODE `ode` by shape and with a condition that matches. For example, v'=A matches
+   * v'=a@invariant(v'=A->v>=old(v), v'=-2 -> v<=old(v)).
+   */
+  private def findConditionalDiffInv(ode: ODESystem): Seq[A] = {
+    // @note UnificationMatch and RenUSubst won't allow numbers, use own naive subst in these cases
+    products
+      .find({
+        case (ODESystem(key, _), _) =>
+          val subs = Try(NonSubstUnificationMatch.unifyODE(key, ode.ode)).toOption.getOrElse(Nil)
+          NonSubstUnificationMatch.unifier(subs)(key) == ode.ode
+        case _ => false
+      })
+      .map({ case (_, odeProducts) =>
+        odeProducts.map(extractConditionalDiffInv(DifferentialHelper.atomicOdes(ode.ode), _))
+      })
+      .getOrElse(Nil)
+      .filter(_.isDefined)
+      .flatten
+      .distinct
+  }
+
+  /** Extracts the right-hand side of a conditional differential invariant. */
+  private def extractConditionalDiffInv(odeAtoms: List[AtomicODE], product: A): Option[A] = product match {
+    case (Imply(Equal(xp: DifferentialSymbol, e), invCandidate), hint) =>
+      if (odeAtoms.exists(a => a.xp == xp && a.e == e)) { Some((invCandidate, hint).asInstanceOf[A]) }
+      else None
+    case _ => Some(product)
+  }
+}
 
 /**
  * Invariant generators and differential invariant generators.
