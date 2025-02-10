e2cc859bdd287a0cca3bd1a0c67ba5933f327232
Joscha Mennicken <keymaerax@plugh.de>
2025-02-10 17:08:26 +0100

    Set up bibliography object

---
+++ keymaerax-core/src/main/scala/org/keymaerax/Bibliography.scala
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Carnegie Mellon University, Karlsruhe Institute of Technology.
+ * See LICENSE.txt for the conditions of this license.
+ */
+
+package org.keymaerax
+
+/**
+ * A collection of papers referenced in the scaladoc of KeYmaera X.
+ *
+ * The papers are sorted by year, then alphabetically by the object name.
+ */
+object Bibliography {}
