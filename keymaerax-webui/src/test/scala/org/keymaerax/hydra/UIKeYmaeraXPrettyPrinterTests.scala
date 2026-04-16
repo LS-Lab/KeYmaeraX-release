4171a6477e77df76298926097ebaf638dd6ec9a2
E. Prebet <enguerrand.prebet@kit.edu>
2026-04-16 17:34:32 +0200

    Fix test failing by initialising DerivationInfo

--- keymaerax-webui/src/test/scala/org/keymaerax/hydra/UIKeYmaeraXPrettyPrinterTests.scala
+++ keymaerax-webui/src/test/scala/org/keymaerax/hydra/UIKeYmaeraXPrettyPrinterTests.scala
@@ -11,7 +11,7 @@ import org.keymaerax.btactics.TacticTestBase
 /** Created by smitsch on 6/14/17. */
 class UIKeYmaeraXPrettyPrinterTests extends TacticTestBase {
 
-  "Diamond modality" should "be printed correctly" in {
+  "Diamond modality" should "be printed correctly" in withTactics {
     UIKeYmaeraXPrettyPrinter("0", plainText = false)("<x:=x+1;>x=1".asFormula) should
       (include("""<span class="k4-mod-open">&lt;""") and not include """<span class="k4-mod-open">&#8743;lt;""" and
         include("""<span class="k4-mod-close">&gt;""") and not include """<span class="k4-mod-close">&#8743;gt;""")
